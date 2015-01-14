ac6570ecd06a14f3aa0e26054fd23387c643d539
Andrei Stoleru <andrei.stoleru@gmail.com>
2015-01-14 09:48:05 +0100

    Initial Commit

---
+++ iMEGATests/iMEGATests.m
@@ -0,0 +1,40 @@
+//
+//  iMEGATests.m
+//  iMEGATests
+//
+//  Created by Andrei Stoleru on 14/01/15.
+//  Copyright (c) 2015 MEGA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iMEGATests : XCTestCase
+
+@end
+
+@implementation iMEGATests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
