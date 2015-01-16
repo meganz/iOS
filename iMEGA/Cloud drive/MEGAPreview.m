0bcede29d59bb96ffd1d27fcf69ff559f48e79f5
Javier Navarro <jn@mega.co.nz>
2015-01-16 12:40:06 +0100

    Added Demo code

---
+++ iMEGA/Cloud drive/MEGAPreview.m
@@ -0,0 +1,112 @@
+/**
+ * @file MEGAPreview.m
+ * @brief Photo preview of an image.
+ *
+ * (c) 2013-2014 by Mega Limited, Auckland, New Zealand
+ *
+ * This file is part of the MEGA SDK - Client Access Engine.
+ *
+ * Applications using the MEGA API must present a valid application key
+ * and comply with the the rules set forth in the Terms of Service.
+ *
+ * The MEGA SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright Simplified (2-clause) BSD License.
+ *
+ * You should have received a copy of the license along with this
+ * program.
+ */
+
+#import "MEGAPreview.h"
+#import "SVProgressHUD.h"
+#import "Helper.h"
+
+
+@interface MEGAPreview () {
+
+    BOOL _isLoading;
+
+}
+
+- (void)imageLoaded;
+
+@end
+
+@implementation MEGAPreview
+
+
+#pragma mark - Class Methods
+
++ (MEGAPreview *)photoWithNode:(MEGANode *)node {
+    return [[MEGAPreview alloc] initWithNode:node];
+}
+
+
+#pragma mark - Init
+
+- (id)initWithNode:(MEGANode*)node {
+    self = [super init];
+    if (self) {
+        _node = node;
+    }
+    return self;
+}
+
+#pragma mark - MWPhoto Protocol Methods
+
+- (UIImage *)underlyingImage {
+    self.imagePath = [Helper pathForNode:self.node searchPath:NSCachesDirectory directory:@"previews"];
+    if(![[NSFileManager defaultManager] fileExistsAtPath:self.imagePath])return nil;
+
+    return [UIImage imageWithContentsOfFile:self.imagePath];
+}
+
+- (void)loadUnderlyingImageAndNotify {
+  
+    if(_isLoading) return;
+    
+    _isLoading = YES;
+    
+    if(self.underlyingImage)
+        [self imageLoaded];
+    else
+        [self performLoadUnderlyingImageAndNotify];
+    
+}
+
+- (void)performLoadUnderlyingImageAndNotify {
+    
+    if([self.node hasPreview])
+        [[MEGASdkManager sharedMEGASdk] getPreviewNode:self.node destinationFilePath:self.imagePath delegate:self];
+}
+
+- (void)unloadUnderlyingImage {
+
+}
+
+- (void)imageLoaded {
+    _isLoading = NO;
+    [[NSNotificationCenter defaultCenter] postNotificationName:MWPHOTO_LOADING_DID_END_NOTIFICATION object:self];
+}
+
+
+#pragma mark - MEGARequestDelegate
+
+
+- (void)onRequestStart:(MEGASdk *)api request:(MEGARequest *)request {
+}
+
+- (void)onRequestFinish:(MEGASdk *)api request:(MEGARequest *)request error:(MEGAError *)error{
+    self.caption = [self.node name];
+    [self performSelector:@selector(imageLoaded) withObject:nil afterDelay:0];
+
+}
+
+- (void)onRequestTemporaryError:(MEGASdk *)api request:(MEGARequest *)request error:(MEGAError *)error{
+}
+
+@end
+
+
---
+++ iMEGA/Login/MainTabBarController.m
@@ -0,0 +1,43 @@
+/**
+ * @file MainTabBarController.h
+ * @brief Main tab bar of the app
+ *
+ * (c) 2013-2014 by Mega Limited, Auckland, New Zealand
+ *
+ * This file is part of the MEGA SDK - Client Access Engine.
+ *
+ * Applications using the MEGA API must present a valid application key
+ * and comply with the the rules set forth in the Terms of Service.
+ *
+ * The MEGA SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright Simplified (2-clause) BSD License.
+ *
+ * You should have received a copy of the license along with this
+ * program.
+ */
+
+#import "MainTabBarController.h"
+
+@interface MainTabBarController () 
+
+@end
+
+@implementation MainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSMutableArray *viewControllerArray = [[NSMutableArray alloc] initWithCapacity:5];
+
+    [viewControllerArray addObject:[[UIStoryboard storyboardWithName:@"Cloud" bundle:nil] instantiateInitialViewController]];
+    [viewControllerArray addObject:[[UIStoryboard storyboardWithName:@"Offline" bundle:nil] instantiateInitialViewController]];
+    [viewControllerArray addObject:[[UIStoryboard storyboardWithName:@"Contacts" bundle:nil] instantiateInitialViewController]];
+    [viewControllerArray addObject:[[UIStoryboard storyboardWithName:@"Settings" bundle:nil] instantiateInitialViewController]];
+
+    [self setViewControllers:viewControllerArray];
+}
+
+@end
