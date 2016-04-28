5106502e965fbefac3dfbc783a467f1b9f17fc31
Javier Santana <jse@mega.co.nz>
2016-04-28 12:50:55 +0200

    Changed bar tint color in all storyboards to #F9F9F9

--- iMEGA/Login/MainTabBarController.m
+++ iMEGA/Login/MainTabBarController.m
@@ -158,6 +158,8 @@
             [cell.textLabel setFont:[UIFont fontWithName:@"SFUIText-Regular" size:18.0]];
         }
     }
+    
+    [self.moreNavigationController.navigationBar setBarTintColor:[UIColor colorWithRed:247.0/255.0 green:247.0/255.0 blue:247.0/255.0 alpha:1.0]];
 }
 
 #pragma mark - UITabBarControllerDelegate
