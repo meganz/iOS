b097e531518570fad13aefd7905d45cbf7ec998b
Javier Navarro <jn@mega.co.nz>
2016-07-22 14:24:08 +0200

    Save photos to camera roll from the preview creates two photos in the gallery (#5017)

--- iMEGA/Cloud drive/Activities/SaveToCameraRollActivity.m
+++ iMEGA/Cloud drive/Activities/SaveToCameraRollActivity.m
@@ -53,21 +53,22 @@
     if (!error) {
         NSString *imagePath = CFBridgingRelease(contextInfo);
         [[NSFileManager defaultManager] removeItemAtPath:imagePath error:nil];
-        [SVProgressHUD showImage:[UIImage imageNamed:@"hudSuccess"] status:AMLocalizedString(@"savedInCameraRoll", nil)];
     }
 }
 
 #pragma mark - MEGATransferDelegate
 
 - (void)onTransferStart:(MEGASdk *)api transfer:(MEGATransfer *)transfer {
-    [SVProgressHUD show];
+    [SVProgressHUD showImage:[UIImage imageNamed:@"hudDownload"] status:NSLocalizedString(@"downloadStarted", @"Message shown when a download starts")];
 }
 
 - (void)onTransferFinish:(MEGASdk *)api transfer:(MEGATransfer *)transfer error:(MEGAError *)error {
-    UIImage *image = [UIImage imageWithContentsOfFile:transfer.path];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^(void){
-        UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), (void*)CFBridgingRetain(transfer.path));
-    });
+    if (![[NSUserDefaults standardUserDefaults] boolForKey:@"IsSavePhotoToGalleryEnabled"]) {
+        UIImage *image = [UIImage imageWithContentsOfFile:transfer.path];
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^(void){
+            UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), (void*)CFBridgingRetain(transfer.path));
+        });
+    }
 }
 
 @end
