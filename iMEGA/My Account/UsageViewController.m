380c8f9322fafb8ba6523d5f7af9b198c8e4dbce
Javier Santana <jse@mega.nz>
2016-08-03 15:07:04 +0200

    Deleted scroll view and embedded UI into two different views in UsageViewController
    This way Usage view is compatible with size classes.

--- iMEGA/My Account/UsageViewController.m
+++ iMEGA/My Account/UsageViewController.m
@@ -34,10 +34,9 @@
     NSByteCountFormatter *byteCountFormatter;
 }
 
-@property (weak, nonatomic) IBOutlet UIScrollView *usageScrollView;
-
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *logoutBarButtonItem;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *pieChartTopLayoutConstraint;
 @property (weak, nonatomic) IBOutlet PieChartView *pieChartView;
 @property (weak, nonatomic) IBOutlet UILabel *pieChartMainLabel;
 @property (weak, nonatomic) IBOutlet UILabel *pieChartSecondaryLabel;
@@ -68,8 +67,8 @@
     self.pieChartView.delegate = self;
     self.pieChartView.datasource = self;
     
-    if (![[UIDevice currentDevice] iPhone4X]) {
-        [_usageScrollView setScrollEnabled:NO];
+    if ([[UIDevice currentDevice] iPhone4X]) {
+        self.pieChartTopLayoutConstraint.constant = 22.0;
     }
     
     [self.logoutBarButtonItem setTitle:AMLocalizedString(@"logoutLabel", nil)];
