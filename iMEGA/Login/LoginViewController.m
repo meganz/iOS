8395fff294186e0f136c902035c8279fd692a001
Javier Navarro <jn@mega.co.nz>
2016-08-03 09:49:04 +0200

    Manage all possible errors for login request (#4988)

--- iMEGA/Login/LoginViewController.m
+++ iMEGA/Login/LoginViewController.m
@@ -151,6 +151,16 @@
     
 }
 
+- (NSString *)timeFormatted:(NSUInteger)totalSeconds {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateStyle:NSDateFormatterNoStyle];
+    [formatter setTimeStyle:NSDateFormatterMediumStyle];
+    
+    NSDate *date = [NSDate dateWithTimeIntervalSinceNow:totalSeconds];
+    
+    return [formatter stringFromDate:date];
+}
+
 #pragma mark - UIResponder
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -222,6 +232,39 @@
                 break;
             }
                 
+            case MEGAErrorTypeApiETooMany: {
+                NSString *message = [NSString stringWithFormat:AMLocalizedString(@"tooManyAttemptsLogin", @"Error message when to many attempts to login"), [self timeFormatted:3600]];
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:AMLocalizedString(@"error", nil)
+                                                                message:message
+                                                               delegate:self
+                                                      cancelButtonTitle:AMLocalizedString(@"ok", nil)
+                                                      otherButtonTitles:nil];
+                [alert show];
+            
+                break;
+            }
+                
+            case MEGAErrorTypeApiEIncomplete: {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:AMLocalizedString(@"error", nil)
+                                                                message:AMLocalizedString(@"accountNotValidated", @"Error message when trying to login to an account not validated")
+                                                               delegate:self
+                                                      cancelButtonTitle:AMLocalizedString(@"ok", nil)
+                                                      otherButtonTitles:nil];
+                [alert show];
+            
+                break;
+            }
+                
+            case MEGAErrorTypeApiEBlocked: {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:AMLocalizedString(@"error", nil)
+                                                                message:AMLocalizedString(@"accountBlocked", @"Error message when trying to login and the account is suspended")
+                                                               delegate:self
+                                                      cancelButtonTitle:AMLocalizedString(@"ok", nil)
+                                                      otherButtonTitles:nil];
+                [alert show];
+                break;
+            }
+                
             default:
                 break;
         }
@@ -246,12 +289,6 @@
         default:
             break;
     }
-}
-
-- (void)onRequestUpdate:(MEGASdk *)api request:(MEGARequest *)request {
-}
-
-- (void)onRequestTemporaryError:(MEGASdk *)api request:(MEGARequest *)request error:(MEGAError *)error {
 }
 
 @end
