b33851fc1343d5747088780e6deaa5450c968fb0
Javier Santana <jse@mega.nz>
2016-10-05 17:44:18 +0200

    Reset password with master key (Logged in)
    - Process #recover links.
    - Added new mode to ChangePasccodeViewController to reset password.
    - Refactored email validation to Helper.
    - Added new strings related to this process.

--- iMEGA/Contacts/ContactsViewController.m
+++ iMEGA/Contacts/ContactsViewController.m
@@ -321,20 +321,6 @@
     }
 }
 
-- (BOOL)validateEmail:(NSString *)email {
-    NSString *emailRegex =
-    @"(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"
-    @"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"
-    @"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"
-    @"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"
-    @"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"
-    @"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"
-    @"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES[c] %@", emailRegex];
-    
-    return [emailTest evaluateWithObject:email];
-}
-
 - (BOOL)userTypeHasChanged:(MEGAUser *)user {
     BOOL userHasChanged = NO;
     
@@ -884,7 +870,7 @@
     BOOL shouldEnable = YES;
     if ([alertView tag] == 3) {
         NSString *email = [[alertView textFieldAtIndex:0] text];
-        shouldEnable = [self validateEmail:email];
+        shouldEnable = [Helper validateEmail:email];
     }
     
     return shouldEnable;
