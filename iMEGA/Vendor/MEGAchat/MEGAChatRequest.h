e0925b591f5ab04c3f82c5bfcf589e3afeaed65b
Javier Navarro <jn@mega.co.nz>
2016-12-02 13:41:38 +0100

    Add new properties to MEGAChatRequest (iOS)

--- iMEGA/Vendor/MEGAchat/MEGAChatRequest.h
+++ iMEGA/Vendor/MEGAchat/MEGAChatRequest.h
@@ -1,4 +1,5 @@
 #import <Foundation/Foundation.h>
+#import "MEGAChatPeerList.h"
 
 typedef NS_ENUM (NSInteger, MEGAChatRequestType) {
     MEGAChatRequestTypeInitialize,
@@ -26,6 +27,13 @@ typedef NS_ENUM (NSInteger, MEGAChatRequestType) {
 
 @property (readonly, nonatomic) MEGAChatRequestType type;
 @property (readonly, nonatomic) NSString *requestString;
+@property (readonly, nonatomic) NSInteger tag;
+@property (readonly, nonatomic, getter=isFlag) BOOL flag;
+@property (readonly, nonatomic) MEGAChatPeerList *megaChatPeerList;
+@property (readonly, nonatomic) uint64_t chatHandle;
+@property (readonly, nonatomic) uint64_t userHandle;
+@property (readonly, nonatomic) NSInteger privilege;
+@property (readonly, nonatomic) NSString *text;
 
 - (instancetype)clone;
 
