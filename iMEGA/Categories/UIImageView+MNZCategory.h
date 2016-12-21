835898c8f8d30033fb5964e6ca816dd3f02b3b37
Javier Navarro <jn@mega.co.nz>
2016-12-21 19:10:22 +0100

    Remove model `MEGAParticipant`, use `peerXXXX` methods

--- iMEGA/Categories/UIImageView+MNZCategory.h
+++ iMEGA/Categories/UIImageView+MNZCategory.h
@@ -1,9 +1,7 @@
 #import <UIKit/UIKit.h>
-#import "MEGAParticipant.h"
 
 @interface UIImageView (MNZCategory) <MEGARequestDelegate>
 
 - (void)mnz_setImageForUserHandle:(uint64_t)userHandle;
-- (void)mnz_setImageForParticipant:(MEGAParticipant *)participant;
 
 @end
--- iMEGA/Categories/UIImageView+MNZCategory.m
+++ iMEGA/Categories/UIImageView+MNZCategory.m
@@ -33,29 +33,6 @@
     }
 }
 
-- (void)mnz_setImageForParticipant:(MEGAParticipant *)participant {
-    self.layer.cornerRadius = self.frame.size.width / 2;
-    self.layer.masksToBounds = YES;
-    
-    NSString *participantBase64Handle = [MEGASdk base64HandleForUserHandle:participant.handle];
-    NSString *avatarFilePath = [[[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"thumbnailsV3"] stringByAppendingPathComponent:participantBase64Handle];
-    if ([[NSFileManager defaultManager] fileExistsAtPath:avatarFilePath]) {
-        self.image = [UIImage imageWithContentsOfFile:avatarFilePath];
-    } else {
-        NSString *colorString = [[MEGASdkManager sharedMEGASdk] avatarColorForBase64UserHandle:participantBase64Handle];
-        NSString *initialsForAvatar = nil;
-        if (participant.email.length) {
-            initialsForAvatar = participant.email.uppercaseString;
-        } else {
-            initialsForAvatar = participant.name.uppercaseString;
-        }
-        UIImage *avatar = [UIImage imageForName:initialsForAvatar size:self.frame.size backgroundColor:[UIColor colorFromHexString:colorString] textColor:[UIColor whiteColor] font:[UIFont fontWithName:@"SFUIText-Light" size:(self.frame.size.width/2)]];
-        self.image = avatar;
-        
-        [[MEGASdkManager sharedMEGASdk] getAvatarUserWithEmailOrHandle:participantBase64Handle destinationFilePath:avatarFilePath delegate:self];
-    }
-}
-
 - (void)onRequestFinish:(MEGASdk *)api request:(MEGARequest *)request error:(MEGAError *)error {
     if (error.type != MEGAErrorTypeApiOk) {
         return;
