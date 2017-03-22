b013f03aff8ee11759faf74160a048d959825b6a
Javier Navarro <jn@mega.co.nz>
2017-03-22 12:08:32 +0100

    Add property `signalActivityRequired to MEGAChatPresenceConfig class (Obj-C)

--- iMEGA/Vendor/MEGAchat/MEGAChatPresenceConfig.h
+++ iMEGA/Vendor/MEGAchat/MEGAChatPresenceConfig.h
@@ -10,6 +10,7 @@ typedef NS_ENUM (NSInteger, MEGAChatStatus);
 @property (readonly, nonatomic) NSDate *autoAwayTimeout;
 @property (readonly, nonatomic, getter=isPersist) BOOL persist;
 @property (readonly, nonatomic, getter=isPending) BOOL pending;
+@property (readonly, nonatomic, getter=isSignalActivityRequired) BOOL signalActivityRequired;
 
 - (instancetype)clone;
 
