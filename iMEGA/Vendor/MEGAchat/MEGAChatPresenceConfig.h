6c63a8421c7418438daf9266c305f385124bce7e
Javier Santana Espejo <jse@mega.nz>
2017-03-31 11:24:53 +0200

    Presence config autoAwayTimeout now returns an int64_t instead of a NSDate (Obj-C)

--- iMEGA/Vendor/MEGAchat/MEGAChatPresenceConfig.h
+++ iMEGA/Vendor/MEGAchat/MEGAChatPresenceConfig.h
@@ -7,7 +7,7 @@ typedef NS_ENUM (NSInteger, MEGAChatStatus);
 
 @property (readonly, nonatomic) MEGAChatStatus onlineStatus;
 @property (readonly, nonatomic, getter=isAutoAwayEnabled) BOOL autoAwayEnabled;
-@property (readonly, nonatomic) NSDate *autoAwayTimeout;
+@property (readonly, nonatomic) int64_t autoAwayTimeout;
 @property (readonly, nonatomic, getter=isPersist) BOOL persist;
 @property (readonly, nonatomic, getter=isPending) BOOL pending;
 @property (readonly, nonatomic, getter=isSignalActivityRequired) BOOL signalActivityRequired;
