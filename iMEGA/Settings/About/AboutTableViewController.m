ad84f44754af600884ccc4f7368d8d6664bc5a6e
Javier Navarro <jn@mega.co.nz>
2017-04-10 19:08:31 +0200

    Bump version 3.4.4-54

--- iMEGA/Settings/About/AboutTableViewController.m
+++ iMEGA/Settings/About/AboutTableViewController.m
@@ -36,7 +36,7 @@
     self.versionCell.gestureRecognizers = @[tapGestureRecognizer];
     
     [self.sdkVersionLabel setText:[NSString stringWithFormat:@"MEGA SDK %@", AMLocalizedString(@"version", nil)]];
-    self.sdkVersionSHALabel.text = @"4144b6c";
+    self.sdkVersionSHALabel.text = @"ed20d75";
     
     [self.acknowledgementsLabel setText:AMLocalizedString(@"acknowledgements", nil)];
 }
