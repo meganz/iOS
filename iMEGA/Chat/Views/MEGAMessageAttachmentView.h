fbbc61c142f8398932d0c306384361b5dd10d8e1
Javier Navarro <jn@mega.co.nz>
2017-04-21 15:04:30 +0200

    Show messages that contains nodes and contact attached. Allow attach contacts to a chat.
    - New custom bubble message view for contacts and nodes. If the message contains only one contact / node, show information relative to the specific item (contact name, email, avatar or file name, size, thumbnail). If the message contains multiple contacts / nodes, show generic information (3 contacts, contact's emails or 4 files, X Gb)
    - New ContactMode AttachParticipant
    - Don't add MEGAChatMessageType RevokeAttachment to the messages array to avoid add empty bubble messages
    - MEGAChatMessageType: Attachment, RevokeAttachment and Contact are not management messages. They should be managed independently

---
+++ iMEGA/Chat/Views/MEGAMessageAttachmentView.h
@@ -0,0 +1,10 @@
+
+#import <UIKit/UIKit.h>
+
+@interface MEGAMessageAttachmentView : UIView
+
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImage;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *detailLabel;
+
+@end
