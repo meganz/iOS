49ba637d27732156d9dd9ff4d5b61c6ddb37f638
Javier Navarro <jn@mega.co.nz>
2017-06-20 17:48:01 +0200

    Update SDK and Karere submodules

--- iMEGA/Settings/About/AboutTableViewController.m
+++ iMEGA/Settings/About/AboutTableViewController.m
@@ -36,7 +36,7 @@
     self.versionCell.gestureRecognizers = @[tapGestureRecognizer];
     
     [self.sdkVersionLabel setText:[NSString stringWithFormat:@"MEGA SDK %@", AMLocalizedString(@"version", nil)]];
-    self.sdkVersionSHALabel.text = @"5d53663";
+    self.sdkVersionSHALabel.text = @"3a2bc39";
     
     [self.acknowledgementsLabel setText:AMLocalizedString(@"acknowledgements", nil)];
 }
