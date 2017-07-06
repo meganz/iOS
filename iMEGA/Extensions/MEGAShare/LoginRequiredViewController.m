43005723539881f1c4d77b8ec647f560e0877f98
Javier Trujillo <jth@mega.nz>
2017-07-06 14:00:35 +0200

    Localization of the strings in the Share Extension

--- iMEGA/Extensions/MEGAShare/LoginRequiredViewController.m
+++ iMEGA/Extensions/MEGAShare/LoginRequiredViewController.m
@@ -3,12 +3,17 @@
 
 @interface LoginRequiredViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextView *loginTextView;
+@property (weak, nonatomic) IBOutlet UIButton *openButton;
+
 @end
 
 @implementation LoginRequiredViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.loginTextView.text = AMLocalizedString(@"openMEGAAndSignInToContinue", @"Text shown when you try to use a MEGA extension in iOS and you aren't logged");
+    [self.openButton setTitle:AMLocalizedString(@"openButton", @"Button title to trigger the action of opening the file without downloading or opening it.") forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
