c84482ace4169a4c1185adc16dcad0b9c9d9466f
Javier Navarro <jn@mega.co.nz>
2017-07-12 13:14:45 +0200

    Allow navigate in a chatroom between previews loaded

--- iMEGA/Chat/ChatAttachedNodesViewController.h
+++ iMEGA/Chat/ChatAttachedNodesViewController.h
@@ -4,5 +4,6 @@
 @interface ChatAttachedNodesViewController : UIViewController
 
 @property (nonatomic, strong) MEGAChatMessage *message;
+@property (nonatomic, strong) NSArray *nodesLoadedInChatroom;
 
 @end
