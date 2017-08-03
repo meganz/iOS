0a38719207c4adf0bdb63d57a6a1831f5bb18966
Javier Navarro <jn@mega.co.nz>
2017-08-03 16:13:12 +0200

    Use node for fingerprint with parent node.
    Improvements on logs

--- iMEGA/Utils/MEGAProcessAsset.h
+++ iMEGA/Utils/MEGAProcessAsset.h
@@ -2,9 +2,11 @@
 #import <Foundation/Foundation.h>
 #import <Photos/Photos.h>
 
+static NSString *MEGAProcessAssetErrorDomain = @"MEGAProcessAssetErrorDomain";
+
 @interface MEGAProcessAsset : NSObject
 
-- (instancetype)initWithAsset:(PHAsset *)asset filePath:(void (^)(NSString *filePath))filePath node:(void(^)(MEGANode *node))node error:(void (^)(NSError *error))error;
+- (instancetype)initWithAsset:(PHAsset *)asset parentNode:(MEGANode *)parentNode filePath:(void (^)(NSString *filePath))filePath node:(void(^)(MEGANode *node))node error:(void (^)(NSError *error))error;
 - (void)prepare;
 
 @end
