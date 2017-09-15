b2cd313a3e783c2b9705f4c0798df8fffc3c69a8
Javier Santana Espejo <jse@mega.nz>
2017-09-15 13:43:59 +0200

    Minor fixes
    - Fixed date formatters along the app to show the date style regarding to the current locale.
    - Code styling.

--- iMEGA/MEGAExportRequestDelegate.m
+++ iMEGA/MEGAExportRequestDelegate.m
@@ -14,10 +14,11 @@
 
 - (instancetype)initWithCompletion:(void (^)(MEGARequest *request))completion multipleLinks:(BOOL)multipleLinks {
     self = [super init];
-    if(self) {
+    if (self) {
         _completion = completion;
         _multipleLinks = multipleLinks;
     }
+    
     return self;
 }
 
@@ -26,8 +27,8 @@
 - (void)onRequestStart:(MEGASdk *)api request:(MEGARequest *)request {
     [super onRequestStart:api request:request];
 
-    if ([request access]) {
-        NSString *status = self.multipleLinks ? AMLocalizedString(@"generatingLinks", nil) : AMLocalizedString(@"generatingLink", nil);
+    if (request.access) {
+        NSString *status = self.multipleLinks ? AMLocalizedString(@"generatingLinks", @"Message shown when some links to files and/or folders are being generated") : AMLocalizedString(@"generatingLink", @"Message shown when some links to files and/or folders are being generated");
         [SVProgressHUD showWithStatus:status];
     } else {
         [SVProgressHUD show];
@@ -37,7 +38,7 @@
 - (void)onRequestFinish:(MEGASdk *)api request:(MEGARequest *)request error:(MEGAError *)error {
     [super onRequestFinish:api request:request error:error];
 
-    if ([error type]) {
+    if (error.type) {
         [SVProgressHUD showErrorWithStatus:error.name];
         return;
     }
