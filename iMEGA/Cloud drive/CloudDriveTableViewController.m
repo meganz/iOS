388f78e82a511b32dedc74a2b91f6bda5dfaa33d
Javier Trujillo <jth@mega.nz>
2017-10-16 14:54:32 +0200

    Notify properly a user if the Cloud Drive is full

--- iMEGA/Cloud drive/CloudDriveTableViewController.m
+++ iMEGA/Cloud drive/CloudDriveTableViewController.m
@@ -973,8 +973,9 @@
     static BOOL alreadyPresented = NO;
     if (!alreadyPresented && ![[MEGASdkManager sharedMEGASdk] mnz_isProAccount]) {
         MEGAAccountDetails *accountDetails = [[MEGASdkManager sharedMEGASdk] mnz_accountDetails];
-        if (accountDetails && ((accountDetails.storageUsed.doubleValue / accountDetails.storageMax.doubleValue) > 0.95)) { // +95% used storage
-            NSString *alertMessage = AMLocalizedString(@"cloudDriveIsAlmostFull", @"Informs the user that they’ve almost reached the full capacity of their Cloud Drive for a Free account. Please leave the [S], [/S], [A], [/A] placeholders as they are.");
+        double percentage = accountDetails.storageUsed.doubleValue / accountDetails.storageMax.doubleValue;
+        if (accountDetails && percentage > 0.95) { // +95% used storage
+            NSString *alertMessage = percentage < 1 ? AMLocalizedString(@"cloudDriveIsAlmostFull", @"Informs the user that they’ve almost reached the full capacity of their Cloud Drive for a Free account. Please leave the [S], [/S], [A], [/A] placeholders as they are.") : AMLocalizedString(@"cloudDriveIsFull", @"Informs the user that they’ve reached the full capacity of their Cloud Drive for a Free account. Please leave the [S], [/S], [A], [/A] placeholders as they are.");
             alertMessage = [alertMessage mnz_removeWebclientFormatters];
             
             UIAlertController *alertController = [UIAlertController alertControllerWithTitle:AMLocalizedString(@"upgradeAccount", @"Button title which triggers the action to upgrade your MEGA account level") message:alertMessage preferredStyle:UIAlertControllerStyleAlert];
