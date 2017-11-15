ee45869c52507ffbf5990734e9d1d6e3908fb041
Javier Navarro <jn@mega.co.nz>
2017-11-15 14:47:26 +0100

    Allow move the local video image view

--- iMEGA/Chat/Views/MEGALocalImageView.m
+++ iMEGA/Chat/Views/MEGALocalImageView.m
@@ -2,6 +2,12 @@
 #import "MEGALocalImageView.h"
 #import "UIImage+MNZCategory.h"
 
+@interface MEGALocalImageView ()
+
+@property (nonatomic) CGPoint offset;
+
+@end
+
 @implementation MEGALocalImageView
 
 #pragma mark - MEGAChatVideoDelegate
@@ -9,6 +15,19 @@
 - (void)onChatVideoData:(MEGAChatSdk *)api chatId:(uint64_t)chatId width:(NSInteger)width height:(NSInteger)height buffer:(NSData *)buffer {
     UIImage *image = [UIImage mnz_convertBitmapRGBA8ToUIImage:(unsigned char *)buffer.bytes withWidth:width withHeight:height];
     self.image = image;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *aTouch = [touches anyObject];
+    _offset = [aTouch locationInView:self];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self.superview];
+    [UIView beginAnimations:@"Dragging" context:nil];
+    self.frame = CGRectMake(location.x - self.offset.x, location.y - self.offset.y, self.frame.size.width, self.frame.size.height);
+    [UIView commitAnimations];
 }
 
 @end
