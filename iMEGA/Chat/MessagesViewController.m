87b8790ce71007d9cc4915acf41bc2d2277a9f8e
Javier Trujillo <jth@mega.nz>
2017-11-27 11:16:37 +0100

    Margins of chat rooms set according to the new design

--- iMEGA/Chat/MessagesViewController.m
+++ iMEGA/Chat/MessagesViewController.m
@@ -303,6 +303,9 @@ const CGFloat kAvatarImageDiameter = 24.0f;
     self.collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSizeMake(kAvatarImageDiameter, kAvatarImageDiameter);
     
     self.collectionView.collectionViewLayout.minimumLineSpacing = 2.0f;
+    
+    self.collectionView.collectionViewLayout.sectionInset = UIEdgeInsetsMake(10.0f, 30.0f, 10.0f, 30.0f);
+    self.collectionView.collectionViewLayout.messageBubbleLeftRightMargin = 10.0f;
 }
 
 - (void)customToolbarContentView {
