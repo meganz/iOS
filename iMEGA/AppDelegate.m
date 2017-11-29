1e223121fd84d3a0239132d51998929fe5e9fc25
Javier Navarro <jn@mega.co.nz>
2017-11-29 14:49:32 +0100

    Add more logs

--- iMEGA/AppDelegate.m
+++ iMEGA/AppDelegate.m
@@ -130,7 +130,7 @@ typedef NS_ENUM(NSUInteger, URLType) {
         [[MEGALogger sharedLogger] startLogging];
     }
     
-    MEGALogDebug(@"The launch process is almost done and the app is almost ready to run. Launch options: %@", launchOptions);
+    MEGALogDebug(@"Application did finish launching with options %@", launchOptions);
     
     _signalActivityRequired = NO;
     
@@ -361,11 +361,14 @@ typedef NS_ENUM(NSUInteger, URLType) {
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    MEGALogDebug(@"Application will resign active");
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    MEGALogDebug(@"Application did enter background");
+    
     [[MEGASdkManager sharedMEGAChatSdk] setBackgroundStatus:YES];
     [[MEGASdkManager sharedMEGAChatSdk] saveCurrentState];
 
@@ -389,6 +392,8 @@ typedef NS_ENUM(NSUInteger, URLType) {
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
+    MEGALogDebug(@"Application will enter foreground");
+    
     [[MEGAReachabilityManager sharedManager] reconnectIfIPHasChanged];
     [[MEGASdkManager sharedMEGAChatSdk] setBackgroundStatus:NO];
     
@@ -408,6 +413,7 @@ typedef NS_ENUM(NSUInteger, URLType) {
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    MEGALogDebug(@"Application did become active");
     
     [[MEGASdkManager sharedMEGASdk] retryPendingConnections];
     [[MEGASdkManager sharedMEGASdkFolder] retryPendingConnections];
@@ -415,6 +421,7 @@ typedef NS_ENUM(NSUInteger, URLType) {
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    MEGALogDebug(@"Application will terminate");
     
     [[SKPaymentQueue defaultQueue] removeTransactionObserver:[MEGAPurchase sharedInstance]];
     
@@ -437,6 +444,7 @@ typedef NS_ENUM(NSUInteger, URLType) {
 }
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    MEGALogDebug(@"Application open URL %@, source application %@", url, sourceApplication);
     self.link = url;
     
     if ([SAMKeychain passwordForService:@"MEGA" account:@"sessionV3"]) {
@@ -453,12 +461,13 @@ typedef NS_ENUM(NSUInteger, URLType) {
 }
 
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
+    MEGALogDebug(@"Application did register user notification settings");
     [application registerForRemoteNotifications];
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     if([deviceToken length] == 0) {
-        MEGALogError(@"Token length is 0");
+        MEGALogError(@"Application did register for remote notifications with device token length 0");
         return;
     }
     
@@ -472,15 +481,17 @@ typedef NS_ENUM(NSUInteger, URLType) {
     }
     
     NSString *deviceTokenString = [NSString stringWithString:hexString];
-    MEGALogDebug(@"Device token %@", deviceTokenString);
+    MEGALogDebug(@"Application did register for remote notifications with device token %@", deviceTokenString);
     [[MEGASdkManager sharedMEGASdk] registeriOSdeviceToken:deviceTokenString];
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    MEGALogError(@"Failed to register for remote notifications %@", error);
+    MEGALogError(@"Application did fail to register for remote notifications with error %@", error);
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *restorableObjects))restorationHandler {
+    MEGALogDebug(@"Application continue user activity");
+    
     if ([userActivity.activityType isEqualToString:CSSearchableItemActionType] && [MEGAReachabilityManager isReachable]) {
         self.nodeToPresentBase64Handle = userActivity.userInfo[@"kCSSearchableItemActivityIdentifier"];
         if ([self.window.rootViewController isKindOfClass:[MainTabBarController class]] && ![LTHPasscodeViewController doesPasscodeExist]) {
@@ -493,19 +504,24 @@ typedef NS_ENUM(NSUInteger, URLType) {
 }
 
 - (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL succeeded))completionHandler {
+    MEGALogDebug(@"Application perform action for shortcut item");
+    
     if (isFetchNodesDone) {
         completionHandler([self manageQuickActionType:shortcutItem.type]);
     }
 }
 
 - (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    MEGALogWarning(@"Application did receive memory warning");
+    
     if (@available(iOS 9.0, *)) {
-        MEGALogWarning(@"Memory warning, stopping spotlight indexing");
         [self.indexer stopIndexing];
     }
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    MEGALogDebug(@"Application did receive remote notification");
+    
     if (application.applicationState == UIApplicationStateInactive) {
         _megatype = [[userInfo objectForKey:@"megatype"] unsignedIntegerValue];
         [self openTabBasedOnNotificationMegatype];
@@ -552,6 +568,7 @@ typedef NS_ENUM(NSUInteger, URLType) {
 }
 
 - (void)startBackgroundTask {
+    MEGALogDebug(@"Start background task");
     bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
         [[UIApplication sharedApplication] endBackgroundTask:bgTask];
         bgTask = UIBackgroundTaskInvalid;
