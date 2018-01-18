331254c214b54e52f1adf3184934a350fc2a7833
Javier Navarro <jn@mega.co.nz>
2018-01-18 11:07:06 +0100

    No pdf thumbnails when sharing from cloud drive in chat (8313)

--- iMEGA/Categories/UIImageView+MNZCategory.h
+++ iMEGA/Categories/UIImageView+MNZCategory.h
@@ -3,6 +3,7 @@
 @interface UIImageView (MNZCategory) <MEGARequestDelegate>
 
 - (void)mnz_setImageForUserHandle:(uint64_t)userHandle;
+- (void)mnz_setThumbnailByNodeHandle:(uint64_t)nodeHandle;
 - (void)mnz_setImageForExtension:(NSString *)extension;
 
 @end
