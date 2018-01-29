198f6fbd2d3a6ca51115b6ac5a7d3b95e4feae9a
Carlos Martin Acera <cma@mega.nz>
2018-01-29 11:06:40 +0100

    + Prepare layout for landscape orientation and three buttons option + Added string "Get Bonus"

--- iMEGA/Utils/ViewControllers/CustomModalAlertViewController.m
+++ iMEGA/Utils/ViewControllers/CustomModalAlertViewController.m
@@ -1,6 +1,9 @@
 
 #import "CustomModalAlertViewController.h"
 
+#import "AchievementsViewController.h"
+#import "UIApplication+MNZCategory.h"
+
 @interface CustomModalAlertViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
@@ -8,6 +11,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *detailLabel;
 @property (weak, nonatomic) IBOutlet UIButton *actionButton;
 @property (weak, nonatomic) IBOutlet UIButton *dismissButton;
+@property (weak, nonatomic) IBOutlet UIButton *bonusButton;
 
 @end
 
@@ -40,6 +44,10 @@
     if (!self.dismiss) {
         self.dismissButton.hidden = YES;
     }
+    
+    if (!self.bonus) {
+        self.bonusButton.hidden = YES;
+    }
 }
 
 #pragma mark - IBActions
@@ -50,6 +58,15 @@
 
 - (IBAction)dismissTouchUpInside:(UIButton *)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)bonusTouchUpInside:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:^{
+        AchievementsViewController *achievementsVC = [[UIStoryboard storyboardWithName:@"MyAccount" bundle:nil] instantiateViewControllerWithIdentifier:@"AchievementsViewControllerID"];
+        achievementsVC.enableCloseBarButton = YES;
+        UINavigationController *navigation = [[UINavigationController alloc] initWithRootViewController:achievementsVC];
+        [[UIApplication mnz_visibleViewController] presentViewController:navigation animated:YES completion:nil];
+    }];
 }
 
 @end
