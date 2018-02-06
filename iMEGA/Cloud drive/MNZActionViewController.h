c3c3713fc7ecd4688823a86c843e55458685ce26
Carlos Martin Acera <cma@mega.nz>
2018-02-06 11:53:35 +0100

    Added logic to get actions by node access level and view (cloud drive, shared and rubbish bin)

--- iMEGA/Cloud drive/MNZActionViewController.h
+++ iMEGA/Cloud drive/MNZActionViewController.h
@@ -1,8 +1,11 @@
 
 #import <UIKit/UIKit.h>
+#import "CloudDriveTableViewController.h"
 
 @interface MNZActionViewController : UIViewController
 
 @property (strong, nonatomic) MEGANode *node;
+@property (nonatomic) DisplayMode displayMode;
+@property (nonatomic, getter=isIncomingShareChildView) BOOL incomingShareChildView;
 
 @end
