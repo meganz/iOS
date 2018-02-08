ec2f2dd47df5a1bfcd8ff3febc956524865ddf60
Carlos Martin Acera <cma@mega.nz>
2018-02-08 12:17:48 +0100

    Shared items cell new layout and custom edit

--- iMEGA/Shared Items/SharedItemsTableViewCell.h
+++ iMEGA/Shared Items/SharedItemsTableViewCell.h
@@ -1,5 +1,14 @@
 #import <UIKit/UIKit.h>
 
+
+@protocol SharedItemsTableViewCellDelegate
+
+@optional
+
+@property (nonatomic, readonly, getter=isPseudoEditing) BOOL pseudoEdit;
+
+@end
+
 @interface SharedItemsTableViewCell : UITableViewCell
 
 @property (weak, nonatomic) IBOutlet UIImageView *thumbnailImageView;
@@ -9,5 +18,9 @@
 @property (weak, nonatomic) IBOutlet UIButton *infoButton;
 
 @property (nonatomic) uint64_t nodeHandle;
+
+@property (nonatomic, assign) id <SharedItemsTableViewCellDelegate> customEditDelegate;
+
+@property (assign, nonatomic) BOOL isSwiping;
 
 @end
--- iMEGA/Shared Items/SharedItemsTableViewCell.m
+++ iMEGA/Shared Items/SharedItemsTableViewCell.m
@@ -1,5 +1,49 @@
 #import "SharedItemsTableViewCell.h"
 
+static NSInteger const kCustomEditControlWidth=50;
+
+@interface SharedItemsTableViewCell ()
+
+@property (nonatomic, getter=isPseudoEditing) BOOL pseudoEdit;
+@property (weak, nonatomic) IBOutlet UIView *mainView;
+@property (weak, nonatomic) IBOutlet UIButton *customEditControl;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leadingSpaceMainViewConstraint;
+
+@end
+
 @implementation SharedItemsTableViewCell
+
+#pragma mark - Life Cycle
+
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+    if ([self.customEditDelegate isPseudoEditing]) {
+        self.pseudoEdit = editing;
+//        [self setSwipeOffset:0];
+        [self beginEditMode];
+    } else {
+        [super setEditing:editing animated:animated];
+    }
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    self.customEditControl.selected = selected;
+}
+
+#pragma mark - Private Method
+
+// Animate view to show/hide custom edit control/button
+- (void)beginEditMode {
+    if (!self.isSwiping) {
+        self.leadingSpaceMainViewConstraint.constant = self.isPseudoEditing ? 0 : -kCustomEditControlWidth;
+    } else {
+        self.isSwiping = NO;
+    }
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.mainView.superview layoutIfNeeded];
+    }];
+}
 
 @end
