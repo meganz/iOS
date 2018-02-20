d673cd080fc405e95dcdf377b120eb22a85e335c
Carlos Martin Acera <cma@mega.nz>
2018-02-20 10:32:49 +0100

    Created delegate to navigate to parent node from NodeInfoView

--- iMEGA/Cloud drive/NodeInfoViewController.h
+++ iMEGA/Cloud drive/NodeInfoViewController.h
@@ -1,11 +1,19 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol NodeInfoViewControllerDelegate <NSObject>
+
+- (void)presentParentNode:(MEGANode *)node inNavigation:(UINavigationController *)navigationController;
+
+@end
+
 @class MEGANode;
 
 @interface NodeInfoViewController : UIViewController
 
 @property (strong, nonatomic) MEGANode *node;
+
+@property (weak, nonatomic) id<NodeInfoViewControllerDelegate> nodeInfoDelegate;
 
 - (void)configureCloseBarButton;
 
