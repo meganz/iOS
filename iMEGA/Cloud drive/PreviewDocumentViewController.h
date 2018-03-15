1da759363bf4177a7bba9df0fae5bf48a174d60a
Carlos Martin Acera <cma@mega.nz>
2018-03-15 17:05:57 +0100

    Changes in Preview Documents view
    - Present modally all documents in PreviewDocumentViewController
    - Adjusted QLPreview frame subview to view bounds in order to adapt to orientation changes
    - Added share action

--- iMEGA/Cloud drive/PreviewDocumentViewController.h
+++ iMEGA/Cloud drive/PreviewDocumentViewController.h
@@ -5,5 +5,8 @@
 
 @property (nonatomic, strong) MEGANode *node;
 @property (nonatomic, strong) MEGASdk *api;
+@property (nonatomic, strong) NSString *previewDocumentPath;
+
+- (void)doneTapped;
 
 @end
