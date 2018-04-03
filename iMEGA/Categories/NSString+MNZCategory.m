9f04d38f693020de49a093ab7666dbe5c28e3669
Javier Trujillo <jth@mega.nz>
2018-04-03 14:11:23 +0200

    Updated localized strings
    - Also, adapted to the new HTML received in the agreeWithTheMEGATermsOfService string

--- iMEGA/Categories/NSString+MNZCategory.m
+++ iMEGA/Categories/NSString+MNZCategory.m
@@ -216,6 +216,8 @@ static NSString* const B = @"[B]";
     string = [string stringByReplacingOccurrencesOfString:@"[/A]" withString:@""];
     string = [string stringByReplacingOccurrencesOfString:@"[S]" withString:@""];
     string = [string stringByReplacingOccurrencesOfString:@"[/S]" withString:@""];
+    string = [string stringByReplacingOccurrencesOfString:@"<a href='terms'>" withString:@""];
+    string = [string stringByReplacingOccurrencesOfString:@"</a>" withString:@""];
     
     return string;
 }
