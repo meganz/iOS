46ff4e6a01802a18bca5d8f344a3e55797975ef6
Carlos Martin Acera <cma@mega.nz>
2018-04-18 14:10:04 +0200

    Refactoring of TransfersViewController
    - Allow to pause individual transfers
    - Moved configure cell code to cell class
    - Added string for paused state
    - Use new property "state" of transfers

--- iMEGA/Transfers/TransferTableViewCell.m
+++ iMEGA/Transfers/TransferTableViewCell.m
@@ -1,6 +1,17 @@
 #import "TransferTableViewCell.h"
 #import "MEGASdkManager.h"
 
+#import "MEGAPauseTransferRequestDelegate.h"
+#import "MEGAGetThumbnailRequestDelegate.h"
+
+#import "Helper.h"
+
+@interface TransferTableViewCell ()
+
+@property (assign, nonatomic) BOOL isPaused;
+
+@end
+
 @implementation TransferTableViewCell
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -27,14 +38,109 @@
     }
 }
 
+- (void)configureCellForActiveTransfer:(MEGATransfer *)transfer {
+    self.transfer = transfer;
+    if (transfer.type == MEGATransferTypeDownload) {
+        self.arrowImageView.image = [Helper downloadingTransferImage];
+        [self.percentageLabel setTextColor:UIColor.mnz_green31B500];
+    } else {
+        self.arrowImageView.image = [Helper uploadingTransferImage];
+        [self.percentageLabel setTextColor:UIColor.mnz_blue2BA6DE];
+    }
+    
+    [self.pauseButton setImage:[UIImage imageNamed:@"pauseTransfers"] forState:UIControlStateNormal];
+
+    [self updatePercentAndSpeedLabelsForTransfer:transfer];
+    
+    [self configureCell];
+}
+
+- (void)configureCellForPausedTransfer:(MEGATransfer *)transfer {
+    self.transfer = transfer;
+    [self.pauseButton setImage:[UIImage imageNamed:@"resumeTransfers"] forState:UIControlStateNormal];
+    [self.percentageLabel setText:AMLocalizedString(@"paused", @"Paused")];
+    [self.percentageLabel setTextColor:UIColor.mnz_gray666666];
+
+    [self configureCell];
+}
+
+- (void)configureCellForQueuedTransfer:(MEGATransfer *)transfer {
+    self.transfer = transfer;
+    [self.pauseButton setImage:[UIImage imageNamed:@"pauseTransfers"] forState:UIControlStateNormal];
+    [self.percentageLabel setText:AMLocalizedString(@"queued", @"Queued")];
+    [self.percentageLabel setTextColor:UIColor.mnz_gray666666];
+
+    [self configureCell];
+}
+
+- (void)configureCell {
+    if (self.transfer.type == MEGATransferTypeDownload) {
+        [self.arrowImageView setImage:[Helper downloadQueuedTransferImage]];
+    } else {
+        [self.arrowImageView setImage:[Helper uploadQueuedTransferImage]];
+    }
+    [self.nameLabel setText:[[MEGASdkManager sharedMEGASdk] unescapeFsIncompatible:self.transfer.fileName]];
+    
+    MEGANode *node = [[MEGASdkManager sharedMEGASdk] nodeForHandle:self.transfer.nodeHandle];
+    if (node.hasThumbnail) {
+        NSString *thumbnailFilePath = [Helper pathForNode:node inSharedSandboxCacheDirectory:@"thumbnailsV3"];
+        if ([[NSFileManager defaultManager] fileExistsAtPath:thumbnailFilePath]) {
+            self.iconImageView.image = [UIImage imageWithContentsOfFile:thumbnailFilePath];
+        } else {
+            MEGAGetThumbnailRequestDelegate *getThumbnailRequestDelegate = [[MEGAGetThumbnailRequestDelegate alloc] initWithCompletion:^(MEGARequest *request) {
+                self.iconImageView.image = [UIImage imageWithContentsOfFile:request.file];
+            }];
+            [[MEGASdkManager sharedMEGASdk] getThumbnailNode:node destinationFilePath:thumbnailFilePath delegate:getThumbnailRequestDelegate];
+            self.iconImageView.image = [Helper imageForNode:node];
+        }
+    } else {
+        self.iconImageView.image = [Helper imageForNode:node];
+    }
+}
+
+- (void)updatePercentAndSpeedLabelsForTransfer:(MEGATransfer *)transfer {
+    self.transfer = transfer;
+    
+    if (transfer.type == MEGATransferTypeDownload) {
+        [self.arrowImageView setImage:[Helper downloadingTransferImage]];
+        [self.percentageLabel setTextColor:UIColor.mnz_green31B500];
+    } else {
+        [self.arrowImageView setImage:[Helper uploadingTransferImage]];
+        [self.percentageLabel setTextColor:UIColor.mnz_blue2BA6DE];
+    }
+    
+    float percentage = (transfer.transferredBytes.floatValue / transfer.totalBytes.floatValue * 100);
+    NSString *percentageCompleted = [NSString stringWithFormat:@"%.f %%", percentage];
+    self.percentageLabel.text = percentageCompleted;
+    NSString *speed = [NSString stringWithFormat:@"%@/s", [NSByteCountFormatter stringFromByteCount:transfer.speed.longLongValue  countStyle:NSByteCountFormatterCountStyleMemory]];
+    self.speedLabel.text = speed;
+}
+
 #pragma mark - IBActions
 
 - (IBAction)cancelTransfer:(id)sender {
-    if ([[MEGASdkManager sharedMEGASdk] transferByTag:self.transferTag] != nil) {
-        [[MEGASdkManager sharedMEGASdk] cancelTransferByTag:self.transferTag];
+    if ([[MEGASdkManager sharedMEGASdk] transferByTag:self.transfer.tag] != nil) {
+        [[MEGASdkManager sharedMEGASdk] cancelTransferByTag:self.transfer.tag];
     } else {
-        if ([[MEGASdkManager sharedMEGASdkFolder] transferByTag:self.transferTag] != nil) {
-            [[MEGASdkManager sharedMEGASdkFolder] cancelTransferByTag:self.transferTag];
+        if ([[MEGASdkManager sharedMEGASdkFolder] transferByTag:self.transfer.tag] != nil) {
+            [[MEGASdkManager sharedMEGASdkFolder] cancelTransferByTag:self.transfer.tag];
+        }
+    }
+}
+
+- (IBAction)pauseTransfer:(id)sender {
+    self.isPaused = self.transfer.state == MEGATransferStatePaused;
+    
+    MEGAPauseTransferRequestDelegate *pauseTransferDelegate = [[MEGAPauseTransferRequestDelegate alloc] initWithCompletion:^(MEGARequest *request) {
+        self.isPaused = self.transfer.state == MEGATransferStatePaused;
+        [self.delegate pauseTransferCell:self];
+    }];
+    
+    if ([[MEGASdkManager sharedMEGASdk] transferByTag:self.transfer.tag] != nil) {
+        [[MEGASdkManager sharedMEGASdk] pauseTransferByTag:self.transfer.tag pause:!self.isPaused delegate:pauseTransferDelegate];
+    } else {
+        if ([[MEGASdkManager sharedMEGASdkFolder] transferByTag:self.transfer.tag] != nil) {
+            [[MEGASdkManager sharedMEGASdkFolder] pauseTransferByTag:self.transfer.tag pause:!self.isPaused delegate:pauseTransferDelegate];
         }
     }
 }
