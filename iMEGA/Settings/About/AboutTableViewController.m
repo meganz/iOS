4d03d19fb6e693dab6cf348b856a65a07a52cd28
Javier Navarro <jn@mega.co.nz>
2018-04-24 09:26:54 +0200

    Bump build version and update submodules

--- iMEGA/Settings/About/AboutTableViewController.m
+++ iMEGA/Settings/About/AboutTableViewController.m
@@ -40,10 +40,10 @@
     self.versionCell.gestureRecognizers = @[tapGestureRecognizer];
     
     self.sdkVersionLabel.text = AMLocalizedString(@"sdkVersion", @"Title of the label where the SDK version is shown");
-    self.sdkVersionSHALabel.text = @"861f8b48";
+    self.sdkVersionSHALabel.text = @"20be39c0";
     
     self.megachatSdkVersionLabel.text = AMLocalizedString(@"megachatSdkVersion", @"Title of the label where the MEGAchat SDK version is shown");
-    self.megachatSdkSHALabel.text = @"34cf2e2a";
+    self.megachatSdkSHALabel.text = @"f5cb59d";
     
     [self.acknowledgementsLabel setText:AMLocalizedString(@"acknowledgements", nil)];
 }
