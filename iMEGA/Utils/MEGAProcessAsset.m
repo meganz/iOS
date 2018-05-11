5612c3511c6256f81acff08b5100b289be73ae12
Javier Navarro <jn@mega.co.nz>
2018-05-11 14:38:08 +0200

    Avoid downscale image if the factor is greater or equal than 1

--- iMEGA/Utils/MEGAProcessAsset.m
+++ iMEGA/Utils/MEGAProcessAsset.m
@@ -77,38 +77,47 @@ static const NSUInteger DOWNSCALE_IMAGES_PX = 2000000;
         options.version = PHImageRequestOptionsVersionOriginal;
     }
     
-    // Optimized image
+    
     if (self.toShareThroughChat && ![MEGAReachabilityManager isReachableViaWiFi]) {
-        options.synchronous = YES;
-        options.resizeMode = PHImageRequestOptionsResizeModeExact;
         NSUInteger totalPixels = self.asset.pixelWidth * self.asset.pixelHeight;
         float factor = MIN(sqrtf((float)DOWNSCALE_IMAGES_PX / totalPixels), 1);
-        [[PHImageManager defaultManager] requestImageForAsset:self.asset targetSize:CGSizeMake(self.asset.pixelWidth * factor, self.asset.pixelHeight * factor) contentMode:PHImageContentModeAspectFit options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
-            if (result) {
-                NSData *imageData = UIImageJPEGRepresentation(result, 0.75);
-                [self proccessImageData:imageData withInfo:info];
-            } else {
-                NSError *error = [info objectForKey:@"PHImageErrorKey"];
-                MEGALogError(@"Request image data for asset: %@ failed with error: %@", self.asset, error);
-                if (self.retries < 20) {
-                    self.retries++;
-                    [self requestImageAsset];
+        if (factor >= 1) {
+            [self requestImageWithOptions:options];
+        } else { // Optimize image
+            options.synchronous = YES;
+            options.resizeMode = PHImageRequestOptionsResizeModeExact;
+            [[PHImageManager defaultManager] requestImageForAsset:self.asset targetSize:CGSizeMake(self.asset.pixelWidth * factor, self.asset.pixelHeight * factor) contentMode:PHImageContentModeAspectFit options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+                if (result) {
+                    NSData *imageData = UIImageJPEGRepresentation(result, 0.75);
+                    [self proccessImageData:imageData withInfo:info];
                 } else {
-                    if (self.error) {
-                        MEGALogDebug(@"Max attempts reached");
-                        self.error(error);
+                    NSError *error = [info objectForKey:@"PHImageErrorKey"];
+                    MEGALogError(@"Request image data for asset: %@ failed with error: %@", self.asset, error);
+                    if (self.retries < 20) {
+                        self.retries++;
+                        [self requestImageAsset];
+                    } else {
+                        if (self.error) {
+                            MEGALogDebug(@"Max attempts reached");
+                            self.error(error);
+                        }
                     }
                 }
-            }
-        }];
+            }];
+        }
     } else {
-        [[PHImageManager defaultManager]
-         requestImageDataForAsset:self.asset
-         options:options
-         resultHandler:^(NSData *imageData, NSString *dataUTI, UIImageOrientation orientation, NSDictionary *info) {
-             [self proccessImageData:imageData withInfo:info];
-         }];
+        [self requestImageWithOptions:options];
     }
+}
+
+// Request image and don't downscale it
+- (void)requestImageWithOptions:(PHImageRequestOptions *)options {
+    [[PHImageManager defaultManager]
+     requestImageDataForAsset:self.asset
+     options:options
+     resultHandler:^(NSData *imageData, NSString *dataUTI, UIImageOrientation orientation, NSDictionary *info) {
+         [self proccessImageData:imageData withInfo:info];
+     }];
 }
 
 - (void)requestVideoAsset {
