cecbb34fd6ace5d14d9bca293b16b22e5f311eff
Javier Trujillo <jth@mega.nz>
2018-07-02 17:10:07 +0200

    Allow to rename items before being uploaded with the Share Extension
    - The workflow in the Share Extension has changed. Previously, the files were loaded and uploaded after selecting a destination. Now, they are loaded at launch, and stored in a array. This way, they can be renamed before being uploaded.
    - Added ShareAttachment class to handle attached files. It is responsible for suggesting the initial name of the files, when they don't have one. The new behavior naming files may fix some bugs:
      - Previously, contacts where not uploaded if the last item of the array didn't have a name or email. Now, an UUID string is assigned to these files.
      - Previously, if there were multiple items in the array of contacts without name and email, the number of unsupported assets could be wrongly calculated.
      - In iOS 11+, the suggested name for images is retrieved from the NSItemProvider. In iOS 10-, an UUID string is used to avoid a possible bug assigning the same name to different images without name.
    - Custom cell for files with an icon and a text field that allows renaming the file.
    - Added localizable string.
    - Other UI improvements according to the new design.

---
+++ iMEGA/Extensions/MEGAShare/Model/ShareAttachment.h
@@ -0,0 +1,24 @@
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, ShareAttachmentType) {
+    ShareAttachmentTypePNG,
+    ShareAttachmentTypeImage,
+    ShareAttachmentTypeFile,
+    ShareAttachmentTypeURL,
+    ShareAttachmentTypeContact
+};
+
+@interface ShareAttachment : NSObject
+
+@property (nonatomic) ShareAttachmentType type;
+@property (nonatomic) NSString *name;
+@property (nonatomic) id content;
+
++ (NSMutableArray<ShareAttachment *> *)attachmentsArray;
++ (void)addImage:(UIImage *)image fromItemProvider:(NSItemProvider *)itemProvider;
++ (void)addFileURL:(NSURL *)url;
++ (void)addURL:(NSURL *)url;
++ (void)addContact:(NSData *)vCardData;
+
+@end
