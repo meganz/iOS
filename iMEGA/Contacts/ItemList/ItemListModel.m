b973f90cb58b70babe702a58fdb8bfccfa51248b
Carlos Martin Acera <cma@mega.nz>
2018-07-19 13:38:22 +0200

    - Refactored UsersListViewController into ItemListViewController to allow show different kind of objects, not just MEGAUser

---
+++ iMEGA/Contacts/ItemList/ItemListModel.m
@@ -0,0 +1,67 @@
+#import "ItemListModel.h"
+#import "MEGAChatListItem.h"
+#import "MEGAUser+MNZCategory.h"
+#import "UIImage+MNZCategory.h"
+
+@interface ItemListModel ()
+
+@property MEGAChatListItem *chat;
+@property MEGAUser *user;
+
+@end
+
+@implementation ItemListModel
+
+- (instancetype)initWithChat:(MEGAChatListItem *)chat {
+    self = [super init];
+    if (self) {
+        self.chat = chat;
+    }
+    return self;
+}
+
+- (instancetype)initWithUser:(MEGAUser *)user {
+    self = [super init];
+    if (self) {
+        self.user = user;
+    }
+    return self;
+}
+
+- (BOOL)isGroup {
+    if (self.chat) {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+- (NSString *)name {
+    if (self.chat) {
+        return self.chat.title;
+    } else {
+        return self.user.mnz_firstName;
+    }
+}
+
+- (uint64_t)handle {
+    if (self.user) {
+        return self.user.handle;
+    } else {
+        return self.chat.peerHandle;
+    }
+}
+
+- (BOOL)isEqual:(ItemListModel *)item {
+    return self.handle == item.handle;
+}
+
+- (id)model {
+    if (self.user) {
+        return self.user;
+    } else {
+        return self.chat;
+    }
+}
+
+@end
