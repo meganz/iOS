e9851a4a54c1c1d92d1a37a1559f55ce6647c664
Carlos Martin Acera <cma@mega.nz>
2018-07-27 14:04:10 +0200

    - Load thumbnail in cell for upload transfers - Fixed getting index path for uploads (using tag instead of nodeHandle) - Allow init a transfer cached in core data

--- iMEGA/Transfers/TransferTableViewCell.m
+++ iMEGA/Transfers/TransferTableViewCell.m
@@ -7,6 +7,10 @@
 #import "UIImageView+MNZCategory.h"
 #import <Photos/Photos.h>
 #import "MEGAStore.h"
+#import "MEGAProcessAsset.h"
+#import "NSString+MNZCategory.h"
+#import "NSFileManager+MNZCategory.h"
+#import "SVProgressHUD.h"
 
 @interface TransferTableViewCell ()
 
@@ -62,24 +66,39 @@
     self.nameLabel.text = [[MEGASdkManager sharedMEGASdk] unescapeFsIncompatible:self.transfer.fileName];
     
     MEGANode *node = [[MEGASdkManager sharedMEGASdk] nodeForHandle:self.transfer.nodeHandle];
-    if (node.hasThumbnail) {
-        NSString *thumbnailFilePath = [Helper pathForNode:node inSharedSandboxCacheDirectory:@"thumbnailsV3"];
-        if ([[NSFileManager defaultManager] fileExistsAtPath:thumbnailFilePath]) {
-            self.iconImageView.image = [UIImage imageWithContentsOfFile:thumbnailFilePath];
-        } else {
-            MEGAGetThumbnailRequestDelegate *getThumbnailRequestDelegate = [[MEGAGetThumbnailRequestDelegate alloc] initWithCompletion:^(MEGARequest *request) {
-                self.iconImageView.image = [UIImage imageWithContentsOfFile:request.file];
-            }];
-            [[MEGASdkManager sharedMEGASdk] getThumbnailNode:node destinationFilePath:thumbnailFilePath delegate:getThumbnailRequestDelegate];
-            [self.iconImageView mnz_imageForNode:node];
+    switch (self.transfer.type) {
+        case MEGATransferTypeDownload: {
+            if (node.hasThumbnail) {
+                NSString *thumbnailFilePath = [Helper pathForNode:node inSharedSandboxCacheDirectory:@"thumbnailsV3"];
+                if ([[NSFileManager defaultManager] fileExistsAtPath:thumbnailFilePath]) {
+                    self.iconImageView.image = [UIImage imageWithContentsOfFile:thumbnailFilePath];
+                } else {
+                    MEGAGetThumbnailRequestDelegate *getThumbnailRequestDelegate = [[MEGAGetThumbnailRequestDelegate alloc] initWithCompletion:^(MEGARequest *request) {
+                        self.iconImageView.image = [UIImage imageWithContentsOfFile:request.file];
+                    }];
+                    [[MEGASdkManager sharedMEGASdk] getThumbnailNode:node destinationFilePath:thumbnailFilePath delegate:getThumbnailRequestDelegate];
+                    [self.iconImageView mnz_imageForNode:node];
+                }
+            } else {
+                if (transfer.type == MEGATransferTypeDownload) {
+                    [self.iconImageView mnz_imageForNode:node];
+                } else {
+                    [self.iconImageView mnz_setImageForExtension:transfer.fileName.pathExtension];
+                }
+            }
+            break;
         }
-    } else {
-        if (transfer.type == MEGATransferTypeDownload) {
-            [self.iconImageView mnz_imageForNode:node];
-        } else {
-            [self.iconImageView mnz_setImageForExtension:transfer.fileName.pathExtension];
+            
+        case MEGATransferTypeUpload: {
+            NSString *transferAbsolutePath = [[NSHomeDirectory() stringByAppendingPathComponent:transfer.path] stringByAppendingString:@"_thumbnail"];
+            self.iconImageView.image = [UIImage imageWithContentsOfFile:transferAbsolutePath];
+            break;
         }
+            
+        default:
+            break;
     }
+    
     
     [self configureCellState];
 }
@@ -131,7 +150,6 @@
     switch (self.transfer.state) {
             
         case MEGATransferStateActive:
-            self.pauseButton.hidden = NO;
             [self.pauseButton setImage:[UIImage imageNamed:@"pauseTransfers"] forState:UIControlStateNormal];
             [self updatePercentAndSpeedLabelsForTransfer:self.transfer];
             break;
@@ -160,7 +178,6 @@
 }
 
 - (void)inactiveStateLayout {
-    self.pauseButton.hidden = NO;
     [self.pauseButton setImage:[UIImage imageNamed:@"pauseTransfers"] forState:UIControlStateNormal];
     self.percentageLabel.textColor = UIColor.mnz_gray666666;
     
@@ -172,7 +189,6 @@
 }
 
 - (void)queuedStateLayout {
-    self.pauseButton.hidden = YES;
     self.percentageLabel.textColor = UIColor.mnz_gray666666;
     self.arrowImageView.image = [Helper uploadQueuedTransferImage];
     self.percentageLabel.text = AMLocalizedString(@"queued", @"Queued");
@@ -192,24 +208,83 @@
     } else if (self.asset) {
         [[MEGAStore shareInstance] deleteUploadTransferWithLocalIdentifier:self.asset.localIdentifier];
     }
-    
 }
 
 - (IBAction)pauseTransfer:(id)sender {
-    self.isPaused = self.transfer.state == MEGATransferStatePaused;
-    
-    MEGAPauseTransferRequestDelegate *pauseTransferDelegate = [[MEGAPauseTransferRequestDelegate alloc] initWithCompletion:^(MEGARequest *request) {
+    if (self.transfer) {
         self.isPaused = self.transfer.state == MEGATransferStatePaused;
-        [self.delegate pauseTransferCell:self];
-    }];
-    
-    if ([[MEGASdkManager sharedMEGASdk] transferByTag:self.transfer.tag] != nil) {
-        [[MEGASdkManager sharedMEGASdk] pauseTransferByTag:self.transfer.tag pause:!self.isPaused delegate:pauseTransferDelegate];
-    } else {
-        if ([[MEGASdkManager sharedMEGASdkFolder] transferByTag:self.transfer.tag] != nil) {
-            [[MEGASdkManager sharedMEGASdkFolder] pauseTransferByTag:self.transfer.tag pause:!self.isPaused delegate:pauseTransferDelegate];
+        
+        MEGAPauseTransferRequestDelegate *pauseTransferDelegate = [[MEGAPauseTransferRequestDelegate alloc] initWithCompletion:^(MEGARequest *request) {
+            self.isPaused = self.transfer.state == MEGATransferStatePaused;
+            [self.delegate pauseTransferCell:self];
+        }];
+        
+        if ([[MEGASdkManager sharedMEGASdk] transferByTag:self.transfer.tag] != nil) {
+            [[MEGASdkManager sharedMEGASdk] pauseTransferByTag:self.transfer.tag pause:!self.isPaused delegate:pauseTransferDelegate];
+        } else {
+            if ([[MEGASdkManager sharedMEGASdkFolder] transferByTag:self.transfer.tag] != nil) {
+                [[MEGASdkManager sharedMEGASdkFolder] pauseTransferByTag:self.transfer.tag pause:!self.isPaused delegate:pauseTransferDelegate];
+            }
         }
+    } else if (self.asset) {
+        
+        MOUploadTransfer *uploadTransfer = [[MEGAStore shareInstance] fetchTransferUpdateWithLocalIdentifier:self.asset.localIdentifier];
+        
+        MEGANode *parentNode = [[MEGASdkManager sharedMEGASdk] nodeForHandle:uploadTransfer.parentNodeHandle.unsignedLongLongValue];
+        MEGAProcessAsset *processAsset = [[MEGAProcessAsset alloc] initWithAsset:self.asset parentNode:parentNode filePath:^(NSString *filePath) {
+            NSString *name = filePath.lastPathComponent;
+            NSString *newName = [self sequenceNameForNodeNamed:name parentNode:parentNode];
+            
+            NSString *appData = [NSString new];
+            
+            appData = [appData mnz_appDataToSaveCoordinates:[filePath mnz_coordinatesOfPhotoOrVideo]];
+            
+            if (![name isEqualToString:newName]) {
+                NSString *newFilePath = [[NSFileManager defaultManager].uploadsDirectory stringByAppendingPathComponent:newName];
+                
+                NSError *error = nil;
+                NSString *absoluteFilePath = [NSHomeDirectory() stringByAppendingPathComponent:filePath];
+                if (![[NSFileManager defaultManager] moveItemAtPath:absoluteFilePath toPath:newFilePath error:&error]) {
+                    MEGALogError(@"Move item at path failed with error: %@", error);
+                }
+                [[MEGASdkManager sharedMEGASdk] startUploadWithLocalPath:[newFilePath stringByReplacingOccurrencesOfString:[NSHomeDirectory() stringByAppendingString:@"/"] withString:@""] parent:parentNode appData:appData isSourceTemporary:YES];
+            } else {
+                [[MEGASdkManager sharedMEGASdk] startUploadWithLocalPath:[filePath stringByReplacingOccurrencesOfString:[NSHomeDirectory() stringByAppendingString:@"/"] withString:@""] parent:parentNode appData:appData isSourceTemporary:YES];
+            }
+        } node:^(MEGANode *node) {
+            if ([[[MEGASdkManager sharedMEGASdk] parentNodeForNode:node] handle] == parentNode.handle) {
+                MEGALogDebug(@"The asset exists in MEGA in the parent folder");
+            } else {
+                [[MEGASdkManager sharedMEGASdk] copyNode:node newParent:parentNode];
+            }
+        } error:^(NSError *error) {
+            [SVProgressHUD showImage:[UIImage imageNamed:@"hudError"] status:[NSString stringWithFormat:@"%@ %@", AMLocalizedString(@"Transfer failed:", nil), self.nameLabel.text]];
+        }];
+        [processAsset prepare];
+        
+        [[MEGAStore shareInstance] deleteUploadTransferWithLocalIdentifier:self.asset.localIdentifier];
     }
+}
+
+#pragma mark - Private
+
+- (NSString *)sequenceNameForNodeNamed:(NSString *)name parentNode:(MEGANode *)parentNode {
+    NSString *nameWithoutExtension = [name stringByDeletingPathExtension];
+    NSString *extension = [name pathExtension];
+    int index = 0;
+    int listSize = 0;
+    
+    do {
+        if (index != 0) {
+            nameWithoutExtension = [[name stringByDeletingPathExtension] stringByAppendingString:[NSString stringWithFormat:@"_%d", index]];
+        }
+        
+        MEGANodeList *nameNodeList = [[MEGASdkManager sharedMEGASdk] nodeListSearchForNode:parentNode searchString:[nameWithoutExtension stringByAppendingPathExtension:extension]];
+        listSize = [nameNodeList.size intValue];
+        index++;
+    } while (listSize != 0);
+    
+    return [nameWithoutExtension stringByAppendingPathExtension:extension];
 }
 
 @end
