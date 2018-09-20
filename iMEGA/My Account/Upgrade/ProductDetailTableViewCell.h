3543cc313fa0542f16df15af7b551713413ec06f
Javier Santana Espejo <jse@mega.nz>
2018-09-20 14:13:52 +0200

    Fixed issues on My Account section when using RTL languages
    - Also applied fixes on the views related.

--- iMEGA/My Account/Upgrade/ProductDetailTableViewCell.h
+++ iMEGA/My Account/Upgrade/ProductDetailTableViewCell.h
@@ -6,4 +6,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *periodLabel;
 @property (weak, nonatomic) IBOutlet UILabel *priceLabel;
 
+@property (weak, nonatomic) IBOutlet UIView *underLineView;
+
 @end
