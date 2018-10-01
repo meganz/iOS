0d4c5b004560f5408694f1bfd07919385213dd51
Javier Navarro <jn@mega.co.nz>
2018-10-01 11:33:17 +0200

    Remove "lineView" from cell My Account storyboard (10615)

--- iMEGA/My Account/MyAccountHallTableViewCell.m
+++ iMEGA/My Account/MyAccountHallTableViewCell.m
@@ -3,14 +3,6 @@
 
 @implementation MyAccountHallTableViewCell
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-    
-    if (selected) {
-        self.lineView.backgroundColor = UIColor.mnz_grayCCCCCC;
-    }
-}
-
 - (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
     [super setHighlighted:highlighted animated:animated];
     
@@ -20,8 +12,6 @@
         self.selectedBackgroundView = view;
         
         self.pendingView.backgroundColor = UIColor.mnz_redMain;
-        
-        self.lineView.backgroundColor = UIColor.mnz_grayCCCCCC;
     }
 }
 
