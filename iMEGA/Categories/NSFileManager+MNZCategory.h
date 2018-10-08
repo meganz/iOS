03e4263a36b3938adb78907404917e06122ef3c4
Javier Santana Espejo <jse@mega.nz>
2018-10-08 15:34:24 +0200

    Refactored methods to remove files and folders and/or its contents to the NSFileManager category

--- iMEGA/Categories/NSFileManager+MNZCategory.h
+++ iMEGA/Categories/NSFileManager+MNZCategory.h
@@ -3,7 +3,16 @@
 
 @interface NSFileManager (MNZCategory)
 
+#pragma mark - Paths
+
 - (NSString *)downloadsDirectory;
 - (NSString *)uploadsDirectory;
+
+#pragma mark - Remove files and folders
+
+- (void)mnz_removeItemAtPath:(NSString *)path;
+- (void)mnz_removeFolderContentsAtPath:(NSString *)folderPath;
+- (void)mnz_removeFolderContentsAtPath:(NSString *)folderPath forItemsContaining:(NSString *)filesContaining;
+- (void)mnz_removeFolderContentsRecursivelyAtPath:(NSString *)folderPath forItemsContaining:(NSString *)filesContaining;
 
 @end
