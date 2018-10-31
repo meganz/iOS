ce2f76c22fdff67fb50b5397aa61ee68272b3e08
Simon Wang <swg@mega.co.nz>
2018-11-01 10:42:00 +1300

    add dynamic type support for chat room view controller

--- iMEGA/Categories/UIFont+MNZCategory.h
+++ iMEGA/Categories/UIFont+MNZCategory.h
@@ -11,4 +11,24 @@
 
 + (UIFont *)mnz_defaultFontForPureEmojiStringWithEmojis:(NSUInteger)emojiCount;
 
+
+/**
+ Returns a matching font by the given text style and weight
+
+ @param style UIFontTextStyle type
+ @param weight UIFontWeight type
+ @return a new font matching the given style and weight
+ */
++ (UIFont *)preferredFontForTextStyle:(UIFontTextStyle)style weight:(UIFontWeight)weight;
+
+/**
+ @return a bold font based on the current font
+ */
+- (UIFont *)bold;
+
+/**
+ @return an italic font based on the current font
+ */
+- (UIFont *)italic;
+
 @end
