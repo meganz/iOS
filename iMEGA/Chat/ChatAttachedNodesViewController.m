c43bbaadd15c214f01f301f61cf58f39c2079692
Javier Trujillo <jth@mega.nz>
2018-11-13 19:31:59 +0100

    Authorize nodes in chat previews before import/download them

--- iMEGA/Chat/ChatAttachedNodesViewController.m
+++ iMEGA/Chat/ChatAttachedNodesViewController.m
@@ -305,6 +305,7 @@
         
         return;
     } else {
+        // Nodes should be authorized here before being opened, if at any moment attaching multiple nodes in a single message is allowed for public chats.
         if (node.name.mnz_isImagePathExtension || node.name.mnz_isVideoPathExtension) {
             NSMutableArray<MEGANode *> *mediaNodesArray = [self.message.nodeList mnz_mediaNodesMutableArrayFromNodeList];
             
