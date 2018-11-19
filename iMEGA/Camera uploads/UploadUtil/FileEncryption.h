c4d2f75ee248708a5d2f9308a899d0d26704c617
Simon Wang <swg@mega.co.nz>
2018-11-19 21:29:29 +1300

    add shouldTruncateInputFile parameter to add the ability to opt out of truncating

--- iMEGA/Camera uploads/UploadUtil/FileEncryption.h
+++ iMEGA/Camera uploads/UploadUtil/FileEncryption.h
@@ -7,7 +7,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FileEncryption : NSObject
 
-- (instancetype)initWithMediaUpload:(MEGABackgroundMediaUpload *)mediaUpload outputFileURL:(NSURL *)outputFileURL;
+- (instancetype)initWithMediaUpload:(MEGABackgroundMediaUpload *)mediaUpload outputDirectoryURL:(NSURL *)outputDirectoryURL shouldTruncateInputFile:(BOOL)shouldTruncateInputFile;
 
 - (void)encryptFileAtURL:(NSURL *)fileURL completion:(void (^)(BOOL success, unsigned long long fileSize, NSDictionary<NSString *, NSURL *> * _Nullable chunkURLsKeyedByUploadSuffix, NSError * _Nullable error))completion;
 
