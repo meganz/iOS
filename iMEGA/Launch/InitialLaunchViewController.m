317aaf6aa64c46a83480c71c17fb4ac8c68305c0
Javier Trujillo <jth@mega.nz>
2019-01-09 18:20:19 +0100

    Center the labels vertically in the initial launch view

--- iMEGA/Launch/InitialLaunchViewController.m
+++ iMEGA/Launch/InitialLaunchViewController.m
@@ -10,6 +10,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *setupButton;
 @property (weak, nonatomic) IBOutlet UIButton *skipButton;
 
+@property (nonatomic) BOOL logoMoved;
+
 @end
 
 @implementation InitialLaunchViewController
@@ -21,6 +23,20 @@
     self.descriptionLabel.text = AMLocalizedString(@"To fully take advantage of your MEGA account we need to ask you some permissions.", @"Detailed explanation of why the user should give some permissions to MEGA");
     [self.setupButton setTitle:AMLocalizedString(@"Setup MEGA", @"Button which triggers the initial setup") forState:UIControlStateNormal];
     [self.skipButton setTitle:AMLocalizedString(@"skipButton", @"Button title that skips the current action") forState:UIControlStateNormal];
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    if (!self.logoMoved) {
+        return;
+    }
+    
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+        [self moveLogo];
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+        [self centerLabels];
+    }];
 }
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
@@ -36,13 +52,32 @@
 - (void)performAnimation {
     [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
         self.circularShapeLayer.hidden = YES;
-        CGFloat newY = MIN(self.logoImageView.frame.origin.x, 145.0f);
-        self.logoImageView.frame = CGRectMake(self.logoImageView.frame.origin.x, newY, self.logoImageView.frame.size.width, self.logoImageView.frame.size.height);
+        [self moveLogo];
     } completion:^(BOOL finished) {
+        [self centerLabels];
         self.titleLabel.hidden = self.descriptionLabel.hidden = NO;
         self.setupButton.hidden = self.skipButton.hidden = NO;
         self.activityIndicatorView.hidden = YES;
+        self.logoMoved = YES;
     }];
+}
+
+- (void)moveLogo {
+    CGFloat newY = MIN(self.logoImageView.frame.origin.x, 145.0f);
+    self.logoImageView.frame = CGRectMake(self.logoImageView.frame.origin.x, newY, self.logoImageView.frame.size.width, self.logoImageView.frame.size.height);
+}
+
+- (void)centerLabels {
+    CGFloat bottomSeparation = 28.0f;
+    CGFloat verticalIncrement = (self.titleLabel.frame.origin.y - (self.logoImageView.frame.origin.y + self.logoImageView.frame.size.height) - bottomSeparation) / 2;
+    
+    CGRect titleFrame = self.titleLabel.frame;
+    titleFrame.origin.y -= verticalIncrement;
+    self.titleLabel.frame = titleFrame;
+    
+    CGRect descriptionFrame = self.descriptionLabel.frame;
+    descriptionFrame.origin.y -= verticalIncrement;
+    self.descriptionLabel.frame = descriptionFrame;
 }
 
 #pragma mark - IBActions
