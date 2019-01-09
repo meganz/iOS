89f21e6036973be19ffce8dff236f26c350998d4
Javier Navarro <jn@mega.co.nz>
2019-01-09 19:26:05 +0100

    Remove unused header

--- iMEGA/MEGA-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-
-#import <PureLayout/PureLayout.h>
-#import "LocalizationSystem.h"
-
-#import "DevicePermissionsHelper.h"
-
-#import "UIColor+MNZCategory.h"
-#import "UIDevice+MNZCategory.h"
-#import "UIFont+MNZCategory.h"
