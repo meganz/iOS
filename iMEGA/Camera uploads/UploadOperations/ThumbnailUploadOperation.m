f69958fe3dd464fa56ad13c96a74b916fea89846
Simon Wang <swg@mega.co.nz>
2019-02-21 22:00:33 +1300

    cache thumbnail and preview files if they are already attached to the node

--- iMEGA/Camera uploads/UploadOperations/ThumbnailUploadOperation.m
+++ iMEGA/Camera uploads/UploadOperations/ThumbnailUploadOperation.m
@@ -14,13 +14,20 @@
     [MEGASdkManager.sharedMEGASdk setThumbnailNode:self.node sourceFilePath:self.attributeURL.path delegate:[[CameraUploadRequestDelegate alloc] initWithCompletion:^(MEGARequest * _Nonnull request, MEGAError * _Nonnull error) {
         if (error.type) {
             MEGALogError(@"[Camera Upload] Upload thumbnail failed for node: %@, error: %@", weakSelf.node.name, error.nativeError);
+            if (error.type == MEGAErrorTypeApiEExist) {
+                [weakSelf cacheAttributeFile];
+            }
         } else {
             MEGALogDebug(@"[Camera Upload] Upload thumbnail succeeded for node %@", weakSelf.node.name);
-            [weakSelf moveAttributeToDirectoryURL:[Helper urlForSharedSandboxCacheDirectory:@"thumbnailsV3"] newFileName:self.node.base64Handle];
+            [weakSelf cacheAttributeFile];
         }
         
         [weakSelf finishOperation];
     }]];
+}
+
+- (void)cacheAttributeFile {
+    [self moveAttributeToDirectoryURL:[Helper urlForSharedSandboxCacheDirectory:@"thumbnailsV3"] newFileName:self.node.base64Handle];
 }
 
 @end
