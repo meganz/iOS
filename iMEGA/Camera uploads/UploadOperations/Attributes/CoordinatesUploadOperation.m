69f7fdc9b1181d074db957a0b955244fd4519d7f
Simon Wang <swg@mega.co.nz>
2019-03-11 16:45:35 +1300

    remove background task mode for attributes upload to reduce the memory overhead in expiring and recreating the attribute operations

--- iMEGA/Camera uploads/UploadOperations/Attributes/CoordinatesUploadOperation.m
+++ iMEGA/Camera uploads/UploadOperations/Attributes/CoordinatesUploadOperation.m
@@ -12,7 +12,7 @@
     
     CLLocation *location = [NSKeyedUnarchiver unarchiveObjectWithFile:self.attributeURL.path];
     if (location == nil) {
-        MEGALogError(@"[Camera Upload] can not unarchive location for node %@ %@", self.node.name, self.attributeURL);
+        MEGALogError(@"[Camera Upload] can not unarchive location %@", self);
         [self finishOperation];
         return;
     }
@@ -20,12 +20,12 @@
     __weak __typeof__(self) weakSelf = self;
     [MEGASdkManager.sharedMEGASdk setUnshareableNodeCoordinates:self.node latitude:@(location.coordinate.latitude) longitude:@(location.coordinate.longitude) delegate:[[CameraUploadRequestDelegate alloc] initWithCompletion:^(MEGARequest * _Nonnull request, MEGAError * _Nonnull error) {
         if (error.type) {
-            MEGALogError(@"[Camera Upload] Upload coordinate failed for node: %@ at %@, error: %@", weakSelf.node.name, weakSelf.attributeURL, error.nativeError);
+            MEGALogError(@"[Camera Upload] Upload coordinate failed %@ error: %@", weakSelf, error.nativeError);
             if (error.type == MEGAErrorTypeApiEExist) {
                 [NSFileManager.defaultManager removeItemIfExistsAtURL:weakSelf.attributeURL];
             }
         } else {
-            MEGALogDebug(@"[Camera Upload] Upload coordinate succeeded for node %@ at %@", weakSelf.node.name, weakSelf.attributeURL);
+            MEGALogDebug(@"[Camera Upload] Upload coordinate succeeded %@", weakSelf);
             [NSFileManager.defaultManager removeItemIfExistsAtURL:weakSelf.attributeURL];
         }
         
