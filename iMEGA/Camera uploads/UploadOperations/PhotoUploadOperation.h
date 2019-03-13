e1f7481b5fe9173cdeface0ed5045b24dc325705
Simon Wang <swg@mega.co.nz>
2019-03-14 11:52:30 +1300

    use PHImageManager to request images

--- iMEGA/Camera uploads/UploadOperations/PhotoUploadOperation.h
+++ iMEGA/Camera uploads/UploadOperations/PhotoUploadOperation.h
@@ -1,9 +1,9 @@
 
-#import "AssetResourceUploadOperation.h"
+#import "CameraUploadOperation.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface PhotoUploadOperation : AssetResourceUploadOperation
+@interface PhotoUploadOperation : CameraUploadOperation
 
 @end
 
--- iMEGA/Camera uploads/UploadOperations/PhotoUploadOperation.m
+++ iMEGA/Camera uploads/UploadOperations/PhotoUploadOperation.m
@@ -14,10 +14,16 @@
 #import "MEGAConstants.h"
 #import "ImageExportManager.h"
 #import "CameraUploadOperation+Utils.h"
-#import "PHAssetResource+CameraUpload.h"
 @import CoreServices;
 
-static NSString * const PhotoResourceExportName = @"photoResourceExport";
+static const NSInteger PhotoExportDiskSizeScalingFactor = 2;
+static NSString * const PhotoExportTempName = @"photoExportTemp";
+
+@interface PhotoUploadOperation ()
+
+@property (nonatomic) PHImageRequestID imageRequestId;
+
+@end
 
 @implementation PhotoUploadOperation
 
@@ -25,48 +31,113 @@ static NSString * const PhotoResourceExportName = @"photoResourceExport";
 
 - (void)start {
     [super start];
-
-    [self requestImageResource];
+    
+    [self requestImageData];
 }
 
-#pragma mark - image resource
+#pragma mark - data processing
 
-- (void)requestImageResource {
+- (void)requestImageData {
     if (self.isCancelled) {
         [self finishOperationWithStatus:CameraAssetUploadStatusCancelled shouldUploadNextAsset:NO];
         return;
     }
     
-    PHAssetResource *photoResource = [self.uploadInfo.asset searchAssetResourceByTypes:@[@(PHAssetResourceTypeFullSizePhoto), @(PHAssetResourceTypePhoto), @(PHAssetResourceTypeAdjustmentBasePhoto), @(PHAssetResourceTypeAlternatePhoto)]];
-    if (photoResource) {
-        NSURL *imageURL = [self.uploadInfo.directoryURL URLByAppendingPathComponent:PhotoResourceExportName];
-        [self exportAssetResource:photoResource toURL:imageURL];
-    } else {
-        MEGALogError(@"[Camera Upload] %@ can not find photo resource", self);
-        [self finishOperationWithStatus:CameraAssetUploadStatusFailed shouldUploadNextAsset:YES];
-    }
+    __weak __typeof__(self) weakSelf = self;
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    options.networkAccessAllowed = YES;
+    options.version = PHImageRequestOptionsVersionCurrent;
+    options.synchronous = YES;
+    options.progressHandler = ^(double progress, NSError * _Nullable error, BOOL * _Nonnull stop, NSDictionary * _Nullable info) {
+        if (weakSelf.isCancelled) {
+            *stop = YES;
+            [weakSelf finishOperationWithStatus:CameraAssetUploadStatusCancelled shouldUploadNextAsset:NO];
+        }
+        
+        if (error) {
+            MEGALogError(@"[Camera Upload] %@ error when to download images from iCloud: %@", weakSelf, error);
+            [weakSelf handleCloudDownloadError:error];
+        }
+    };
+    
+    self.imageRequestId = [[PHImageManager defaultManager] requestImageDataForAsset:self.uploadInfo.asset options:options resultHandler:^(NSData * _Nullable imageData, NSString * _Nullable dataUTI, UIImageOrientation orientation, NSDictionary * _Nullable info) {
+        if (weakSelf.isFinished) {
+            return;
+        }
+        
+        if (weakSelf.isCancelled) {
+            [weakSelf finishOperationWithStatus:CameraAssetUploadStatusCancelled shouldUploadNextAsset:NO];
+            return;
+        }
+        
+        NSError *error = info[PHImageErrorKey];
+        if (error) {
+            MEGALogError(@"[Camera Upload] %@ error when to request photo %@", weakSelf, error);
+            [weakSelf finishOperationWithStatus:CameraAssetUploadStatusFailed shouldUploadNextAsset:YES];
+            return;
+        }
+        
+        if ([info[PHImageCancelledKey] boolValue]) {
+            MEGALogDebug(@"[Camera Upload] %@ photo request is cancelled", weakSelf);
+            [weakSelf finishOperationWithStatus:CameraAssetUploadStatusCancelled shouldUploadNextAsset:NO];
+            return;
+        }
+        
+        [weakSelf processImageData:imageData dataUTI:dataUTI dataInfo:info];
+    }];
 }
 
-#pragma mark - asset resource export delegate
-
-- (void)assetResource:(PHAssetResource *)resource didExportToURL:(NSURL *)URL {
-    [super assetResource:resource didExportToURL:URL];
+- (void)processImageData:(NSData *)imageData dataUTI:(NSString *)dataUTI dataInfo:(NSDictionary *)dataInfo {
+    if (self.isCancelled) {
+        [self finishOperationWithStatus:CameraAssetUploadStatusCancelled shouldUploadNextAsset:NO];
+        return;
+    }
     
+    if (imageData == nil) {
+        MEGALogError(@"[Camera Upload] %@ the requested image data is empty", self);
+        [self finishOperationWithStatus:CameraAssetUploadStatusFailed shouldUploadNextAsset:YES];
+        return;
+    }
+    
+    if (imageData.length * PhotoExportDiskSizeScalingFactor > NSFileManager.defaultManager.deviceFreeSize) {
+        [self finishUploadWithNoEnoughDiskSpace];
+        return;
+    }
+    
+    NSURL *imageURL = [self.uploadInfo.directoryURL URLByAppendingPathComponent:PhotoExportTempName];
+    if (![imageData writeToURL:imageURL atomically:YES]) {
+        [self finishOperationWithStatus:CameraAssetUploadStatusFailed shouldUploadNextAsset:YES];
+        return;
+    }
+    
+    self.uploadInfo.originalFingerprint = [MEGASdkManager.sharedMEGASdk fingerprintForFilePath:imageURL.path modificationTime:self.uploadInfo.asset.creationDate];
+    MEGANode *matchingNode = [self nodeForOriginalFingerprint:self.uploadInfo.originalFingerprint];
+    if (matchingNode) {
+        MEGALogDebug(@"[Camera Upload] %@ found node by original fingerprint", self);
+        [self finishUploadForFingerprintMatchedNode:matchingNode];
+        return;
+    }
+    
+    [self exportImageAtURL:imageURL dataUTI:dataUTI dataInfo:dataInfo];
+}
+
+- (void)exportImageAtURL:(NSURL *)URL dataUTI:(NSString *)dataUTI dataInfo:(NSDictionary *)dataInfo {
     if (self.isCancelled) {
         [self finishOperationWithStatus:CameraAssetUploadStatusCancelled shouldUploadNextAsset:NO];
         return;
     }
     
     NSString *outputTypeUTI;
-    if ([self shouldConvertToJPGForUTI:resource.uniformTypeIdentifier]) {
+    if ([self shouldConvertToJPGForUTI:dataUTI]) {
         self.uploadInfo.fileName = [self mnz_generateLocalFileNamewithExtension:MEGAJPGFileExtension];
         outputTypeUTI = (__bridge NSString *)kUTTypeJPEG;
     } else {
-        self.uploadInfo.fileName = [self mnz_generateLocalFileNamewithExtension:resource.originalFilename.pathExtension];
+        NSString *fileExtension = [self.uploadInfo.asset mnz_fileExtensionFromAssetInfo:dataInfo];
+        self.uploadInfo.fileName = [self mnz_generateLocalFileNamewithExtension:fileExtension];
     }
     
     __weak __typeof__(self) weakSelf = self;
-    [ImageExportManager.shared exportImageAtURL:URL dataTypeUTI:resource.uniformTypeIdentifier toURL:self.uploadInfo.fileURL outputTypeUTI:outputTypeUTI shouldStripGPSInfo:YES completion:^(BOOL succeeded) {
+    [ImageExportManager.shared exportImageAtURL:URL dataTypeUTI:dataUTI toURL:self.uploadInfo.fileURL outputTypeUTI:outputTypeUTI shouldStripGPSInfo:YES completion:^(BOOL succeeded) {
         if (weakSelf.isCancelled) {
             [weakSelf finishOperationWithStatus:CameraAssetUploadStatusCancelled shouldUploadNextAsset:NO];
             return;
@@ -87,6 +158,17 @@ static NSString * const PhotoResourceExportName = @"photoResourceExport";
         return [CameraUploadManager shouldConvertHEICPhoto] && UTTypeConformsTo((__bridge CFStringRef)dataUTI, (__bridge CFStringRef)AVFileTypeHEIC);
     } else {
         return NO;
+    }
+}
+
+#pragma mark - cancel operation
+
+- (void)cancel {
+    [super cancel];
+    
+    if (self.imageRequestId != PHInvalidImageRequestID) {
+        MEGALogDebug(@"[Camera Upload] %@ cancel photo data request with request Id %d", self, self.imageRequestId);
+        [PHImageManager.defaultManager cancelImageRequest:self.imageRequestId];
     }
 }
 
