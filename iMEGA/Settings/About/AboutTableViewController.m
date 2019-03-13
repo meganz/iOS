e54fa7a4483bfc0edaba4326ea0ef0cf9da242f0
Javier Navarro <jn@mega.co.nz>
2019-03-13 18:54:00 +0100

    Bump build version

--- iMEGA/Settings/About/AboutTableViewController.m
+++ iMEGA/Settings/About/AboutTableViewController.m
@@ -35,10 +35,10 @@
     self.versionCell.gestureRecognizers = @[tapGestureRecognizer, longPressGestureRecognizer];
     
     self.sdkVersionLabel.text = AMLocalizedString(@"sdkVersion", @"Title of the label where the SDK version is shown");
-    self.sdkVersionSHALabel.text = @"7e38fe11";
+    self.sdkVersionSHALabel.text = @"6ecca6a7";
     
     self.megachatSdkVersionLabel.text = AMLocalizedString(@"megachatSdkVersion", @"Title of the label where the MEGAchat SDK version is shown");
-    self.megachatSdkSHALabel.text = @"fe3bea63";
+    self.megachatSdkSHALabel.text = @"49122ece";
     
     self.viewSourceCodeLabel.text = AMLocalizedString(@"View source code", @"Link to the public code of the ap");
     
