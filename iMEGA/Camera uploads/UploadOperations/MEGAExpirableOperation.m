7d9fe27c5ebaf5c635226e3229df9e7f23f9a4b8
Simon Wang <swg@mega.co.nz>
2019-03-28 21:37:22 +1300

    use dispatch_walltime to improve the accuracy as the timeout time interval is relatively big

--- iMEGA/Camera uploads/UploadOperations/MEGAExpirableOperation.m
+++ iMEGA/Camera uploads/UploadOperations/MEGAExpirableOperation.m
@@ -22,7 +22,7 @@
     [super start];
     
     self.expireTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(QOS_CLASS_UTILITY, 0));
-    dispatch_source_set_timer(self.expireTimer, dispatch_time(DISPATCH_TIME_NOW, (int64_t)(self.expireTimeInterval * NSEC_PER_SEC)), (uint64_t)(self.expireTimeInterval * NSEC_PER_SEC), 1 * NSEC_PER_SEC);
+    dispatch_source_set_timer(self.expireTimer, dispatch_walltime(NULL, (int64_t)(self.expireTimeInterval * NSEC_PER_SEC)), (uint64_t)(self.expireTimeInterval * NSEC_PER_SEC), 1 * NSEC_PER_SEC);
     
     __weak __typeof__(self) weakSelf = self;
     dispatch_source_set_event_handler(self.expireTimer, ^{
