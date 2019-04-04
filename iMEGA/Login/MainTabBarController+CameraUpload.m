e9e206629e8a4171c6f32a08382a8d9cd9584665
Simon Wang <swg@mega.co.nz>
2019-04-04 14:30:07 +1300

    UX feedbacks - remove the mention of Live Photos and bursts in the migration dialog to let users focus on the most important formats conversion capability.

--- iMEGA/Login/MainTabBarController+CameraUpload.m
+++ iMEGA/Login/MainTabBarController+CameraUpload.m
@@ -19,7 +19,7 @@
     migrationVC.modalPresentationStyle = UIModalPresentationOverFullScreen;
     migrationVC.image = [UIImage imageNamed:@"cameraUploadsV2Migration"];
     migrationVC.viewTitle = @"New Camera Upload!";
-    migrationVC.detail = @"Now you can choose to convert the HEIF/HEVC format photos and videos to the most compatible JPEG/H.264.\n\nWe now also upload live photos and burst photos to make sure all of your memorable moments are backed up.";
+    migrationVC.detail = @"Now you can choose to convert the HEIF/HEVC photos and videos to the most compatible JPEG/H.264 formats.";
     migrationVC.action = @"Use Most Compatible Formats";
     migrationVC.actionColor = [UIColor mnz_green00BFA5];
     migrationVC.dismiss = @"Custom Settings";
