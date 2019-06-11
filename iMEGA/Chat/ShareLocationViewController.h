4de687ffb2c431dace2cfa35839207e2bd9064fb
Javier Trujillo <jth@mega.nz>
2019-06-11 10:12:26 +0200

    Some fixes related to the chat inset and scroll
    - Don't lock the toolbar frame when presenting another view controller. In these cases, the unlock in viewWillAppear was being called after the toolbar was presented, then the inset was not being calculated properly.
    - Send the location message after dismissing the view, so that the scroll to bottom can be performed properly. This way, there is no need for a delegate, because a onMessageUpdate will be received.

--- iMEGA/Chat/ShareLocationViewController.h
+++ iMEGA/Chat/ShareLocationViewController.h
@@ -6,17 +6,10 @@ NS_ASSUME_NONNULL_BEGIN
 @class MEGAChatRoom;
 @class MEGAChatMessage;
 
-@protocol ShareLocationViewControllerDelegate <NSObject>
-
-- (void)locationMessage:(MEGAChatMessage *)message editing:(BOOL)editing;
-
-@end
-
 @interface ShareLocationViewController : UIViewController
 
 @property (nonatomic) MEGAChatRoom *chatRoom;
 @property (nonatomic) MEGAChatMessage *editMessage;
-@property (nonatomic, weak) id<ShareLocationViewControllerDelegate> shareLocationViewControllerDelegate;
 
 @end
 
