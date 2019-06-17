ab1de5d464dc8b94e167952003c085ecb4957a55
Simon Wang <swg@mega.co.nz>
2019-06-17 16:42:53 +1200

    implement the scroll view animation behaviours according to the design

--- iMEGA/My Account/SMS/SMSVerificationViewController.swift
+++ iMEGA/My Account/SMS/SMSVerificationViewController.swift
@@ -7,7 +7,10 @@ class SMSVerificationViewController: UIViewController {
     @IBOutlet private var headerImageView: UIImageView!
     @IBOutlet private var nextButton: UIButton!
     @IBOutlet private var nextButtonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private var scrollContentViewPreferredEqualHeightConstraint: NSLayoutConstraint!
+    @IBOutlet private var phoneNumberTextField: UITextField!
 
+    // MARK: View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,18 +19,41 @@ class SMSVerificationViewController: UIViewController {
 
         disableAutomaticAdjustmentContentInsetsBehavior()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveTextDidChangeNotification(_:)), name: UITextField.textDidChangeNotification, object: self.phoneNumberTextField)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         NotificationCenter.default.addObserver(self, selector: #selector(didReceiveKeyboardWillShowNotification(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(didReceiveKeyboardWillHideNotification(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        if navigationController?.isNavigationBarHidden ?? false {
-            return .lightContent
-        } else {
-            return .default
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    // MARK: UI actions
+    @IBAction private func didTapCountryView() {
+        self.phoneNumberTextField.resignFirstResponder()
+    }
+    
+    private func animateViewAdjustments(withDuration duration: Double, keyboardHeight: CGFloat) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        UIView.animate(withDuration: duration, animations: {
+            self.enableAutomaticAdjustmentContentInsetsBehavior()
+            self.headerImageView.isHidden = true
+            self.setNeedsStatusBarAppearanceUpdate()
+            self.nextButtonBottomConstraint.constant = keyboardHeight
+        }) { _ in
+            self.adjustScrollViewContentPreferredHeight()
         }
     }
     
+    // MARK: Notification handlers
     @objc private func didReceiveKeyboardWillShowNotification(_ notification: Notification) {
         guard let duration = (notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue,
             let keyboardHeight = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.height else {
@@ -37,22 +63,32 @@ class SMSVerificationViewController: UIViewController {
         animateViewAdjustments(withDuration: duration, keyboardHeight: keyboardHeight)
     }
     
-    @IBAction private func didTapCountryView() {
-        
-    }
-    
-    private func animateViewAdjustments(withDuration duration: Double, keyboardHeight: CGFloat) {
-        navigationController?.setNavigationBarHidden(false, animated: true)
-        UIView.animate(withDuration: duration, animations: {
-            self.headerImageView.isHidden = true
-            self.setNeedsStatusBarAppearanceUpdate()
-            self.enableAutomaticAdjustmentContentInsetsBehavior()
-            self.nextButtonBottomConstraint.constant = keyboardHeight
-        })
-    }
-    
     @objc private func didReceiveKeyboardWillHideNotification(_ notification: Notification) {
-        
+        nextButtonBottomConstraint.constant = 0
+        adjustScrollViewContentPreferredHeight()
+    }
+    
+    @objc private func didReceiveTextDidChangeNotification(_ notification: Notification) {
+        if notification.object as? UITextField === self.phoneNumberTextField {
+            self.nextButton.isEnabled = !(self.phoneNumberTextField.text?.isEmpty ?? true)
+        }
+    }
+    
+    // MARK: UI configurations
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if navigationController?.isNavigationBarHidden ?? false {
+            return .lightContent
+        } else {
+            return .default
+        }
+    }
+    
+    private func adjustScrollViewContentPreferredHeight() {
+        if #available(iOS 11.0, *) {
+            self.scrollContentViewPreferredEqualHeightConstraint.constant = -self.scrollView.adjustedContentInset.top
+        } else {
+            self.scrollContentViewPreferredEqualHeightConstraint.constant = -self.scrollView.contentInset.top
+        }
     }
     
     private func enableAutomaticAdjustmentContentInsetsBehavior() {
@@ -72,6 +108,7 @@ class SMSVerificationViewController: UIViewController {
     }
 }
 
+// MARK: - UIScrollViewDelegate
 extension SMSVerificationViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard !headerImageView.isHidden else {
@@ -88,8 +125,4 @@ extension SMSVerificationViewController: UIScrollViewDelegate {
             headerImageView.layer.transform = CATransform3DIdentity
         }
     }
-}
-
-extension SMSVerificationViewController: UITextFieldDelegate {
-    
 }
