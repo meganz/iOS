7fc720b69539e7833f438ac4d733b4df70ef5d1b
Javier Santana <jse@mega.nz>
2019-07-15 17:11:00 +0200

    Apply suggestions from code review
    Adapted the comment to the format on the bindings.
    
    Co-Authored-By: Javier Navarro <jn@mega.co.nz>

--- iMEGA/Categories/MEGASdk+MNZCategory.h
+++ iMEGA/Categories/MEGASdk+MNZCategory.h
@@ -7,8 +7,8 @@
 #pragma mark - Chat
 
 /**
+ * @brief This method checks if the folder 'My chat files' exists as target folder. If not, it creates it and set it.
  *
- * This method checks if the folder 'My chat files' exists as target folder. If not, it creates it and set it.
  * In the case that this folder already exists and has not been set as the chat target folder, it is localized to the user language. Also if 'My chat files' has been deleted from MEGA, it creates and set the folder again.
  *
  * @param completion MEGANode 'My chat files' target node.
