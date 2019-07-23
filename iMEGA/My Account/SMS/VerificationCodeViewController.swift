b26ce065768e7cf576f6c3dd22f0b14676f40f25
Simon Wang <swg@mega.co.nz>
2019-07-23 16:18:48 +1200

    add basic error handling when to request SMS verification code

---
+++ iMEGA/My Account/SMS/VerificationCodeViewController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class VerificationCodeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
+}
