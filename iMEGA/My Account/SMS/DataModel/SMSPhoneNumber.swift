84377ddee6cbe249401b277a7fbc0a7a7d50249c
Simon Wang <swg@mega.co.nz>
2019-08-26 21:31:07 +1200

    code format refinements. Move the return clause of the guard to the same line as guard if there is nothing to return.

--- iMEGA/My Account/SMS/DataModel/SMSPhoneNumber.swift
+++
@@ -1,9 +0,0 @@
-
-struct SMSPhoneNumber {
-    let country: SMSCountry
-    let localNumber: String
-    
-    var fullPhoneNumberString: String {
-        return "\(country.displayCallingCode)\(localNumber)"
-    }
-}
