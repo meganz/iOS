172b309eaa0de87609e8fd4b29c44065f338ae85
Simon Wang <swg@mega.co.nz>
2019-09-25 22:26:58 +1200

    prefer to use trailing closure for callbacks

--- iMEGA/AppDelegate+Additions.swift
+++ iMEGA/AppDelegate+Additions.swift
@@ -5,11 +5,10 @@ extension AppDelegate {
     @objc func showAddPhoneNumberIfNeeded() {
         guard !MEGASdkManager.sharedMEGASdk().hasVerifiedPhoneNumber else { return }
         
-        MEGASdkManager.sharedMEGASdk().getUserData(with: MEGAGenericRequestDelegate(completion: { (request, error) in
+        MEGASdkManager.sharedMEGASdk().getUserData(with: MEGAGenericRequestDelegate() { request, error in
             guard error.type == .apiOk else { return }
             guard !MEGASdkManager.sharedMEGASdk().hasVerifiedPhoneNumber else { return }
-            
             UIApplication.mnz_presentingViewController()?.present(AddPhoneNumberViewController.mnz_instantiate(withStoryboardName: "SMSVerification"), animated: true, completion: nil)
-        }))
+        })
     }
 }
