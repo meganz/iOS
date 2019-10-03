1871df2423ffa71e017340081bebd09edbb27a91
Carlos Martin Acera <cma@mega.nz>
2019-10-03 17:07:28 +0200

    Revert remove Share Folder and Get Link activities form UIActivityExtension.
    - Removed activityDidFinish code from ShareActivity preventing a non replicable iOS7 bug.

---
+++ iMEGA/Cloud drive/Activities/ShareFolderActivity.m
@@ -0,0 +1,56 @@
+#import "ShareFolderActivity.h"
+
+#import "MEGANavigationController.h"
+#import "ContactsViewController.h"
+
+@interface ShareFolderActivity ()
+
+@property (strong, nonatomic) NSArray *nodes;
+
+@end
+
+@implementation ShareFolderActivity
+
+- (instancetype)initWithNodes:(NSArray *)nodesArray {
+    self = [super init];
+    if (self) {
+        _nodes = nodesArray;
+    }
+    return self;
+}
+
+- (NSString *)activityType {
+    return @"ShareFolderActivity";
+}
+
+- (NSString *)activityTitle {
+    if ([self.nodes count] > 1) {
+        return AMLocalizedString(@"shareFolders", nil);
+    }
+    
+    return AMLocalizedString(@"shareFolder", nil);
+}
+
+- (UIImage *)activityImage {
+    return [UIImage imageNamed:@"activity_shareFolder"];
+}
+
+- (BOOL)canPerformWithActivityItems:(NSArray *)activityItems {
+    return YES;
+}
+
+- (UIViewController *)activityViewController {
+    
+    MEGANavigationController *navigationController = [[UIStoryboard storyboardWithName:@"Contacts" bundle:nil] instantiateViewControllerWithIdentifier:@"ContactsNavigationControllerID"];
+    ContactsViewController *contactsVC = navigationController.viewControllers.firstObject;
+    contactsVC.nodesArray = self.nodes;
+    contactsVC.contactsMode = ContactsModeShareFoldersWith;
+    
+    return navigationController;
+}
+
++ (UIActivityCategory)activityCategory {
+    return UIActivityCategoryAction;
+}
+
+@end
