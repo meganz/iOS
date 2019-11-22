4311b6b758b51350a86bf7eb0e9390ccfbc8bce0
Peter Li <hl@mega.co.nz>
2019-11-22 16:05:23 +1300

    complete localisation for image quality setting

--- iMEGA/Settings/Chat/ChatImageQualityTableViewController.swift
+++ iMEGA/Settings/Chat/ChatImageQualityTableViewController.swift
@@ -13,7 +13,7 @@ class ChatImageQualityTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.title = NSLocalizedString("Image quality", comment: "Image quality")
     }
 
     // MARK: - Table view data source
@@ -28,15 +28,15 @@ class ChatImageQualityTableViewController: UITableViewController {
 
         switch indexPath.row {
         case 0:
-            cell.textLabel?.text = "Automatic"
-            cell.detailTextLabel?.text = "Send smaller size images through cellular networks and original size images through wifi"
+            cell.textLabel?.text = NSLocalizedString("Automatic", comment: "Label indicating that Send smaller size images through cellular networks and original size images through wifi")
+            cell.detailTextLabel?.text = NSLocalizedString("Send smaller size images through cellular networks and original size images through wifi", comment: "Automatic description")
             break
         case 1:
-            cell.textLabel?.text = "High"
-            cell.detailTextLabel?.text = "Send original size, increased quality images"
+            cell.textLabel?.text = NSLocalizedString("High", comment: "High")
+            cell.detailTextLabel?.text = NSLocalizedString("Send original size, increased quality images", comment: "High description")
         case 2:
             cell.textLabel?.text = "Optimised"
-            cell.detailTextLabel?.text = "Send smaller size images optimised for lower data consumption"
+            cell.detailTextLabel?.text = NSLocalizedString("Send smaller size images optimised for lower data consumption", comment: "Optimised description")
             
         default:
             return cell
