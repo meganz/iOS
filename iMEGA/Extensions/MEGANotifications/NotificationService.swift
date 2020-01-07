03ad4116d8e0d5bdd222b10235c35efc7dfc062d
Javier Trujillo <jth@mega.nz>
2020-01-07 17:06:08 +0100

    Adapted code to the lean init

--- iMEGA/Extensions/MEGANotifications/NotificationService.swift
+++ iMEGA/Extensions/MEGANotifications/NotificationService.swift
@@ -6,7 +6,7 @@ class NotificationService: UNNotificationServiceExtension, MEGAChatDelegate {
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
-    var session:String!
+    var session: String!
     
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         
@@ -20,6 +20,7 @@ class NotificationService: UNNotificationServiceExtension, MEGAChatDelegate {
             return
         }
         
+        copyDatabasesFromMainApp()
         initChat()
         loginToMEGA()
         MEGASdkManager.sharedMEGAChatSdk()?.add(self)
@@ -45,7 +46,93 @@ class NotificationService: UNNotificationServiceExtension, MEGAChatDelegate {
         contentHandler(bestAttemptContent)
     }
 
-    // MARK: Init, login, fetchnodes and connect
+    // MARK: Lean init, login and connect
+    
+    // As part of the lean init, a cache is required. It will not be generated from scratch.
+    func copyDatabasesFromMainApp() {
+        let fileManager = FileManager.default
+        
+        guard let applicationSupportDirectoryURL = try? fileManager.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true) else {
+            print("Failed to locate/create .applicationSupportDirectory.")
+            postNotification()
+            return
+        }
+        
+        guard let groupContainerURL = fileManager.containerURL(forSecurityApplicationGroupIdentifier: MEGAGroupIdentifier) else {
+            postNotification()
+            return
+        }
+        
+        let groupSupportURL = groupContainerURL.appendingPathComponent(MEGAExtensionGroupSupportFolder)
+        if !fileManager.fileExists(atPath: groupSupportURL.path) {
+            postNotification()
+            return
+        }
+        
+        guard let incomingDate = try? newestMegaclientModificationDateForDirectory(at: groupSupportURL),
+            let extensionDate = try? newestMegaclientModificationDateForDirectory(at: applicationSupportDirectoryURL)
+            else {
+                postNotification()
+                return
+        }
+        
+        if incomingDate <= extensionDate {
+            return
+        }
+        
+        guard let applicationSupportContent = try? fileManager.contentsOfDirectory(atPath: applicationSupportDirectoryURL.path),
+            let groupSupportPathContent = try? fileManager.contentsOfDirectory(atPath: groupSupportURL.path)
+            else {
+                postNotification()
+                return
+        }
+        
+        for filename in applicationSupportContent {
+            if filename.contains("megaclient") || filename.contains("karere") {
+                let pathToRemove = applicationSupportDirectoryURL.appendingPathComponent(filename).path
+                fileManager.mnz_removeItem(atPath: pathToRemove)
+            }
+        }
+        
+        for filename in groupSupportPathContent {
+            if filename.contains("megaclient") || filename.contains("karere") {
+                let sourceURL = groupSupportURL.appendingPathComponent(filename)
+                let destinationURL = applicationSupportDirectoryURL.appendingPathComponent(filename)
+                do {
+                    try fileManager.copyItem(at: sourceURL, to: destinationURL)
+                } catch {
+                    print("Copy item at path failed with error: \(error)")
+                }
+            }
+        }
+    }
+    
+    func newestMegaclientModificationDateForDirectory(at url: URL) throws -> Date {
+        let fileManager = FileManager.default
+        var newestDate = Date.init(timeIntervalSince1970: 0)
+        var pathContent: [String]!
+        do {
+            pathContent = try fileManager.contentsOfDirectory(atPath: url.path)
+        } catch {
+            throw error
+        }
+        for filename in pathContent {
+            if filename.contains("megaclient") || filename.contains("karere") {
+                do {
+                    let attributes = try fileManager.attributesOfItem(atPath: url.appendingPathComponent(filename).path)
+                    guard let date = attributes[.modificationDate] as? Date else {
+                        continue
+                    }
+                    if date > newestDate {
+                        newestDate = date
+                    }
+                } catch {
+                    throw error
+                }
+            }
+        }
+        return newestDate
+    }
     
     func initChat() {
         if MEGASdkManager.sharedMEGAChatSdk() == nil {
@@ -54,9 +141,8 @@ class NotificationService: UNNotificationServiceExtension, MEGAChatDelegate {
         
         var chatInit = MEGASdkManager.sharedMEGAChatSdk()?.initState()
         if chatInit == .notDone {
-            chatInit = MEGASdkManager.sharedMEGAChatSdk()?.initKarere(withSid: session)
-            // TODO: If we decide to import the db from the main app, at this point we should reset the client id
-            
+            chatInit = MEGASdkManager.sharedMEGAChatSdk()?.initKarereLeanMode(withSid: session)
+            MEGASdkManager.sharedMEGAChatSdk()?.resetClientId()
             if chatInit == .error {
                 MEGASdkManager.sharedMEGAChatSdk()?.logout()
             }
@@ -72,17 +158,8 @@ class NotificationService: UNNotificationServiceExtension, MEGAChatDelegate {
                 return
             }
             
-            let fetchNodesDelegate = MEGAGenericRequestDelegate { [weak self] (request, error) in
-                if error?.type != .apiOk {
-                    self!.postNotification()
-                    return
-                }
-                
-                MEGASdkManager.sharedMEGAChatSdk()?.connectInBackground()
-            }
-            
-            MEGASdkManager.sharedMEGASdk()?.fetchNodes(with: fetchNodesDelegate)
-        }
+            MEGASdkManager.sharedMEGAChatSdk()?.connectInBackground()
+        }!
         MEGASdkManager.sharedMEGASdk()?.fastLogin(withSession: session, delegate: loginDelegate)
     }
     
