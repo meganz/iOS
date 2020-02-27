462baf371620205dccf3bc847db66b29ec7bd874
Peter Li <hl@mega.co.nz>
2020-02-28 11:49:07 +1300

    ci(jenkinsfile): using script instead of hard code the 3rd-party address

--- jenkinsfile/jenkinsfile_declarative_iOS.groovy
+++ jenkinsfile/jenkinsfile_declarative_iOS.groovy
@@ -38,16 +38,7 @@ pipeline {
         stage('Downloading dependencies') {
             steps {
                 injectEnvironments({
-                    sh "mega-get https://mega.nz/#!BUtgzAQL!rf6stzMWq-RJ9u9-l8jeYZ0kSd07fwSDSG3P3Uj9Mx0 $WORKSPACE/iMEGA/Vendor/SDK/bindings/ios/3rdparty/"
-                })
-            }
-        }
-
-
-        stage('Unzipping dependencies and moving files/folders to appropriate path') {
-            steps {
-                injectEnvironments({
-                    sh "unzip -o $WORKSPACE/iMEGA/Vendor/SDK/bindings/ios/3rdparty/3rdparty.zip -d $WORKSPACE/iMEGA/Vendor/SDK/bindings/ios/3rdparty/"
+                    sh "sh $WORKSPACE/download_3rdparty.sh"
                 })
             }
         }
