88bc6d788bcde13e2e6630a03cb0918c68d945ff
Peter Li <hl@mega.co.nz>
2020-02-28 11:49:07 +1300

    ci(jenkinsfile): using script instead of hard code the 3rd-party address

--- download_3rdparty.sh
+++ download_3rdparty.sh
@@ -1,10 +1,10 @@
+export PATH=/Applications/MEGAcmd.app/Contents/MacOS:$PATH
 
 if ! type "megacmd" > /dev/null; then
   # install foobar here
 	echo "Install MEGACmd before run this script https://mega.nz/cmd"
 	exit 1
 fi
-export PATH=/Applications/MEGAcmd.app/Contents/MacOS:$PATH
 
 
 mega-get https://mega.nz/#!BUtgzAQL!rf6stzMWq-RJ9u9-l8jeYZ0kSd07fwSDSG3P3Uj9Mx0 ./iMEGA/Vendor/SDK/bindings/ios/3rdparty/
