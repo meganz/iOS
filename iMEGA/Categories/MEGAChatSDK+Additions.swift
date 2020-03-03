99e68b68c5df59eeb0a20f1099f79552abae440e
Javier Navarro <jn@mega.co.nz>
2020-03-03 13:07:10 +0100

    Change the way the app identifies an active call (14804)

--- iMEGA/Categories/MEGAChatSDK+Additions.swift
+++ iMEGA/Categories/MEGAChatSDK+Additions.swift
@@ -1,6 +1,6 @@
 
 extension MEGAChatSdk {
     @objc var mnz_existsActiveCall: Bool {
-        return self.chatCalls(withState: .inProgress)?.size != 0 || self.chatCalls(withState: .requestSent)?.size != 0 || self.chatCalls(withState: .joining)?.size != 0
+        return ((self.chatCalls(withState: .undefined)?.size ?? 0) - (self.chatCalls(withState: .userNoPresent)?.size ?? 0)) > 0 ? true : false
     }
 }
