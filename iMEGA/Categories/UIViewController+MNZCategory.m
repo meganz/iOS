26c2007ba948b012d2adb816f05a143c36c8f72a
Javier Navarro <jn@mega.co.nz>
2020-03-10 17:10:52 +0100

    Present view controller in fullscreen if the modal presentation is automatic pagesheet or formsheet

--- iMEGA/Categories/UIViewController+MNZCategory.m
+++ iMEGA/Categories/UIViewController+MNZCategory.m
@@ -26,4 +26,36 @@
 
 }
 
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        SEL originalSelector = @selector(presentViewController:animated:completion:);
+        SEL swizzledSelector = @selector(swizzled_presentViewController:animated:completion:);
+
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+
+        BOOL methodExists = !class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+
+        if (methodExists) {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        } else {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        }
+    });
+}
+
+- (void)swizzled_presentViewController:(nonnull UIViewController *)viewController animated:(BOOL)animated completion:(void (^)(void))completion {
+
+    if (@available(iOS 13.0, *)) {
+        if (viewController.modalPresentationStyle == UIModalPresentationAutomatic || viewController.modalPresentationStyle == UIModalPresentationPageSheet || viewController.modalPresentationStyle == UIModalPresentationFormSheet) {
+            viewController.modalPresentationStyle = UIModalPresentationFullScreen;
+        }
+    }
+
+    [self swizzled_presentViewController:viewController animated:animated completion:completion];
+}
+
+
 @end
