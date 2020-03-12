0371affb6d3b95026073cdc1058d52631fdf52a4
Peter Li <hl@mega.co.nz>
2020-03-12 14:17:12 +1300

    feat: add ActionSheetViewController

---
+++ iMEGA/Utils/ViewControllers/CustomActionViewController/ActionSheetViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ActionSheetViewController.swift
+//  MEGA
+//
+//  Created by Haoran Li on 12/03/20.
+//  Copyright © 2020 MEGA. All rights reserved.
+//
+
+import UIKit
+
+class ActionSheetViewController: UIViewController {
+    
+    var tableView: UITableView?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView = UITableView()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
