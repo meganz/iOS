6bc9ee16a2a5abcfc3a706cd2c69491a7f0c93ef
Peter Li <56808021+lhr000lhrmega@users.noreply.github.com>
2020-04-01 21:31:43 +1300

    fix(gradientview): add top black gradient (#575)
    * fix(gradientview): add top black gradient
    
    * fix: add shadow under back button
    
    15107

--- iMEGA/Utils/Views/GradientView.m
+++ iMEGA/Utils/Views/GradientView.m
@@ -10,8 +10,12 @@
     [super awakeFromNib];
     
     CAGradientLayer *gradientLayer = (CAGradientLayer*)self.layer;
-    gradientLayer.colors = @[(id)[[UIColor colorWithRed:1 green:1 blue:1 alpha:0] CGColor], (id)[[UIColor colorWithRed:0 green:0 blue:0 alpha:0.2] CGColor]];
-    gradientLayer.locations = @[@(0), @(1)];
+    gradientLayer.colors = @[
+        (id)[[UIColor colorWithRed:0 green:0 blue:0 alpha:0.4] CGColor],
+        (id)[[UIColor colorWithRed:1 green:1 blue:1 alpha:0] CGColor],
+        (id)[[UIColor colorWithRed:0 green:0 blue:0 alpha:0.2] CGColor]
+    ];
+    gradientLayer.locations = @[@(0), @(0.5), @(1)];
     gradientLayer.startPoint = CGPointMake(0.5, 0);
     gradientLayer.endPoint = CGPointMake(0.5, 1);
 }
