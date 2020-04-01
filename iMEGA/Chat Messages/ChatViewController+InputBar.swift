36197cf67ea98cd0ae5e62e3a348fe94c46da3d4
Peter Li <hl@mega.co.nz>
2020-04-01 17:50:40 +1300

    feat(inputbar): configurue input bar

--- iMEGA/Chat Messages/ChatViewController+InputBar.swift
+++ iMEGA/Chat Messages/ChatViewController+InputBar.swift
@@ -1,9 +1,6 @@
-//
-//  ChatViewController+InputBar.swift
-//  MEGA
-//
-//  Created by Haoran Li on 1/04/20.
-//  Copyright © 2020 MEGA. All rights reserved.
-//
-
 import Foundation
+extension ChatViewController {
+    func configureInputBar() {
+        
+    }
+}
