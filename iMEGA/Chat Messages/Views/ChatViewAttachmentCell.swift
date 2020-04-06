9712309f8c1a0ff15b06dd97e998945c45390977
Peter Li <hl@mega.co.nz>
2020-04-06 13:50:20 +1200

    feat: bind IBOutlet

--- iMEGA/Chat Messages/Views/ChatViewAttachmentCell.swift
+++ iMEGA/Chat Messages/Views/ChatViewAttachmentCell.swift
@@ -6,20 +6,42 @@ class ChatViewAttachmentCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
     
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var messageTimeLabel: UILabel!
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var attachmentImageView: UIImageView!
+    @IBOutlet weak var leftForward: UIImageView!
+    @IBOutlet weak var rightForward: UIImageView!
     func configure(with message: MessageType,
                      at indexPath: IndexPath,
                      and messagesCollectionView: MessagesCollectionView) {
           guard let chatMessage = message as? ChatMessage else {
               return
           }
+        
+        let megaMessage = chatMessage.message
+        if megaMessage.userHandle == MEGASdkManager.sharedMEGAChatSdk().myUserHandle {
+            avatarImageView.isHidden = true
+            messageTimeLabel.textAlignment = .left
+        } else {
+            avatarImageView.image = chatMessage.avatarImage
+            messageTimeLabel.textAlignment = .right
+
+            
+        }
+        
       }
 }
 
+
+
+
 class ChatViewAttachmentCellCalculator: MessageSizeCalculator {
-    
+
     override func messageContainerSize(for message: MessageType) -> CGSize {
         guard let layout = layout else { return .zero }
-        
+
         let collectionViewWidth = layout.collectionView?.bounds.width ?? 0
         let contentInset = layout.collectionView?.contentInset ?? .zero
         let inset = layout.sectionInset.left + layout.sectionInset.right + contentInset.left + contentInset.right
