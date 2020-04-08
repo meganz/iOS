d0a1c52424d0f69071172c97fdef430e6634f756
Lancy <lf@mega.co.nz>
2020-04-08 13:24:18 +1200

    #15094: Some code refactoring.

--- iMEGA/Chat Messages/ChatViewMessagesFlowLayout.swift
+++ iMEGA/Chat Messages/ChatViewMessagesFlowLayout.swift
@@ -3,6 +3,7 @@ import MessageKit
 class ChatViewMessagesFlowLayout: MessagesCollectionViewFlowLayout {
     lazy var chatViewCallCollectionCellCalculator = ChatViewCallCollectionCellCalculator(layout: self)
     lazy var chatViewAttachmentCellCalculator = ChatViewAttachmentCellCalculator(layout: self)
+    lazy var customContactMessageSizeCalculator = CustomContactMessageSizeCalculator(layout: self)
 
     override func cellSizeCalculatorForItem(at indexPath: IndexPath) -> CellSizeCalculator {
         let message = messagesDataSource.messageForItem(at: indexPath, in: messagesCollectionView)
@@ -17,11 +18,22 @@ class ChatViewMessagesFlowLayout: MessagesCollectionViewFlowLayout {
                 return chatViewAttachmentCellCalculator
             case .callEnded, .callStarted:
                 return chatViewCallCollectionCellCalculator
-
+            case .contact:
+                return customContactMessageSizeCalculator
             default:
                 return super.cellSizeCalculatorForItem(at: indexPath)
             }
         }
         return super.cellSizeCalculatorForItem(at: indexPath)
+    }
+    
+    override func messageSizeCalculators() -> [MessageSizeCalculator] {
+        var calculators = super.messageSizeCalculators()
+        calculators.append(contentsOf: [
+            chatViewAttachmentCellCalculator,
+            chatViewCallCollectionCellCalculator,
+            customContactMessageSizeCalculator
+        ])
+        return calculators
     }
 }
