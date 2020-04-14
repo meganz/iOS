f4f188ff6350e76b61c1d12abf11bbbe8020ea32
Peter Li <hl@mega.co.nz>
2020-04-14 15:35:31 +1200

    build(cocoapods): integrate cocoapods

---
+++ Pods/InputBarAccessoryView/README.md
@@ -0,0 +1,142 @@
+[![Image](./Screenshots/Preview.gif)]()
+
+# InputBarAccessoryView
+
+### Features
+
+- [x] Autocomplete text with @mention, #hashtag or any other prefix 
+- [x] A self-sizing `UITextView` with an optional fixed height (can be replaced with any other view)
+- [x] Image paste support   
+- [x] Autocomplete attributed text highlighting
+- [x] Reactive components that respond to given events
+- [x] Top/Bottom/Left/Right `InputStackView`s that act as toolbars to place buttons
+- [x] `RxSwift`/`RxCocoa` Support with `RxExtensions` Cocoapod subspec
+- [x] Drop in attachment view for file/photo management
+- [x] Plugin support for your own `InputPlugin`s
+- [x] Compatible with all iPhones and iPads
+- [x] RTL Support
+
+### Installation via CocoaPods
+
+```ruby
+# Swift 5.0
+pod 'InputBarAccessoryView'
+# Swift 4.2
+pod 'InputBarAccessoryView', '4.2.2'
+```
+
+### Installation via Carthage
+
+```ruby
+# Swift 5.0
+github "nathantannar4/InputBarAccessoryView"
+# Swift 4.2
+github "nathantannar4/InputBarAccessoryView" "4.2.2"
+```
+
+### Requirements
+
+iOS 9.0+
+Swift 5.0
+
+> The latest Swift 4.2 release is v4.2.2 
+
+### Documentation
+
+[Getting Started](./GETTING_STARTED.md)
+
+> See the Example project to see how you can make the iMessage, Slack, Facebook and GitHawk input bars!
+
+### Example Usage
+
+<img src="https://raw.githubusercontent.com/nathantannar4/InputBarAccessoryView/master/Screenshots/ScreenshotA.png" width="242" height="432"> <img src="https://raw.githubusercontent.com/nathantannar4/InputBarAccessoryView/master/Screenshots/ScreenshotB.png" width="242" height="432"> <img src="https://raw.githubusercontent.com/nathantannar4/InputBarAccessoryView/master/Screenshots/ScreenshotC.png" width="242" height="432"> <img src="https://raw.githubusercontent.com/nathantannar4/InputBarAccessoryView/master/Screenshots/ScreenshotD.png" width="242" height="432"> <img src="https://raw.githubusercontent.com/nathantannar4/InputBarAccessoryView/master/Screenshots/ScreenshotE.png" width="242" height="432"> <img src="https://raw.githubusercontent.com/nathantannar4/InputBarAccessoryView/master/Screenshots/ScreenshotF.png" width="242" height="432">
+
+### Featured In
+
+Add your app to the list of apps using this library and make a pull request.
+
+- [MessageKit](https://github.com/MessageKit/MessageKit) *(renamed to MessageInputBar)*
+<p>
+  <img src="https://raw.githubusercontent.com/MessageKit/MessageKit/master/Assets/mklogo.png" title="MessageKit Logo" height="50">
+</p>
+
+- [MessageViewController](https://github.com/GitHawkApp/MessageViewController) *(Autocomplete Highlighting Algorithm)*
+<p>
+  <img src="https://avatars3.githubusercontent.com/u/32285710?s=200&v=4" title="GitHawk Logo" height="50">
+</p>
+
+### See Also
+
+iMessage style [TypingIndicator](https://github.com/nathantannar4/TypingIndicator) for chat apps
+
+## Latest Releases
+- 4.3.2
+    - Fixed a bug where editing immediately before a tag prefix would break the tag
+- 4.3.1
+    - Add `RxSwift`/`RxCocoa` support through extensions and delegate proxies, requires Cocoapods installation of  `InputBarAccessoryView/RxExtensions`
+- 4.3.0
+    - Swift 5 support
+- 4.2.2
+    - Fixed image paste orientation issue in `InputTextView`
+- 4.2.1
+    - Fixed autocompletes of completions that contain spaces
+    - Depricated `isCaseSensitive` in favor of a function asignment to pre-process autocompletions
+- 4.2.0
+    - Added new API for overriding the main middle view, normally the `InputTextView` so it can be replaced with views such as a "Join" button", `setMiddleContentView(_ view: UIView?, animated: Bool)`
+- 4.1.2
+        - Add `InputBarViewController` which contains an `InputBarAccessoryView` as the `inputAccessoryView` by default with a convenient `isInputBarHidden: Bool` property to show/hide it 
+- 4.1.1
+        - Add `frameInsets: HorizontalEdgePadding` property to `InputBarAccessoryView` to inset the view to be compatible with `UISplitViewController` or other custom containers where the view should not be the full width
+- 4.1.0
+        - Fix issue where setting long strings in `viewDidLoad` broke inital layout, Issue #41
+        - Add `deleteCompletionByParts: Bool` flag to `AutocompleteManager` to allow for partial deletions of autocompletes rather than just the entire substring. 
+        - Add `InputBarSendButton` to use as the `sendButton` in `InputBarAccessoryView`. This subclass of `InputBarButtonItem` has a `UIActivityIndicatorView` to show a spinner when making HTTP requests
+
+### [CHANGELOG](./CHANGELOG.md)
+
+**Find a bug? Open an issue!**
+
+## Layout
+
+The layout of the `InputBarAccessoryView` is made of of 4  `InputStackView`'s and an `InputTextView`. The padding of the subviews can be easily adjusted by changing the `padding` and `textViewPadding` properties. The constraints will automatically be updated.
+
+<img src="https://raw.githubusercontent.com/nathantannar4/InputBarAccessoryView/master/Screenshots/Layout.png">
+
+It is important to note that each of the `InputStackView `'s to the left and right of the `InputTextView` are anchored by a width constraint. This way the `InputTextView` will always fill the space inbetween in addition to providing methods that can easily be called to hide all buttons to the right or left of the `InputTextView` by setting the width constraint constant to 0. The bottom and top stack views are not height constraint and rely on their `intrinsicContentSize`
+
+```swift
+func setLeftStackViewWidthConstant(to newValue: CGFloat, animated: Bool)
+
+func setRightStackViewWidthConstant(to newValue: CGFloat, animated: Bool)
+```
+
+### Reactive Hooks
+
+Each `InputBarButtonItem` has properties that can hold actions that will be executed during various hooks such as the button being touched, the `UITextView `text changing and more! Thanks to these easy hooks with a few lines of code the items can be easily resized and animated similar to that of the Facebook messenger app.
+
+```swift
+// MARK: - Hooks
+    
+public typealias InputBarButtonItemAction = ((InputBarButtonItem) -> Void)    
+    
+private var onTouchUpInsideAction: InputBarButtonItemAction?
+private var onKeyboardEditingBeginsAction: InputBarButtonItemAction?
+private var onKeyboardEditingEndsAction: InputBarButtonItemAction?
+private var onKeyboardSwipeGestureAction: ((InputBarButtonItem, UISwipeGestureRecognizer) -> Void)?
+private var onTextViewDidChangeAction: ((InputBarButtonItem, InputTextView) -> Void)?
+private var onSelectedAction: InputBarButtonItemAction?
+private var onDeselectedAction: InputBarButtonItemAction?
+private var onEnabledAction: InputBarButtonItemAction?
+private var onDisabledAction: InputBarButtonItemAction?
+```
+
+## Author
+<p>
+	<img src="https://github.com/nathantannar4/NTComponents/raw/master/NTComponents/Assets/Nathan.png" width="100" height="100">
+</p>
+
+**Nathan Tannar** - [https://nathantannar.me](https://nathantannar.me)
+
+## License
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ Pods/MessageKit/Sources/Controllers/MessagesViewController.swift
@@ -0,0 +1,428 @@
+/*
+ MIT License
+
+ Copyright (c) 2017-2019 MessageKit
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import UIKit
+import InputBarAccessoryView
+
+/// A subclass of `UIViewController` with a `MessagesCollectionView` object
+/// that is used to display conversation interfaces.
+open class MessagesViewController: UIViewController,
+UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+    /// The `MessagesCollectionView` managed by the messages view controller object.
+    open var messagesCollectionView = MessagesCollectionView()
+
+    /// The `InputBarAccessoryView` used as the `inputAccessoryView` in the view controller.
+    open lazy var messageInputBar = InputBarAccessoryView()
+
+    /// A Boolean value that determines whether the `MessagesCollectionView` scrolls to the
+    /// last item whenever the `InputTextView` begins editing.
+    ///
+    /// The default value of this property is `false`.
+    /// NOTE: This is related to `scrollToLastItem` whereas the below flag is related to `scrollToBottom` - check each function for differences
+    open var scrollsToLastItemOnKeyboardBeginsEditing: Bool = false
+
+    /// A Boolean value that determines whether the `MessagesCollectionView` scrolls to the
+    /// bottom whenever the `InputTextView` begins editing.
+    ///
+    /// The default value of this property is `false`.
+    /// NOTE: This is related to `scrollToBottom` whereas the above flag is related to `scrollToLastItem` - check each function for differences
+    open var scrollsToBottomOnKeyboardBeginsEditing: Bool = false
+    
+    /// A Boolean value that determines whether the `MessagesCollectionView`
+    /// maintains it's current position when the height of the `MessageInputBar` changes.
+    ///
+    /// The default value of this property is `false`.
+    open var maintainPositionOnKeyboardFrameChanged: Bool = false
+
+    open override var canBecomeFirstResponder: Bool {
+        return true
+    }
+
+    open override var inputAccessoryView: UIView? {
+        return messageInputBar
+    }
+
+    open override var shouldAutorotate: Bool {
+        return false
+    }
+
+    /// A CGFloat value that adds to (or, if negative, subtracts from) the automatically
+    /// computed value of `messagesCollectionView.contentInset.bottom`. Meant to be used
+    /// as a measure of last resort when the built-in algorithm does not produce the right
+    /// value for your app. Please let us know when you end up having to use this property.
+    open var additionalBottomInset: CGFloat = 0 {
+        didSet {
+            let delta = additionalBottomInset - oldValue
+            messageCollectionViewBottomInset += delta
+        }
+    }
+
+    public var isTypingIndicatorHidden: Bool {
+        return messagesCollectionView.isTypingIndicatorHidden
+    }
+
+    public var selectedIndexPathForMenu: IndexPath?
+
+    private var isFirstLayout: Bool = true
+    
+    internal var isMessagesControllerBeingDismissed: Bool = false
+
+    internal var messageCollectionViewBottomInset: CGFloat = 0 {
+        didSet {
+            messagesCollectionView.contentInset.bottom = messageCollectionViewBottomInset
+            messagesCollectionView.scrollIndicatorInsets.bottom = messageCollectionViewBottomInset
+        }
+    }
+
+    // MARK: - View Life Cycle
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        setupDefaults()
+        setupSubviews()
+        setupConstraints()
+        setupDelegates()
+        addMenuControllerObservers()
+        addObservers()
+    }
+    
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        isMessagesControllerBeingDismissed = false
+    }
+    
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        isMessagesControllerBeingDismissed = true
+    }
+    
+    open override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        isMessagesControllerBeingDismissed = false
+    }
+    
+    open override func viewDidLayoutSubviews() {
+        // Hack to prevent animation of the contentInset after viewDidAppear
+        if isFirstLayout {
+            defer { isFirstLayout = false }
+            addKeyboardObservers()
+            messageCollectionViewBottomInset = requiredInitialScrollViewBottomInset()
+        }
+        adjustScrollViewTopInset()
+    }
+
+    open override func viewSafeAreaInsetsDidChange() {
+        if #available(iOS 11.0, *) {
+            super.viewSafeAreaInsetsDidChange()
+        }
+        messageCollectionViewBottomInset = requiredInitialScrollViewBottomInset()
+    }
+
+    // MARK: - Initializers
+
+    deinit {
+        removeKeyboardObservers()
+        removeMenuControllerObservers()
+        removeObservers()
+        clearMemoryCache()
+    }
+
+    // MARK: - Methods [Private]
+
+    private func setupDefaults() {
+        extendedLayoutIncludesOpaqueBars = true
+        automaticallyAdjustsScrollViewInsets = false
+        view.backgroundColor = .backgroundColor
+        messagesCollectionView.keyboardDismissMode = .interactive
+        messagesCollectionView.alwaysBounceVertical = true
+        messagesCollectionView.backgroundColor = .backgroundColor
+    }
+
+    private func setupDelegates() {
+        messagesCollectionView.delegate = self
+        messagesCollectionView.dataSource = self
+    }
+
+    private func setupSubviews() {
+        view.addSubview(messagesCollectionView)
+    }
+
+    private func setupConstraints() {
+        messagesCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let top = messagesCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: topLayoutGuide.length)
+        let bottom = messagesCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        if #available(iOS 11.0, *) {
+            let leading = messagesCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+            let trailing = messagesCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+            NSLayoutConstraint.activate([top, bottom, trailing, leading])
+        } else {
+            let leading = messagesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+            let trailing = messagesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            NSLayoutConstraint.activate([top, bottom, trailing, leading])
+        }
+    }
+
+    // MARK: - Typing Indicator API
+
+    /// Sets the typing indicator sate by inserting/deleting the `TypingBubbleCell`
+    ///
+    /// - Parameters:
+    ///   - isHidden: A Boolean value that is to be the new state of the typing indicator
+    ///   - animated: A Boolean value determining if the insertion is to be animated
+    ///   - updates: A block of code that will be executed during `performBatchUpdates`
+    ///              when `animated` is `TRUE` or before the `completion` block executes
+    ///              when `animated` is `FALSE`
+    ///   - completion: A completion block to execute after the insertion/deletion
+    open func setTypingIndicatorViewHidden(_ isHidden: Bool, animated: Bool, whilePerforming updates: (() -> Void)? = nil, completion: ((Bool) -> Void)? = nil) {
+
+        guard isTypingIndicatorHidden != isHidden else {
+            completion?(false)
+            return
+        }
+
+        let section = messagesCollectionView.numberOfSections
+        messagesCollectionView.setTypingIndicatorViewHidden(isHidden)
+
+        if animated {
+            messagesCollectionView.performBatchUpdates({ [weak self] in
+                self?.performUpdatesForTypingIndicatorVisability(at: section)
+                updates?()
+                }, completion: completion)
+        } else {
+            performUpdatesForTypingIndicatorVisability(at: section)
+            updates?()
+            completion?(true)
+        }
+    }
+
+    /// Performs a delete or insert on the `MessagesCollectionView` on the provided section
+    ///
+    /// - Parameter section: The index to modify
+    private func performUpdatesForTypingIndicatorVisability(at section: Int) {
+        if isTypingIndicatorHidden {
+            messagesCollectionView.deleteSections([section - 1])
+        } else {
+            messagesCollectionView.insertSections([section])
+        }
+    }
+
+    /// A method that by default checks if the section is the last in the
+    /// `messagesCollectionView` and that `isTypingIndicatorViewHidden`
+    /// is FALSE
+    ///
+    /// - Parameter section
+    /// - Returns: A Boolean indicating if the TypingIndicator should be presented at the given section
+    public func isSectionReservedForTypingIndicator(_ section: Int) -> Bool {
+        return !messagesCollectionView.isTypingIndicatorHidden && section == self.numberOfSections(in: messagesCollectionView) - 1
+    }
+
+    // MARK: - UICollectionViewDataSource
+
+    open func numberOfSections(in collectionView: UICollectionView) -> Int {
+        guard let collectionView = collectionView as? MessagesCollectionView else {
+            fatalError(MessageKitError.notMessagesCollectionView)
+        }
+        let sections = collectionView.messagesDataSource?.numberOfSections(in: collectionView) ?? 0
+        return collectionView.isTypingIndicatorHidden ? sections : sections + 1
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let collectionView = collectionView as? MessagesCollectionView else {
+            fatalError(MessageKitError.notMessagesCollectionView)
+        }
+        if isSectionReservedForTypingIndicator(section) {
+            return 1
+        }
+        return collectionView.messagesDataSource?.numberOfItems(inSection: section, in: collectionView) ?? 0
+    }
+
+    /// Notes:
+    /// - If you override this method, remember to call MessagesDataSource's customCell(for:at:in:)
+    /// for MessageKind.custom messages, if necessary.
+    ///
+    /// - If you are using the typing indicator you will need to ensure that the section is not
+    /// reserved for it with `isSectionReservedForTypingIndicator` defined in
+    /// `MessagesCollectionViewFlowLayout`
+    open func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        guard let messagesCollectionView = collectionView as? MessagesCollectionView else {
+            fatalError(MessageKitError.notMessagesCollectionView)
+        }
+
+        guard let messagesDataSource = messagesCollectionView.messagesDataSource else {
+            fatalError(MessageKitError.nilMessagesDataSource)
+        }
+
+        if isSectionReservedForTypingIndicator(indexPath.section) {
+            return messagesDataSource.typingIndicator(at: indexPath, in: messagesCollectionView)
+        }
+
+        let message = messagesDataSource.messageForItem(at: indexPath, in: messagesCollectionView)
+
+        switch message.kind {
+        case .text, .attributedText, .emoji:
+            let cell = messagesCollectionView.dequeueReusableCell(TextMessageCell.self, for: indexPath)
+            cell.configure(with: message, at: indexPath, and: messagesCollectionView)
+            return cell
+        case .photo, .video:
+            let cell = messagesCollectionView.dequeueReusableCell(MediaMessageCell.self, for: indexPath)
+            cell.configure(with: message, at: indexPath, and: messagesCollectionView)
+            return cell
+        case .location:
+            let cell = messagesCollectionView.dequeueReusableCell(LocationMessageCell.self, for: indexPath)
+            cell.configure(with: message, at: indexPath, and: messagesCollectionView)
+            return cell
+        case .audio:
+            let cell = messagesCollectionView.dequeueReusableCell(AudioMessageCell.self, for: indexPath)
+            cell.configure(with: message, at: indexPath, and: messagesCollectionView)
+            return cell
+        case .contact:
+            let cell = messagesCollectionView.dequeueReusableCell(ContactMessageCell.self, for: indexPath)
+            cell.configure(with: message, at: indexPath, and: messagesCollectionView)
+            return cell
+        case .custom:
+            return messagesDataSource.customCell(for: message, at: indexPath, in: messagesCollectionView)
+        }
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+
+        guard let messagesCollectionView = collectionView as? MessagesCollectionView else {
+            fatalError(MessageKitError.notMessagesCollectionView)
+        }
+
+        guard let displayDelegate = messagesCollectionView.messagesDisplayDelegate else {
+            fatalError(MessageKitError.nilMessagesDisplayDelegate)
+        }
+
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            return displayDelegate.messageHeaderView(for: indexPath, in: messagesCollectionView)
+        case UICollectionView.elementKindSectionFooter:
+            return displayDelegate.messageFooterView(for: indexPath, in: messagesCollectionView)
+        default:
+            fatalError(MessageKitError.unrecognizedSectionKind)
+        }
+    }
+
+    // MARK: - UICollectionViewDelegateFlowLayout
+
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        guard let messagesFlowLayout = collectionViewLayout as? MessagesCollectionViewFlowLayout else { return .zero }
+        return messagesFlowLayout.sizeForItem(at: indexPath)
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+
+        guard let messagesCollectionView = collectionView as? MessagesCollectionView else {
+            fatalError(MessageKitError.notMessagesCollectionView)
+        }
+        guard let layoutDelegate = messagesCollectionView.messagesLayoutDelegate else {
+            fatalError(MessageKitError.nilMessagesLayoutDelegate)
+        }
+        if isSectionReservedForTypingIndicator(section) {
+            return .zero
+        }
+        return layoutDelegate.headerViewSize(for: section, in: messagesCollectionView)
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? TypingIndicatorCell else { return }
+        cell.typingBubble.startAnimating()
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        guard let messagesCollectionView = collectionView as? MessagesCollectionView else {
+            fatalError(MessageKitError.notMessagesCollectionView)
+        }
+        guard let layoutDelegate = messagesCollectionView.messagesLayoutDelegate else {
+            fatalError(MessageKitError.nilMessagesLayoutDelegate)
+        }
+        if isSectionReservedForTypingIndicator(section) {
+            return .zero
+        }
+        return layoutDelegate.footerViewSize(for: section, in: messagesCollectionView)
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        guard let messagesDataSource = messagesCollectionView.messagesDataSource else { return false }
+
+        if isSectionReservedForTypingIndicator(indexPath.section) {
+            return false
+        }
+
+        let message = messagesDataSource.messageForItem(at: indexPath, in: messagesCollectionView)
+
+        switch message.kind {
+        case .text, .attributedText, .emoji, .photo:
+            selectedIndexPathForMenu = indexPath
+            return true
+        default:
+            return false
+        }
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        if isSectionReservedForTypingIndicator(indexPath.section) {
+            return false
+        }
+        return (action == NSSelectorFromString("copy:"))
+    }
+
+    open func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+        guard let messagesDataSource = messagesCollectionView.messagesDataSource else {
+            fatalError(MessageKitError.nilMessagesDataSource)
+        }
+        let pasteBoard = UIPasteboard.general
+        let message = messagesDataSource.messageForItem(at: indexPath, in: messagesCollectionView)
+
+        switch message.kind {
+        case .text(let text), .emoji(let text):
+            pasteBoard.string = text
+        case .attributedText(let attributedText):
+            pasteBoard.string = attributedText.string
+        case .photo(let mediaItem):
+            pasteBoard.image = mediaItem.image ?? mediaItem.placeholderImage
+        default:
+            break
+        }
+    }
+
+    // MARK: - Helpers
+    
+    private func addObservers() {
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(clearMemoryCache), name: UIApplication.didReceiveMemoryWarningNotification, object: nil)
+    }
+    
+    private func removeObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didReceiveMemoryWarningNotification, object: nil)
+    }
+    
+    @objc private func clearMemoryCache() {
+        MessageStyle.bubbleImageCache.removeAllObjects()
+    }
+}
