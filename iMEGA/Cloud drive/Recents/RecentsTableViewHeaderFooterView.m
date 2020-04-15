0254e4d82bc00ba7ac10d096b29f3c46608fbdbe
Javier Santana Espejo <jse@mega.nz>
2020-04-15 12:17:40 +0200

    feat(v5): Update Recents view
    - Also deleted 'disclosure', 'discloserArrow', and 'Disclosure_dark_gray' assets and replaced them for the new standard one with light and dark versions.

--- iMEGA/Cloud drive/Recents/RecentsTableViewHeaderFooterView.m
+++ iMEGA/Cloud drive/Recents/RecentsTableViewHeaderFooterView.m
@@ -5,10 +5,25 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
 
-    self.backgroundColor = UIColor.mnz_background;
+    [self setup];
+    
+    self.bottomSeparatorView.layer.borderWidth = 0.5;
+}
+
+- (void)prepareForReuse {
+    [super prepareForReuse];
+
+    [self setup];
+}
+
+#pragma mark - Private
+
+- (void)setup {
+    self.contentView.backgroundColor = [UIColor mnz_notificationSeenBackgroundForTraitCollection:self.traitCollection];
+    
+    self.dateLabel.textColor = [UIColor mnz_primaryGrayForTraitCollection:self.traitCollection];
     
     self.bottomSeparatorView.layer.borderColor = [UIColor mnz_separatorColorForTraitCollection:self.traitCollection].CGColor;
-    self.bottomSeparatorView.layer.borderWidth = 0.5;
 }
 
 @end
