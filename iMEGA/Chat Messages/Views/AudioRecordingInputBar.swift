9d48d374b191cc8073b19b08ad7f503acd45e1fa
Lancy <lf@mega.co.nz>
2020-04-16 14:47:21 +1200

    #15094: Animation of the Drag left to cancel and up to lock.

--- iMEGA/Chat Messages/Views/AudioRecordingInputBar.swift
+++ iMEGA/Chat Messages/Views/AudioRecordingInputBar.swift
@@ -2,21 +2,30 @@
 import UIKit
 
 class AudioRecordingInputBar: UIView {
-
-    @IBOutlet weak var voiceViewTrailingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var voiceViewBottomConstraint: NSLayoutConstraint!
-    
-    var voiceViewTrailingDefaultValue: CGFloat!
-    var voiceViewBottomDefaultValue: CGFloat!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        voiceViewTrailingDefaultValue = voiceViewTrailingConstraint.constant
-        voiceViewBottomDefaultValue = voiceViewBottomConstraint.constant
-    }
+    @IBOutlet weak var trashView: EnlargementView!
+    @IBOutlet weak var lockView: EnlargementView!
+    @IBOutlet weak var voiceView: CondensationView!
 
     override var intrinsicContentSize: CGSize {
-        return .zero
+        var size = super.intrinsicContentSize
+        // Audio recording view height remains constant throught.
+        size.height = 236.0
+        return size
+    }
+    
+    func moveToTrash(_ progress: CGFloat) {
+        trashView.progress = progress
+        voiceView.progress = progress
+        
+        // When moving to trash locView does not animate
+        lockView.progress = 0.0
+    }
+    
+    func lock(_ progress: CGFloat) {
+        lockView.progress = progress
+        
+        // When locking, trashview and voiceview does not animate
+        trashView.progress = 0.0
+        voiceView.progress = 0.0
     }
 }
