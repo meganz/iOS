c046199d1de94a134938f4cc36109e0f815140b1
Peter Li <hl@mega.co.nz>
2020-04-29 17:15:18 +1200

    feat(chatview): forward message to other chat room

--- iMEGA/Chat Messages/ChatViewController+Menus.swift
+++ iMEGA/Chat Messages/ChatViewController+Menus.swift
@@ -4,7 +4,43 @@ import Foundation
 extension ChatViewController {
     
     func forwardMessage(_ message: MEGAChatMessage) {
+        let chatStoryboard = UIStoryboard(name: "Chat", bundle: nil)
+        let sendToNC = chatStoryboard.instantiateViewController(withIdentifier: "SendToNavigationControllerID") as! UINavigationController
+        let sendToViewController = sendToNC.viewControllers.first as! SendToViewController
         
+        sendToViewController.sendMode = .forward
+        sendToViewController.messages = [message]
+        sendToViewController.sourceChatId = chatRoom.chatId
+        sendToViewController.completion = { (chatIdNumbers, sentMessages) in
+            var selfForwarded = false
+            var showSuccess = false
+            
+            chatIdNumbers?.forEach({ (chatIdNumber) in
+                let chatId = chatIdNumber.uint64Value
+                if chatId == self.chatRoom.chatId {
+                    selfForwarded = true
+                }
+            })
+            
+            if selfForwarded {
+                sentMessages?.forEach({ (message) in
+                    let filteredArray = self.messages.filter { chatMessage in
+                        return chatMessage.message.temporalId == message.temporalId
+                    }
+                    if filteredArray.count > 0 {
+                        MEGALogWarning("Forwarded message was already added to the array, probably onMessageUpdate received before now.")
+                    } else {
+                        message.chatId = self.chatRoom.chatId
+                        self.chatRoomDelegate.insertMessage(message)
+                        self.messagesCollectionView.scrollToBottom()
+                    }
+                    
+                })
+            }
+            
+        }
+        
+        present(sendToNC, animated: true, completion: nil)
     }
     
 }
