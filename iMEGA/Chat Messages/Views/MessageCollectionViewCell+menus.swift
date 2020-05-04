818d865380d7b386f4207bb4847c4243db69bfa5
Peter Li <hl@mega.co.nz>
2020-05-04 15:22:29 +1200

    feat(chatroom): remove richlink

--- iMEGA/Chat Messages/Views/MessageCollectionViewCell+menus.swift
+++ iMEGA/Chat Messages/Views/MessageCollectionViewCell+menus.swift
@@ -50,6 +50,39 @@ extension MessageCollectionViewCell {
           }
     }
     
+    @objc func download(_ sender: Any?) {
+        // Get the collectionView
+          if let collectionView = self.superview as? UICollectionView {
+              // Get indexPath
+              if let indexPath = collectionView.indexPath(for: self) {
+                  // Trigger action
+                  collectionView.delegate?.collectionView?(collectionView, performAction: #selector(MessageCollectionViewCell.download(_:)), forItemAt: indexPath, withSender: sender)
+              }
+          }
+    }
+    
+    @objc func removeRichPreview(_ sender: Any?) {
+        // Get the collectionView
+          if let collectionView = self.superview as? UICollectionView {
+              // Get indexPath
+              if let indexPath = collectionView.indexPath(for: self) {
+                  // Trigger action
+                  collectionView.delegate?.collectionView?(collectionView, performAction: #selector(MessageCollectionViewCell.removeRichPreview(_:)), forItemAt: indexPath, withSender: sender)
+              }
+          }
+    }
+    
+    @objc func addContact(_ sender: Any?) {
+        // Get the collectionView
+          if let collectionView = self.superview as? UICollectionView {
+              // Get indexPath
+              if let indexPath = collectionView.indexPath(for: self) {
+                  // Trigger action
+                  collectionView.delegate?.collectionView?(collectionView, performAction: #selector(MessageCollectionViewCell.addContact(_:)), forItemAt: indexPath, withSender: sender)
+              }
+          }
+    }
+
     open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
         // Get the collectionView
         if let collectionView = self.superview as? UICollectionView {
