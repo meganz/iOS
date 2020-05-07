64ac83b6f07130b49bb20b69680958b515cb0c1b
Peter Li <hl@mega.co.nz>
2020-05-07 14:57:17 +1200

    feat(chatroom): add topbanner button for show call status

--- iMEGA/Chat Messages/ChatRoomCallDelegate.swift
+++ iMEGA/Chat Messages/ChatRoomCallDelegate.swift
@@ -2,4 +2,21 @@ import UIKit
 
 class ChatRoomCallDelegate: NSObject, MEGAChatCallDelegate {
 
+    // MARK: - Properties
+
+    let chatRoom: MEGAChatRoom
+    weak var chatViewController: ChatViewController!
+    
+    // MARK: - Init
+
+    init(chatRoom: MEGAChatRoom, chatViewController: ChatViewController) {
+        self.chatRoom = chatRoom
+        self.chatViewController = chatViewController
+        super.init()
+        
+        MEGASdkManager.sharedMEGAChatSdk()?.add(self)
+    }
+        
+    // MARK: - MEGAChatCallDelegate methods
+
 }
