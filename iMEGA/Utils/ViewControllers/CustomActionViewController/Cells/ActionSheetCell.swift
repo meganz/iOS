3d4950ef429fef1d28d9414317d27333fb205bd0
Javier Trujillo <jth@mega.nz>
2020-05-15 09:44:56 +0200

    task(chat): added accessory view for online status

--- iMEGA/Utils/ViewControllers/CustomActionViewController/Cells/ActionSheetCell.swift
+++ iMEGA/Utils/ViewControllers/CustomActionViewController/Cells/ActionSheetCell.swift
@@ -5,6 +5,7 @@ final class ActionSheetCell: UITableViewCell {
     func configureCell(action: ActionSheetAction) {
         textLabel?.text = action.title
         detailTextLabel?.text = action.detail
+        accessoryView = action.accessoryView
         imageView?.image = action.image
         imageView?.tintColor = #colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1)
         switch action.style {
