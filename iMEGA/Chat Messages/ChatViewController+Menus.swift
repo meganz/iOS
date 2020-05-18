67591709220264c2557072fc15ab1043f328b007
Peter Li <hl@mega.co.nz>
2020-05-18 15:40:16 +1200

    feat(chatroom): support sorted messages

--- iMEGA/Chat Messages/ChatViewController+Menus.swift
+++ iMEGA/Chat Messages/ChatViewController+Menus.swift
@@ -9,62 +9,7 @@ extension ChatViewController {
     }
     
     func forwardMessage(_ message: ChatMessage) {
-        
         self.setEditing(true, animated: true)
-        return
-        let megaMessage = message.message
-        let chatStoryboard = UIStoryboard(name: "Chat", bundle: nil)
-        let sendToNC = chatStoryboard.instantiateViewController(withIdentifier: "SendToNavigationControllerID") as! UINavigationController
-        let sendToViewController = sendToNC.viewControllers.first as! SendToViewController
-        
-        sendToViewController.sendMode = .forward
-        sendToViewController.messages = [megaMessage]
-        sendToViewController.sourceChatId = chatRoom.chatId
-        sendToViewController.completion = { (chatIdNumbers, sentMessages) in
-            var selfForwarded = false
-            var showSuccess = false
-            
-            chatIdNumbers?.forEach({ (chatIdNumber) in
-                let chatId = chatIdNumber.uint64Value
-                if chatId == self.chatRoom.chatId {
-                    selfForwarded = true
-                }
-            })
-            
-            if selfForwarded {
-                sentMessages?.forEach({ (message) in
-                    let filteredArray = self.messages.filter { chatMessage in
-                        return chatMessage.message.temporalId == message.temporalId
-                    }
-                    if filteredArray.count > 0 {
-                        MEGALogWarning("Forwarded message was already added to the array, probably onMessageUpdate received before now.")
-                    } else {
-                        message.chatId = self.chatRoom.chatId
-                        self.chatRoomDelegate.insertMessage(message)
-                        self.messagesCollectionView.scrollToBottom()
-                    }
-                    
-                })
-                
-                showSuccess = chatIdNumbers?.count ?? 0 > 1;
-            } else if chatIdNumbers?.count == 1 && self.chatRoom.isPreview {
-                let chatId = chatIdNumbers?.first!.uint64Value
-                let chatRoom = MEGASdkManager.sharedMEGAChatSdk()?.chatRoom(forChatId: chatId!)
-                let messagesVC = ChatViewController()
-                messagesVC.chatRoom = chatRoom
-                self.replaceCurrentViewController(withViewController: messagesVC)
-            } else {
-                showSuccess = true
-            }
-            
-            
-            
-            if showSuccess {
-                SVProgressHUD.showSuccess(withStatus: AMLocalizedString("messagesSent", "Success message shown after forwarding messages to other chats"))
-            }
-        }
-        
-        present(sendToNC, animated: true, completion: nil)
     }
     
     
--- iMEGA/Chat Messages/ChatViewController+Toolbar.swift
+++ iMEGA/Chat Messages/ChatViewController+Toolbar.swift
@@ -23,8 +23,67 @@ extension ChatViewController {
     }
     
     @objc func forwardSelectedMessages() {
-//        ind
+      
+        var megaMessages = [MEGAChatMessage]()
+        for chatMessage in selectedMessages {
+            megaMessages.append(chatMessage.message)
+        }
+        let chatStoryboard = UIStoryboard(name: "Chat", bundle: nil)
+        let sendToNC = chatStoryboard.instantiateViewController(withIdentifier: "SendToNavigationControllerID") as! UINavigationController
+        let sendToViewController = sendToNC.viewControllers.first as! SendToViewController
+        
+        sendToViewController.sendMode = .forward
+        sendToViewController.messages = megaMessages.sorted(by: { (obj1, obj2) -> Bool in
+            obj1.messageIndex < obj2.messageIndex
+        })
+        sendToViewController.sourceChatId = chatRoom.chatId
+        sendToViewController.completion = { (chatIdNumbers, sentMessages) in
+            var selfForwarded = false
+            var showSuccess = false
+            self.setEditing(false, animated: true)
+
+            chatIdNumbers?.forEach({ (chatIdNumber) in
+                let chatId = chatIdNumber.uint64Value
+                if chatId == self.chatRoom.chatId {
+                    selfForwarded = true
+                }
+            })
+            
+            if selfForwarded {
+                sentMessages?.forEach({ (message) in
+                    let filteredArray = self.messages.filter { chatMessage in
+                        return chatMessage.message.temporalId == message.temporalId
+                    }
+                    if filteredArray.count > 0 {
+                        MEGALogWarning("Forwarded message was already added to the array, probably onMessageUpdate received before now.")
+                    } else {
+                        message.chatId = self.chatRoom.chatId
+                        self.chatRoomDelegate.insertMessage(message)
+                        self.messagesCollectionView.scrollToBottom()
+                    }
+                    
+                })
+                
+                showSuccess = chatIdNumbers?.count ?? 0 > 1;
+            } else if chatIdNumbers?.count == 1 && self.chatRoom.isPreview {
+                let chatId = chatIdNumbers?.first!.uint64Value
+                let chatRoom = MEGASdkManager.sharedMEGAChatSdk()?.chatRoom(forChatId: chatId!)
+                let messagesVC = ChatViewController()
+                messagesVC.chatRoom = chatRoom
+                self.replaceCurrentViewController(withViewController: messagesVC)
+            } else {
+                showSuccess = true
+            }
+
+            if showSuccess {
+                SVProgressHUD.showSuccess(withStatus: AMLocalizedString("messagesSent", "Success message shown after forwarding messages to other chats"))
+                
+            }
+        }
+        
+        present(sendToNC, animated: true, completion: nil)
     }
+    
     
     @objc func shareSelectedMessages() {
         
