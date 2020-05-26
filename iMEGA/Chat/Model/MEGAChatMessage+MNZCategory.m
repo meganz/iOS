231faa36a28478e69407c8da914141058243cb79
Javier Trujillo <jth@mega.nz>
2020-05-26 16:33:07 +0200

    fix(chat): update bubble colors under updateAppearance method
    - Also, consider the current dark/light mode for caches.

--- iMEGA/Chat/Model/MEGAChatMessage+MNZCategory.m
+++ iMEGA/Chat/Model/MEGAChatMessage+MNZCategory.m
@@ -472,7 +472,12 @@ static const void *richTitleTagKey = &richTitleTagKey;
 - (NSUInteger)hash {
     NSUInteger contentHash = self.type == MEGAChatMessageTypeAttachment || self.type == MEGAChatMessageTypeVoiceClip ? (NSUInteger)[self.nodeList nodeAtIndex:0].handle : self.content.hash ^ self.richNumber.hash;
     NSUInteger metaHash = self.type == MEGAChatMessageTypeContainsMeta ? self.containsMeta.type : MEGAChatContainsMetaTypeInvalid;
-    return self.chatId ^ self.messageId ^ contentHash ^ self.warningDialog ^ metaHash ^ self.localPreview;
+    NSUInteger messageHash = self.chatId ^ self.messageId ^ contentHash ^ self.warningDialog ^ metaHash ^ self.localPreview;
+    if (@available(iOS 13.0, *)) {
+        return messageHash ^ UITraitCollection.currentTraitCollection.userInterfaceStyle;
+    } else {
+        return messageHash;
+    }
 }
 
 - (id)debugQuickLookObject {
