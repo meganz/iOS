d58b6f585ff84b67befd3b877f13fab7fb6ba703
Joel Meng <jm@mega.co.nz>
2020-06-03 21:35:55 +1000

    Add OverDiskQuotaWarningView

---
+++ iMEGA/Cloud drive/OverDiskQuota/OverDisckQuotaWarningView.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+@IBDesignable
+final class OverDisckQuotaWarningView: UIView, NibLoadable {
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var detailLabel: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    // MARK: - Setup View
+    
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .yellow
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.yellow.cgColor
+    }
+}
