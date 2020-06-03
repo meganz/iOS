62e87081c43954b9493707bc6457bedca6bb1637
Joel Meng <jm@mega.co.nz>
2020-06-03 13:49:37 +1000

    Add storage full assets and xib

---
+++ iMEGA/Cloud drive/OverDiskQuota/OverDiskQuotaViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OverDiskQuotaViewController.swift
+//  MEGA
+//
+//  Created by Jun Meng on 3/6/20.
+//  Copyright © 2020 MEGA. All rights reserved.
+//
+
+import UIKit
+
+class OverDiskQuotaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
