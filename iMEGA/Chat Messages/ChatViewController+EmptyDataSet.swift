ca98dbfb1f5e795dbd3d33d5e97894eb09833f70
Peter Li <hl@mega.co.nz>
2020-06-03 16:56:37 +1200

    improvement(chatroom): optimse loading status

--- iMEGA/Chat Messages/ChatViewController+EmptyDataSet.swift
+++ iMEGA/Chat Messages/ChatViewController+EmptyDataSet.swift
@@ -27,4 +27,7 @@ extension ChatViewController: DZNEmptyDataSetDelegate {
         return true
     }
     
+    func emptyDataSetShouldDisplay(_ scrollView: UIScrollView) -> Bool {
+        return chatRoomDelegate.loadingState
+    }
 }
