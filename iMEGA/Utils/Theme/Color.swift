784b8f1872411f5d7988f4da5e737ccd09da8ef9
Joel Meng <jm@mega.co.nz>
2020-06-05 11:13:15 +1000

    Update Color to add some constant color

--- iMEGA/Utils/Theme/Color.swift
+++ iMEGA/Utils/Theme/Color.swift
@@ -9,37 +9,63 @@ struct Color: Codable {
     var alpha: ColorValue = 255
 }
 
-func uiColor(from colorProvider: ColorProviding) -> UIColor {
-    return colorProvider.uiColor
-}
-
-extension Color {
-    static var white: Color { return Color(red: 255, green: 255, blue: 255) }
-}
-
 extension Color: ColorProviding {
     var uiColor: UIColor {
         UIColor.rgbColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
 
+func uiColor(from colorProvider: ColorProviding) -> UIColor {
+    return colorProvider.uiColor
+}
+
+extension Color {
+
+    // MARK: - Text
+
+    /// RGB(0, 0, 0, 0.8), Dark gray
+    static var textLightPrimary: Color { Color.Text.lightPrimary }
+
+    /// RGB(255, 255, 255, 1), White
+    static var textDarkPrimary: Color { Color.Text.darkPrimary }
+
+    // MARK: - Background
+
+    /// RGB(0, 0, 0, 1), White
+    static var backgroundDefaultLight: Color { Color.Background.defaultLight }
+
+    /// RGB(255, 204, 0, 0.15), Very light yellow
+    static var backgroundWarningYellow: Color { Color.Background.warning }
+
+    // MARK: - Border
+
+    /// RGB(255, 204, 0, 1), Yellow
+    static var borderWarningYellow: Color { Color.Border.warning }
+}
+
 extension Color {
 
     enum Text {
 
-        /// RGB(0, 0, 0, 0.8)
+        /// RGB(0, 0, 0, 0.8), Dark gray
         static let darkPrimary = Color(red: 0, green: 0, blue: 0, alpha: 204)
 
-        /// RGB(255, 255, 255, 1)
+        /// RGB(255, 255, 255, 1), White
         static let lightPrimary = Color(red: 255, green: 255, blue: 255)
     }
 
     enum Background {
-        static let `default` = Color(red: 0, green: 0, blue: 0)
+
+        /// RGB(0, 0, 0, 1), White
+        static let defaultLight = Color(red: 0, green: 0, blue: 0)
+
+        /// RGB(255, 204, 0, 0.15), Very light yellow
         static let warning = Color(red: 255, green: 204, blue: 0, alpha: 38)
     }
 
     enum Border {
+
+        /// RGB(255, 204, 0, 1), Yellow
         static let warning = Color(red: 255, green: 204, blue: 0)
     }
 }
