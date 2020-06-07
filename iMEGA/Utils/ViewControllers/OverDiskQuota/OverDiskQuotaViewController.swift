a7928d1401e9686b41c204f6aa0d5507f90bd2e7
Joel Meng <jm@mega.co.nz>
2020-06-07 22:01:43 +1000

    Localize texts

--- iMEGA/Utils/ViewControllers/OverDiskQuota/OverDiskQuotaViewController.swift
+++ iMEGA/Utils/ViewControllers/OverDiskQuota/OverDiskQuotaViewController.swift
@@ -23,7 +23,7 @@ class OverDiskQuotaViewController: UIViewController {
     // MARK: - UI Customize
 
     private func setupNavigationController(_ navigationController: UINavigationController?) {
-        title = "Stoage Full"
+        title = AMLocalizedString("Stoage Full")
         navigationController?.navigationBar.setTranslucent()
         navigationController?.setTitleStyle(TextStyle(font: .headline, color: Color.Text.lightPrimary))
     }
@@ -33,24 +33,27 @@ class OverDiskQuotaViewController: UIViewController {
     }
 
     private func setupTitleLabel(_ titleLabel: UILabel) {
+        titleLabel.text = AMLocalizedString("Your Data is at Risk!")
         let textStyle = TextStyle(font: .headline, color: .textDarkPrimary)
         textStyle.applied(on: titleLabel)
     }
 
     private func setupWarningDescriptionLabel(_ descriptionLabel: UILabel) {
         let attributes = AttributedTextStyle.paragraph.style([:])
-        let attributedString = NSAttributedString(string: "We have contacted you by email to bk@mega.nz on March 1 2020,  March 30 2020, April 30 2020 and May 15 2020, but you still have 45302 files taking up 234.54 GB in your MEGA account, which requires you to upgrade to PRO Lite.",
+        let warningText = AMLocalizedString("We have contacted you by email to bk@mega.nz on March 1 2020,  March 30 2020, April 30 2020 and May 15 2020, but you still have 45302 files taking up 234.54 GB in your MEGA account, which requires you to upgrade to PRO Lite.")
+        let attributedString = NSAttributedString(string: warningText,
                                                   attributes: attributes)
         descriptionLabel.attributedText = attributedString
     }
 
     private func setupUpgradeButton(_ button: UIButton) {
         ButtonStyle.active.style(button)
-        button.setTitle("Upgrade", for: .normal)
+        button.setTitle(AMLocalizedString("Upgrade"), for: .normal)
 
     }
 
     private func setupDismissButton(_ button: UIButton) {
         ButtonStyle.inactive.style(button)
+        button.setTitle(AMLocalizedString("Dismiss"), for: .normal)
     }
 }
