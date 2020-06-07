53e8cc5478eab5633b404bbe5a4230e15445580c
Peter Li <hl@mega.co.nz>
2020-06-08 10:29:12 +1200

    feat(edit overlay): align checkbox with messageContainerView

--- iMEGA/Chat Messages/Views/MessageEditCollectionOverlayView.swift
+++ iMEGA/Chat Messages/Views/MessageEditCollectionOverlayView.swift
@@ -5,6 +5,7 @@ protocol MessagesEditCollectionOverlayViewDelegate : AnyObject {
 }
 
 class MessageEditCollectionOverlayView : MessageReusableView {
+    @IBOutlet weak var centerConstraint: NSLayoutConstraint!
     @IBOutlet weak var leftIconView: UIImageView!
     open weak var delegate: MessagesEditCollectionOverlayViewDelegate?
     var isActive = false {
@@ -34,5 +35,20 @@ class MessageEditCollectionOverlayView : MessageReusableView {
     
     func configureDisplaying(isActive: Bool) {
         self.isActive = isActive
+        // Get the collectionView
+        if let collectionView = self.superview as? UICollectionView {
+            // Get indexPath
+            guard  let indexPath = indexPath,
+                let cell = collectionView.cellForItem(at: indexPath) as? MessageContentCell else {
+                return
+            }
+            let messageContainerView = cell.messageContainerView
+            centerConstraint.constant = messageContainerView.frame.minY / 2
+            
+            
+            
+        }
+        
+        
     }
 }
