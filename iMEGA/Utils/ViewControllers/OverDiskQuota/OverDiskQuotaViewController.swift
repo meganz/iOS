8cce3a9634b8613775b98f7ec6c5a99c28fa35cc
Joel Meng <jm@mega.co.nz>
2020-06-15 17:59:33 +1000

    Update days left text

--- iMEGA/Utils/ViewControllers/OverDiskQuota/OverDiskQuotaViewController.swift
+++ iMEGA/Utils/ViewControllers/OverDiskQuota/OverDiskQuotaViewController.swift
@@ -13,7 +13,7 @@ final class OverDiskQuotaViewController: UIViewController {
 
     @objc var dismissAction: (() -> Void)?
 
-    private var warning: OverDiskQuota?
+    private var overDiskQuota: OverDiskQuotaInternal?
 
     private lazy var dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
@@ -37,10 +37,10 @@ final class OverDiskQuotaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTitleLabel(titleLabel)
-        setupMessageLabel(warningParagaphLabel, overDiskInformation: warning)
+        setupMessageLabel(warningParagaphLabel, overDiskInformation: overDiskQuota)
         setupUpgradeButton(upgradeButton)
         setupDismissButton(dismissButton)
-        warningView.updateTimeLeftForTitle(withText: "33 days")
+        setupWarningView(warningView, withDeadline: overDiskQuota?.deadline)
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -52,15 +52,15 @@ final class OverDiskQuotaViewController: UIViewController {
     // MARK: - Setup MEGA UserData
 
     @objc func setup(with overDiskQuota: OverDiskQuotaInfomationType) {
-        warning = OverDiskQuota(email: overDiskQuota.email,
-                                       deadline: overDiskQuota.deadline,
-                                       warningDates: overDiskQuota.warningDates,
-                                       numberOfFilesOnCloud: overDiskQuota.numberOfFilesOnCloud,
-                                       cloudStorage: overDiskQuota.cloudStorage,
-                                       plan: overDiskQuota.availablePlan)
+        self.overDiskQuota = OverDiskQuotaInternal(email: overDiskQuota.email,
+                                                   deadline: overDiskQuota.deadline,
+                                                   warningDates: overDiskQuota.warningDates,
+                                                   numberOfFilesOnCloud: overDiskQuota.numberOfFilesOnCloud,
+                                                   cloudStorage: overDiskQuota.cloudStorage,
+                                                   plan: overDiskQuota.availablePlan)
     }
 
-    struct OverDiskQuota {
+    struct OverDiskQuotaInternal {
         typealias Email = String
         typealias Deadline = Date
         typealias WarningDates = [Date]
@@ -88,12 +88,23 @@ final class OverDiskQuotaViewController: UIViewController {
         disableAdjustingContentInsets(for: contentScrollView)
     }
 
+    private func setupWarningView(_ warningView: OverDisckQuotaWarningView, withDeadline deadline: Date?) {
+        guard let deadline = deadline else {
+            warningView.updateTimeLeftForTitle(withText: "0 days")
+            return
+        }
+
+        let daysLeft = daysDistance(from: Date(), endDate: deadline)
+        let daysLeftLocalized = AMLocalizedString("Over Disk Quota Warning Days Left", "%@ days")
+        warningView.updateTimeLeftForTitle(withText: String(format: daysLeftLocalized, daysLeft))
+    }
+
     private func setupTitleLabel(_ titleLabel: UILabel) {
         titleLabel.text = AMLocalizedString("Over Disk Quota Title", "Your Data is at Risk!")
         LabelStyle.headline.style(titleLabel)
     }
 
-    private func setupMessageLabel(_ descriptionLabel: UILabel, overDiskInformation: OverDiskQuota?) {
+    private func setupMessageLabel(_ descriptionLabel: UILabel, overDiskInformation: OverDiskQuotaInternal?) {
         guard let infor = overDiskInformation else { descriptionLabel.text = nil; return }
         let message = AMLocalizedString("Over Disk Quota Message", "We have contacted you by email to bk@mega.nz on March 1 2020,  March 30 2020, April 30 2020 and May 15 2020, but you still have 45302 files taking up 234.54 GB in your MEGA account, which requires you to upgrade to PRO Lite.")
         let formattedMessage = String(format: message,
@@ -124,11 +135,18 @@ final class OverDiskQuotaViewController: UIViewController {
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.pushViewController(upgradeViewController, animated: true)
     }
+
+    // MARK: - Days left
+
+    private func daysDistance(from startDate: Date, endDate: Date) -> Int {
+        let calendar = Calendar.current
+        return calendar.dateComponents([.day], from: startDate, to: endDate).day!
+    }
 }
 
 // MARK: - Attributed Warning Text
 
-fileprivate extension OverDiskQuotaViewController.OverDiskQuota {
+fileprivate extension OverDiskQuotaViewController.OverDiskQuotaInternal {
 
     func formattedWarningDates(with formatter: DateFormatter) -> String {
         return warningDates.reduce("") { (result, date) in
