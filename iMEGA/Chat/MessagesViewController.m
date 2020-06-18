003456381752287b9121b7d7ec8f13e4ff32cc2a
Javier Trujillo <jth@mega.nz>
2020-06-18 15:50:52 +0200

    fix: crash clearing chat history with unread messages and receiving messages after this scenario (15133)

--- iMEGA/Chat/MessagesViewController.m
+++ iMEGA/Chat/MessagesViewController.m
@@ -1330,6 +1330,9 @@ static NSMutableSet<NSString *> *tapForInfoSet;
 }
 
 - (NSIndexPath *)indexPathForCellWithUnreadMessagesLabel {
+    if (self.messages.count < self.unreadMessages) {
+        return nil;
+    }
     return [NSIndexPath indexPathForItem:(self.messages.count - self.unreadMessages) inSection:0];
 }
 
@@ -1346,7 +1349,9 @@ static NSMutableSet<NSString *> *tapForInfoSet;
         self.unreadMessages = unreads;
         unreadMessagesIndexPath = [self indexPathForCellWithUnreadMessagesLabel];
     }
-    [self.collectionView reloadItemsAtIndexPaths:@[unreadMessagesIndexPath]];
+    if (unreadMessagesIndexPath) {
+        [self.collectionView reloadItemsAtIndexPaths:@[unreadMessagesIndexPath]];
+    }
 }
 
 - (void)updateOffsetForCellAtIndexPath:(NSIndexPath *)indexPath previousHeight:(CGFloat)previousHeight {
