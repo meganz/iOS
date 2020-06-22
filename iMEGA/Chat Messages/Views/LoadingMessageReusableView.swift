1dbde2cb26f355c99add6f3c1f7e228da5a6258b
Peter Li <hl@mega.co.nz>
2020-06-22 17:06:54 +1200

    improvement(ladingmessage): support dark mode for loading view

--- iMEGA/Chat Messages/Views/LoadingMessageReusableView.swift
+++ iMEGA/Chat Messages/Views/LoadingMessageReusableView.swift
@@ -3,10 +3,27 @@ import MessageKit
 class LoadingMessageReusableView: MessageReusableView {
 
     @IBOutlet weak var loadingView: UIView!
-
+    @IBOutlet var loadingBubbles: [UIView]!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        updateAppearance()
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+         super.traitCollectionDidChange(previousTraitCollection)
+         
+         if #available(iOS 13.0, *),
+             traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+                 updateAppearance()
+         }
+     }
+    
+    func updateAppearance() {
+        loadingView.backgroundColor = UIColor.mnz_background()
+        loadingBubbles.forEach { (view) in
+            view.backgroundColor = UIColor.mnz_chatLoadingBubble(traitCollection)
+        }
     }
     
 }
