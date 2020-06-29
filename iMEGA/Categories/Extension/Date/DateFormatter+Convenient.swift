ac6adb1ce6bc45b5e23726260ff8292c3607abc3
Joel Meng <jm@mega.co.nz>
2020-06-29 16:01:18 +1000

    🎨 (DateFormatting): Add DateFormatting for shadowing DateFormatter

--- iMEGA/Categories/Extension/Date/DateFormatter+Convenient.swift
+++ iMEGA/Categories/Extension/Date/DateFormatter+Convenient.swift
@@ -5,19 +5,19 @@ extension DateFormatter {
     // MARK: - Date Formatter
 
     /// Monday Jun 1, 2020
-    static var dateMediumWithWeekday: DateFormatter {
+    static var dateMediumWithWeekday: DateFormatting {
         DateFormatterPool.shared.dateFormatter(of: .dateMediumWithWeekday)
     }
 
     /// Jun 1, 2020
-    static var dateMedium: DateFormatter {
+    static var dateMedium: DateFormatting {
         DateFormatterPool.shared.dateFormatter(of: .dateMedium(isRelative: false))
     }
 
     // MARK: - Relative date formatter, e.g. date in the next day will be "Tomorrow" etc.
 
     /// Jun 1, 2020 or "Tomorrow", "Today", "Yesterday"
-    static var dateMediumRelative: DateFormatter {
+    static var dateMediumRelative: DateFormatting {
         DateFormatterPool.shared.dateFormatter(of: .dateMedium(isRelative: true))
     }
 }
