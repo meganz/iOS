4510938cbb387a431b336ff5b133840d1cd0643c
Joel Meng <jm@mega.co.nz>
2020-06-29 15:32:22 +1000

    👌 (DateFormatter+Convenient): Change static stored variable to static compute variable
    Update DateFormatter+Convenient, change static stored variable to compute variable

--- iMEGA/Categories/Extension/Date/DateFormatter+Convenient.swift
+++ iMEGA/Categories/Extension/Date/DateFormatter+Convenient.swift
@@ -5,13 +5,19 @@ extension DateFormatter {
     // MARK: - Date Formatter
 
     /// Monday Jun 1, 2020
-    static let dateMediumWithWeekday = DateFormatterPool.shared.dateFormatter(of: .dateMediumWithWeekday)
+    static var dateMediumWithWeekday: DateFormatter {
+        DateFormatterPool.shared.dateFormatter(of: .dateMediumWithWeekday)
+    }
 
     /// Jun 1, 2020
-    static let dateMedium = DateFormatterPool.shared.dateFormatter(of: .dateMedium(relative: false))
+    static var dateMedium: DateFormatter {
+        DateFormatterPool.shared.dateFormatter(of: .dateMedium(isRelative: false))
+    }
 
     // MARK: - Relative date formatter, e.g. date in the next day will be "Tomorrow" etc.
 
     /// Jun 1, 2020 or "Tomorrow", "Today", "Yesterday"
-    static let dateMediumRelative = DateFormatterPool.shared.dateFormatter(of: .dateMedium(relative: true))
+    static var dateMediumRelative: DateFormatter {
+        DateFormatterPool.shared.dateFormatter(of: .dateMedium(isRelative: true))
+    }
 }
