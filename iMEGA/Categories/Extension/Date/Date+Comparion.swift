0aff625b8558d02903e05aced463522a11c232e4
Joel Meng <jm@mega.co.nz>
2020-06-29 13:41:55 +1000

    📝 (Date+Comparation): Update documentation for dayDistance function

--- iMEGA/Categories/Extension/Date/Date+Comparion.swift
+++ iMEGA/Categories/Extension/Date/Date+Comparion.swift
@@ -4,13 +4,13 @@ extension Date {
 
     typealias NumberOfDays = Int
 
-    /// Will return an count of calendar days from `self` to a given *future* date.
+    /// Will return a positive number which indicates calendar days on specified calendar from `self` to a given *future* date.
     /// Discussion: This function is used to calculate distance of calendar days to a *future* date which means
     /// at least `futureDate` is later than or equal to current instance, so that the `numberOfDays` will be a positive number,
     /// or else, nil will be return.
     /// - Parameters:
-    ///   - futureDate: A date in the future on which this distance of days will be calculated based.
-    ///   - calendar: A calendar object on which the calendar days to be based on.
+    ///   - futureDate: A date in the future on which this distance of days will be calculated.
+    ///   - calendar: A calendar object on which the calendar days to be based.
     /// - Returns:
     func dayDistance(toFutureDate futureDate: Date, on calendar: Calendar) -> NumberOfDays? {
         guard futureDate >= self else {
@@ -20,7 +20,7 @@ extension Date {
         return calendar.dateComponents([.day], from: self, to: futureDate).day
     }
 
-    /// Will return an count of calendar days from `self` to a given *past* date.
+    /// Will return a positive number which indicates calendar days on specified calendar from `self` to a given *past* date.
     /// Discussion: This function is used to calculate distance of calendar days to a *past* date which means
     /// at least `pastDate` is earlier than or equal to current instance, so that the `numberOfDays` will be a positive number,
     /// or else, nil will be return.
