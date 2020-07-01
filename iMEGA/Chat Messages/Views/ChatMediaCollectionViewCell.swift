84c257d5431417349013d7ac4df9745ed3218a26
Peter Li <hl@mega.co.nz>
2020-07-01 13:19:37 +1200

    fix(mediacell): limit the max width of media cell

--- iMEGA/Chat Messages/Views/ChatMediaCollectionViewCell.swift
+++ iMEGA/Chat Messages/Views/ChatMediaCollectionViewCell.swift
@@ -1,8 +1,7 @@
 import MessageKit
 
 class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
-    
-    var currentNode : MEGANode?
+    var currentNode: MEGANode?
     open var imageView: YYAnimatedImageView = {
         let imageView = YYAnimatedImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         imageView.contentMode = .scaleAspectFit
@@ -14,14 +13,14 @@ class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
         playIconView.isHidden = true
         return playIconView
     }()
-    
+
     open var loadingIndicator: UIActivityIndicatorView = {
         let loadingIndicator = UIActivityIndicatorView(style: .gray)
         loadingIndicator.startAnimating()
         loadingIndicator.isHidden = true
         return loadingIndicator
     }()
-    
+
     open var durationLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
@@ -33,9 +32,8 @@ class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
         label.isHidden = true
         return label
     }()
-    
-    
-    public override init(frame: CGRect) {
+
+    override public init(frame: CGRect) {
         super.init(frame: frame)
         MEGASdkManager.sharedMEGASdk()?.add(self)
     }
@@ -44,7 +42,7 @@ class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
         super.init(coder: aDecoder)
         MEGASdkManager.sharedMEGASdk()?.add(self)
     }
-    
+
     // MARK: - Methods
 
     /// Responsible for setting up the constraints of the cell's subviews.
@@ -55,12 +53,12 @@ class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
         durationLabel.autoPinEdge(toSuperviewEdge: .trailing, withInset: 10)
         durationLabel.autoPinEdge(toSuperviewEdge: .bottom, withInset: 10)
         durationLabel.autoSetDimension(.height, toSize: 14)
-        
+
         loadingIndicator.autoCenterInSuperview()
         loadingIndicator.autoSetDimensions(to: CGSize(width: 20, height: 20))
     }
 
-    open override func setupSubviews() {
+    override open func setupSubviews() {
         super.setupSubviews()
         messageContainerView.addSubview(imageView)
         messageContainerView.addSubview(loadingIndicator)
@@ -68,16 +66,16 @@ class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
         imageView.addSubview(durationLabel)
         setupConstraints()
     }
-    
+
     override func configure(with message: MessageType, at indexPath: IndexPath, and messagesCollectionView: MessagesCollectionView) {
         super.configure(with: message, at: indexPath, and: messagesCollectionView)
-        
+
         guard let chatMessage = message as? ChatMessage else {
             return
         }
 
         let megaMessage = chatMessage.message
-        
+
         if chatMessage.transfer != nil {
             loadingIndicator.isHidden = false
             loadingIndicator.startAnimating()
@@ -88,20 +86,20 @@ class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
             imageView.yy_imageURL = URL(fileURLWithPath: path)
             return
         }
-        
+
         let node = megaMessage.nodeList.node(at: 0)!
         currentNode = node
         let name = node.name! as NSString
-        
-        imageView.mnz_setPreview(by: node) {(request) in
+
+        imageView.mnz_setPreview(by: node) { _ in
             let visibleIndexPaths = messagesCollectionView.indexPathsForVisibleItems
             guard visibleIndexPaths.contains(indexPath) else {
                 return
             }
-            
+
             messagesCollectionView.reloadItems(at: [indexPath])
         }
-        
+
         durationLabel.isHidden = true
         playIconView.isHidden = true
         loadingIndicator.isHidden = true
@@ -116,32 +114,31 @@ class ChatMediaCollectionViewCell: MessageContentCell, MEGATransferDelegate {
             durationLabel.text = NSString.mnz_string(fromTimeInterval: TimeInterval(node.duration))
             durationLabel.isHidden = false
             playIconView.isHidden = false
-            
         }
-        
-        
     }
-    
-    func onTransferFinish(_ api: MEGASdk, transfer: MEGATransfer, error: MEGAError) {
+
+    func onTransferFinish(_: MEGASdk, transfer: MEGATransfer, error _: MEGAError) {
         if currentNode?.handle == transfer.nodeHandle {
-            if transfer.path != nil && FileManager.default.fileExists(atPath: transfer.path) {
+            if transfer.path != nil, FileManager.default.fileExists(atPath: transfer.path) {
                 imageView.yy_imageURL = URL(fileURLWithPath: transfer.path)
             }
         }
     }
-    
-    
 }
 
 open class ChatMediaCollectionViewSizeCalculator: MessageSizeCalculator {
-   
     let cell = ChatMediaCollectionViewCell()
 
-    public override init(layout: MessagesCollectionViewFlowLayout? = nil) {
+    override public init(layout: MessagesCollectionViewFlowLayout? = nil) {
         super.init(layout: layout)
         configureAccessoryView()
     }
-    open override func messageContainerSize(for message: MessageType) -> CGSize {
+
+    override open func messageContainerMaxWidth(for message: MessageType) -> CGFloat {
+        return min(UIDevice.current.mnz_maxSideForChatBubble(withMedia: true), super.messageContainerMaxWidth(for: message))
+    }
+
+    override open func messageContainerSize(for message: MessageType) -> CGSize {
         switch message.kind {
         case .custom:
             let maxWidth = messageContainerMaxWidth(for: message)
@@ -149,21 +146,20 @@ open class ChatMediaCollectionViewSizeCalculator: MessageSizeCalculator {
             guard let chatMessage = message as? ChatMessage else {
                 return .zero
             }
-            
+
             var width: CGFloat = 200
             var height: CGFloat = 200
-            
+
             if chatMessage.transfer != nil {
-                
                 let path = NSHomeDirectory().append(pathComponent: chatMessage.transfer!.path)
-                
+
                 if FileManager.default.fileExists(atPath: path) {
                     guard let previewImage = UIImage(contentsOfFile: path) else {
                         return CGSize(width: width, height: height)
                     }
                     width = previewImage.size.width
                     height = previewImage.size.height
-                    
+
                     let ratio = width / height
                     if ratio > 1 {
                         width = min(maxWidth, width)
@@ -172,20 +168,19 @@ open class ChatMediaCollectionViewSizeCalculator: MessageSizeCalculator {
                         height = min(maxHeight, height)
                         width = height * ratio
                     }
-                    
                 }
             } else {
                 let megaMessage = chatMessage.message
                 let node = megaMessage.nodeList.node(at: 0)!
                 let previewFilePath = Helper.path(for: node, inSharedSandboxCacheDirectory: "previewsV3")
-                
+
                 if FileManager.default.fileExists(atPath: previewFilePath) {
                     let previewImage = UIImage(contentsOfFile: previewFilePath)
                     width = previewImage!.size.width
                     height = previewImage!.size.height
                 }
-                if node.hasPreview() &&
-                    node.height > 0 &&
+                if node.hasPreview(),
+                    node.height > 0,
                     node.width > 0 {
                     width = CGFloat(node.width)
                     height = CGFloat(node.height)
@@ -199,7 +194,7 @@ open class ChatMediaCollectionViewSizeCalculator: MessageSizeCalculator {
                 height = min(maxHeight, height)
                 width = height * ratio
             }
-            
+
             return CGSize(width: width, height: height)
         default:
             fatalError("messageContainerSize received unhandled MessageDataType: \(message.kind)")
