4920021c0114f4bf0a366279dda48d758ce3af93
Joel Meng <jm@mega.co.nz>
2020-07-08 09:56:19 +1000

    Update Basic button text and background color

--- iMEGA/Utils/Theme/Style/ThemeColor/ThemeButton/SecondaryThemeButtonTextColorFactory.swift
+++ iMEGA/Utils/Theme/Style/ThemeColor/ThemeButton/SecondaryThemeButtonTextColorFactory.swift
@@ -2,10 +2,12 @@ import Foundation
 
 extension LightColorThemeFactory {
 
+    // MARK: - Light Text Color
+
     struct LightSecondaryThemeButtonTextColorFactory: ButtonColorFactory {
 
         func normalColor() -> Color {
-            Color(red: 0, green: 168, blue: 134, alpha: 255)
+            Color(red: 0, green: 168, blue: 134)
         }
 
         func disabledColor() -> Color {
@@ -16,6 +18,8 @@ extension LightColorThemeFactory {
             Color(red: 155, green: 155, blue: 155)
         }
     }
+
+    // MARK: - Light Background Color
 
     struct LightSecondaryThemeButtonBackgroundColorFactory: ButtonColorFactory {
 
@@ -35,10 +39,12 @@ extension LightColorThemeFactory {
 
 extension DarkColorThemeFactory {
 
+    // MARK: - Dark Text Color
+
     struct DarkSecondaryThemeButtonTextColorFactory: ButtonColorFactory {
 
         func normalColor() -> Color {
-            Color(red: 0, green: 168, blue: 134, alpha: 255)
+            Color(red: 0, green: 194, blue: 154)
         }
 
         func disabledColor() -> Color {
@@ -50,10 +56,12 @@ extension DarkColorThemeFactory {
         }
     }
 
+    // MARK: - Dark Background Color
+
     struct DarkSecondaryThemeButtonBackgroundColorFactory: ButtonColorFactory {
 
         func normalColor() -> Color {
-            Color(red: 255, green: 255, blue: 255)
+            Color(red: 54, green: 54, blue: 56)
         }
 
         func disabledColor() -> Color {
