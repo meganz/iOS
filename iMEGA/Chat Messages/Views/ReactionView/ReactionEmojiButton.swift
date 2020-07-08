46bf42672edf51333511792ec16e76510a214e33
Peter Li <hl@mega.co.nz>
2020-07-08 12:09:38 +1200

    feat(reactionview): adjust horizontal padding to meet the design

--- iMEGA/Chat Messages/Views/ReactionView/ReactionEmojiButton.swift
+++ iMEGA/Chat Messages/Views/ReactionView/ReactionEmojiButton.swift
@@ -14,11 +14,28 @@ class ReactionEmojiButton: UIButton {
         
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPress(_:)))
         addGestureRecognizer(longPressGesture)
-        setTitleColor(.black, for: .normal)
-        setTitle("\(emoji) \(count)", for: .normal)
+        let attributedEmoji = NSAttributedString(string: emoji, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 22)])
+        let attributedCount = NSAttributedString(string: " \(count)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .medium),
+                                                                                   NSAttributedString.Key.baselineOffset:3,
+                                                                                
+        ])
+        let attributedString = NSMutableAttributedString()
+        attributedString.append(attributedEmoji)
+        attributedString.append(attributedCount)
+        
+        contentVerticalAlignment = .center
+        setAttributedTitle(attributedString, for: .normal)
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        
+        layer.borderWidth = 1
+        layer.borderColor = UIColor(hexString: "009476")?.cgColor
+        layer.cornerRadius = 12
+        
+        backgroundColor = #colorLiteral(red: 0, green: 0.5803921569, blue: 0.462745098, alpha: 0.1)
         addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        sizeToFit()
     }
- 
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
