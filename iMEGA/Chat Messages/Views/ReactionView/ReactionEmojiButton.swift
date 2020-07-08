04230e62c479f4a7ca41a66f97af07e71f20e3e9
Peter Li <hl@mega.co.nz>
2020-07-08 15:20:54 +1200

    feat(reactionview): showing reaction status

--- iMEGA/Chat Messages/Views/ReactionView/ReactionEmojiButton.swift
+++ iMEGA/Chat Messages/Views/ReactionView/ReactionEmojiButton.swift
@@ -4,16 +4,31 @@ class ReactionEmojiButton: UIButton {
     
     let count: Int
     let emoji: String
+    let emojiSelected: Bool
+
     var buttonPressed:((String, UIView) -> Void)?
     var buttonLongPress:((String, UIView) -> Void)?
     
-    init(count:Int, emoji:String) {
+    init(count:Int, emoji:String, emojiSelected: Bool) {
         self.count = count
         self.emoji = emoji
+        self.emojiSelected = emojiSelected
         super.init(frame: .zero)
         
         let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPress(_:)))
         addGestureRecognizer(longPressGesture)
+        addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        
+        configure()
+        sizeToFit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        
         let attributedEmoji = NSAttributedString(string: emoji, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 22)])
         let attributedCount = NSAttributedString(string: " \(count)", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13, weight: .medium),
                                                                                    NSAttributedString.Key.baselineOffset:3,
@@ -28,17 +43,18 @@ class ReactionEmojiButton: UIButton {
         contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
         
         layer.borderWidth = 1
-        layer.borderColor = UIColor(hexString: "009476")?.cgColor
         layer.cornerRadius = 12
         
-        backgroundColor = #colorLiteral(red: 0, green: 0.5803921569, blue: 0.462745098, alpha: 0.1)
-        addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
-        sizeToFit()
+        if emojiSelected {
+            backgroundColor = #colorLiteral(red: 0, green: 0.5803921569, blue: 0.462745098, alpha: 0.1)
+            layer.borderColor = UIColor(hexString: "009476")?.cgColor
+        } else {
+            backgroundColor = UIColor(hexString: "f9f9f9")
+            layer.borderColor = UIColor(red: 3/255, green: 3/255, blue: 3/255, alpha: 0.1).cgColor
+        }
+        
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     @objc func longPress(_ tapGesture: UITapGestureRecognizer) {
 
         buttonLongPress?(emoji, self)
