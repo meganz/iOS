fa2cc7454294df95425e59b0f2cdc7ff7a2eddd9
Peter Li <56808021+lhr000lhrmega@users.noreply.github.com>
2020-07-10 09:14:49 +1200

    Update iMEGA/Categories/NSObject+Debounce.m
    Co-authored-by: Javier Navarro <jn@mega.co.nz>

--- iMEGA/Categories/NSObject+Debounce.m
+++ iMEGA/Categories/NSObject+Debounce.m
@@ -1,10 +1,9 @@
 @implementation NSObject (Debounce)
 
-- (void)debounce:(SEL)action delay:(NSTimeInterval)delay
-{
-  __weak typeof(self) weakSelf = self;
-  [NSObject cancelPreviousPerformRequestsWithTarget:weakSelf selector:action object:nil];
-  [weakSelf performSelector:action withObject:nil afterDelay:delay];
+- (void)debounce:(SEL)action delay:(NSTimeInterval)delay {
+    __weak typeof(self) weakSelf = self;
+    [NSObject cancelPreviousPerformRequestsWithTarget:weakSelf selector:action object:nil];
+    [weakSelf performSelector:action withObject:nil afterDelay:delay];
 }
 
 @end
