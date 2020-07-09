491f3eacb920ae23e1fffe0f1aa1c28efcdc0d02
Peter Li <hl@mega.co.nz>
2020-07-09 15:34:20 +1200

    fix: app no reponse for a long time when first login
    adding debounce to avoid extremely frequent I/O from ChatSDK

---
+++ iMEGA/Categories/NSObject+Debounce.m
@@ -0,0 +1,10 @@
+@implementation NSObject (Debounce)
+
+- (void)debounce:(SEL)action delay:(NSTimeInterval)delay
+{
+  __weak typeof(self) weakSelf = self;
+  [NSObject cancelPreviousPerformRequestsWithTarget:weakSelf selector:action object:nil];
+  [weakSelf performSelector:action withObject:nil afterDelay:delay];
+}
+
+@end
