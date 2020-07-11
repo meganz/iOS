bfe60d5ce54e7291471017e99232e4311591998b
Joel Meng <jm@mega.co.nz>
2020-07-11 12:25:18 +1000

    ♻️ (Bridging DateFormatting tools to Objc): Briding DateFormatting tools to Objc

--- iMEGA/Categories/Extension/Date/DateFormatter+Convenient.swift
+++ iMEGA/Categories/Extension/Date/DateFormatter+Convenient.swift
@@ -5,7 +5,7 @@ extension DateFormatter {
     // MARK: - Formatter Custom Template Style
 
     /// Monday Jun 1, 2020
-    static func dateMediumWithWeekday(calendar: Calendar? = nil,
+    @objc static func dateMediumWithWeekday(calendar: Calendar? = nil,
                                       timeZone: TimeZone? = nil,
                                       locale: Locale? = nil) -> DateFormatting {
         let style = DateStyle.dateStyleFactory.templateStyle(fromTemplate: "EEEEMMMddyyyy",
@@ -18,30 +18,35 @@ extension DateFormatter {
     // MARK: - Formatter Style
 
     /// English -- `Jan 1, 1970`    Spanish -- `1 ene 1970`    Chinese -- `1970年1月1日`
-    static func dateMediumSystem(calendar: Calendar? = nil,
+    @objc static func dateMediumSystem(calendar: Calendar? = nil,
                                  timeZone: TimeZone? = nil,
                                  locale: Locale? = nil) -> DateFormatting {
-        let style = DateStyle.dateStyleFactory.systemStyle(ofDateStyle: .medium,
-                                                           timeStyle: .none,
-                                                           relativeDateFormatting: false,
-                                                           calendar: calendar,
-                                                           timeZone: timeZone,
-                                                           locale: locale)
-        return DateFormatterPool.shared.dateFormatter(of: style)
+        dateMedium(dateStyle: .medium, timeStyle: .none, isRelative: false)
     }
 
     // MARK: - Relative date formatter, e.g. date in the next day will be "Tomorrow" etc.
 
     /// `Yesterday`,  `Today`, `Tomorrow`    /   `hoy`, `mañana`, `pasado mañana`    /   `昨天`, `今天`,  `明天`,  `后天`
-    static func relativeDateMedium(calendar: Calendar? = nil,
+    @objc static func relativeDateMedium(calendar: Calendar? = nil,
                                    timeZone: TimeZone? = nil,
                                    locale: Locale? = nil) -> DateFormatting {
-        let style = DateStyle.dateStyleFactory.systemStyle(ofDateStyle: .medium,
-                                                         timeStyle: .none,
-                                                         relativeDateFormatting: true,
-                                                         calendar: calendar,
-                                                         timeZone: timeZone,
-                                                         locale: locale)
+        dateMedium(dateStyle: .medium, timeStyle: .none, isRelative: true)
+    }
+
+    // MARK: - Privates
+
+    static func dateMedium(dateStyle: DateFormatter.Style,
+                           timeStyle: DateFormatter.Style,
+                           isRelative: Bool,
+                           calendar: Calendar? = nil,
+                           timeZone: TimeZone? = nil,
+                           locale: Locale? = nil) -> DateFormatting {
+        let style = DateStyle.dateStyleFactory.systemStyle(ofDateStyle: dateStyle,
+                                                           timeStyle: timeStyle,
+                                                           relativeDateFormatting: isRelative,
+                                                           calendar: calendar,
+                                                           timeZone: timeZone,
+                                                           locale: locale)
         return DateFormatterPool.shared.dateFormatter(of: style)
     }
 }
