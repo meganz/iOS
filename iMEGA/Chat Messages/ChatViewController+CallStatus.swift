7fc5e8283002ec82081f83f4d0060ad66371dab5
Peter Li <hl@mega.co.nz>
2020-07-13 16:38:39 +1200

    feat(chatroom): banner content sometimes empty

--- iMEGA/Chat Messages/ChatViewController+CallStatus.swift
+++ iMEGA/Chat Messages/ChatViewController+CallStatus.swift
@@ -53,15 +53,19 @@ extension ChatViewController {
     }
     
     func initTimerForCall(_ call: MEGAChatCall) {
+        initDuration = TimeInterval(call.duration  ?? 0)
         if !(timer?.isValid ?? false) {
             
             print(call.duration)
             let startTime = Date().timeIntervalSince1970
+            let time = Date().timeIntervalSince1970 - startTime + self.initDuration!
+            
+            self.setTopBannerButton(title: String(format: AMLocalizedString("Touch to return to call %@", "Message shown in a chat room for a group call in progress displaying the duration of the call"), NSString.mnz_string(fromTimeInterval: time)), color:  UIColor.mnz_turquoise(for: self.traitCollection))
             timer = Timer(timeInterval: 1, repeats: true, block: { (timer) in
                 if self.chatCall?.status == .reconnecting {
                     return;
                 }
-                let time = Date().timeIntervalSince1970 - (call.initialTimeStamp == 0 ? startTime : TimeInterval(call.initialTimeStamp))
+                let time = Date().timeIntervalSince1970 - startTime + self.initDuration!
 
                 self.setTopBannerButton(title: String(format: AMLocalizedString("Touch to return to call %@", "Message shown in a chat room for a group call in progress displaying the duration of the call"), NSString.mnz_string(fromTimeInterval: time)), color:  UIColor.mnz_turquoise(for: self.traitCollection))
             })
