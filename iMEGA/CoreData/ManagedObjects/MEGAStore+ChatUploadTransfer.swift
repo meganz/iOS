62f1d3211dc76ab52accf87084d9780eea5de513
Lancy <lf@mega.co.nz>
2020-07-14 13:57:05 +1200

    feat(chat messages assets post in order): Refactored the code as the SDK takes sometime prepare the transfer list and accessing the transfer list during the launching returns nil.
    #16277

--- iMEGA/CoreData/ManagedObjects/MEGAStore+ChatUploadTransfer.swift
+++ iMEGA/CoreData/ManagedObjects/MEGAStore+ChatUploadTransfer.swift
@@ -8,8 +8,9 @@ extension MEGAStore {
                                         allowDuplicateFilePath: Bool,
                                         context: NSManagedObjectContext) {
         if !allowDuplicateFilePath {
-            if let transfer = fetchChatUploadTransfer(filepath: filepath, chatRoomId: chatRoomId, transferTag: transferTag, context: context) {
-                MEGALogError("ChatUploadTransfer object already exsists \(transfer.filepath)")
+            let transfers = fetchChatUploadTransfers(filepath: filepath, chatRoomId: chatRoomId, transferTag: transferTag, context: context)
+            if !transfers.isEmpty {
+                MEGALogError("ChatUploadTransfer object already exsists \(transfers.count)")
                 return
             }
         }
@@ -24,13 +25,15 @@ extension MEGAStore {
     }
     
     func updateChatUploadTransfer(filepath: String, chatRoomId: String, nodeHandle: String, transferTag: String, appData: String, context: NSManagedObjectContext) {
-        if let transfer = fetchChatUploadTransfer(filepath: filepath, chatRoomId: chatRoomId, transferTag: transferTag, context: context) {
+        let transfers = fetchChatUploadTransfers(filepath: filepath, chatRoomId: chatRoomId, transferTag: transferTag, context: context)
+        transfers.forEach { transfer in
             transfer.nodeHandle = nodeHandle
             transfer.appData = appData
-            MEGAStore.shareInstance()?.save(context)
-        } else {
+        }
+        MEGAStore.shareInstance()?.save(context)
+        
+        if transfers.isEmpty {
             MEGALogError("ChatUploadTransfer object does not exists")
-            return
         }
     }
     
@@ -69,7 +72,7 @@ extension MEGAStore {
         }
     }
     
-    func fetchChatUploadTransfer(filepath: String, chatRoomId: String, transferTag: String?, context: NSManagedObjectContext) -> ChatUploadTransfer? {
+    func fetchChatUploadTransfers(filepath: String, chatRoomId: String, transferTag: String?, context: NSManagedObjectContext) -> [ChatUploadTransfer] {
         let fetchRequest: NSFetchRequest<ChatUploadTransfer> = ChatUploadTransfer.fetchRequest()
         
         if let transferTag = transferTag {
@@ -79,10 +82,10 @@ extension MEGAStore {
         }
         
         do {
-            return try context.fetch(fetchRequest).first
+            return try context.fetch(fetchRequest)
         } catch let error as NSError {
             MEGALogError("Could not fetch ChatUploadTransfer object for path \(filepath) : \(error.localizedDescription)")
-            return nil
+            return []
         }
     }
     
