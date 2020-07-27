fd74fc18bd8fbafc5e2d1e060488b5fac1261071
Javier Santana Espejo <jse@mega.nz>
2020-07-27 18:06:33 +0200

    fix(Contacts): Incoming contact request of just deleted contact makes the app crash if it is on the background
    16538

--- iMEGA/Contacts/ContactsViewController.m
+++ iMEGA/Contacts/ContactsViewController.m
@@ -2045,8 +2045,12 @@
             for (NSIndexPath *indexPath in deleteContactsOnIndexPathsArray) {
                 [self.visibleUsersArray removeObjectAtIndex:indexPath.row];
                 
-                NSMutableArray *usersInSectionMutableArray = self.visibleUsersIndexedMutableArray[[self currentIndexedSection:indexPath.section]];
-                [usersInSectionMutableArray removeObjectAtIndex:indexPath.row];
+                if (indexPath.section == 0) {
+                    [self.recentlyAddedUsersArray removeObjectAtIndex:indexPath.row];
+                } else {
+                    NSMutableArray *usersInSectionMutableArray = self.visibleUsersIndexedMutableArray[[self currentIndexedSection:indexPath.section]];
+                    [usersInSectionMutableArray removeObjectAtIndex:indexPath.row];
+                }
             }
             [self.tableView deleteRowsAtIndexPaths:deleteContactsOnIndexPathsArray withRowAnimation:UITableViewRowAnimationAutomatic];
             
