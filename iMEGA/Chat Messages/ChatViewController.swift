55bf2db64048398e8cc9487e532ece2ca5e5f997
Peter Li <hl@mega.co.nz>
2020-07-31 23:46:58 +1200

    fix(chatroom): chatroom may not be able to update message when using back gesture

--- iMEGA/Chat Messages/ChatViewController.swift
+++ iMEGA/Chat Messages/ChatViewController.swift
@@ -250,8 +250,8 @@ class ChatViewController: MessagesViewController {
         loadDraft()
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
         saveDraft()
         MEGASdkManager.sharedMEGAChatSdk()?.remove(self as MEGAChatDelegate)
         MEGASdkManager.sharedMEGAChatSdk()?.remove(self as MEGAChatCallDelegate)
