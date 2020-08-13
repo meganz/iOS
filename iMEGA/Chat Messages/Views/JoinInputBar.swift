57930d7c13f67992c3745adf41a3cf14ce69baf5
Peter Li <hl@mega.co.nz>
2020-08-13 12:39:55 +1200

    feat(join input bar): add joining indicator

--- iMEGA/Chat Messages/Views/JoinInputBar.swift
+++ iMEGA/Chat Messages/Views/JoinInputBar.swift
@@ -6,11 +6,16 @@ class JoinInputBar: UIView {
     @IBOutlet weak var joinButton: UIButton!
     var buttonTappedHandler: ((UIButton) -> Void)?
 
+    @IBOutlet weak var joiningOrLeavingLabel: UILabel!
+    @IBOutlet weak var joiningOrLeavingView: UIView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     override func awakeFromNib() {
         super.awakeFromNib()
         
         let title = AMLocalizedString("Join", "Button text in public chat previews that allows the user to join the chat")
-        joinButton.setTitle(title, for: .normal)        
+        joinButton.setTitle(title, for: .normal)
+        indicator.startAnimating()
+        
     }
     
     @IBAction func joinButtonTapped(_ sender: UIButton) {
