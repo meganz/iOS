073c264a066b81ead9b4c4689c7a5d8aaef77365
Roberto Gómez Muñoz <rg@mega.nz>
2020-10-21 15:19:30 +0200

    4251: Chat settings quick access alert new design (#863)
    * feat(chat): Adapt chat settings quick access alert to the new design
    
    Apply chat settings quick access alert new design, create new action sheet action and cell
    
    4251
    
    * feat(chat): Adapt chat settings quick access alert to the new design
    
    Change ActionSheetSwitch Cell and define custom cell instead. Add detail label hide animation.
    
    4251
    
    * feat(chat): Adapt chat settings quick access alert to the new design
    
    Fix crash opening Do Not Disturb mode in iPad and new action sheet switch cell animation improve.
    
    4251
    
    * feat(chat): Adapt chat settings quick access alert to the new design
    
    Using turn off DnD completion instead of MegaRequest delegate
    
    4251
    
    * feat(chat): Adapt chat settings quick access alert to the new design
    
    Fixed some problems performing detail label animation.
    
    4251

---
+++ iMEGA/Utils/ViewControllers/CustomActionViewController/Cells/ActionSheetSwitchCell.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+final class ActionSheetSwitchCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    func configureCell(action: ActionSheetSwitchAction) {
+        titleLabel.text = action.title
+        
+        if let detail = action.detail {
+            detailLabel.text = detail
+            detailLabel.textColor = .mnz_gray848484()
+            detailLabel.isHidden = false
+            detailLabel.alpha = 1.0
+        } else {
+            performCellAnimation()
+        }
+        
+        accessoryView = action.switchView
+        
+        action.switchView?.addTarget(self, action: #selector(switchValueChange(sender:)), for: .valueChanged)
+    }
+    
+    @objc
+    func switchValueChange(sender: Any) {
+        guard let actionSwitch = sender as? UISwitch else { return }
+        
+        if !actionSwitch.isOn {
+            DispatchQueue.main.async {
+                self.performCellAnimation()
+            }
+        }
+    }
+    
+    private func performCellAnimation() {
+        detailLabel.alpha = 0.0
+        
+        UIView.animate(withDuration: 0.6,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.9,
+                       initialSpringVelocity: 1,
+                       options: [],
+                       animations: { [weak self] in
+                            self?.detailLabel.isHidden = true
+                            self?.stackView.layoutIfNeeded()
+                       },
+                       completion: nil)
+    }
+}
