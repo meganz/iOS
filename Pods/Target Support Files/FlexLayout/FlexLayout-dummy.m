2f9979d703b27799aa775dd7477a3df20ec5dff6
Peter Li <56808021+lhr000lhrmega@users.noreply.github.com>
2020-11-11 23:30:16 +1300

    build(pod): remove pod files (#922)

--- Pods/Target Support Files/FlexLayout/FlexLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FlexLayout : NSObject
-@end
-@implementation PodsDummy_FlexLayout
-@end
--- Pods/Target Support Files/ISEmojiView/ISEmojiView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ISEmojiView : NSObject
-@end
-@implementation PodsDummy_ISEmojiView
-@end
