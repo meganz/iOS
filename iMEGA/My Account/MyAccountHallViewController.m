b2ad6a61de631a2f2f2ec4c16a67c378d7010da2
Lancy <lf@mega.co.nz>
2020-12-01 17:18:17 +1300

    fix(New Home): Removed the title from My Account screen and also changed the text from ""

--- iMEGA/My Account/MyAccountHallViewController.m
+++ iMEGA/My Account/MyAccountHallViewController.m
@@ -236,11 +236,9 @@ typedef NS_ENUM(NSInteger, MyAccount) {
 - (void)configNavigationItem {
     if (MEGASdkManager.sharedMEGASdk.isBusinessAccount) {
         self.navigationItem.rightBarButtonItem = nil;
-        self.navigationItem.title = AMLocalizedString(@"myAccount", @"Title of the app section where you can see your account details");
         self.businessLabel.text = AMLocalizedString(@"Business", nil);
     } else {
         self.buyPROBarButtonItem.title = AMLocalizedString(@"upgrade", @"Caption of a button to upgrade the account to Pro status");
-        self.navigationItem.title = AMLocalizedString(@"myAccount", @"Title of the app section where you can see your account details");
         self.businessLabel.text = @"";
     }
 }
@@ -315,7 +313,7 @@ typedef NS_ENUM(NSInteger, MyAccount) {
     switch (indexPath.row) {
         case MyAccountStorage: {
             if (MEGASdkManager.sharedMEGASdk.isBusinessAccount) {
-                cell.sectionLabel.text = AMLocalizedString(@"Usage", @"Button title that goes to the section Usage where you can see how your MEGA space is used");
+                cell.sectionLabel.text = AMLocalizedString(@"Storage", @"Button title that goes to the section Usage where you can see how your MEGA space is used");
                 cell.storageLabel.text = AMLocalizedString(@"Storage", @"Label for any ‘Storage’ button, link, text, title, etc. - (String as short as possible).");
                 cell.transferLabel.text = AMLocalizedString(@"Transfer", nil);
                 MEGAAccountDetails *accountDetails = MEGASdkManager.sharedMEGASdk.mnz_accountDetails;
@@ -333,7 +331,7 @@ typedef NS_ENUM(NSInteger, MyAccount) {
                 cell.transferLabel.textColor = cell.transferUsedLabel.textColor = UIColor.systemGreenColor;
             } else {
                 cell.iconImageView.image = [UIImage imageNamed:@"icon-storage"].imageFlippedForRightToLeftLayoutDirection;
-                cell.sectionLabel.text = AMLocalizedString(@"Usage", @"Button title that goes to the section Usage where you can see how your MEGA space is used");
+                cell.sectionLabel.text = AMLocalizedString(@"Storage", @"Button title that goes to the section Usage where you can see how your MEGA space is used");
                 
                 if (MEGASdkManager.sharedMEGASdk.mnz_accountDetails) {
                     MEGAAccountDetails *accountDetails = MEGASdkManager.sharedMEGASdk.mnz_accountDetails;
