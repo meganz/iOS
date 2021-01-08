dda8d324fc2feed7d80595a963cf1acfa772dab4
Simon Wang <43154373+simonwng@users.noreply.github.com>
2021-01-08 21:23:01 +1100

    iOS-4291 Integrate Crashlytics into extensions (#1154)
    * task(extension): integrate Crashlytics into extensions
    
    IOS-4291
    
    * update Firebase/Crashlytics pod to latest version
    
    * remove test code
    
    * Remove crash test code
    
    * revert back to 5.5.1 version as the test of extension crashes on 5.5.2 is done

--- iMEGA/Extensions/MEGAWidget/MEGAWidget.swift
+++ iMEGA/Extensions/MEGAWidget/MEGAWidget.swift
@@ -1,9 +1,14 @@
 
 import WidgetKit
 import SwiftUI
+import Firebase
 
 @main
 struct MEGAWidgetsBundle: SwiftUI.WidgetBundle {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     @WidgetBundleBuilder
     var body: some Widget {
         ShortcutsWidget()
