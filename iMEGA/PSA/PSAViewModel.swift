b9e1200fea8cc790e11b57ecc6a7ffac4ff1dc81
Lancy <lf@mega.co.nz>
2021-01-14 11:39:54 +1300

    Avoid showing PSA if it was shown a hour back
    #IOS-4485

--- iMEGA/PSA/PSAViewModel.swift
+++ iMEGA/PSA/PSAViewModel.swift
@@ -10,20 +10,28 @@ final class PSAViewModel: NSObject, ViewModelType {
     private let useCase: PSAUseCase
     private var psaEntity: PSAEntity?
     
+    @PreferenceWrapper(key: .lastPSAShownTimestamp, defaultValue: -1.0)
+    private var lastPSAShownTimestampPreference: TimeInterval
+
     enum Command: CommandType {
         case configView(PSAEntity)
     }
     
     var invokeCommand: ((Command) -> Void)?
     
-    init(router: PSAViewRouter, useCase: PSAUseCase) {
+    init(router: PSAViewRouter,
+         useCase: PSAUseCase,
+         preferenceUseCase: PreferenceUseCaseProtocol = PreferenceUseCase.default) {
         self.router = router
         self.useCase = useCase
+        super.init()
+        $lastPSAShownTimestampPreference.useCase = preferenceUseCase
     }
     
     func dispatch(_ action: PSAViewAction) {
         switch action {
         case .onViewReady:
+            lastPSAShownTimestampPreference = Date().timeIntervalSince1970
             if let psaEntity = psaEntity {
                 invokeCommand?(.configView(psaEntity))
             }
@@ -32,6 +40,13 @@ final class PSAViewModel: NSObject, ViewModelType {
     }
     
     func shouldShowView(completion: @escaping ((Bool) -> Void)) {
+        // Avoid showing PSA if it is shown already within 1 hour (3600) time span.
+        guard (lastPSAShownTimestampPreference <= 0 || (Date().timeIntervalSince1970 - lastPSAShownTimestampPreference) >= 3600) else {
+            MEGALogDebug("The PSA is already shown \(Date().timeIntervalSince1970 - lastPSAShownTimestampPreference) seconds back")
+            completion(false)
+            return
+        }
+        
         useCase.getPSA { [weak self] result in
             switch result {
             case .success(let psaEntity):
