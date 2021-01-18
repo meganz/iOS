1dcfc46ae418a3fd3daab9f9ba88fd27688f05e7
Simon Wang <swg@mega.co.nz>
2021-01-18 15:54:28 +1100

    fix(crash): fix a EXC_BAD_ACCESS crash when a node object is released

--- iMEGA/Utils/Foundation/Debouncer.swift
+++ iMEGA/Utils/Foundation/Debouncer.swift
@@ -1,10 +1,8 @@
 import Foundation
 
 final class Debouncer {
-
-    private var delay: TimeInterval
-
-    private var dispatchQueue: DispatchQueue
+    private let delay: TimeInterval
+    private let dispatchQueue: DispatchQueue
 
     private var dispatchWork: DispatchWorkItem?
 
@@ -13,10 +11,8 @@ final class Debouncer {
         self.dispatchQueue = dispatchQueue
     }
 
-    fileprivate func execute(_ action: @escaping () -> Void) {
-        let dispatchWork = DispatchWorkItem {
-            action()
-        }
+    private func execute(_ action: @escaping () -> Void) {
+        let dispatchWork = DispatchWorkItem { action() }
         self.dispatchWork = dispatchWork
         dispatchQueue.asyncAfter(deadline: .now() + delay, execute: dispatchWork)
     }
