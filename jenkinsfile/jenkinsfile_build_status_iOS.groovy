2296e2749367539f1c1d62546d62a19a408eb659
Peter Li <56808021+lhr000lhrmega@users.noreply.github.com>
2021-01-28 11:17:04 +1300

    ci: pipeline add time limit (#1219)

--- jenkinsfile/jenkinsfile_build_status_iOS.groovy
+++ jenkinsfile/jenkinsfile_build_status_iOS.groovy
@@ -11,7 +11,9 @@ def injectEnvironments(Closure body) {
 
 pipeline {
    agent any
-
+   options {
+        timeout(time: 1, unit: 'HOURS') 
+   }
    stages {
       stage('Submodule update') {
          steps {
