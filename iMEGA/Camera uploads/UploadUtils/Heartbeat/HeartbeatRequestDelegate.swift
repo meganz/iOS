2b7c66a2b2688550a5c51912daf6d1540fbd93d0
Simon Wang <43154373+simonwng@users.noreply.github.com>
2021-01-27 11:51:24 +1100

    IOS-4582 create a generic swift request delegate fits background thread callbacks (#1211)
    * task(engineering): create a generic request delegate for swift code
    
    * revert back the file name

--- iMEGA/Camera uploads/UploadUtils/Heartbeat/HeartbeatRequestDelegate.swift
+++ iMEGA/Camera uploads/UploadUtils/Heartbeat/HeartbeatRequestDelegate.swift
@@ -9,12 +9,12 @@ extension MEGAError {
     }
 }
 
-typealias MEGARequestCompletion = (_ result: Result<MEGARequest, Error>) -> Void
+typealias HeartbeatRequestCompletion = (_ result: Result<MEGARequest, Error>) -> Void
 
 final class HeartbeatRequestDelegate: NSObject, MEGARequestDelegate {
-    private let completion: MEGARequestCompletion
+    private let completion: HeartbeatRequestCompletion
     
-    init(completion: @escaping MEGARequestCompletion) {
+    init(completion: @escaping HeartbeatRequestCompletion) {
         self.completion = completion
     }
     
@@ -26,4 +26,3 @@ final class HeartbeatRequestDelegate: NSObject, MEGARequestDelegate {
         }
     }
 }
-
