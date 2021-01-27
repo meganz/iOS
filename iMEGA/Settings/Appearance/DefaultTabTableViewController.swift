3aeffdf4e04eb0acd5a1a63b7a82a65c862d685a
nhMEGA <70303100+nhMEGA@users.noreply.github.com>
2021-01-27 18:08:44 +1300

    IOS-4537 Launch tab preference (#1193)
    * task(settings): rename Appearance to UserInterface
    
    * task(settings): launch tab preference
    
    * task(l10n): update change from others
    
    * task(settings): rename header text
    
    * refactor(settings): move tab func to Tab.swift
    
    * refactor(settings): final class
    
    * refactor(settings): rename variable
    
    * refactor(settings): register cell in storyboard
    
    * refactor(settings): use image name not literal
    
    * refactor(settings): use PreferenceWrapper
    
    * refactor: enum TabOption TabIcon
    
    * refactor(settings): improve enum
    
    * refactor(settings): improve Tab class
    
    * refactor(settings): use TabType not TabOption
    
    * refactor(settings): computed property

---
+++ iMEGA/Settings/Appearance/DefaultTabTableViewController.swift
@@ -0,0 +1,45 @@
+
+import UIKit
+
+final class DefaultTabTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = NSLocalizedString("Default Tab", comment: "Inside of Settings - User Interface, there is a view on which you can change the default tab when launch the app.")
+        tableView.separatorColor = UIColor.mnz_separator(for: traitCollection)
+        tableView.backgroundColor = UIColor.mnz_backgroundGrouped(for: traitCollection)
+    }
+
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TabManager.avaliableTabs
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.reuseIdentifier, for: indexPath)
+        
+        if let tabType = TabType(rawValue: indexPath.row) {
+            let tab = Tab(tabType: tabType)
+            cell.imageView?.image = tab.icon?.byTintColor(UIColor.mnz_primaryGray(for: traitCollection))
+            let title = tab.title
+            cell.textLabel?.text = NSLocalizedString(title, comment: title)
+        }
+        cell.accessoryView = UIImageView(image: UIImage(named: "turquoise_checkmark"))
+        cell.backgroundColor = UIColor.mnz_secondaryBackgroundGrouped(traitCollection)
+        cell.accessoryView?.isHidden = TabManager.getPreferenceTab().tabType.rawValue != indexPath.row
+        
+        return cell;
+    }
+
+    // MARK: - UITableViewDelegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if let tabType = TabType(rawValue: indexPath.row) {
+            TabManager.setPreferenceTab(tab: Tab(tabType: tabType))
+            tableView.reloadData()
+        }
+    }
+}
