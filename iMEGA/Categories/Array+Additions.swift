6aa279786603deb3420918179e338cd973b021f4
Peter Li <56808021+lhr000lhrmega@users.noreply.github.com>
2021-01-27 16:57:57 +1300

    fix(chatroom): crash in chatroom and provide a dummy message instead (#1212)
    * fix(chatroom): crash in chatroom and provide a dummy message instead
    
    * Update iMEGA/Categories/Array+Additions.swift
    
    Co-authored-by: Simon Wang <43154373+simonwng@users.noreply.github.com>
    
    Co-authored-by: Simon Wang <43154373+simonwng@users.noreply.github.com>

--- iMEGA/Categories/Array+Additions.swift
+++ iMEGA/Categories/Array+Additions.swift
@@ -4,4 +4,8 @@ extension Array {
             Array(self[$0 ..< Swift.min($0 + size, count)])
         }
     }
+    
+    subscript (safe index: Int) -> Element? {
+        return self.indices ~= index ? self[index] : nil
+    }
 }
