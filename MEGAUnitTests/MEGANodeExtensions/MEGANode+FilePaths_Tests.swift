7138d9b921bd6fdd1309cddf78c626bbd697dc8f
Lancy <lf@mega.co.nz>
2021-02-05 09:53:52 +1300

    feat(File path): Handled PR comments.
    #IOS-4400

--- MEGAUnitTests/MEGANodeExtensions/MEGANode+FilePaths_Tests.swift
+++ MEGAUnitTests/MEGANodeExtensions/MEGANode+FilePaths_Tests.swift
@@ -1,111 +1,115 @@
 import XCTest
 @testable import MEGA
 
-class MEGANode_FilePaths_Tests: XCTestCase {
-
-    func testFilePath_withGreaterThanDelimeter() {
+final class MEGANode_FilePaths_Tests: XCTestCase {
+    
+    func testFilePath_withGreaterThanDelimeter() throws {
         let nodes = sampleNodes()
-        let sdk = testSDK(withNodes: nodes)
+        let sdk = MockSDK(withNodes: nodes)
+        let leafNode = try XCTUnwrap(nodes.last)
         
-        let path = nodes.last!.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: false)
+        let path = leafNode.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: false)
         XCTAssertEqual(path.components(separatedBy: ">").count, 5)
     }
     
-    func testFilePath_withForwardSlashDelimeter() {
+    func testFilePath_withForwardSlashDelimeter() throws {
         let nodes = sampleNodes()
-        let sdk = testSDK(withNodes: nodes)
-        
-        let path = nodes.last!.filePath(withDelimeter: " / ", sdk: sdk, includeRootFolderName: true, excludeFileName: false)
+        let sdk = MockSDK(withNodes: nodes)
+        let leafNode = try XCTUnwrap(nodes.last)
+
+        let path = leafNode.filePath(withDelimeter: " / ", sdk: sdk, includeRootFolderName: true, excludeFileName: false)
         XCTAssertEqual(path.components(separatedBy: "/").count, 5)
     }
     
-    func testFilePath_includingRootFolderNameAndIncludingFileName() {
+    func testFilePath_includingRootFolderNameAndIncludingFileName() throws {
         let nodes = sampleNodes()
-        let sdk = testSDK(withNodes: nodes)
-        
-        let path = nodes.last!.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: false)
+        let sdk = MockSDK(withNodes: nodes)
+        let leafNode = try XCTUnwrap(nodes.last)
+
+        let path = leafNode.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: false)
         XCTAssertEqual(path, "Test1 > Test2 > Test3 > Test4 > Test5")
     }
     
-    func testFilePath_excludingRootFolderNameAndIncludingFileName() {
+    func testFilePath_excludingRootFolderNameAndIncludingFileName() throws {
         let nodes = sampleNodes()
-        let sdk = testSDK(withNodes: nodes)
-        
-        let path = nodes.last!.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: false, excludeFileName: false)
+        let sdk = MockSDK(withNodes: nodes)
+        let leafNode = try XCTUnwrap(nodes.last)
+
+        let path = leafNode.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: false, excludeFileName: false)
         XCTAssertEqual(path, "Test2 > Test3 > Test4 > Test5")
     }
     
-    func testFilePath_includingRootFolderNameAndExcludingFileName() {
+    func testFilePath_includingRootFolderNameAndExcludingFileName() throws {
         let nodes = sampleNodes()
-        let sdk = testSDK(withNodes: nodes)
-        
-        let path = nodes.last!.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: true)
+        let sdk = MockSDK(withNodes: nodes)
+        let leafNode = try XCTUnwrap(nodes.last)
+
+        let path = leafNode.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: true)
         XCTAssertEqual(path, "Test1 > Test2 > Test3 > Test4")
     }
     
-    func testFilePath_excludingRootFolderNameAndExcludingFileName() {
+    func testFilePath_excludingRootFolderNameAndExcludingFileName() throws {
         let nodes = sampleNodes()
-        let sdk = testSDK(withNodes: nodes)
-        
-        let path = nodes.last!.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: false, excludeFileName: true)
+        let sdk = MockSDK(withNodes: nodes)
+        let leafNode = try XCTUnwrap(nodes.last)
+
+        let path = leafNode.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: false, excludeFileName: true)
         XCTAssertEqual(path, "Test2 > Test3 > Test4")
     }
     
     func testFilePath_withNoParentFolderInlcudingRootFolderNameAndIncludingFileName() {
-        let node = Node(name: "Test1", handle: 1, parentHandle: -1)
-        let sdk = testSDK(withNodes: [node])
+        let node = MockNode(name: "Test1", handle: 1, parentHandle: -1)
+        let sdk = MockSDK(withNodes: [node])
         
         let path = node.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: false)
         XCTAssertEqual(path, "Test1")
     }
     
     func testFilePath_withNoParentFolderExcludingRootFolderNameAndExcludingFileName() {
-        let node = Node(name: "Test1", handle: 1, parentHandle: -1)
-        let sdk = testSDK(withNodes: [node])
+        let node = MockNode(name: "Test1", handle: 1, parentHandle: -1)
+        let sdk = MockSDK(withNodes: [node])
         
         let path = node.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: false, excludeFileName: true)
         XCTAssertEqual(path, "")
     }
     
     func testFilePath_withNoParentFolderIncludingRootFolderNameAndExcludingFileName() {
-        let node = Node(name: "Test1", handle: 1, parentHandle: -1)
-        let sdk = testSDK(withNodes: [node])
+        let node = MockNode(name: "Test1", handle: 1, parentHandle: -1)
+        let sdk = MockSDK(withNodes: [node])
         
         let path = node.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: true)
         XCTAssertEqual(path, "")
     }
     
     func testFilePath_withNoParentFolderExcludingRootFolderNameAndIncludingFileName() {
-        let node = Node(name: "Test1", handle: 1, parentHandle: -1)
-        let sdk = testSDK(withNodes: [node])
+        let node = MockNode(name: "Test1", handle: 1, parentHandle: -1)
+        let sdk = MockSDK(withNodes: [node])
         
         let path = node.filePath(withDelimeter: " > ", sdk: sdk, includeRootFolderName: true, excludeFileName: true)
         XCTAssertEqual(path, "")
     }
     
-    private func sampleNodes() -> [Node] {
-        let node1 = Node(name: "Test1", handle: 1, parentHandle: -1)
-        let node2 = Node(name: "Test2", handle: 2, parentHandle: 1)
-        let node3 = Node(name: "Test3", handle: 3, parentHandle: 2)
-        let node4 = Node(name: "Test4", handle: 4, parentHandle: 3)
-        let node5 = Node(name: "Test5", handle: 5, parentHandle: 4)
+    private func sampleNodes() -> [MockNode] {
+        let node1 = MockNode(name: "Test1", handle: 1, parentHandle: -1)
+        let node2 = MockNode(name: "Test2", handle: 2, parentHandle: 1)
+        let node3 = MockNode(name: "Test3", handle: 3, parentHandle: 2)
+        let node4 = MockNode(name: "Test4", handle: 4, parentHandle: 3)
+        let node5 = MockNode(name: "Test5", handle: 5, parentHandle: 4)
         return [node1, node2, node3, node4, node5]
     }
-    
-    private func testSDK(withNodes nodes: [Node]) -> TestSDK {
-        let testSDK = TestSDK()
-        testSDK.nodes = nodes
-        return testSDK
-    }
-
 }
 
 
-class TestSDK: MEGASdk {
-    var nodes: [Node] = []
+final class MockSDK: MEGASdk {
+    let nodes: [MockNode]
+    
+    init(withNodes nodes: [MockNode]) {
+        self.nodes = nodes
+        super.init()
+    }
     
     override func parentNode(for node: MEGANode) -> MEGANode? {
-        guard let inputNode = node as? Node else {
+        guard let inputNode = node as? MockNode else {
             return nil
         }
         
@@ -113,7 +117,7 @@ class TestSDK: MEGASdk {
     }
 }
 
-class Node: MEGANode {
+final class MockNode: MEGANode {
     let _name: String
     let _handle: Int
     let _parentHandle: Int
