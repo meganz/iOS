74707047ee198b312313e64beb384e7b59718752
Lancy <lf@mega.co.nz>
2021-02-15 15:14:22 +1300

    fix(chat): Possible fix for the crash in IndexPath+Additions.swift
    IOS-4642

--- iMEGA/Categories/IndexPath+Additions.swift
+++ iMEGA/Categories/IndexPath+Additions.swift
@@ -1,7 +1,7 @@
 
 extension IndexPath {
     var previousSectionIndexPath: IndexPath? {
-        guard section > 0 else { return nil }
+        guard !isEmpty, section > 0 else { return nil }
 
         let previousIndexPath = IndexPath(item: item, section: section - 1)
         return previousIndexPath
