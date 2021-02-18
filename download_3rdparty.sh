37568908c3eaa01148d5d8a6b657e2a23d03adea
Peter Li <56808021+lhr000lhrmega@users.noreply.github.com>
2021-02-19 09:38:10 +1300

    ci(jenkinsfile): add clean submodule command (#1283)

--- download_3rdparty.sh
+++ download_3rdparty.sh
@@ -9,8 +9,8 @@ fi
 file="ZZlSzKCQ"
 key="pCnK7UKbV3bjZvnRxkHkudcHGQcoarEE8bNlN2WDGfM"
 fileUrl="https://mega.nz/#!${file}!${key}"
-filePath="./iMEGA/Vendor/SDK/bindings/ios/3rdparty/${file}"
+filePath="./download_3rdparty/${file}"
 
-mkdir $filePath
+mkdir -p $filePath
 mega-get $fileUrl $filePath
 unzip -o ${filePath}/3rdparty.zip -d ./iMEGA/Vendor/SDK/bindings/ios/3rdparty/
