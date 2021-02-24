57730eaa2da5585b813c83d963aae8d2fe9bc481
nhMEGA <70303100+nhMEGA@users.noreply.github.com>
2021-02-24 15:18:19 +1300

    task(passcode): add logout button (#1303)

--- iMEGA/Utils/MEGAAVViewController.m
+++ iMEGA/Utils/MEGAAVViewController.m
@@ -142,8 +142,8 @@ static const NSUInteger MIN_SECOND = 10; // Save only where the users were playi
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"presentPasscodeLater"] && [LTHPasscodeViewController doesPasscodeExist]) {
         [[LTHPasscodeViewController sharedUser] showLockScreenOver:UIApplication.mnz_presentingViewController.view
                                                      withAnimation:YES
-                                                        withLogout:NO
-                                                    andLogoutTitle:nil];
+                                                        withLogout:YES
+                                                    andLogoutTitle:NSLocalizedString(@"logoutLabel", nil)];
     }
 }
 
