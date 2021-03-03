fe072018587886770d3bc7992ec76579856eaffa
Carlos Martín <cma@mega.nz>
2021-03-03 11:01:57 +0100

    fix(widget): avoid crash unwrapping optional values (#1334)
    4701

--- MEGADomain/Entity/OfflineFiles/OfflineFileEntity.swift
+++ MEGADomain/Entity/OfflineFiles/OfflineFileEntity.swift
@@ -2,7 +2,7 @@
 struct OfflineFileEntity: Equatable {
     let base64Handle: String
     let localPath: String
-    let parentBase64Handle: String
-    let fingerprint: String
-    let timestamp: Date
+    let parentBase64Handle: String?
+    let fingerprint: String?
+    let timestamp: Date?
 }
