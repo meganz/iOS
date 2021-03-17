8c0327d2d7727547f52f9368f1c6bee3c513dbd0
Ning Huang <nh@mega.co.nz>
2021-03-17 21:22:06 +0000

    IOS-4768 qrcode display issue

--- iMEGA/Contacts/Invite Contact/InviteContactViewController.swift
+++ iMEGA/Contacts/Invite Contact/InviteContactViewController.swift
@@ -105,6 +105,7 @@ class InviteContactViewController: UIViewController {
     @IBAction func scanQrCodeButtonTapped(_ sender: Any) {
         guard let contactLinkVC = UIStoryboard(name: "ContactLinkQR", bundle: nil).instantiateViewController(withIdentifier: "ContactLinkQRViewControllerID") as? ContactLinkQRViewController  else { return }
         contactLinkVC.scanCode = true
+        contactLinkVC.modalPresentationStyle = .fullScreen
         present(contactLinkVC, animated: true, completion: nil)
     }
 
