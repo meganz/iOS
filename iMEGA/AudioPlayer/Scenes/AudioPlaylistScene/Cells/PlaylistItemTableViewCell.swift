6b6e245f7be2a8add2f1d1aeb06c55005f795ad9
Roberto Gómez <rg@mega.nz>
2021-03-22 21:36:59 +0000

    fix(ap): Update tableview cell edit control image in the main thread to avoid refresh problem
    T1410390

--- iMEGA/AudioPlayer/Scenes/AudioPlaylistScene/Cells/PlaylistItemTableViewCell.swift
+++ iMEGA/AudioPlayer/Scenes/AudioPlaylistScene/Cells/PlaylistItemTableViewCell.swift
@@ -7,47 +7,16 @@ final class PlaylistItemTableViewCell: UITableViewCell {
     @IBOutlet weak var artistLabel: UILabel!
 
     var item: AudioPlayerItem?
-    
-    override var isSelected: Bool {
-        didSet {
-            setEditControlImage()
-        }
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        setEditControlImage()
-    }
-    
+ 
     override func prepareForReuse() {
         super.prepareForReuse()
         
         thumbnailImageView.image = nil
     }
     
-    // MARK: - Private functions
-    private func setSelectionImage() {
-        guard let editControlClass = NSClassFromString("UITableViewCellEditControl") else { return }
-        
-        let imageView = subviews.first(where: { $0.isMember(of: editControlClass) })?.subviews.first(where: { $0.isKind(of: UIImageView.self)}) as? UIImageView
-        imageView?.image = isSelected ? UIImage(named: "checkBoxSelected") : UIImage(named: "checkBoxUnselected")
-    }
-
-    private func setEditControlImage() {
-        guard let tableViewCellEditControlClass = NSClassFromString("UITableViewCellEditControl") else { return }
-        
-        subviews
-            .filter { $0.isMember(of: tableViewCellEditControlClass) }
-            .forEach {
-                let img = $0.subviews.filter { $0.isKind(of: UIImageView.self) }.first as? UIImageView
-                img?.image = isSelected ? UIImage(named: "checkBoxSelected") : UIImage(named: "checkBoxUnselected")
-            }
-    }
-    
     // MARK: - Public functions
     func configure(item: AudioPlayerItem?) {
         self.item = item
-        
         titleLabel.text = item?.name
         artistLabel.text = item?.artist ?? ""
         
