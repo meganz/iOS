9f60634516ddf5cc99926d0e48845a98fb019f77
Javier Santana Espejo <jse@mega.nz>
2021-05-26 15:22:02 +0000

    When Recents are hidden, put the 'Show activity' button under the description of the empty state
    - Adapt the empty state dynamically to the sliding events of the Home view.

--- iMEGA/Utils/Views/EmptyState/EmptyStateView.h
+++ iMEGA/Utils/Views/EmptyState/EmptyStateView.h
@@ -7,6 +7,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (UIView *)initWithImage:(nullable UIImage *)image title:(nullable NSString *)title description:(nullable NSString *)description buttonTitle:(nullable NSString *)buttonTitle;
 
+- (UIView *)initForHomeWithImage:(nullable UIImage *)image title:(nullable NSString *)title description:(nullable NSString *)description buttonTitle:(nullable NSString *)buttonTitle;
+
+@property (nullable, weak, nonatomic) IBOutlet UIButton *descriptionButton;
 @property (nullable, weak, nonatomic) IBOutlet UIButton *button;
 
 @end
