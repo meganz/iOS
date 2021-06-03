7b588382e6b2d2aa3bcf22e8830bd7d6e45e3dd9
Ning Huang <nh@mega.co.nz>
2021-06-03 01:32:10 +0000

    IOS-5037: achievement, change 50GB to 20 GB

--- iMEGA/My Account/Achievements/AchievementsTableViewCell.h
+++ iMEGA/My Account/Achievements/AchievementsTableViewCell.h
@@ -4,9 +4,6 @@
 @property (weak, nonatomic) IBOutlet UIView *storageQuotaRewardView;
 @property (weak, nonatomic) IBOutlet UILabel *storageQuotaRewardLabel;
 
-@property (weak, nonatomic) IBOutlet UIView *transferQuotaRewardView;
-@property (weak, nonatomic) IBOutlet UILabel *transferQuotaRewardLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *subtitleLabel;
 
