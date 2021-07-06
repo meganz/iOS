4be746fdcfb90e1fee8ffbe099b4d5dd6fab3aed
Lancy <lf@mega.co.nz>
2021-07-06 15:08:50 +1200

    Fixed: T1600921 There should be a dismiss button for the dialog at the top right corner of the dialog

--- iMEGA/CallScene/ParticipantsLayout/OptionsMenu/MeetingOptionsMenuViewController.swift
+++ iMEGA/CallScene/ParticipantsLayout/OptionsMenu/MeetingOptionsMenuViewController.swift
@@ -40,4 +40,8 @@ class MeetingOptionsMenuViewController: ActionSheetViewController, ViewType {
             self.actions = actions
         }
     }
+    
+    func popoverPresentationControllerDidDismissPopover(_ popoverPresentationController: UIPopoverPresentationController) {
+        viewModel?.dispatch(.dismiss)
+    }
 }
