420b80f018663bafda1aadbf2b719cde52762bd4
Lancy <lf@mega.co.nz>
2021-07-24 20:40:00 +1200

    Meetings: First time login - Avatar image does not show nickname first name when calling.

--- iMEGA/CoreData/MEGAStore+User.swift
+++ iMEGA/CoreData/MEGAStore+User.swift
@@ -8,6 +8,17 @@ extension MEGAStore {
             for name in names {
                 if let user = fetchUser(withUserHandle: name.handle, context: context), user.nickname != name.nickname {
                     user.nickname = name.nickname
+                } else { // user does not exsist in database yet. Delegate the task to main context
+                    let handle = name.handle
+                    let nickname = name.nickname
+                    DispatchQueue.main.async {
+                        if let user = self.fetchUser(withUserHandle: handle) {
+                            user.nickname = nickname
+                            self.save(self.stack.viewContext)
+                        } else {
+                            self.insertUser(withUserHandle: handle, firstname: nil, lastname: nil, nickname: nickname, email: nil)
+                        }
+                    }
                 }
             }
             
