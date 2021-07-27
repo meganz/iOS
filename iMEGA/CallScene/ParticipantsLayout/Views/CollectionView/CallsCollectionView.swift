91e006d4392de48a598632d58fde6b4d99fbd6a9
Carlos Martin Acera <cma@mega.nz>
2021-07-27 17:16:23 +0200

    T1701018 Fix video flickering issue for meetings with several participants

--- iMEGA/CallScene/ParticipantsLayout/Views/CollectionView/CallsCollectionView.swift
+++ iMEGA/CallScene/ParticipantsLayout/Views/CollectionView/CallsCollectionView.swift
@@ -98,6 +98,12 @@ extension CallsCollectionView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
         guard let cell = cell as? CallParticipantCell, let participant = cell.participant, let image = avatars[participant.participantId] else { return }
         cell.setAvatar(image: image)
+        participant.videoDataDelegate = cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? CallParticipantCell, let participant = cell.participant else { return }
+        participant.videoDataDelegate = nil
     }
 }
 
