5a482f46a1d2cf87682b76f520e3a48eef1b6d06
Roberto Gómez <rg@mega.nz>
2021-08-04 15:16:40 +0000

    IOS-5093. Dynanmic Type - Verify Email

--- iMEGA/Utils/ViewControllers/VerifyEmailViewcontroller/VerifyEmailViewController.swift
+++ iMEGA/Utils/ViewControllers/VerifyEmailViewcontroller/VerifyEmailViewController.swift
@@ -30,6 +30,11 @@ class VerifyEmailViewController: UIViewController {
         super.traitCollectionDidChange(previousTraitCollection)
         
         updateAppearance()
+        
+        if traitCollection.preferredContentSizeCategory != previousTraitCollection?.preferredContentSizeCategory {
+            localizeLabels()
+            boldenText()
+        }
     }
 
     override func viewDidLayoutSubviews() {
@@ -77,8 +82,8 @@ class VerifyEmailViewController: UIViewController {
         let bottomStringComponents = bottomString.components(separatedBy: "[/S]")
         guard let textToBolden = bottomStringComponents.first, let textRegular = bottomStringComponents.last else { return }
 
-        let attributtedString = NSMutableAttributedString(string: textToBolden, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .semibold)])
-        let regularlString = NSAttributedString(string: textRegular, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .regular)])
+        let attributtedString = NSMutableAttributedString(string: textToBolden, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(style: .callout, weight: .semibold)])
+        let regularlString = NSAttributedString(string: textRegular, attributes: [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .callout)])
         attributtedString.append(regularlString)
 
         bottomDescriptionLabel.attributedText = attributtedString
