2f95d3da68e61a63cea8a783145f8d9040f291fe
Roberto Gómez <rg@mega.nz>
2021-08-11 12:09:37 +0000

    IOS-5085. Dynamic Type - Upgrade Account

---
+++ iMEGA/My Account/Upgrade/ProductDetailViewController+Additions.swift
@@ -0,0 +1,10 @@
+
+extension ProductDetailViewController {
+    
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.sizeHeaderToFit()
+        tableView.sizeFooterToFit()
+    }
+}
--- iMEGA/My Account/Upgrade/UpgradeTableViewController.h
+++ iMEGA/My Account/Upgrade/UpgradeTableViewController.h
@@ -2,6 +2,7 @@
 
 @interface UpgradeTableViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, getter=isChoosingTheAccountType) BOOL chooseAccountType;
 
 @end
