a4d217b9e73ad24cb5fff2858e853d2282b785f3
Lancy <lf@mega.co.nz>
2021-09-02 11:21:20 +1200

    Meetings: Fixed the unit tests and PR comments handled.

--- MEGAUnitTests/Meeting/Mocks/MockUserUseCase.swift
+++ MEGAUnitTests/Meeting/Mocks/MockUserUseCase.swift
@@ -4,12 +4,14 @@ struct MockUserUseCase: UserUseCaseProtocol {
     let handle: UInt64
     var isLoggedIn: Bool
     var isGuest: Bool
+    var userSDKEntity = UserSDKEntity(email: "", handle: 100, base64Handle: nil, change: nil, contact: nil)
     
     var myHandle: UInt64? {
         return handle
     }
     
     func user(withHandle handle: UInt64) -> UserSDKEntity? {
-        return UserSDKEntity(email: "", handle: handle, base64Handle: nil, change: nil, contact: nil)
+        guard userSDKEntity.handle == handle else { return nil }
+        return userSDKEntity
     }
 }
