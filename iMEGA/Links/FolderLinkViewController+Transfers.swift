1db0ee29855f0b6260198f976fc997099d52715e
Roberto Gómez <rg@mega.nz>
2021-09-06 12:32:31 +0000

    T1797517. App is getting crashed when the user streams a playable node from a folder link

--- iMEGA/Links/FolderLinkViewController+Transfers.swift
+++ iMEGA/Links/FolderLinkViewController+Transfers.swift
@@ -3,28 +3,36 @@ import Foundation
 extension FolderLinkViewController: MEGATransferDelegate {
     
     public func onTransferStart(_ api: MEGASdk, transfer: MEGATransfer) {
-        guard !transfer.isStreamingTransfer,
-              transfer.type == .download,
-              let node = transfer.node(),
-              nodesArray.contains(node) else { return }
-        
-        didDownloadTransferStart(node)
+        if let node = nodeFromDownload(transfer: transfer) {
+            didDownloadTransferStart(node)
+        }
     }
     
     public func onTransferUpdate(_ api: MEGASdk, transfer: MEGATransfer) {
-        guard !transfer.isStreamingTransfer,
-              transfer.type == .download,
-              let node = transfer.node(),
-              nodesArray.contains(node) else { return }
-        didDownloadTransferUpdated(node, transferredBytes: transfer.transferredBytes, totalBytes: transfer.totalBytes, speed: transfer.speed)
+        if let node = nodeFromDownload(transfer: transfer) {
+            didDownloadTransferUpdated(node, transferredBytes: transfer.transferredBytes, totalBytes: transfer.totalBytes, speed: transfer.speed)
+        }
     }
     
     public func onTransferFinish(_ api: MEGASdk, transfer: MEGATransfer, error: MEGAError) {
-        guard transfer.type == .download,
-              let node = transfer.node(),
-              nodesArray.contains(node) else { return }
-        
-        didDownloadTransferFinish(node)
+        if let node = nodeFromDownload(transfer: transfer) {
+            didDownloadTransferFinish(node)
+        }
+    }
+    
+    private func nodeFromDownload(transfer: MEGATransfer) -> MEGANode? {
+        guard isOffline(transfer: transfer),
+              isFromFolderLink(node: transfer.node()) else { return nil }
+        return transfer.node()
+    }
+    
+    private func isOffline(transfer: MEGATransfer) -> Bool {
+        !transfer.isStreamingTransfer && transfer.type == .download
+    }
+    
+    private func isFromFolderLink(node: MEGANode?) -> Bool {
+        guard let node = node else { return false }
+        return nodesArray.contains(node)
     }
 }
 
