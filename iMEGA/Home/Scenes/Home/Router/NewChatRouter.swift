a4cc5d80e036bff0559a03d86c9d58b033764237
Javier Navarro <jn@mega.co.nz>
2021-09-28 13:59:35 +0000

    IOS-5298: address swift warnings

--- iMEGA/Home/Scenes/Home/Router/NewChatRouter.swift
+++ iMEGA/Home/Scenes/Home/Router/NewChatRouter.swift
@@ -34,7 +34,7 @@ final class NewChatRouter {
     private func setupContactsSelection(for contactController: ContactsViewController) {
         contactController.userSelected = { [weak self] users in
             guard let self = self else { return }
-            guard let selectedUser: MEGAUser = users?.first as? MEGAUser else { return }
+            guard let selectedUser: MEGAUser = users?.first else { return }
             self.chatRoom(byUserHandle: selectedUser.handle) { chatRoom in
                 self.switchToChat(withChatID: chatRoom.chatId)
             }
