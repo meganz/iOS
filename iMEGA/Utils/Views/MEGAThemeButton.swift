c4539aa322c0de951ae4b1e20362a459cc17bf47
Roberto Gómez <rg@mega.nz>
2021-11-02 23:12:21 +1300

    IOS-5190. ODQ text improvements

--- iMEGA/Utils/Views/MEGAThemeButton.swift
+++ iMEGA/Utils/Views/MEGAThemeButton.swift
@@ -7,9 +7,40 @@ protocol ButtonBackgroundStateAware {
     func setBackgroundColor(_ color: UIColor, for state: UIControl.State)
 }
 
-final class MEGAThemeButton: UIButton, ButtonBackgroundStateAware {
-
+final class MEGAThemeButton: UIButton, ButtonBackgroundStateAware, DynamicTypeComponentProtocol {
+    
+    @IBInspectable var textStyle: String?
+    @IBInspectable var weight: String?
+    @IBInspectable var selectedTextStyle: String?
+    @IBInspectable var selectedWeight: String?
+    
     private(set) var statedColor: [UIControl.State: UIColor] = [:]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        observeContentSizeUpdates()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        observeContentSizeUpdates()
+    }
+    
+    deinit {
+        removeObserver()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        titleLabel?.adjustsFontForContentSizeCategory = true
+
+        applyFontSizes()
+    }
+
+    
 
     override var isHighlighted: Bool {
         didSet {
@@ -30,5 +61,24 @@ final class MEGAThemeButton: UIButton, ButtonBackgroundStateAware {
         if case .normal = state {
             backgroundColor = color
         }
+    }
+    
+    func applyFontSizes() {
+        if isSelected {
+            guard let textStyle = Font.TextStyle(rawValue: selectedTextStyle ?? ""),
+                  let weight = Font.Weight(rawValue: selectedWeight ?? "") else {
+                defaultSetup()
+                return
+            }
+            titleLabel?.font = Font(style: textStyle, weight: weight).value
+        } else {
+            defaultSetup()
+        }
+    }
+    
+    private func defaultSetup() {
+        guard let textStyle = Font.TextStyle(rawValue: textStyle ?? ""),
+              let weight = Font.Weight(rawValue: weight ?? "") else { return }
+        titleLabel?.font = Font(style: textStyle, weight: weight).value
     }
 }
