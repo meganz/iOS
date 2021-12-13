86c4abe72dc9a9b630dae97a7d30364f83be16fb
Carlos Martin Acera <cma@mega.nz>
2021-12-13 13:18:17 +0100

    Fix crash when accessing node.parent from ShareExtension
    IOS-5616

--- iMEGA/Categories/MEGANode+Backup.swift
+++ iMEGA/Categories/MEGANode+Backup.swift
@@ -2,8 +2,8 @@
 extension MEGANode {
     private func isBackupChildNode() -> Bool {
         var currentNode: MEGANode? = self
-        while let node = currentNode, MEGASdkManager.sharedMEGASdk().parentNode(for: node) != nil {
-            if BackupRootNodeAccess.shared.isTargetNode(for: node.parent) && node.deviceId?.isEmpty == false {
+        while let node = currentNode, let parent = MEGASdkManager.sharedMEGASdk().parentNode(for: node) {
+            if BackupRootNodeAccess.shared.isTargetNode(for: parent) && node.deviceId?.isEmpty == false {
                 return true
             }
             currentNode = MEGASdkManager.sharedMEGASdk().parentNode(for: node)
