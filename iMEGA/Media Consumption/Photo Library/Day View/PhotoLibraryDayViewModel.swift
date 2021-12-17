7cd2d4305cc3808ed3b05cb60e2f4a52c0926210
Simon Wang <swg@mega.co.nz>
2021-12-17 14:43:14 +1300

    Cherry-pick Fix blank thumbnail test case

--- iMEGA/Media Consumption/Photo Library/Day View/PhotoLibraryDayViewModel.swift
+++ iMEGA/Media Consumption/Photo Library/Day View/PhotoLibraryDayViewModel.swift
@@ -1,16 +1,8 @@
 import Foundation
 
 final class PhotoLibraryDayViewModel: PhotoLibraryModeViewModel<PhotosByDay> {
-    override var position: PhotoScrollPosition {
-        if let date = libraryViewModel.currentPosition {
-            return date.removeTimestamp()
-        } else {
-            return super.position
-        }
-    }
-    
     func didTapDayCard(_ photoByDay: PhotosByDay) {
-        libraryViewModel.currentPosition = photoByDay.coverPhoto?.createTime
+        libraryViewModel.currentPosition = photoByDay.position
         libraryViewModel.selectedMode = .all
     }
 }
