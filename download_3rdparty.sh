40d1cf48c0e001dd716e5629b9d9c8c52543375e
Lancy Norbert Fernandes <lf@mega.co.nz>
2021-12-23 14:06:33 +1300

    CI-Update the link in 3rd party script

--- download_3rdparty.sh
+++ download_3rdparty.sh
@@ -8,18 +8,12 @@ if ! type "megacmd" > /dev/null; then
 	exit 1
 fi
 
-file="9JkihTSQ"
-key="os6Y7slroj3lUeYgzec5VdAP6PjzTzu1oa-bq8yzcAw"
+file="EYECGBDQ"
+key="2iFuoLVTqzXA2ARoNJr3tgM0eAlXoT_ce0skKycEF98"
 fileUrl="https://mega.nz/#!${file}!${key}"
 filePath="./download_3rdparty/${file}"
 
-echo "create directory started $filePath"
 mkdir -p $filePath
-echo "create directory completed"
-echo "Downloading 3rd party libraries from $fileUrl to file path $filePath"
 mega-get $fileUrl $filePath 
-echo "Downloading 3rd party libraries complete"
-echo "Unzip 3rd party libraries start"
 unzip -o ${filePath}/3rdparty.zip -d ./iMEGA/Vendor/SDK/bindings/ios/3rdparty/
-echo "Unzip 3rd party libraries complete"
 exit 0
