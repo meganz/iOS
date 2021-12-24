2e0b7463054accdd573c48440144a4e69cef51a9
Simon Wang <swg@mega.co.nz>
2021-12-24 15:23:13 +1300

    CC-197: Fix the bug where new account could not create CU node

--- iMEGA/Utils/NodeAccess/Backups/BackupRootNodeAccess.swift
+++ iMEGA/Utils/NodeAccess/Backups/BackupRootNodeAccess.swift
@@ -1,17 +1,21 @@
 import Foundation
 
 final class BackupRootNodeAccess: NodeAccess {
-    @objc static let shared = BackupRootNodeAccess(configuration:
-                                                            NodeAccessConfiguration(updateInMemoryNotificationName: .MEGABackupRootFolderUpdatedInMemory,
-                                                                                    updateInRemoteNotificationName: .MEGABackupRootFolderUpdatedInRemote,
-                                                                                    loadNodeRequest: MEGASdkManager.sharedMEGASdk().getMyBackupsFolder))
+    @objc static let shared = BackupRootNodeAccess(
+        configuration:
+            NodeAccessConfiguration(
+                updateInMemoryNotificationName: .MEGABackupRootFolderUpdatedInMemory,
+                updateInRemoteNotificationName: .MEGABackupRootFolderUpdatedInRemote,
+                loadNodeRequest: MEGASdkManager.sharedMEGASdk().getMyBackupsFolder
+            )
+    )
     
     override init(configuration: NodeAccessConfiguration) {
         super.init(configuration: configuration)
         
         NotificationCenter.default.addObserver(self, selector: #selector(didReceiveNodesCurrentNotification), name: .MEGANodesCurrent, object: nil)
     }
-         
+    
     @objc func didReceiveNodesCurrentNotification() {
         BackupRootNodeAccess.shared.loadNode()
     }
--- iMEGA/Utils/NodeAccess/Camera Uploads/CameraUploadNodeAccess.swift
+++ iMEGA/Utils/NodeAccess/Camera Uploads/CameraUploadNodeAccess.swift
@@ -1,12 +1,15 @@
 import Foundation
 
 final class CameraUploadNodeAccess: NodeAccess {
-    @objc static let shared = CameraUploadNodeAccess(configuration:
-                                                        NodeAccessConfiguration(autoCreate: { CameraUploadManager.isCameraUploadEnabled },
-                                                                                updateInMemoryNotificationName: .MEGACameraUploadTargetFolderUpdatedInMemory,
-                                                                                updateInRemoteNotificationName: .MEGACameraUploadTargetFolderChangedInRemote,
-                                                                                loadNodeRequest: MEGASdkManager.sharedMEGASdk().getCameraUploadsFolder,
-                                                                                setNodeRequest: MEGASdkManager.sharedMEGASdk().setCameraUploadsFolderWithHandle,
-                                                                                nodeName: Strings.Localizable.cameraUploadsLabel,
-                                                                                createNodeRequest: MEGASdkManager.sharedMEGASdk().createFolder))
+    @objc static let shared = CameraUploadNodeAccess(
+        configuration: NodeAccessConfiguration(
+            autoCreate: { CameraUploadManager.isCameraUploadEnabled },
+            updateInMemoryNotificationName: .MEGACameraUploadTargetFolderUpdatedInMemory,
+            updateInRemoteNotificationName: .MEGACameraUploadTargetFolderChangedInRemote,
+            loadNodeRequest: MEGASdkManager.sharedMEGASdk().getCameraUploadsFolder,
+            setNodeRequest: MEGASdkManager.sharedMEGASdk().setCameraUploadsFolderWithHandle,
+            nodeName: Strings.Localizable.cameraUploadsLabel,
+            createNodeRequest: MEGASdkManager.sharedMEGASdk().createFolder
+        )
+    )
 }
