7872abef6feccbc3c87196151ee2b9860fad91e6
Javier Navarro <jn@mega.co.nz>
2022-01-19 15:57:02 +0100

    IOS-5687: Build with ENABLE_LOG_PERFORMANCE turned on
    print only filename, not full path

--- iMEGA/Utils/MEGALogMacros.h
+++ iMEGA/Utils/MEGALogMacros.h
@@ -1,9 +1,11 @@
 
 #import "MEGASdk.h"
 
-#define MEGALogFatal(frmt, ...)   [MEGASdk logWithLevel:MEGALogLevelFatal message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILE__] line:__LINE__];
-#define MEGALogError(frmt, ...)   [MEGASdk logWithLevel:MEGALogLevelError message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILE__] line:__LINE__];
-#define MEGALogWarning(frmt, ...) [MEGASdk logWithLevel:MEGALogLevelWarning message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILE__] line:__LINE__];
-#define MEGALogInfo(frmt, ...)    [MEGASdk logWithLevel:MEGALogLevelInfo message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILE__] line:__LINE__];
-#define MEGALogDebug(frmt, ...)   [MEGASdk logWithLevel:MEGALogLevelDebug message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILE__] line:__LINE__];
-#define MEGALogMax(frmt, ...)     [MEGASdk logWithLevel:MEGALogLevelMax message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILE__] line:__LINE__];
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+#define MEGALogFatal(frmt, ...)   [MEGASdk logWithLevel:MEGALogLevelFatal message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILENAME__] line:__LINE__];
+#define MEGALogError(frmt, ...)   [MEGASdk logWithLevel:MEGALogLevelError message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILENAME__] line:__LINE__];
+#define MEGALogWarning(frmt, ...) [MEGASdk logWithLevel:MEGALogLevelWarning message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILENAME__] line:__LINE__];
+#define MEGALogInfo(frmt, ...)    [MEGASdk logWithLevel:MEGALogLevelInfo message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILENAME__] line:__LINE__];
+#define MEGALogDebug(frmt, ...)   [MEGASdk logWithLevel:MEGALogLevelDebug message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILENAME__] line:__LINE__];
+#define MEGALogMax(frmt, ...)     [MEGASdk logWithLevel:MEGALogLevelMax message:[NSString stringWithFormat:@"[iOS] " frmt, ##__VA_ARGS__] filename:[NSString stringWithFormat:@"%s", __FILENAME__] line:__LINE__];
