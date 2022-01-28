20efbe125c4e198c4362eb9f653b234ebfea3b60
Roberto Gómez <rg@mega.nz>
2022-01-28 18:46:41 +0100

    IOS-5708: Fix the import to cloud drive action from the audio player more menu

---
+++ iMEGA/Categories/MegaNode+Additions.swift
@@ -0,0 +1,13 @@
+
+extension MEGANode {
+    func openBrowserToImport(in viewController: UIViewController) {
+        let storyboard = UIStoryboard(name: "Cloud", bundle: nil)
+        guard let browserVC = storyboard.instantiateViewController(withIdentifier: "BrowserViewControllerID") as? BrowserViewController
+        else { return }
+        browserVC.selectedNodesArray = [self]
+        browserVC.browserAction = .import
+        let browserNC = MEGANavigationController(rootViewController: browserVC)
+        browserNC.setToolbarHidden(false, animated: false)
+        viewController.present(browserNC, animated: true, completion: nil)
+    }
+}
