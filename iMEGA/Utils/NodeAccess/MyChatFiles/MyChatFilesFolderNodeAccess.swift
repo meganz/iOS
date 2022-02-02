e4aff29edca2a80978135be996189198cca8cfbc
Roberto Gómez <rg@mega.nz>
2022-02-03 10:31:33 +1300

    IOS-5710. Node Access - Investigate a clearer workaround for MyChatFiles folder creation status

--- iMEGA/Utils/NodeAccess/MyChatFiles/MyChatFilesFolderNodeAccess.swift
+++ iMEGA/Utils/NodeAccess/MyChatFiles/MyChatFilesFolderNodeAccess.swift
@@ -3,11 +3,15 @@ import Foundation
 final class MyChatFilesFolderNodeAccess: NodeAccess {
     @objc static let shared = MyChatFilesFolderNodeAccess(
         configuration: NodeAccessConfiguration(
-            autoCreate: { MyChatFilesFolderManager.shared.isMyChatFilesFolderCreationEnabled },
+            autoCreate: { false },
             updateInMemoryNotificationName: .MEGAMyChatFilesFolderUpdatedInMemory,
             updateInRemoteNotificationName: .MEGAMyChatFilesFolderUpdatedInRemote,
             loadNodeRequest: MEGASdkManager.sharedMEGASdk().getMyChatFilesFolder,
             setNodeRequest: MEGASdkManager.sharedMEGASdk().setMyChatFilesFolderWithHandle,
             nodeName: Strings.Localizable.myChatFiles,
             createNodeRequest: MEGASdkManager.sharedMEGASdk().createFolder))
+    
+    @objc func updateAutoCreate(status: @escaping @autoclosure () -> Bool) {
+        MyChatFilesFolderNodeAccess.shared.nodeAccessConfiguration.autoCreate = status
+    }
 }
