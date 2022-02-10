3ea0a4480cffa5fc8b11df219af8c673abddff65
Roberto Gómez <rg@mega.nz>
2022-02-10 18:24:24 +0100

    IOS-5724: Reload current cell instead of the entire table, use prepare for reuse to avoid reuse issues

--- iMEGA/My Account/Profile/ProfileTableViewCell.swift
+++ iMEGA/My Account/Profile/ProfileTableViewCell.swift
@@ -5,4 +5,12 @@ class ProfileTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        nameLabel.isEnabled = true
+        accessoryView = nil
+        accessoryType = .disclosureIndicator
+    }
 }
