9b51a81933f6e95cb1993ac160056db4b1bf4443
Roberto Gómez <rg@mega.nz>
2022-03-14 22:41:46 +1300

    iOS-5802. MEGA Xcode Templates - Improve UR-MVVM template

--- Xcode Templates/MVVM.xctemplate/Xib file/___VARIABLE_sceneIdentifier___ViewRouter.swift
+++ Xcode Templates/MVVM.xctemplate/Xib file/___VARIABLE_sceneIdentifier___ViewRouter.swift
@@ -1,13 +1,12 @@
-import Foundation
+import UIKit
 
 final class ___VARIABLE_sceneName:identifier___ViewRouter: ___VARIABLE_sceneName:identifier___ViewRouting {
     private weak var baseViewController: UIViewController?
     private weak var presenter: UIViewController?
-    private weak var navigationController: UINavigationController?
+//    private weak var navigationController: UINavigationController?
     
     init(presenter: UIViewController) {
         self.presenter = presenter
-        super.init()
     }
 
 //    init(navigationController: UINavigationController?) {
@@ -16,10 +15,7 @@ final class ___VARIABLE_sceneName:identifier___ViewRouter: ___VARIABLE_sceneName
 //
     func build() -> UIViewController {
         let vm = ___VARIABLE_sceneName:identifier___ViewModel(router: self)
-        
-        let vc = UIStoryboard(name: "", bundle: nil).instantiateViewController(withIdentifier: "") as! ___VARIABLE_sceneName:identifier___ViewController
-        vc.viewModel = vm
-
+        let vc = ___VARIABLE_sceneName:identifier___ViewController(viewModel: vm)
         baseViewController = vc
         return vc
     }
