2fe399b8552dbb71c7ceb968a214b3d0f730bade
Harry Yan <hy@mega.co.nz>
2022-03-23 11:53:38 +1300

    CC-752: Disable MD

--- iMEGA/Utils/FeatureFlag.swift
+++ iMEGA/Utils/FeatureFlag.swift
@@ -3,4 +3,6 @@ import Foundation
 final class FeatureFlag: NSObject {
     // Add your feature flag here, for example:
     // @objc static let isNewPhotosLibraryEnabled = true
+    
+    @objc static let isMediaDiscoveryEnabled = false
 }
