13a5209e5c8b144c965f04639f69655c9bf4bcc9
Glazel Magpayo <gma@mega.co.nz>
2022-03-25 14:11:14 +1300

    IOS-5245 CU - adopt selected photos permission - L10n

--- iMEGA/SwiftGen/Strings+Generated.swift
+++ iMEGA/SwiftGen/Strings+Generated.swift
@@ -2978,6 +2978,10 @@ public enum Strings {
       }
     }
     public enum CameraUploads {
+      public enum Warning {
+        /// ⚠ MEGA has limited access to photo library and cannot backup all your photos. Tap to change permissions.
+        public static let limitedAccessToPhotoMessage = Strings.tr("Localizable", "cameraUploads.warning.limitedAccessToPhotoMessage")
+      }
       public enum Years {
         /// Years
         public static let title = Strings.tr("Localizable", "cameraUploads.years.title")
