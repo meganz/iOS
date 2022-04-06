2f6e66ac5603abd48c3969f672b4abeb4cc56b93
Glazel Magpayo <gma@mega.co.nz>
2022-04-06 17:15:48 +0800

    Revert "CC-843: Update Photo Cell UI(including video duration icon and favorite badge)"
    This reverts commit 8faec2c67630d777bf5b3c6c5c18be06d947c677.

--- iMEGA/Media Consumption/Photo Library/All View/Photo Cell/ViewModifier/PhotoCellVideoDurationViewModel.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-final class PhotoCellVideoDurationViewModel {
-    private var fontSizeMapping: [Int: CGFloat] = [1:16,3:12,5:8]
-    
-    func fontSize(with scaleFactor: Int) -> CGFloat {
-        return fontSizeMapping[scaleFactor] ?? 12
-    }
-}
