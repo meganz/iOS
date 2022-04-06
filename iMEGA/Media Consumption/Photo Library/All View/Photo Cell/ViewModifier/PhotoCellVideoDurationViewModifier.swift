6bf56bea9bfc5c62b59527ff2354455d1f01e112
Harry Yan <hy@mega.co.nz>
2022-04-06 15:42:11 +1200

    CC-843: Update Photo Cell UI(including video duration icon and favorite badge)

---
+++ iMEGA/Media Consumption/Photo Library/All View/Photo Cell/ViewModifier/PhotoCellVideoDurationViewModifier.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+@available(iOS 14.0, *)
+struct PhotoCellVideoDurationViewModifier: ViewModifier {
+    let isVideo: Bool
+    let duration: String
+    let scaleFactor: Int
+    let videModel = PhotoCellVideoDurationViewModel()
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(videoOverlay, alignment: .bottomLeading)
+    }
+    
+    @ViewBuilder private var videoOverlay: some View {
+        if isVideo {
+            Text(duration)
+                .font(.system(size: videModel.fontSize(with: scaleFactor)))
+                .foregroundColor(Color.white)
+                .padding(.init(top: 2,leading: 5 ,bottom: 2,trailing: 5))
+                .background(Color.black.opacity(0.3))
+                .cornerRadius(6.0)
+                .frame(height: 20, alignment: .bottomLeading)
+                .offset(x: 5, y: -5)
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+extension View {
+    func videoDuration(_ isVideo: Bool, duration: String, with scaleFactor: Int = 3) -> some View {
+        modifier(PhotoCellVideoDurationViewModifier(isVideo: isVideo, duration: duration, scaleFactor: scaleFactor))
+    }
+}
---
+++ iMEGA/SwiftUI/Custom Views/PhotoCellFavoriteBadge.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+@available(iOS 14.0, *)
+struct PhotoCellFavoriteBadge: ViewModifier {
+    let isFavorite: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(favoriteOverlay, alignment: .topTrailing)
+    }
+    
+    @ViewBuilder private var favoriteOverlay: some View {
+        if isFavorite {
+            ZStack(alignment: .topTrailing) {
+                Rectangle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.3), Color.white.opacity(0.01)]), startPoint: .top, endPoint: .bottom))
+                    .alignmentGuide(.top) { d in d[.top] }
+                
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .foregroundColor(Color.white)
+                    .offset(x: -5, y: 5)
+                    .frame(width: 12, height: 11)
+            }
+            .frame(height: 40, alignment: .top)
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+extension View {
+    func favorite(_ isFavorite: Bool) -> some View {
+        modifier(PhotoCellFavoriteBadge(isFavorite: isFavorite))
+    }
+}
