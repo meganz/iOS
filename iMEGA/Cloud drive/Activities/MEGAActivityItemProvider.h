62ea806f81cca0f35c875baaf36e7d85b592dfe7
Carlos Martín Acera <cma@mega.nz>
2022-04-08 13:08:57 +0200

    IOS-5901 Remove unused MEGAActivityItemProvider class

--- iMEGA/Cloud drive/Activities/MEGAActivityItemProvider.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface MEGAActivityItemProvider : UIActivityItemProvider
-
-- (instancetype)initWithPlaceholderString:(NSString *)placeholder node:(MEGANode *)node api:(MEGASdk *)api;
-
-@end
