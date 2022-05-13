c9d18dd4eb2b4343eebbb7bb7ae4b2f0c0564ff0
Harry Yan <hy@mega.co.nz>
2022-05-13 17:03:27 +1200

    Fix detail page dismissed incorrectly in album

--- iMEGA/Media Consumption/Photo Library/Actors/PhotoLibraryMapper.swift
+++ iMEGA/Media Consumption/Photo Library/Actors/PhotoLibraryMapper.swift
@@ -1,13 +1,10 @@
 import Foundation
 
 actor PhotoLibraryMapper {
-    
     func buildPhotoLibrary(with nodes: [PhotoLibraryNodeProtocol]) -> PhotoLibrary {
         if let nodes = nodes as? [MEGANode] {
-            MEGALogDebug("[Photo] Convert nodes to PhotoLibrary in PhotoLibraryMapper!")
             return nodes.toPhotoLibrary()
         } else if let nodes = nodes as? [NodeEntity] {
-            MEGALogDebug("[Album] Convert nodes to PhotoLibrary in PhotoLibraryMapper!")
             return nodes.toPhotoLibrary()
         }
         
