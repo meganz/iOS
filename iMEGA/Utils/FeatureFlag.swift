4ae865539d402dfc3390925408dfe9c252906196
Mahmud Ahsan <mah@mega.co.nz>
2022-05-25 16:01:01 +1200

    CC-1328: Fix the red bar hiding issue

--- iMEGA/Utils/FeatureFlag.swift
+++ iMEGA/Utils/FeatureFlag.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 final class FeatureFlag: NSObject {
-    // Add your feature flag here, for example:
+    // Add fyour feature flag here, for example:
     // @objc static let isNewPhotosLibraryEnabled = true
     @objc static let isAlbumEnabled = false
 }
