b79ba8cab659ba5020700d26a10f086daacd8d62
Javier Navarro <jn@mega.co.nz>
2022-06-18 00:38:47 +1200

    IOS-5947: remove checklocalpathusecase

--- iMEGA/Utils/LocalPath/CheckLocalPathUseCaseOCWrapper.swift
+++
@@ -1,7 +0,0 @@
-@objc final class CheckLocalPathUseCaseOCWrapper: NSObject {
-    private let checkLocalPathUseCase = CheckLocalPathUseCase(repo: FileCacheRepository.default)
-    
-    @objc func containsOriginalCacheDirectory(path: String) -> Bool {
-        checkLocalPathUseCase.containsOriginalCacheDirectory(path: path)
-    }
-}
