15cbd5fce2459195f84fb3cc9680f47990e9b74f
Carlos Martín Acera <cma@mega.nz>
2022-08-05 09:11:38 +1200

    MEET-600 Update user privilege in meetings UI

--- MEGADomain/RepositoryProtocol/Chat/ChatRoomRepositoryProtocol.swift
+++ MEGADomain/RepositoryProtocol/Chat/ChatRoomRepositoryProtocol.swift
@@ -4,6 +4,7 @@ protocol ChatRoomRepositoryProtocol {
     func chatRoom(forChatId chatId: HandleEntity) -> ChatRoomEntity?
     func chatRoom(forUserHandle userHandle: HandleEntity) -> ChatRoomEntity?
     func peerHandles(forChatId chatId: HandleEntity) -> [HandleEntity]
+    func peerPrivilege(forUserHandle userHandle: HandleEntity, inChatId chatId: HandleEntity) -> ChatRoomEntity.Privilege?
     func createChatRoom(forUserHandle userHandle: HandleEntity, completion: @escaping (Result<ChatRoomEntity, ChatRoomErrorEntity>) -> Void)
     func createPublicLink(forChatId chatId: HandleEntity, completion: @escaping (Result<String, ChatLinkErrorEntity>) -> Void)
     func queryChatLink(forChatId chatId: HandleEntity, completion: @escaping (Result<String, ChatLinkErrorEntity>) -> Void)
@@ -11,5 +12,6 @@ protocol ChatRoomRepositoryProtocol {
     func userFullName(forPeerId peerId: HandleEntity, chatId: HandleEntity) async throws -> String
     func renameChatRoom(chatId: HandleEntity, title: String, completion: @escaping (Result<String, ChatRoomErrorEntity>) -> Void)
     mutating func participantsUpdated(forChatId chatId: HandleEntity) -> AnyPublisher<[HandleEntity], Never>
+    mutating func userPrivilegeChanged(forChatId: HandleEntity) -> AnyPublisher<HandleEntity, Never>
 }
 
