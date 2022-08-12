4448f3e25d99aebfbd8a7c210bac2cfd030436f6
Simon Wang <swg@mega.co.nz>
2022-08-12 16:22:53 +1200

    IOS-6266: Fix warnings on the open access in extensions

--- iMEGA/Utils/Categories/UserDefaults+Additions.swift
+++ iMEGA/Utils/Categories/UserDefaults+Additions.swift
@@ -1,10 +1,10 @@
 extension UserDefaults {
-    open func setStruct<T: Codable>(_ value: T?, forKey defaultName: String) {
+    func setStruct<T: Codable>(_ value: T?, forKey defaultName: String) {
         let data = try? JSONEncoder().encode(value)
         set(data, forKey: defaultName)
     }
 
-    open func structData<T>(_ type: T.Type, forKey defaultName: String) -> T? where T: Decodable {
+    func structData<T>(_ type: T.Type, forKey defaultName: String) -> T? where T: Decodable {
         guard let encodedData = data(forKey: defaultName) else {
             return nil
         }
@@ -12,13 +12,13 @@ extension UserDefaults {
         return try! JSONDecoder().decode(type, from: encodedData)
     }
 
-    open func setStructArray<T: Codable>(_ value: [T], forKey defaultName: String) {
+    func setStructArray<T: Codable>(_ value: [T], forKey defaultName: String) {
         let data = value.map { try? JSONEncoder().encode($0) }
 
         set(data, forKey: defaultName)
     }
 
-    open func structArrayData<T>(_ type: T.Type, forKey defaultName: String) -> [T] where T: Decodable {
+    func structArrayData<T>(_ type: T.Type, forKey defaultName: String) -> [T] where T: Decodable {
         guard let encodedData = array(forKey: defaultName) as? [Data] else {
             return []
         }
