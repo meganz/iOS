e3a2aa7dace031fec00779fedd11a719183ce446
AMAN <ak@mega.co.nz>
2022-08-18 22:55:43 +0530

    Removed addition file and moved its content to additions file

--- iMEGA/Camera uploads/Extension/PhotosViewController+Additions.swift
+++ iMEGA/Camera uploads/Extension/PhotosViewController+Additions.swift
@@ -117,3 +117,22 @@ extension PhotosViewController {
         setRightNavigationBarButtons()
     }
 }
+
+extension PhotosViewController {
+    @objc func configureStackViewHeight(view: UIView, perviousConstraint: NSLayoutConstraint?) -> NSLayoutConstraint? {
+
+        var newConstraint: NSLayoutConstraint?
+        let verticalPadding = 24.0
+
+        let maxHeight = view.subviews.flatMap({ $0.subviews }).map({ $0.intrinsicContentSize.height }).max() ?? 0
+
+        if perviousConstraint == nil {
+            newConstraint = view.heightAnchor.constraint(equalToConstant: maxHeight > 0 ? maxHeight + verticalPadding : 0)
+        } else {
+            perviousConstraint?.isActive = false
+            newConstraint = view.heightAnchor.constraint(equalToConstant: maxHeight + verticalPadding)
+        }
+        newConstraint?.isActive = true
+        return newConstraint
+    }
+}
