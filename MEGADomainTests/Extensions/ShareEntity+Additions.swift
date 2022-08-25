104e2490c75c999670074005e26c8da415ee0460
Simon Wang <swg@mega.co.nz>
2022-08-26 11:16:59 +1200

    IOS-6280: Move Node, Transfer, Share and Account Details entity test initialisers to MEGADomainMock

--- MEGADomainTests/Extensions/ShareEntity+Additions.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-@testable import MEGA
-import MEGADomain
-
-extension ShareEntity {
-    init() {
-        self.init(sharedUserEmail: nil, nodeHandle: 0, accessLevel: .unknown, createdDate: Date(), isPending: false)
-    }
-}
