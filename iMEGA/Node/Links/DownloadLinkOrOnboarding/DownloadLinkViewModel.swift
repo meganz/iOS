2f1867cc719bb436e3b7e787bd585b95ef07a148
Javier Navarro <jn@mega.co.nz>
2022-09-01 00:44:34 +1200

    IOS-6375: unable to make file offline from a public folder link

--- iMEGA/Node/Links/DownloadLinkOrOnboarding/DownloadLinkViewModel.swift
+++ iMEGA/Node/Links/DownloadLinkOrOnboarding/DownloadLinkViewModel.swift
@@ -9,7 +9,6 @@ protocol DownloadLinkRouterProtocol {
 final class DownloadLinkViewModel {
     private let router: DownloadLinkRouterProtocol
     private let authUseCase: AuthUseCaseProtocol
-    private let networkMonitorUseCase: NetworkMonitorUseCaseProtocol
     
     private let link: URL?
     private let isFolderLink: Bool
@@ -17,12 +16,10 @@ final class DownloadLinkViewModel {
 
     init(router: DownloadLinkRouterProtocol,
          authUseCase: AuthUseCaseProtocol,
-         networkMonitorUseCase: NetworkMonitorUseCaseProtocol,
          link: URL,
          isFolderLink: Bool) {
         self.router = router
         self.authUseCase = authUseCase
-        self.networkMonitorUseCase = networkMonitorUseCase
         self.link = link
         self.nodes = nil
         self.isFolderLink = isFolderLink
@@ -30,42 +27,38 @@ final class DownloadLinkViewModel {
     
     init(router: DownloadLinkRouterProtocol,
          authUseCase: AuthUseCaseProtocol,
-         networkMonitorUseCase: NetworkMonitorUseCaseProtocol,
          nodes: [NodeEntity],
          isFolderLink: Bool) {
         self.router = router
         self.authUseCase = authUseCase
-        self.networkMonitorUseCase = networkMonitorUseCase
         self.isFolderLink = isFolderLink
         self.link = nil
         self.nodes = nodes
     }
     
     func checkIfLinkCanBeDownloaded() {
-        if networkMonitorUseCase.isConnected() {
-            if authUseCase.sessionId() != nil {
-                if isFolderLink {
-                    router.downloadFolderLinkNodes()
-                } else {
-                    router.downloadFileLink()
-                }
+        if authUseCase.sessionId() != nil {
+            if isFolderLink {
+                router.downloadFolderLinkNodes()
             } else {
-                if isFolderLink {
-                    guard let nodes = nodes else {
-                        return
-                    }
-                    MEGALinkManager.selectedOption = .downloadFolderOrNodes
-                    MEGALinkManager.nodesFromLinkToDownloadAfterLogin(nodes: nodes)
-                } else {
-                    guard let link = link else {
-                        return
-                    }
-                    MEGALinkManager.selectedOption = .downloadNode
-                    MEGALinkManager.linkSavedString = link.absoluteString
-                }
-                
-                router.showOnboarding()
+                router.downloadFileLink()
             }
+        } else {
+            if isFolderLink {
+                guard let nodes = nodes else {
+                    return
+                }
+                MEGALinkManager.selectedOption = .downloadFolderOrNodes
+                MEGALinkManager.nodesFromLinkToDownloadAfterLogin(nodes: nodes)
+            } else {
+                guard let link = link else {
+                    return
+                }
+                MEGALinkManager.selectedOption = .downloadNode
+                MEGALinkManager.linkSavedString = link.absoluteString
+            }
+            
+            router.showOnboarding()
         }
     }
 }
