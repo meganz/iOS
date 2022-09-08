e47682ba7882525a399c2e3e1e66a5afaa09bd20
Lancy <lf@mega.co.nz>
2022-09-08 12:15:27 +1200

    T3786814: The "Allow non-host to add participants" settings should be enabled by default for meetings

--- MEGADomain/RepositoryProtocol/Calls/MeetingCreatingRepositoryProtocol.swift
+++ MEGADomain/RepositoryProtocol/Calls/MeetingCreatingRepositoryProtocol.swift
@@ -1,7 +1,15 @@
 protocol MeetingCreatingRepositoryProtocol {
     func getUsername() -> String
     func getCall(forChatId chatId: UInt64) -> CallEntity?
-    func startChatCall(meetingName: String, enableVideo: Bool, enableAudio: Bool,  completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void)
+    func startCall(
+        meetingName: String,
+        enableVideo: Bool,
+        enableAudio: Bool,
+        speakRequest: Bool,
+        waitingRoom: Bool,
+        allowNonHostToAddParticipants: Bool,
+        completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void
+    )
     func joinChatCall(forChatId chatId: UInt64, enableVideo: Bool, enableAudio: Bool, userHandle: UInt64, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void)
     func checkChatLink(link: String, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void)
     func createEphemeralAccountAndJoinChat(firstName: String, lastName: String, link: String, completion: @escaping (Result<Void, MEGASDKErrorType>) -> Void, karereInitCompletion: @escaping () -> Void)
