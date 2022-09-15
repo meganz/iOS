1cdf7962d2fce485e907caea23145b5320ccae6b
Lancy Norbert Fernandes <lf@mega.co.nz>
2022-09-15 16:16:30 +1200

    CI/CD: Script update

--- jenkinsfile/ios_automation_app_upload.groovy
+++ jenkinsfile/ios_automation_app_upload.groovy
@@ -1,7 +1,7 @@
 
 def injectEnvironments(Closure body) {
     withEnv([
-        "PATH=~/.rbenv/shims:~/.rbenv/bin:/Applications/MEGAcmd.app/Contents/MacOS:/Applications/CMake.app/Contents/bin:$PATH:/usr/local/bin",
+        "PATH=/var/lib/jenkins/.rbenv/shims:/var/lib/jenkins/.rbenv/bin:/Applications/MEGAcmd.app/Contents/MacOS:/Applications/CMake.app/Contents/bin:$PATH:/usr/local/bin",
         "LC_ALL=en_US.UTF-8",
         "LANG=en_US.UTF-8"
     ]) {
