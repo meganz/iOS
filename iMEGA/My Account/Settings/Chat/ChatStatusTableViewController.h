869abac08370f45073bae8f844f94c4a3e578a81
Carlos Martín Acera <cma@mega.nz>
2022-09-15 15:13:39 +0200

    MEET-770 Format auto away time in hours and minutes

--- iMEGA/My Account/Settings/Chat/ChatStatusTableViewController.h
+++ iMEGA/My Account/Settings/Chat/ChatStatusTableViewController.h
@@ -1,5 +1,19 @@
 #import <UIKit/UIKit.h>
 
+@class MEGAChatPresenceConfig;
+
 @interface ChatStatusTableViewController : UITableViewController
+
+@property (nonatomic) MEGAChatPresenceConfig *presenceConfig;
+@property (nonatomic) NSInteger autoAwayTimeoutInMinutes;
+@property (nonatomic) BOOL isSelectingTimeout;
+@property (weak, nonatomic) NSIndexPath *currentStatusIndexPath;
+
+@property (weak, nonatomic) IBOutlet UITableViewCell *timeoutAutoAwayCell;
+@property (weak, nonatomic) IBOutlet UIPickerView *autoAwayTimePicker;
+@property (weak, nonatomic) IBOutlet UIButton *autoAwayTimeSaveButton;
+@property (weak, nonatomic) IBOutlet UILabel *timeoutAutoAwayLabel;
+
+- (void)setPresenceAutoAway:(BOOL)boolValue;
 
 @end
