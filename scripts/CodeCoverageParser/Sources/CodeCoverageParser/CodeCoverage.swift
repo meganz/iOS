9d09096d1674c34930034fa11dc2daa15d0cbe76
Lancy <lf@mega.co.nz>
2022-09-22 16:48:37 +1200

    CI/CD - bundle update. Code coverage include targets.

--- scripts/CodeCoverageParser/Sources/CodeCoverageParser/CodeCoverage.swift
+++ scripts/CodeCoverageParser/Sources/CodeCoverageParser/CodeCoverage.swift
@@ -1,11 +1,6 @@
 import Foundation
 
 struct CodeCoverage: Codable {
+    let name: String
     let coverage: Double
-    let targetCodeCoverages: [TargetCodeCoverage]
-    
-    enum CodingKeys: String, CodingKey {
-        case coverage
-        case targetCodeCoverages = "targets"
-    }
 }
