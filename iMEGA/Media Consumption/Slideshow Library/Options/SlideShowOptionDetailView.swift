fb1e461350c9e23712121f4908ea162eba87ac10
Aman Kumar <ak@mega.co.nz>
2022-09-29 16:50:03 +1300

    CC-1460 iOS - Support Slideshow preference setting - UI

--- iMEGA/Media Consumption/Slideshow Library/Options/SlideShowOptionDetailView.swift
+++ iMEGA/Media Consumption/Slideshow Library/Options/SlideShowOptionDetailView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @available(iOS 14.0, *)
 struct SlideShowOptionDetailView: View {
+    @Environment(\.colorScheme) private var colorScheme
     @ObservedObject var viewModel: SlideShowOptionCellViewModel
     @Binding var isShowing: Bool
     
@@ -27,7 +28,19 @@ struct SlideShowOptionDetailView: View {
             .listStyle(.plain)
             .navigationTitle(viewModel.title)
             .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(leading: navBarButton, trailing: EmptyView())
         }
         .navigationViewStyle(.stack)
+    }
+    
+    var navBarButton: some View {
+        Button {
+            isShowing.toggle()
+        } label: {
+            Image(uiImage: UIImage(asset: Asset.Images.Chat.backArrow))
+                .frame(width: 18, height: 24)
+                .foregroundColor(colorScheme == .dark ? .white.opacity(0.8) : Color(Colors.General.Black._161616.color))
+                .contentShape(Rectangle())
+        }
     }
 }
