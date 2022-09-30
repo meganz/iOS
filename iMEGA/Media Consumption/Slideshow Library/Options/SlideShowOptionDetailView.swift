9090e9e9070b2a076f717cf5b9c424dfd8bcf852
Aman Kumar <ak@mega.co.nz>
2022-09-30 14:54:30 +1300

    CC-1460 iOS - Support Slideshow preference setting - UI

--- iMEGA/Media Consumption/Slideshow Library/Options/SlideShowOptionDetailView.swift
+++ iMEGA/Media Consumption/Slideshow Library/Options/SlideShowOptionDetailView.swift
@@ -2,34 +2,29 @@ import SwiftUI
 
 @available(iOS 14.0, *)
 struct SlideShowOptionDetailView: View {
-    @Environment(\.colorScheme) private var colorScheme
     @ObservedObject var viewModel: SlideShowOptionCellViewModel
     @Binding var isShowing: Bool
     
     var body: some View {
-        NavigationView {
-            VStack(spacing: 0) {
-                Divider()
-                LazyVStack {
-                    ForEach(viewModel.children, id: \.self.id) { item in
-                        SlideShowOptionDetailCellView(viewModel: item)
-                            .onTapGesture {
-                                viewModel.didSelectChild(item)
-                                withAnimation(.easeOut(duration: 1)) {
-                                    isShowing.toggle()
-                                }
+        VStack(spacing: 0) {
+            navigationBar
+            Divider()
+            LazyVStack {
+                ForEach(viewModel.children, id: \.self.id) { item in
+                    SlideShowOptionDetailCellView(viewModel: item)
+                        .onTapGesture {
+                            viewModel.didSelectChild(item)
+                            withAnimation(.easeOut(duration: 1)) {
+                                isShowing.toggle()
+                            }
                         }
-                    }
                 }
-                .padding(.leading, 16)
-                .padding(.top)
-                Spacer()
             }
-            .listStyle(.plain)
-            .navigationTitle(viewModel.title)
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(leading: navBarButton, trailing: EmptyView())
+            .padding(.leading, 16)
+            .padding(.top)
+            Spacer().layoutPriority(1)
         }
+        .listStyle(.plain)
         .navigationViewStyle(.stack)
     }
     
@@ -39,8 +34,23 @@ struct SlideShowOptionDetailView: View {
         } label: {
             Image(uiImage: UIImage(asset: Asset.Images.Chat.backArrow))
                 .frame(width: 18, height: 24)
-                .foregroundColor(colorScheme == .dark ? .white.opacity(0.8) : Color(Colors.General.Black._161616.color))
+                .foregroundColor(.primary.opacity(0.8))
                 .contentShape(Rectangle())
         }
+    }
+    
+    var navigationBar: some View {
+        ZStack {
+            Color.secondary.opacity(0.1)
+            Text(viewModel.title)
+                .font(.headline)
+                .padding(.vertical, 26)
+        }
+        .overlay(
+            HStack {
+                navBarButton
+                Spacer()
+            }.padding()
+        )
     }
 }
