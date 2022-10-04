0360b0660072f9fb4c608cc7403ebb9a831f86dc
Javier Navarro <jn@mega.co.nz>
2022-10-04 19:23:08 +1300

    IOS-6388: move SaveNodeUseCase to MEGADomain module

--- iMEGA/Utils/OCWrapper/SaveNodeUseCaseOCWrapper.swift
+++ iMEGA/Utils/OCWrapper/SaveNodeUseCaseOCWrapper.swift
@@ -5,9 +5,25 @@ import MEGADomain
         offlineFilesRepository: OfflineFilesRepository(store: MEGAStore.shareInstance(), sdk: MEGASdkManager.sharedMEGASdk()),
         fileCacheRepository: FileCacheRepository.newRepo,
         nodeRepository: NodeRepository.newRepo,
-        photosLibraryRepository: PhotosLibraryRepository.newRepo)
+        photosLibraryRepository: PhotosLibraryRepository.newRepo,
+        mediaUseCase: MediaUseCase(),
+        preferenceUseCase: PreferenceUseCase.default)
     
     @objc func saveNodeIfNeeded(from transfer: MEGATransfer) {
-        saveNodeUseCase.saveNodeIfNeeded(from: transfer.toTransferEntity())
+        saveNodeUseCase.saveNode(from:transfer.toTransferEntity()) { result in
+            switch result {
+            case .success(let savedToPhotos):
+                if savedToPhotos {
+                    SVProgressHUD.show(Asset.Images.NodeActions.saveToPhotos.image, status: Strings.Localizable.savedToPhotos)
+                }
+            case .failure(let error):
+                switch error {
+                case .videoNotSaved, .imageNotSaved:
+                    SVProgressHUD.showError(withStatus: Strings.Localizable.couldNotSaveItem)
+                default:
+                    SVProgressHUD.showError(withStatus: Strings.Localizable.somethingWentWrong)
+                }
+            }
+        }
     }
 }
