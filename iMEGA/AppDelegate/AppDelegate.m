be5377bfcbe8d3aae5faf809b8b1dd96e301c862
Javier Navarro <jn@mega.co.nz>
2022-11-09 16:48:03 +0100

    IOS-6600: show loading UI when full reload is started by the server

--- iMEGA/AppDelegate/AppDelegate.m
+++ iMEGA/AppDelegate/AppDelegate.m
@@ -223,10 +223,7 @@
         [MEGASdkManager.sharedMEGASdk fastLoginWithSession:sessionV3 delegate:loginRequestDelegate];
         
         if ([MEGAReachabilityManager isReachable]) {
-            LaunchViewController *launchVC = [[UIStoryboard storyboardWithName:@"Launch" bundle:nil] instantiateViewControllerWithIdentifier:@"LaunchViewControllerID"];
-            [UIView transitionWithView:self.window duration:0.5 options:(UIViewAnimationOptionTransitionCrossDissolve | UIViewAnimationOptionAllowAnimatedContent) animations:^{
-                [self.window setRootViewController:launchVC];
-            } completion:nil];
+            [self showLaunchViewController];
         } else {
             if ([LTHPasscodeViewController doesPasscodeExist]) {
                 [[LTHPasscodeViewController sharedUser] setMaxNumberOfAllowedFailedAttempts:10];
@@ -1378,6 +1375,12 @@
         case EventStorageSumChanged:
             [MEGASdkManager.sharedMEGASdk mnz_setShouldRequestAccountDetails:YES];
             break;
+            
+        case EventReloading: {
+            [self showLaunchViewController];
+            break;
+        }
+            
             
         default:
             break;
