6e8b3472933fd1011d14ade1f51a54b599c568d6
Mahmud Ahsan <mah@mega.co.nz>
2022-11-11 17:32:44 +1300

    CC-1040: Raw album content

--- MEGAUnitTests/Mocks/Sdk/MockSDKNodesUpdateListenerRepository.swift
+++ MEGAUnitTests/Mocks/Sdk/MockSDKNodesUpdateListenerRepository.swift
@@ -1,18 +1,13 @@
 import Foundation
 @testable import MEGA
 
-final class MockSDKNodesUpdateListenerRepository: NSObject, MEGAGlobalDelegate, SDKNodesUpdateListenerProtocol {
+final class MockSDKNodesUpdateListenerRepository: NSObject, SDKNodesUpdateListenerProtocol {
     private let sdk: MockSdk
     var onUpdateHandler: (([MEGANode]) -> Void)?
     
     init(sdk: MockSdk) {
         self.sdk = sdk
         super.init()
-        sdk.add(self)
-    }
-    
-    deinit {
-        sdk.remove(self)
     }
     
     func onNodesUpdate(_ api: MockSdk, nodeList: MEGANodeList?) {
