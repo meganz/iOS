5e3f45109d245f543b36d753c5c029deaaee5743
Mauritz Mollentze <mmo@mega.co.nz>
2022-12-07 09:55:09 +1300

    IOS-6571: Add media node updates to MediaDiscoveryUseCase

--- Modules/MEGADomain/Sources/MEGADomain/Entity/Node/NodeEntity+ChangeType.swift
+++ Modules/MEGADomain/Sources/MEGADomain/Entity/Node/NodeEntity+ChangeType.swift
@@ -9,4 +9,16 @@ public extension Sequence where Element == NodeEntity {
     func nodes(for changedTypes: ChangeTypeEntity) -> [NodeEntity] {
         filter { $0.changeTypes.intersection(changedTypes).isNotEmpty }
     }
+    
+    func containsNewNode() -> Bool {
+        contains { $0.changeTypes.contains(.new) }
+    }
+    
+    func hasModifiedAttributes() -> Bool {
+        contains { $0.changeTypes.contains(.attributes) }
+    }
+    
+    func hasModifiedParent() -> Bool {
+        contains { $0.changeTypes.contains(.parent) }
+    }
 }
---
+++ Modules/MEGADomain/Sources/MEGADomain/UseCase/MediaDiscovery/MediaDiscoveryUseCase.swift
@@ -0,0 +1,58 @@
+import Foundation
+import Combine
+
+public protocol MediaDiscoveryUseCaseProtocol {
+    var nodeUpdatesPublisher: AnyPublisher<[NodeEntity], Never> { get }
+    func nodes(forParent parent: NodeEntity) async throws -> [NodeEntity]
+    func shouldReload(parentNode: NodeEntity, loadedNodes: [NodeEntity], updatedNodes: [NodeEntity]) -> Bool
+}
+
+public class MediaDiscoveryUseCase<T: MediaDiscoveryRepositoryProtocol,
+                                   U: NodeUpdateRepositoryProtocol>: MediaDiscoveryUseCaseProtocol  {
+    private let mediaDiscoveryRepository: T
+    private let nodeUpdateRepository: U
+    
+    public lazy var nodeUpdatesPublisher: AnyPublisher<[NodeEntity], Never> = {
+        mediaDiscoveryRepository.nodesUpdatePublisher.handleEvents(receiveSubscription: { [weak self] _ in
+            self?.mediaDiscoveryRepository.startMonitoringNodesUpdate()
+        }, receiveCompletion: { [weak self] _ in
+            self?.mediaDiscoveryRepository.stopMonitoringNodesUpdate()
+        }, receiveCancel: { [weak self] in
+            self?.mediaDiscoveryRepository.stopMonitoringNodesUpdate()
+        })
+        .share()
+        .eraseToAnyPublisher()
+    }()
+
+    public init(mediaDiscoveryRepository: T, nodeUpdateRepository: U) {
+        self.mediaDiscoveryRepository = mediaDiscoveryRepository
+        self.nodeUpdateRepository = nodeUpdateRepository
+    }
+
+    public func nodes(forParent parent: NodeEntity) async throws -> [NodeEntity] {
+        try await mediaDiscoveryRepository.loadNodes(forParent: parent)
+    }
+    
+    public func shouldReload(parentNode: NodeEntity, loadedNodes: [NodeEntity], updatedNodes: [NodeEntity]) -> Bool {
+        guard nodeUpdateRepository.shouldProcessOnNodesUpdate(parentNode: parentNode, childNodes: loadedNodes, updatedNodes: updatedNodes) else { return false }
+        
+        return isAnyNodeMovedToTrash(nodes: loadedNodes, updatedNodes: updatedNodes) ||
+        updatedNodes.containsNewNode() ||
+        updatedNodes.hasModifiedAttributes() ||
+        updatedNodes.hasModifiedParent()
+    }
+    
+    //MARK: Private
+    
+    private func isAnyNodeMovedToTrash(nodes: [NodeEntity], updatedNodes: [NodeEntity]) -> Bool {
+        let existingNodes = Set(nodes.map { $0.handle })
+        return updatedNodes.contains { node in
+            if node.changeTypes.contains(.parent),
+               existingNodes.contains(node.handle),
+               node.nodeType == .rubbish {
+                return true
+            }
+            return false
+        }
+    }
+}
