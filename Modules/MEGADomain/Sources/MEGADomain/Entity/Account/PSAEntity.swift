10c7dbaba2e83c4b02c39b6ad1520a7eabbff65a
Simon Wang <swg@mega.co.nz>
2022-12-12 11:31:23 +1300

    IOS-6713: Move PSAUseCase to Domain module

---
+++ Modules/MEGADomain/Sources/MEGADomain/Entity/Account/PSAEntity.swift
@@ -0,0 +1,27 @@
+public typealias PSAIdentifier = Int
+
+public struct PSAEntity {
+    public let identifier: PSAIdentifier
+    public let title: String
+    public let description: String
+    public let imageURL: String?
+    public let positiveText: String?
+    public let positiveLink: String?
+    public let URLString: String?
+    
+    public init(identifier: PSAIdentifier, title: String, description: String, imageURL: String?, positiveText: String?, positiveLink: String?, URLString: String?) {
+        self.identifier = identifier
+        self.title = title
+        self.description = description
+        self.imageURL = imageURL
+        self.positiveText = positiveText
+        self.positiveLink = positiveLink
+        self.URLString = URLString
+    }
+}
+
+extension PSAEntity: Equatable {
+    public static func ==(lhs: PSAEntity, rhs: PSAEntity) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+}
--- iMEGA/Utils/Categories/MainTabBarController+Additions.swift
+++ iMEGA/Utils/Categories/MainTabBarController+Additions.swift
@@ -1,4 +1,4 @@
-
+import MEGADomain
 
 extension MainTabBarController {
     
