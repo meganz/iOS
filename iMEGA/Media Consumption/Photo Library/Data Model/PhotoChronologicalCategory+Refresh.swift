5aee710e50e70ab787b15153cd8e6bf28b78642f
Simon Wang <swg@mega.co.nz>
2022-12-13 17:10:08 +1300

    T4208199: Improve the refresh algorithm for chronological category

--- iMEGA/Media Consumption/Photo Library/Data Model/PhotoChronologicalCategory+Refresh.swift
+++ iMEGA/Media Consumption/Photo Library/Data Model/PhotoChronologicalCategory+Refresh.swift
@@ -3,35 +3,51 @@ import MEGASwift
 
 extension PhotoChronologicalCategory {
     public static func ↻↻ (lhs: Self, rhs: Self) -> Bool {
-        lhs != rhs ||
         lhs.categoryDate != rhs.categoryDate ||
+        lhs.coverPhoto != rhs.coverPhoto
+    }
+    
+    public static func ↻↻⏿ (lhs: Self, rhs: Self) -> Bool {
+        lhs.categoryDate != rhs.categoryDate ||
+        lhs.coverPhoto != rhs.coverPhoto ||
         lhs.coverPhoto?.hasThumbnail != rhs.coverPhoto?.hasThumbnail ||
-        lhs.coverPhoto?.hasPreview != rhs.coverPhoto?.hasPreview ||
-        lhs.coverPhoto?.isFavourite != rhs.coverPhoto?.isFavourite
+        lhs.coverPhoto?.hasPreview != rhs.coverPhoto?.hasPreview
     }
 }
 
 extension Array where Element: PhotoChronologicalCategory {
     func shouldRefresh(to categories: [Element], visiblePositions: [PhotoScrollPosition?: Bool] = [:]) -> Bool {
-        guard count == categories.count else {
-            MEGALogDebug("[Photos] refresh categories \(Self.self) due to count difference")
-            return true
-        }
+        guard count == categories.count else { return true }
         
         for zip in zip(self, categories) {
-            if visiblePositions[zip.0.position] == true && zip.0 ↻↻ zip.1 {
-                MEGALogDebug("[Photos] refresh categories \(Self.self) due to refreshable is true")
+            if visiblePositions[zip.0.position] == true {
+                if zip.0 ↻↻⏿ zip.1 {
+                    return true
+                }
+            } else {
+                if zip.0 ↻↻ zip.1 {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+}
+
+extension Array where Element: PhotoSection {
+    func shouldRefresh(to categories: [Element], visiblePositions: [PhotoScrollPosition?: Bool] = [:]) -> Bool {
+        guard count == categories.count else { return true }
+        for zip in zip(self, categories) {
+            guard zip.0.title == zip.1.title, zip.0.categoryDate == zip.1.categoryDate else {
                 return true
             }
             
-            if zip.0 == zip.1 {
-                continue
-            } else {
+            if zip.0.contentList.shouldRefresh(to: zip.1.contentList, visiblePositions: visiblePositions) {
                 return true
             }
         }
         
-        MEGALogDebug("[Photos] don't refresh categories \(Self.self)")
         return false
     }
 }
