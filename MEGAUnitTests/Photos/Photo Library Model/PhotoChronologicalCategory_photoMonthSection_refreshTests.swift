87b1205520ee967a6f0c41db732d183a3844ad35
Simon Wang <swg@mega.co.nz>
2022-12-13 17:10:08 +1300

    T4208199: Improve the refresh algorithm for chronological category

--- MEGAUnitTests/Photos/Photo Library Model/PhotoChronologicalCategory_photoMonthSection_refreshTests.swift
+++ MEGAUnitTests/Photos/Photo Library Model/PhotoChronologicalCategory_photoMonthSection_refreshTests.swift
@@ -110,7 +110,7 @@ final class PhotoChronologicalCategory_photoMonthSection_refreshTests: XCTestCas
         let testCategories = [
             NodeEntity(name: "a.jpg", handle: 1, modificationTime: try "2022-08-18T22:01:04Z".date),
             NodeEntity(name: "b.jpg", handle: 2, modificationTime: try "2022-07-18T22:01:04Z".date),
-            NodeEntity(name: "c.mov", handle: 3, modificationTime: try "2022-04-18T22:01:04Z".date),
+            NodeEntity(name: "c.mov", handle: 3, modificationTime: try "2022-07-18T20:01:04Z".date),
             NodeEntity(name: "d.mp4", handle: 4, modificationTime: try "2021-08-18T22:01:04Z".date),
             NodeEntity(name: "e.jpg", handle: 5, modificationTime: try "2021-01-18T22:01:04Z".date),
             NodeEntity(name: "f.jpg", handle: 6, modificationTime: try "2020-12-18T22:01:04Z".date),
@@ -123,7 +123,7 @@ final class PhotoChronologicalCategory_photoMonthSection_refreshTests: XCTestCas
         let newCategories = [
             NodeEntity(name: "a.jpg", handle: 1, hasThumbnail: true, modificationTime: try "2022-08-18T22:01:04Z".date),
             NodeEntity(name: "b.jpg", handle: 2, isFavourite: true, modificationTime: try "2022-07-18T22:01:04Z".date),
-            NodeEntity(name: "c.mov", handle: 3, hasPreview: true, modificationTime: try "2022-04-18T22:01:04Z".date),
+            NodeEntity(name: "c.mov", handle: 3, hasPreview: true, modificationTime: try "2022-07-18T20:01:04Z".date),
             NodeEntity(name: "d.mp4", handle: 4, modificationTime: try "2021-08-18T22:01:04Z".date),
             NodeEntity(name: "e.jpg", handle: 5, modificationTime: try "2021-01-18T22:01:04Z".date),
             NodeEntity(name: "f.jpg", handle: 6, modificationTime: try "2020-12-18T22:01:04Z".date),
@@ -137,8 +137,7 @@ final class PhotoChronologicalCategory_photoMonthSection_refreshTests: XCTestCas
         
         let visibleAndRefreshableNodes = [
             NodeEntity(name: "a.jpg", handle: 1, hasThumbnail: true, modificationTime: try "2022-08-18T22:01:04Z".date),
-            NodeEntity(name: "b.jpg", handle: 2, isFavourite: true, modificationTime: try "2022-07-18T22:01:04Z".date),
-            NodeEntity(name: "c.mov", handle: 3, hasPreview: true, modificationTime: try "2022-04-18T22:01:04Z".date)
+            NodeEntity(name: "c.mov", handle: 3, hasPreview: true, modificationTime: try "2022-07-18T20:01:04Z".date)
         ]
         
         for node in visibleAndRefreshableNodes {
@@ -146,6 +145,7 @@ final class PhotoChronologicalCategory_photoMonthSection_refreshTests: XCTestCas
         }
         
         let visibleAndNonRefreshNodes = [
+            NodeEntity(name: "b.jpg", handle: 2, modificationTime: try "2022-07-18T22:01:04Z".date),
             NodeEntity(name: "d.mp4", handle: 4, modificationTime: try "2021-08-18T22:01:04Z".date),
             NodeEntity(name: "e.jpg", handle: 5, modificationTime: try "2021-01-18T22:01:04Z".date),
             NodeEntity(name: "f.jpg", handle: 6, modificationTime: try "2020-12-18T22:01:04Z".date),
@@ -185,6 +185,51 @@ final class PhotoChronologicalCategory_photoMonthSection_refreshTests: XCTestCas
         
         for node in visibleNodes {
             XCTAssertFalse(testCategories.shouldRefresh(to: newCategories, visiblePositions: [node.position: true]))
+        }
+    }
+    
+    func testShouldRefresh_photoMonthSectionAndMultiplePhotosInOneMonth() throws {
+        let testCategories = [
+            NodeEntity(name: "a.jpg", handle: 1, modificationTime: try "2022-08-18T22:01:04Z".date),
+            NodeEntity(name: "b.jpg", handle: 2, modificationTime: try "2022-08-17T22:01:04Z".date),
+            NodeEntity(name: "c.mov", handle: 3, modificationTime: try "2022-08-16T22:01:04Z".date),
+            NodeEntity(name: "d.mp4", handle: 4, modificationTime: try "2022-08-15T22:01:04Z".date),
+            NodeEntity(name: "e.jpg", handle: 5, modificationTime: try "2022-08-14T22:01:04Z".date),
+            NodeEntity(name: "f.jpg", handle: 6, modificationTime: try "2022-08-13T22:01:04Z".date),
+            NodeEntity(name: "g.jpg", handle: 7, modificationTime: try "2020-10-18T22:01:04Z".date),
+            NodeEntity(name: "h.jpg", handle: 8, modificationTime: try "2020-10-17T22:01:04Z".date),
+            NodeEntity(name: "i.jpg", handle: 9, modificationTime: try "2020-10-16T22:01:04Z".date),
+            NodeEntity(name: "j.jpg", handle: 10, modificationTime: try "2020-10-12T22:01:04Z".date),
+        ]
+            .toPhotoLibrary(withSortType: .newest, in: .GMT)
+            .photoMonthSections
+        
+        let newCategories = [
+            NodeEntity(name: "a.jpg", handle: 1, modificationTime: try "2022-08-18T22:01:04Z".date),
+            NodeEntity(name: "b.jpg", handle: 2, modificationTime: try "2022-08-17T22:01:04Z".date),
+            NodeEntity(name: "c.mov", handle: 3, hasThumbnail: true, modificationTime: try "2022-08-16T22:01:04Z".date),
+            NodeEntity(name: "d.mp4", handle: 4, hasPreview: true, modificationTime: try "2022-08-15T22:01:04Z".date),
+            NodeEntity(name: "e.jpg", handle: 5, modificationTime: try "2022-08-14T22:01:04Z".date),
+            NodeEntity(name: "f.jpg", handle: 6, modificationTime: try "2022-08-13T22:01:04Z".date),
+            NodeEntity(name: "g.jpg", handle: 7, modificationTime: try "2020-10-18T22:01:04Z".date),
+            NodeEntity(name: "h.jpg", handle: 8, hasThumbnail: true, modificationTime: try "2020-10-17T22:01:04Z".date),
+            NodeEntity(name: "i.jpg", handle: 9, modificationTime: try "2020-10-16T22:01:04Z".date),
+            NodeEntity(name: "j.jpg", handle: 10, hasPreview: true, modificationTime: try "2020-10-12T22:01:04Z".date),
+        ]
+            .toPhotoLibrary(withSortType: .newest, in: .GMT)
+            .photoMonthSections
+        
+        XCTAssertFalse(testCategories.shouldRefresh(to: newCategories, visiblePositions: [:]))
+        
+        let visibleAndRefreshableNodes = [
+            NodeEntity(name: "c.mov", handle: 3, hasThumbnail: true, modificationTime: try "2022-08-16T22:01:04Z".date),
+            NodeEntity(name: "d.mp4", handle: 4, hasPreview: true, modificationTime: try "2022-08-15T22:01:04Z".date),
+            NodeEntity(name: "h.jpg", handle: 8, hasThumbnail: true, modificationTime: try "2020-10-17T22:01:04Z".date),
+            NodeEntity(name: "j.jpg", handle: 10, hasPreview: true, modificationTime: try "2020-10-12T22:01:04Z".date),
+        ]
+        
+        for node in visibleAndRefreshableNodes {
+            XCTAssertTrue(testCategories.shouldRefresh(to: newCategories, visiblePositions: [node.position: true]))
         }
     }
 }
