ce7126b3eeef726b8ecbaa59db32223b90732b7a
Simon Wang <swg@mega.co.nz>
2022-12-20 15:41:00 +1300

    IOS-6729: Fix loadPhotos data race crash by protecting mutable states

--- iMEGA/Camera uploads/ViewModel/PhotosViewModel+FilterAndSort.swift
+++ iMEGA/Camera uploads/ViewModel/PhotosViewModel+FilterAndSort.swift
@@ -9,27 +9,15 @@ extension PhotosViewModel {
         return sortType != .newest && sortType != .oldest ? .newest : sortType
     }
     
-    func reorderPhotos(_ sortType: SortOrderType?, mediaNodes: [MEGANode]) -> [MEGANode] {
-        guard let sortType = sortType,
-              sortType == .newest || sortType == .oldest else { return mediaNodes }
-        
-        return mediaNodes.sorted { node1, node2 in
-            guard let date1 = node1.modificationTime,
-                  let date2 = node2.modificationTime else { return node1.name ?? "" < node2.name ?? "" }
-            
-            return sortType == .newest ? date1 > date2 : date1 < date2
-        }
-    }
-    
     // MARK: - Filter
     
     func filter(nodes: inout [MEGANode], with type: PhotosFilterOptions) {
         guard type != .allMedia else { return }
         
         if type == .images {
-            nodes = nodes.filter({ $0.name?.mnz_isImagePathExtension == true })
+            nodes = nodes.filter({ $0.name?.mnz_isImagePathExtension == true && $0.hasThumbnail() })
         } else {
-            nodes = nodes.filter({ $0.name?.mnz_isVideoPathExtension == true })
+            nodes = nodes.filter({ $0.name?.mnz_isVideoPathExtension == true && $0.hasThumbnail() })
         }
     }
 }
