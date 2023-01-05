17a420cd226f5ba3eef95c72ca439ab114ae9987
Aman Kumar <ak@mega.co.nz>
2023-01-05 14:42:05 +1300

    CC-3183 iOS - Create an album stage 3 - Selection dialogue

---
+++ MEGAUnitTests/Album/AlbumContentAdditionViewModelTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+import MEGA
+
+final class AlbumContentAdditionViewModelTests: XCTestCase {
+    func testOnDone_dismissSetToTrue() {
+        let viewModel = albumContentAdditionViewModel()
+        XCTAssertFalse(viewModel.dismiss)
+        viewModel.onDone()
+        XCTAssertTrue(viewModel.dismiss)
+    }
+    
+    func testOnCancel_dismissSetToTrue() {
+        let viewModel = albumContentAdditionViewModel()
+        XCTAssertFalse(viewModel.dismiss)
+        viewModel.onCancel()
+        XCTAssertTrue(viewModel.dismiss)
+    }
+    
+    private func albumContentAdditionViewModel() -> AlbumContentAdditionViewModel {
+        AlbumContentAdditionViewModel(albumName: "item.name", locationName: "All Locations")
+    }
+}
