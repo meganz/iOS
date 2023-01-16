1e6226a4c9c561a1ffa592cf84b6e208400ecef9
Aman Kumar <ak@mega.co.nz>
2023-01-16 16:19:51 +1300

    CC-3182 iOS - Create an album stage 2 - Naming logic

--- iMEGA/Media Consumption/Album Library/Alert/TextFieldAlertViewModel.swift
+++ iMEGA/Media Consumption/Album Library/Alert/TextFieldAlertViewModel.swift
@@ -3,9 +3,14 @@ import Foundation
 public struct TextFieldAlertViewModel {
     let textString: String = ""
     let title: String
-    let invalidTextTitle: String
     let placeholderText: String
     let affirmativeButtonTitle: String
     let message: String?
-    var action: (String?) -> ()
+    var action: ((String?) -> Void)?
+    var validator: ((String?) -> TextFieldAlertError?)?
+}
+
+public struct TextFieldAlertError {
+    let title: String
+    let description: String
 }
--- iMEGA/Media Consumption/Album Library/Alert/UIAlertController+Extension.swift
+++ iMEGA/Media Consumption/Album Library/Alert/UIAlertController+Extension.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension UIAlertController {
     convenience init(alert: TextFieldAlertViewModel) {
-        self.init(title: alert.title, message: nil, preferredStyle: .alert)
+        self.init(title: alert.title, message: alert.message, preferredStyle: .alert)
         self.addTextField { textField in
             textField.placeholder = alert.placeholderText
             textField.text = alert.textString
@@ -11,23 +11,29 @@ extension UIAlertController {
         }
         
         addAction(UIAlertAction(title: Strings.Localizable.cancel, style: .cancel) { _ in
-            alert.action(nil)
+            alert.action?(nil)
         })
         let textField = self.textFields?.first
         let affirmativeAction = UIAlertAction(title: alert.affirmativeButtonTitle, style: .default) { _ in
-            alert.action(textField?.text)
+            alert.action?(textField?.text?.trim ?? "")
         }
         addAction(affirmativeAction)
-        affirmativeAction.isEnabled = !alert.textString.mnz_isEmpty()
         
         func actionFor(textField: UITextField, within alert: UIAlertController, viewModel: TextFieldAlertViewModel) -> UIAction {
             UIAction(handler: { _ in
-                guard let updatedText = textField.text else { return }
-                let containsInvalidChars = updatedText.mnz_containsInvalidChars()
-                alert.title = containsInvalidChars ? viewModel.invalidTextTitle : viewModel.title
-                textField.textColor = containsInvalidChars ? UIColor.mnz_redError() : UIColor.mnz_label()
-                let empty = updatedText.mnz_isEmpty()
-                alert.actions.last?.isEnabled = (!empty && !containsInvalidChars)
+                var isEnabled = true
+                if let errorItem = viewModel.validator?(textField.text?.trim) {
+                    alert.title = errorItem.title
+                    alert.message = errorItem.description
+                    textField.textColor = UIColor.mnz_redError()
+                    isEnabled = false
+                } else {
+                    alert.title = viewModel.title
+                    alert.message = nil
+                    textField.textColor = UIColor.mnz_label()
+                    isEnabled = true
+                }
+                alert.actions.last?.isEnabled = isEnabled
             })
         }
     }
