ea7d44ce3654dfa15a58ac6d449260826d3aa2eb
Roberto Gómez <rg@mega.nz>
2023-01-19 00:00:48 +1300

    IOS-6807. When a folder is deleted from the web client while you are inside that folder in the app, you remain inside the folder.

--- iMEGA/Cloud drive/CloudDriveViewController+NodesUpdate.swift
+++ iMEGA/Cloud drive/CloudDriveViewController+NodesUpdate.swift
@@ -6,4 +6,24 @@ extension CloudDriveViewController {
         
         return parentNode.toNodeEntity().shouldProcessOnNodesUpdate(withChildNodes: childNodes, updatedNodes: nodesUpdatedArray)
     }
+    
+    @objc func updateControllersStackIfNeeded(_ nodeList: MEGANodeList) {
+        let nodes = nodeList.toNodeArray()
+        let removedNodes = nodes.removedChangeTypeNodes()
+        if removedNodes.toNodeEntities().isNotEmpty {
+            guard let navControllers = navigationController?.viewControllers else { return }
+            var removedNodeInStack: MEGANode?
+            self.navigationController?.viewControllers = navControllers
+                .compactMap {
+                    guard let vc = $0 as? CloudDriveViewController else { return $0 }
+                    guard removedNodeInStack == nil else { return nil }
+                    
+                    removedNodeInStack = removedNodes.first(where: {
+                        vc.parentNode?.handle == $0.handle
+                    })
+                    
+                    return removedNodeInStack == nil ? $0 : nil
+                }
+        }
+    }
 }
