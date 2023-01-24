3f790bd075506290f7b4cdc604d67947d4965e71
Lancy Norbert Fernandes <lf@mega.co.nz>
2023-01-24 21:12:10 +1300

    MEET-1013: Scheduled Meeting Notifications

---
+++ Modules/MEGADomain/Sources/MEGADomain/Entity/Call/ScheduledMeetingRulesEntity.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+public struct ScheduledMeetingRulesEntity {
+    public enum Frequency {
+        case invalid
+        case daily
+        case weekly
+        case monthly
+    }
+    
+    public let frequency: Frequency
+    public let interval: Int
+    public let until: Date?
+    public let monthDayList: [Int]?
+    public let weekDayList: [Int]?
+    public let monthWeekDayList: [[Int]]?
+    
+    public init(
+        frequency: Frequency,
+        interval: Int,
+        until: Date?,
+        monthDayList: [Int]?,
+        weekDayList: [Int]?,
+        monthWeekDayList: [[Int]]?
+    ) {
+        self.frequency = frequency
+        self.interval = interval
+        self.until = until
+        self.monthDayList = monthDayList
+        self.weekDayList = weekDayList
+        self.monthWeekDayList = monthWeekDayList
+    }
+}
