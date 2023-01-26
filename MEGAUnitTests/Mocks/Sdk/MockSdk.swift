b50612030eeb10c0a4272d8d5abf25f07fe2d114
Mauritz Mollentze <mmo@mega.co.nz>
2023-01-26 14:27:25 +1300

    CC-3436: Update UserAlbumRepository element filter bindings

--- MEGAUnitTests/Mocks/Sdk/MockSdk.swift
+++ MEGAUnitTests/Mocks/Sdk/MockSdk.swift
@@ -13,10 +13,9 @@ final class MockSdk: MEGASdk {
     private var statsEventMessage: String?
     private let megaRootNode: MEGANode?
     private let rubbishBinNode: MEGANode?
+    private let sets: [MEGASet]
+    private let setElements: [MEGASetElement]
     private let megaSetElementCounts: [MEGAHandle: UInt]
-    
-    let sets: [MEGASet]
-    let setElements: [MEGASetElement]
     
     var hasGlobalDelegate = false
     
@@ -106,7 +105,74 @@ final class MockSdk: MEGASdk {
         return mockNode.nodePath
     }
     
-    override func megaSetElementCount(_ sid: MEGAHandle) -> UInt {
+    //MARK: - Sets
+    
+    override func megaSets() -> [MEGASet] {
+        sets
+    }
+    
+    override func megaSetElements(bySid sid: MEGAHandle, includeElementsInRubbishBin: Bool) -> [MEGASetElement] {
+        setElements
+    }
+    
+    override func megaSetElementCount(_ sid: MEGAHandle, includeElementsInRubbishBin: Bool) -> UInt {
         megaSetElementCounts[sid] ?? 0
+    }
+    
+    override func createSet(_ name: String?, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        mockRequest.megaSet = MockMEGASet(handle: 1, userId: 0, coverId: 1, name: name ?? "")
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
+    }
+    
+    override func updateSetName(_ sid: MEGAHandle, name: String, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        mockRequest.megaSetName = name
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
+    }
+    
+    override func removeSet(_ sid: MEGAHandle, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        mockRequest.megaSetHandle = sid
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
+    }
+    
+    override func createSetElement(_ sid: MEGAHandle, nodeId: MEGAHandle, name: String?, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
+    }
+    
+    override func updateSetElement(_ sid: MEGAHandle, eid: MEGAHandle, name: String, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        mockRequest.megaSetElementName = name
+        mockRequest.updateSet = false
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
+    }
+    
+    override func updateSetElementOrder(_ sid: MEGAHandle, eid: MEGAHandle, order: Int64, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        mockRequest.megaSetElementOrder = order
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
+    }
+    
+    override func removeSetElement(_ sid: MEGAHandle, eid: MEGAHandle, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        mockRequest.updateSet = false
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
+    }
+    
+    override func putSetCover(_ sid: MEGAHandle, eid: MEGAHandle, delegate: MEGARequestDelegate) {
+        let mockRequest = MockRequest(handle: 1)
+        mockRequest.megaCoverId = eid
+        mockRequest.updateSetCover = true
+        
+        delegate.onRequestFinish?(self, request: mockRequest, error: MEGAError())
     }
 }
