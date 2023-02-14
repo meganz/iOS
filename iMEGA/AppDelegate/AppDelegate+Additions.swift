39c480356767bd65f85eaf2d87994fe294e6a5e6
Glazel Magpayo <gma@mega.co.nz>
2023-02-14 20:26:56 +1300

    IOS-6767 Fingerprint authentication - Set sdk Secure feature flag

--- iMEGA/AppDelegate/AppDelegate+Additions.swift
+++ iMEGA/AppDelegate/AppDelegate+Additions.swift
@@ -311,3 +311,12 @@ extension AppDelegate {
         }
     }
 }
+
+//MARK: - Shared Secure fingerprint
+extension AppDelegate {
+    @objc func configSharedSecureFingerprintFlag() {
+        let secureFlagManager = SharedSecureFingerprintManager()
+        let isSecure = secureFlagManager.secureFingerprintVerification
+        secureFlagManager.setSecureFingerprintFlag(isSecure)
+    }
+}
