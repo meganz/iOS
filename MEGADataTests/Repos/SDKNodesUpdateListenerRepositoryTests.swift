9f197db5f92a1b595e0d29c7d01b112c3878c779
Mauritz Mollentze <mmo@mega.co.nz>
2023-02-16 10:06:57 +1300

    IOS-6901: Update SDKNodesUpdateListenerRepository on node updates to match MEGAGlobalDelegate
    (cherry picked from commit 4fc93ca5bb8cfddb591b0295b66191d821da290c)

---
+++ MEGADataTests/Repos/SDKNodesUpdateListenerRepositoryTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import MEGA
+
+final class SDKNodesUpdateListenerRepositoryTests: XCTestCase {
+    
+    func testMEGAGlobalDelegateonNodesUpdate_shouldPassNodeEntitiesToUpdateHandler() {
+        let mockSdk = MockSdk()
+        let repo = SDKNodesUpdateListenerRepository(sdk: mockSdk)
+        let nodes =  [MockNode(handle: 1),
+                      MockNode(handle: 2)]
+        let exp = expectation(description: "SDK onNodesUpdate pass to onNodesUpdateHandler")
+        repo.onNodesUpdateHandler = {
+            XCTAssertEqual($0, nodes.toNodeEntities())
+            exp.fulfill()
+        }
+        repo.onNodesUpdate(mockSdk, nodeList: MockNodeList(nodes: nodes))
+        wait(for: [exp], timeout: 0.5)
+    }
+}
