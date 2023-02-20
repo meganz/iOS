0bfdefc550ca86cc53cf4df8241458d378a7a3cf
Carlos Martín Acera <cma@mega.nz>
2023-02-20 22:21:38 +1300

    MEET-1889 scheduled meeting description field

--- iMEGA/MeetingInfoScene/Views/MeetingDescriptionView.swift
+++ iMEGA/MeetingInfoScene/Views/MeetingDescriptionView.swift
@@ -8,9 +8,13 @@ struct MeetingDescriptionView: View {
     var body: some View {
         VStack (alignment: .leading) {
             Divider()
+            Text(Strings.Localizable.Meetings.Info.descriptionLabel)
+                .font(.body)
+                .padding(.horizontal)
             Text(description)
                 .font(.body)
-                .padding()
+                .foregroundColor(Color(colorScheme == .dark ? UIColor.mnz_grayEBEBF5() : UIColor.mnz_gray3C3C43()).opacity(0.6))
+                .padding(.horizontal)
             Divider()
         }
         .background(colorScheme == .dark ? Color(Colors.General.Black._1c1c1e.name) : .white)
