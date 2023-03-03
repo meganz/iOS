78eb720e683c2bc512cd98e32dd530e64939bb39
Carlos Martín Acera <cma@mega.nz>
2023-03-04 00:06:09 +1300

    MEET-1214Technical Debt- Create an object container for Start Call

--- MEGADomain/RepositoryProtocol/Calls/MeetingCreatingRepositoryProtocol.swift
+++ MEGADomain/RepositoryProtocol/Calls/MeetingCreatingRepositoryProtocol.swift
@@ -3,15 +3,7 @@ import MEGADomain
 protocol MeetingCreatingRepositoryProtocol {
     func getUsername() -> String
     func getCall(forChatId chatId: UInt64) -> CallEntity?
-    func startCall(
-        meetingName: String,
-        enableVideo: Bool,
-        enableAudio: Bool,
-        speakRequest: Bool,
-        waitingRoom: Bool,
-        allowNonHostToAddParticipants: Bool,
-        completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void
-    )
+    func startCall(_ startCall: StartCallEntity, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void)
     func joinChatCall(forChatId chatId: UInt64, enableVideo: Bool, enableAudio: Bool, userHandle: UInt64, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void)
     func checkChatLink(link: String, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void)
     func createEphemeralAccountAndJoinChat(firstName: String, lastName: String, link: String, completion: @escaping (Result<Void, MEGASDKErrorType>) -> Void, karereInitCompletion: @escaping () -> Void)
