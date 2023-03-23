8f485321b7b078108a4190e8b60fcb75da6edba5
Javier Navarro <jn@mega.co.nz>
2023-03-24 01:12:32 +1300

    IOS-4172: MEGASDK local spm module

--- download_3rdparty.sh
+++ download_3rdparty.sh
@@ -21,20 +21,20 @@ mega-get $fileUrl $filePath
 # Sometimes the submodules might take time to run, so the folder might not exists.
 # We have 5 attemps to check if the folder exists with 20 seconds apart.
 attempt=0
-until [ ! $attempt -lt 5 ] || [ -d "./iMEGA/Vendor/SDK/bindings/ios/3rdparty" ]
+until [ ! $attempt -lt 5 ] || [ -d "./Modules/MEGASDK/Sources/MEGASDK/bindings/ios/3rdparty" ]
 do
    attempt=`expr $attempt + 1`
    echo "Attempt number: $attempt. The folder does not exists. Waiting for 20 seconds and trying it again."
    sleep 20 
 done
 
-if [ ! -d "./iMEGA/Vendor/SDK/bindings/ios/3rdparty" ] 
+if [ ! -d "./Modules/MEGASDK/Sources/MEGASDK/bindings/ios/3rdparty" ] 
 then
    echo "Could not unzip the 3rd party libraries"
    exit 1 
 else
    echo "Unzipping the 3rd party libraries"
-   unzip -o ${filePath}/3rdparty.zip -d ./iMEGA/Vendor/SDK/bindings/ios/3rdparty/
+   unzip -o ${filePath}/3rdparty.zip -d ./Modules/MEGASDK/Sources/MEGASDK/bindings/ios/3rdparty/
    echo "Unzip 3rd party libraries complete"
    exit 0
 fi
