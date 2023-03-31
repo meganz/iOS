9e54571591b282c408fc05050be6d5661252bd72
Mahmud Ahsan <mah@mega.co.nz>
2023-03-31 16:29:45 +1300

    CC-4036: rwl and raw with no thumbnails are displaying on Timeline

--- iMEGA/Camera uploads/ViewModel/PhotosViewModel+FilterAndSort.swift
+++ iMEGA/Camera uploads/ViewModel/PhotosViewModel+FilterAndSort.swift
@@ -12,11 +12,11 @@ extension PhotosViewModel {
     // MARK: - Filter
     
     func filter(nodes: inout [NodeEntity], with type: PhotosFilterOptions) {
-        guard type != .allMedia else { return }
-        
-        if type == .images {
+        if type == .allMedia {
+            nodes = nodes.filter { $0.hasThumbnail }
+        } else if type == .images {
             nodes = nodes.filter({ $0.name.mnz_isImagePathExtension && $0.hasThumbnail })
-        } else {
+        } else if type == .videos {
             nodes = nodes.filter({ $0.name.mnz_isVideoPathExtension && $0.hasThumbnail })
         }
     }
