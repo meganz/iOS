ca0340277cf975e411782e7a81991bcba065357b
Aleksa Simic <ami@mega.co.nz>
2023-04-18 23:26:32 +1200

    IOS-5114: Fix problems in landscape orientation on login screen

--- iMEGA/My Account/Login/LoginViewController+Additions.swift
+++ iMEGA/My Account/Login/LoginViewController+Additions.swift
@@ -1,10 +1,20 @@
 import MEGASwift
 
 extension LoginViewController {
+    private enum Constants {
+        static let landscapeLoginButtonTopPadding: CGFloat = 45
+        static let portraitLoginButtonTopPadding: CGFloat = 155
+    }
+
     @objc func recoveryPasswordURL(_ email: String?) -> URL {
         let encodedEmail = email?.base64Encoded
         let recoveryURLString = encodedEmail != nil ? "https://mega.nz/recovery?email=\(encodedEmail ?? "")" : "https://mega.nz/recovery"
     
         return URL(string: recoveryURLString) ?? URL(fileURLWithPath: "")
+    }
+    
+    @objc func updateLoginButtonTopConstraint() {
+        loginButtonTopCostraint.constant =  UIDevice.current.orientation.isLandscape ? Constants.landscapeLoginButtonTopPadding
+        : Constants.portraitLoginButtonTopPadding
     }
 }
