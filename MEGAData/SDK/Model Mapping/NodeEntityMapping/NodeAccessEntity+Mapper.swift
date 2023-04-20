caaeb3ee382e6b74025063b3818d2f4e98d689f2
Roberto Gómez <rg@mega.nz>
2023-04-20 19:26:00 +1200

    IOS-6661. CU folder options menu disappears each time an upload takes place.

--- MEGAData/SDK/Model Mapping/NodeEntityMapping/NodeAccessEntity+Mapper.swift
+++ MEGAData/SDK/Model Mapping/NodeEntityMapping/NodeAccessEntity+Mapper.swift
@@ -4,4 +4,19 @@ extension NodeAccessTypeEntity {
     init?(shareAccess: MEGAShareType) {
         self.init(rawValue: shareAccess.rawValue)
     }
+    
+    func toShareAccessLevel() -> ShareAccessLevelEntity {
+        switch self {
+        case .read:
+            return .read
+        case .readWrite:
+            return .readWrite
+        case .full:
+            return .full
+        case .owner:
+            return .owner
+        case .unknown:
+            return .unknown
+        }
+    }
 }
---
+++ MEGADataTests/SDK/Model Mapping/NodeAccessEntity+Mapper_Tests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import MEGA
+@testable import MEGADomain
+
+final class NodeAccessEntity_Mapper_Tests: XCTestCase {
+    
+    func testInitWithShareAccess() {
+        XCTAssertEqual(NodeAccessTypeEntity(shareAccess: .accessRead), .read)
+        XCTAssertEqual(NodeAccessTypeEntity(shareAccess: .accessReadWrite), .readWrite)
+        XCTAssertEqual(NodeAccessTypeEntity(shareAccess: .accessFull), .full)
+        XCTAssertEqual(NodeAccessTypeEntity(shareAccess: .accessOwner), .owner)
+        XCTAssertEqual(NodeAccessTypeEntity(shareAccess: .accessUnknown), .unknown)
+    }
+    
+    func testToShareAccessLevel() {
+        XCTAssertEqual(NodeAccessTypeEntity.read.toShareAccessLevel(), .read)
+        XCTAssertEqual(NodeAccessTypeEntity.readWrite.toShareAccessLevel(), .readWrite)
+        XCTAssertEqual(NodeAccessTypeEntity.full.toShareAccessLevel(), .full)
+        XCTAssertEqual(NodeAccessTypeEntity.owner.toShareAccessLevel(), .owner)
+        XCTAssertEqual(NodeAccessTypeEntity.unknown.toShareAccessLevel(), .unknown)
+    }
+}
