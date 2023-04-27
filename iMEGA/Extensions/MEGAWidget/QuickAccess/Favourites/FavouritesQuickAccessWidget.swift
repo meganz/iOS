81f6da597262cea01898eebd4b45a0d20615b8b1
Carlos Martín Acera <cma@mega.nz>
2023-04-28 00:43:30 +1200

    IOS-7035 Move data repository folder to megadata module

--- iMEGA/Extensions/MEGAWidget/QuickAccess/Favourites/FavouritesQuickAccessWidget.swift
+++ iMEGA/Extensions/MEGAWidget/QuickAccess/Favourites/FavouritesQuickAccessWidget.swift
@@ -1,13 +1,14 @@
 import WidgetKit
 import SwiftUI
 import MEGADomain
+import MEGAData
 
 struct FavouritesTimelineProvider: TimelineProvider {
     typealias Entry = QuickAccessWidgetEntry
 
     var viewModel = FavouritesQuickAccessWidgetViewModel(
         credentialUseCase: CredentialUseCase(repo: CredentialRepository.newRepo),
-        copyDataBasesUseCase: CopyDataBasesUseCase(repo: CopyDataBasesRepository(fileManager: FileManager.default)),
+        copyDataBasesUseCase: CopyDataBasesUseCase(repo: CopyDataBasesRepository.newRepo),
         favouriteItemsUseCase: FavouriteItemsUseCase(repo: FavouriteItemsRepository(store: MEGAStore.shareInstance()))
     )
     
