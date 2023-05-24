d7bcbba0fd8874ef508ac317fd09027cfa293a78
Javier Navarro <jn@mega.co.nz>
2023-05-24 16:18:36 +0200

    IOS-7205: hang risk due to priority inversion in localLogout

--- iMEGA/Utils/MEGASDK/MEGASdkManager+CleanUp.m
+++ iMEGA/Utils/MEGASDK/MEGASdkManager+CleanUp.m
@@ -8,7 +8,9 @@
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     [MEGASdkManager.sharedMEGASdk localLogoutWithDelegate:[MEGAGenericRequestDelegate.alloc initWithCompletion:^(MEGARequest * _Nonnull request, MEGAError * _Nonnull error) {
         [MEGASdkManager.sharedMEGAChatSdk localLogoutWithDelegate:[MEGAChatGenericRequestDelegate.alloc initWithCompletion:^(MEGAChatRequest * _Nonnull request, MEGAChatError * _Nonnull error) {
-            dispatch_semaphore_signal(semaphore);
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), ^{
+                dispatch_semaphore_signal(semaphore);
+            });
         }]];
     }]];
     dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC));
