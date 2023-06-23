a13142cc6154f056cc6181007a5a71d92a81c5e4
Simon Wang <swg@mega.co.nz>
2023-06-23 15:03:04 +1200

    Remove the flaky AsyncThrowingValue test

--- Modules/Infrastracture/MEGASwift/Tests/MEGASwiftTests/AsyncValuesTests.swift
+++ Modules/Infrastracture/MEGASwift/Tests/MEGASwiftTests/AsyncValuesTests.swift
@@ -10,25 +10,6 @@ final class AsyncValuesTests: XCTestCase {
         }
     }
 
-    func testWithAsyncThrowingValue_onCancelleed_shouldFail() async {
-        do {
-            let task = Task {
-                try await withAsyncThrowingValue { completion in
-                    DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) {
-                        completion(.success)
-                    }
-                }
-            }
-            task.cancel()
-            try await task.value
-            XCTFail("Did not throw error! Expected to catch cancellation error")
-        } catch let error as CancellationError {
-            XCTAssertNotNil(error)
-        } catch {
-            XCTFail("Invalid exception caught")
-        }
-    }
-
     func testWithAsyncValue_onExecuted_shouldSucceed() async {
         await withAsyncValue { completion in
             DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
