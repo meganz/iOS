748f38e4b20eab38a1332d2e457631cefb8e3063
Joanda Febrian <jfe@mega.co.nz>
2023-06-27 15:10:27 +0700

    IOS-7373: Add platforms config in MEGATest Swift Package

--- Modules/Infrastracture/MEGATest/Package.swift
+++ Modules/Infrastracture/MEGATest/Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MEGATest",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v14)
+    ],
     products: [
         .library(
             name: "MEGATest",
