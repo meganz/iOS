a639ee7282b6330f7fd5c0d35da33547c24d2118
Mauritz Mollentze <mmo@mega.co.nz>
2023-06-29 16:56:49 +1200

    CC-4722: Add PublicAlbumUseCase to retrieve photos from link

--- Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockRepos/MockShareAlbumRepository.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockRepos/MockShareAlbumRepository.swift
@@ -5,15 +5,18 @@ public struct MockShareAlbumRepository: ShareAlbumRepositoryProtocol {
     private let shareAlbumResults: [HandleEntity: Result<String?, Error>]
     private let disableAlbumShareResult: Result<Void, Error>
     private let publicAlbumContentsResult: Result<SharedAlbumEntity, Error>
+    private let publicPhotoResults: [HandleEntity: Result<NodeEntity, Error>]
     
     public init(
         shareAlbumResults: [HandleEntity: Result<String?, Error>] = [:],
         disableAlbumShareResult: Result<Void, Error> = .failure(GenericErrorEntity()),
-        publicAlbumContentsResult: Result<SharedAlbumEntity, Error> = .failure(GenericErrorEntity())
+        publicAlbumContentsResult: Result<SharedAlbumEntity, Error> = .failure(GenericErrorEntity()),
+        publicPhotoResults: [HandleEntity: Result<NodeEntity, Error>] = [:]
     ) {
         self.shareAlbumResults = shareAlbumResults
         self.disableAlbumShareResult = disableAlbumShareResult
         self.publicAlbumContentsResult = publicAlbumContentsResult
+        self.publicPhotoResults = publicPhotoResults
     }
     
     public func shareAlbumLink(_ album: AlbumEntity) async throws -> String? {
@@ -34,6 +37,15 @@ public struct MockShareAlbumRepository: ShareAlbumRepositoryProtocol {
     public func publicAlbumContents(forLink link: String) async throws -> SharedAlbumEntity {
         try await withCheckedThrowingContinuation {
             $0.resume(with: publicAlbumContentsResult)
+        }
+    }
+    
+    public func publicPhoto(forPhotoId id: HandleEntity) async throws -> NodeEntity {
+        guard let publicPhotoResult = publicPhotoResults.first(where: { $0.key == id })?.value else {
+            throw SharedPhotoErrorEntity.photoNotFound
+        }
+        return try await withCheckedThrowingContinuation {
+            $0.resume(with: publicPhotoResult)
         }
     }
 }
