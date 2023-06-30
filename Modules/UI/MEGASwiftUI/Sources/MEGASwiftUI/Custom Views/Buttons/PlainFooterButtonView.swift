4a2aae2bf5f8a419035e7c7216e3fc29a2e7e76a
Glazel Magpayo <gma@mega.co.nz>
2023-06-30 18:05:37 +1200

    AP-229 Add Terms and Policies on Upgrade Plan View revamp

--- Modules/UI/MEGASwiftUI/Sources/MEGASwiftUI/Custom Views/Buttons/PlainFooterButtonView.swift
+++ Modules/UI/MEGASwiftUI/Sources/MEGASwiftUI/Custom Views/Buttons/PlainFooterButtonView.swift
@@ -1,21 +1,23 @@
 import SwiftUI
 
 public struct PlainFooterButtonView: View {
-    let title: String
-    let action: (() -> Void)
+    private let title: String
+    @Binding private var didTapButton: Bool
     
     @Environment(\.colorScheme) var colorScheme
     private var textColor: Color {
         colorScheme == .dark ? Color(red: 0, green: 0.76, blue: 0.60) : Color(red: 0, green: 0.65, blue: 0.52)
     }
     
-    public init(title: String, action: @escaping () -> Void) {
+    public init(title: String, didTapButton: Binding<Bool>) {
         self.title = title
-        self.action = action
+        self._didTapButton = didTapButton
     }
     
     public var body: some View {
-        Button(action: action) {
+        Button {
+            didTapButton = true
+        } label: {
             Text(title)
                 .foregroundColor(textColor)
                 .font(.footnote)
