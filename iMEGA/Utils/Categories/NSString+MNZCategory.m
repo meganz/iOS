2a2890e8e16686a1cf86b299075288089301f877
Mike Thornton <mth@mega.co.nz>
2023-07-05 15:07:04 -0700

    IOS-7321: Migrate mnz_fileNameWithoutExtension category method to Swift

--- iMEGA/Utils/Categories/NSString+MNZCategory.m
+++ iMEGA/Utils/Categories/NSString+MNZCategory.m
@@ -830,18 +830,6 @@ static NSString* const B = @"[B]";
     return [nameWithoutExtension stringByAppendingPathExtension:extension];
 }
 
-- (NSString *)mnz_fileNameWithoutExtension {
-    NSString *fileName;
-    
-    NSMutableArray<NSString *> *fileNameComponents = [[self componentsSeparatedByString:@"."] mutableCopy];
-    if (fileNameComponents.count > 1) {
-        [fileNameComponents removeLastObject];
-    }
-    fileName = [fileNameComponents componentsJoinedByString:@"."];
-    
-    return fileName;
-}
-
 - (NSString *)mnz_stringByRemovingInvalidFileCharacters {
     NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@":/\\"];
     return [[self componentsSeparatedByCharactersInSet:set] componentsJoinedByString:@""];
