bfede664b498db35452c4795283bf23e717c144e
Glazel Magpayo <gma@mega.co.nz>
2023-07-06 16:19:05 +1200

    AP-230 Add Restore purchase on new plan page

--- Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockUseCases/MockAccountPlanPurchaseUseCase.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockUseCases/MockAccountPlanPurchaseUseCase.swift
@@ -1,14 +1,51 @@
+import Combine
 import MEGADomain
 
-public struct MockAccountPlanPurchaseUseCase: AccountPlanPurchaseUseCaseProtocol {
+final public class MockAccountPlanPurchaseUseCase: AccountPlanPurchaseUseCaseProtocol {
     private var accountPlanProducts: [AccountPlanEntity]
+    private let _successfulRestorePublisher: PassthroughSubject<Void, Never>
+    private let _incompleteRestorePublisher: PassthroughSubject<Void, Never>
+    private let _failedRestorePublisher: PassthroughSubject<AccountPlanErrorEntity, Never>
     
-    public init(accountPlanProducts: [AccountPlanEntity] = []) {
+    public var restorePurchaseCalled = 0
+    public var registerRestoreDelegateCalled = 0
+    public var deRegisterRestoreDelegateCalled = 0
+    
+    public init(accountPlanProducts: [AccountPlanEntity] = [],
+                successfulRestorePublisher: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>(),
+                incompleteRestorePublisher: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>(),
+                failedRestorePublisher: PassthroughSubject<AccountPlanErrorEntity, Never> = PassthroughSubject<AccountPlanErrorEntity, Never>()) {
         self.accountPlanProducts = accountPlanProducts
+        _successfulRestorePublisher = successfulRestorePublisher
+        _incompleteRestorePublisher = incompleteRestorePublisher
+        _failedRestorePublisher = failedRestorePublisher
     }
     
     public func accountPlanProducts() async -> [AccountPlanEntity] {
         accountPlanProducts
     }
     
+    public var successfulRestorePublisher: AnyPublisher<Void, Never> {
+        _successfulRestorePublisher.eraseToAnyPublisher()
+    }
+    
+    public var incompleteRestorePublisher: AnyPublisher<Void, Never> {
+        _incompleteRestorePublisher.eraseToAnyPublisher()
+    }
+    
+    public var failedRestorePublisher: AnyPublisher<AccountPlanErrorEntity, Never> {
+        _failedRestorePublisher.eraseToAnyPublisher()
+    }
+    
+    public func restorePurchase() async {
+        restorePurchaseCalled += 1
+    }
+    
+    public func registerRestoreDelegate() async {
+        registerRestoreDelegateCalled += 1
+    }
+    
+    public func deRegisterRestoreDelegate() async {
+        deRegisterRestoreDelegateCalled += 1
+    }
 }
