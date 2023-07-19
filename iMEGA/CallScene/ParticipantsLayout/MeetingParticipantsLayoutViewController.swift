bc88881f19345e80e801fb8497bb575b392cf15d
Banghua ZHao <bz@mega.co.nz>
2023-07-19 10:24:03 +0800

    MEET-2155: Call UI issue with 8 Participants in the call caused by page control

--- iMEGA/CallScene/ParticipantsLayout/MeetingParticipantsLayoutViewController.swift
+++ iMEGA/CallScene/ParticipantsLayout/MeetingParticipantsLayoutViewController.swift
@@ -285,10 +285,10 @@ final class MeetingParticipantsLayoutViewController: UIViewController, ViewType 
         pageControl.numberOfPages = Int(ceil(Double(participantsCount) / 6.0))
         if pageControl.isHidden && participantsCount > 6 {
             pageControl.isHidden = false
-            callCollectionView.layoutIfNeeded()
+            callCollectionView.reloadData()
         } else if !pageControl.isHidden && participantsCount <= 6 {
             pageControl.isHidden = true
-            callCollectionView.layoutIfNeeded()
+            callCollectionView.reloadData()
         }
     }
     
