d9866286d989fd28b9f52be6fcd915dec9d3da68
Aleksa Simic <ami@mega.co.nz>
2023-08-01 03:31:45 +1200

    IOS-7486: Fix file link crash when the link is invalid

--- iMEGA/Node/Links/File/FileLinkViewController+Additions.swift
+++ iMEGA/Node/Links/File/FileLinkViewController+Additions.swift
@@ -1,7 +1,7 @@
 
 extension FileLinkViewController {
     @objc func download() {
-        guard let linkUrl = URL(string: publicLinkString) else { return }
+        guard let publicLinkString = publicLinkString, let linkUrl = URL(string: publicLinkString) else { return }
         DownloadLinkRouter(link: linkUrl, isFolderLink: false, presenter: self).start()
     }
 
