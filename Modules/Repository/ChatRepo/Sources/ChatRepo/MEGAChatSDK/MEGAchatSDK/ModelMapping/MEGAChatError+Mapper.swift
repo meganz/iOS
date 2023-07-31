3ca72bd3d00f899df6c220890f3eff418aaf54fa
Javier Navarro <jn@mega.co.nz>
2023-07-31 22:44:06 +1200

    IOS-7438: create chatrepo module

--- Modules/Repository/ChatRepo/Sources/ChatRepo/MEGAChatSDK/MEGAchatSDK/ModelMapping/MEGAChatError+Mapper.swift
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/MEGAChatSDK/MEGAchatSDK/ModelMapping/MEGAChatError+Mapper.swift
@@ -1,3 +1,3 @@
-import Foundation
+import MEGAChatSdk
 
 extension MEGAChatError: Error {}
--- Modules/Repository/ChatRepo/Sources/ChatRepo/MEGAChatSDK/MEGAchatSDK/RequestDelegate/ChatRequestDelegate.swift
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/MEGAChatSDK/MEGAchatSDK/RequestDelegate/ChatRequestDelegate.swift
@@ -1,3 +1,4 @@
+import MEGAChatSdk
 
 public typealias MEGAChatRequestCompletion = (_ result: Result<MEGAChatRequest, MEGAChatError>) -> Void
 
