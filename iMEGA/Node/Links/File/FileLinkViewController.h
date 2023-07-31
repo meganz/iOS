27d5aaec43b3e4120f8fff6d4b3b5ee5a115ea79
Aleksa Simic <ami@mega.co.nz>
2023-08-01 03:31:45 +1200

    IOS-7486: Fix file link crash when the link is invalid

--- iMEGA/Node/Links/File/FileLinkViewController.h
+++ iMEGA/Node/Links/File/FileLinkViewController.h
@@ -3,20 +3,24 @@
 @class ContextMenuManager;
 @class SendLinkToChatsDelegate;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface FileLinkViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *moreBarButtonItem;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *shareLinkBarButtonItem;
 
-@property (strong, nonatomic) MEGANode *node;
-@property (nonatomic, strong) NSString *publicLinkString;
-@property (nonatomic, strong) NSString *linkEncryptedString;
+@property (strong, nonatomic, nullable) MEGANode *node;
+@property (nonatomic, strong, nullable) NSString *publicLinkString;
+@property (nonatomic, strong, nullable) NSString *linkEncryptedString;
 
 @property (nonatomic, strong) MEGARequest *request;
 @property (nonatomic, strong) MEGAError *error;
-@property (nonatomic, strong) ContextMenuManager * contextMenuManager;
+@property (nonatomic, strong, nullable) ContextMenuManager * contextMenuManager;
 @property (nonatomic) SendLinkToChatsDelegate *sendLinkDelegate;
 
 - (void)importFromFiles;
 
 @end
+
+NS_ASSUME_NONNULL_END
