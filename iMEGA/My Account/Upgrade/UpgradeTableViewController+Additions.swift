6540e45e51c4ad4ab4b6329c45755cec07c49382
Glazel Magpayo <gma@mega.co.nz>
2023-08-03 11:23:24 +1200

    AP-338 Eligible pro plans are missing from the upgrade account screen

--- iMEGA/My Account/Upgrade/UpgradeTableViewController+Additions.swift
+++ iMEGA/My Account/Upgrade/UpgradeTableViewController+Additions.swift
@@ -12,15 +12,45 @@ extension UpgradeTableViewController {
     @objc func createUpgradeAccountViewModel() -> UpgradeAccountViewModel {
         UpgradeAccountViewModel(accountPlanAnalyticsUsecase: AccountPlanAnalyticsUseCase(repository: AnalyticsRepository.newRepo))
     }
+    
+    @objc func setCurrentPlanMaxQuotaData() {
+        guard let accountDetails = MEGASdkManager.sharedMEGASdk().mnz_accountDetails,
+              let storage = accountDetails.storageMax as? Int64,
+              let transfer = accountDetails.transferMax as? Int64 else {
+            return
+        }
 
-    @objc func removeSubscriptionOrder(at index: Int) {
-        guard index < proLevelsMutableArray.count else { return }
-        proLevelsMutableArray.removeObject(at: index)
+        let storageMax = String.memoryStyleString(fromByteCount: storage)
+        let transferMax = String.memoryStyleString(fromByteCount: transfer)
+        currentPlanStorageLabel.attributedText = quotaAttributedText(maxQuota: storageMax, fullQuotaString: Strings.Localizable.Account.storageQuota(storageMax))
+        currentPlanBandwidthLabel.attributedText = quotaAttributedText(maxQuota: transferMax, fullQuotaString: Strings.Localizable.Account.storageQuota(transferMax))
+    }
+    
+    private func quotaAttributedText(maxQuota: String, fullQuotaString: String) -> NSMutableAttributedString {
+        let attributedQuota = NSMutableAttributedString(
+            string: fullQuotaString,
+            attributes: [.font: UIFont.preferredFont(style: .caption1, weight: .medium),
+                         .foregroundColor: UIColor.mnz_primaryGray(for: self.traitCollection)]
+        )
+        
+        if let maxQuotaRange = fullQuotaString.range(of: maxQuota) {
+            attributedQuota.addAttributes(
+                [.foregroundColor: UIColor.mnz_label()],
+                range: NSRange(maxQuotaRange, in: fullQuotaString)
+            )
+        }
+        return attributedQuota
     }
 }
 
 // MARK: - Product Plan
 extension UpgradeTableViewController {
+    @objc func removePlanOnAvailablePlans(_ planType: MEGAAccountType) {
+        let accountType = planType.rawValue
+        guard proLevelsMutableArray.contains(accountType) else { return }
+        proLevelsMutableArray.remove(accountType)
+    }
+    
     @objc func getAvailableProductPlans() -> [NSNumber] {
         guard let products = MEGAPurchase.sharedInstance().products as? [SKProduct] else {
             return []
--- iMEGA/My Account/Upgrade/UpgradeTableViewController.h
+++ iMEGA/My Account/Upgrade/UpgradeTableViewController.h
@@ -5,6 +5,8 @@
 @interface UpgradeTableViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UILabel *currentPlanStorageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentPlanBandwidthLabel;
 @property (nonatomic, getter=isChoosingTheAccountType) BOOL chooseAccountType;
 @property (strong, nonatomic) UpgradeAccountViewModel *viewModel;
 @property (strong, nonatomic) NSMutableArray *proLevelsMutableArray;
