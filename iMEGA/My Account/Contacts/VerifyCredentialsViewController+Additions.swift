b954bab07199c6be03d3ad064baf341542fdb6b0
Aleksa Simic <ami@mega.co.nz>
2023-08-02 21:06:36 +1200

    FM-639: Changes to contact verification dialog

--- iMEGA/My Account/Contacts/VerifyCredentialsViewController+Additions.swift
+++ iMEGA/My Account/Contacts/VerifyCredentialsViewController+Additions.swift
@@ -2,16 +2,20 @@
 extension VerifyCredentialsViewController {
     
     @objc func setContentMessages() {
+        navigationItem.title = Strings.Localizable.verifyCredentials
+        myCredentialsHeaderLabel.text = Strings.Localizable.SharedItems.ContactVerification.Section.MyCredentials.message
+
         if isVerifyContactForSharedItem {
-            navigationItem.title = Strings.Localizable.verifyCredentials
-            myCredentialsHeaderLabel.text = Strings.Localizable.SharedItems.ContactVerification.Section.MyCredentials.message
             contactHeaderLabel.text = isIncomingSharedItem ?
             Strings.Localizable.SharedItems.ContactVerification.Section.VerifyContact.Receiver.message:
             Strings.Localizable.SharedItems.ContactVerification.Section.VerifyContact.Owner.message
         } else {
-            navigationItem.title = Strings.Localizable.verifyCredentials
-            myCredentialsHeaderLabel.text = Strings.Localizable.thisIsBestDoneInRealLife
-            contactHeaderLabel.text = ""
+            contactHeaderLabel.text = Strings.Localizable.VerifyCredentials.headerMessage
         }
+    }
+
+    @objc func setLabelColors() {
+        contactHeaderLabel.textColor = UIColor.mnz_primaryGray(for: traitCollection)
+        myCredentialsHeaderLabel.textColor = UIColor.mnz_primaryGray(for: traitCollection)
     }
 }
