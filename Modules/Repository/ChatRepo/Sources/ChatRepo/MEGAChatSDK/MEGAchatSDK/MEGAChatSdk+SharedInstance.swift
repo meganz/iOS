87a25083e21efaa0f274b0fdb65582093e3aadc6
Javier Navarro <jn@mega.co.nz>
2023-08-03 21:01:06 +1200

    IOS-7500 megachatsdk+sharedinstance in chatrepo

---
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/MEGAChatSDK/MEGAchatSDK/MEGAChatSdk+SharedInstance.swift
@@ -0,0 +1,11 @@
+import MEGAChatSdk
+import MEGASDKRepo
+
+public extension MEGAChatSdk {
+    static let sharedChatSdk: MEGAChatSdk = {
+        let chatSdk = MEGAChatSdk(MEGASdk.sharedSdk)
+        MEGASdk.setLogToConsole(false)
+        MEGAChatSdk.setLogToConsole(true)
+        return chatSdk
+    }()
+}
--- iMEGA/Utils/MEGASDK/MEGAChatSdk+SharedInstanceWrapper.swift
+++ iMEGA/Utils/MEGASDK/MEGAChatSdk+SharedInstanceWrapper.swift
@@ -1,12 +1,14 @@
+import ChatRepo
+
 extension MEGAChatSdk {
     @objc static let shared: MEGAChatSdk = {
 #if MNZ_NOTIFICATION_EXTENSION
         let chatSdk = MEGAChatSdk(MEGASdk.sharedNSE)
-#else
-        let chatSdk = MEGAChatSdk(MEGASdk.shared)
-#endif
         MEGASdk.setLogToConsole(false)
         MEGAChatSdk.setLogToConsole(true)
+#else
+        let chatSdk = MEGAChatSdk.sharedChatSdk
+#endif
         return chatSdk
     }()
 }
