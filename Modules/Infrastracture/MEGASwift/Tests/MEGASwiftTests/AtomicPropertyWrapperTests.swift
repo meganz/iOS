0496941a6da432dc113059bfb38ee7f6446f2cd9
Harry Yan <hy@mega.co.nz>
2023-08-04 11:01:46 +1200

    IOS-7453: Add Atomic property wrapper

---
+++ Modules/Infrastracture/MEGASwift/Tests/MEGASwiftTests/AtomicPropertyWrapperTests.swift
@@ -0,0 +1,21 @@
+@testable import MEGASwift
+import XCTest
+
+final class AtomicPropertyWrapperTests: XCTestCase {
+    
+    func testAtomicAccess_concurrentWrite_shouldSucceed() async throws {
+        @Atomic var counter: Int = 0
+        
+        try await withThrowingTaskGroup(of: Void.self) { group in
+            for _ in 0..<1000 {
+                group.addTask {
+                    $counter.mutate { $0 += 1 }
+                }
+            }
+            
+            try await group.waitForAll()
+            
+            XCTAssertEqual(counter, 1000)
+        }
+    }
+}
