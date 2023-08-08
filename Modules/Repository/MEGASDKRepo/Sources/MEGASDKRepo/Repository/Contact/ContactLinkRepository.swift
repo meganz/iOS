59aa5d7a9d83db78190daed6cf993efacf7423be
Javier Navarro <jn@mega.co.nz>
2023-08-08 14:50:59 +0200

    IOS-7532: move ContactLinkRepository to MEGASdkRepo

---
+++ Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Repository/Contact/ContactLinkRepository.swift
@@ -0,0 +1,29 @@
+import Combine
+import MEGADomain
+import MEGASdk
+import MEGASwift
+
+public struct ContactLinkRepository: ContactLinkRepositoryProtocol {
+    public static var newRepo: ContactLinkRepository {
+        ContactLinkRepository(sdk: MEGASdk.sharedSdk)
+    }
+    
+    private let sdk: MEGASdk
+    
+    public init(sdk: MEGASdk) {
+        self.sdk = sdk
+    }
+    
+    public func contactLinkQuery(handle: HandleEntity) async throws -> ContactLinkEntity? {
+        try await withAsyncThrowingValue { completion in
+            sdk.contactLinkQuery(withHandle: handle, delegate: RequestDelegate { result in
+                switch result {
+                case .success(let request):
+                    completion(.success(ContactLinkEntity(email: request.email, name: request.name)))
+                case .failure:
+                    completion(.failure(GenericErrorEntity()))
+                }
+            })
+        }
+    }
+}
