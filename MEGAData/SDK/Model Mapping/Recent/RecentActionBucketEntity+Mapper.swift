1cbf7e146bacdbebb1653e863d1bc71d61296367
Javier Navarro <jn@mega.co.nz>
2023-08-10 03:49:11 +1200

    IOS-7534: move RecentNodesRepository to MEGASdkRepo

--- MEGAData/SDK/Model Mapping/Recent/RecentActionBucketEntity+Mapper.swift
+++
@@ -1,7 +0,0 @@
-import MEGADomain
-
-extension RecentActionBucketEntity {
-    init(with recentActionBucket: MEGARecentActionBucket) {
-        self.init(date: recentActionBucket.timestamp, userEmail: recentActionBucket.userEmail, parentHandle: recentActionBucket.parentHandle, isUpdate: recentActionBucket.isUpdate, isMedia: recentActionBucket.isMedia, nodes: recentActionBucket.nodesList.toNodeEntities())
-    }
-}
