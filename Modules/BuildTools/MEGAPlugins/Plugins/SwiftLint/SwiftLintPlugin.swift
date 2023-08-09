163fbc5ca57e4826c66c61f836d35ba7ab7ca0d8
João Pedro de Amorim <jpa@mega.co.nz>
2023-08-09 15:20:37 -0300

    IOS-7533: Run lint only on new/modified files and add 'disallow_megasdkmanager'

--- Modules/BuildTools/MEGAPlugins/Plugins/SwiftLint/SwiftLintPlugin.swift
+++ Modules/BuildTools/MEGAPlugins/Plugins/SwiftLint/SwiftLintPlugin.swift
@@ -1,8 +1,16 @@
+import Foundation
 import PackagePlugin
 
 @main
 struct SwiftLintPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let swiftFiles = fetchModifiedSwiftFiles()
+
+        guard !swiftFiles.isEmpty else {
+            print("No new or modified .swift files, return")
+            return []
+        }
+
         return [
             .buildCommand(
                 displayName: "Running SwiftLint for \(target.name)",
@@ -13,9 +21,8 @@ struct SwiftLintPlugin: BuildToolPlugin {
                     "--config",
                     "\(context.package.directory.string)/.swiftlint.yml",
                     "--cache-path",
-                    "\(context.pluginWorkDirectory.string)/cache",
-                    target.directory.string
-                ],
+                    "\(context.pluginWorkDirectory.string)/cache"
+                ] + swiftFiles,
                 environment: [:]
             )
         ]
@@ -27,6 +34,13 @@ import XcodeProjectPlugin
 
 extension SwiftLintPlugin: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        let swiftFiles = fetchModifiedSwiftFiles()
+
+        guard !swiftFiles.isEmpty else {
+            print("No new or modified .swift files, return")
+            return []
+        }
+
         return [
             .buildCommand(
                 displayName: "Running SwiftLint for \(target.displayName)",
@@ -37,12 +51,38 @@ extension SwiftLintPlugin: XcodeBuildToolPlugin {
                     "--config",
                     "\(context.xcodeProject.directory.string)/.swiftlint.yml",
                     "--cache-path",
-                    "\(context.pluginWorkDirectory.string)/cache",
-                    context.xcodeProject.directory.string
-                ],
+                    "\(context.pluginWorkDirectory.string)/cache"
+                ] + swiftFiles,
                 environment: [:]
             )
         ]
     }
 }
 #endif
+
+// MARK: - Helper methods
+extension SwiftLintPlugin {
+    /// Fetches newly added or modified `.swift` files so the `swiftlint` plugin only run on those instead of the whole project
+    func fetchModifiedSwiftFiles() -> [String] {
+        let process = Process()
+        process.launchPath = "/usr/bin/env"
+        process.arguments = ["git", "diff", "--diff-filter=AM", "--name-only"]
+
+        let outputPipe = Pipe()
+        process.standardOutput =  outputPipe
+        process.launch()
+        process.waitUntilExit()
+
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+
+        guard let outputString = String(data: outputData, encoding: .utf8) else {
+            return []
+        }
+
+        let changedFiles = outputString
+            .split(separator: "\n")
+            .compactMap { String($0) }
+
+        return changedFiles.filter { $0.hasSuffix(".swift") }
+    }
+}
