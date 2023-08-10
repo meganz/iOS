8154cb6a80d4351b57518cabeea62481d50204f7
Javier Navarro <jn@mega.co.nz>
2023-08-11 01:07:19 +1200

    IOS-7488: move CallLocalvideoRepository to ChatRepo

--- Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Call/CallLocalVideoRepositoryProtocol.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Call/CallLocalVideoRepositoryProtocol.swift
@@ -1,17 +1,17 @@
-import MEGADomain
+import Foundation
 
-protocol CallLocalVideoRepositoryProtocol {
+public protocol CallLocalVideoRepositoryProtocol {
     func enableLocalVideo(for chatId: HandleEntity, completion: @escaping (Result<Void, CallErrorEntity>) -> Void)
     func disableLocalVideo(for chatId: HandleEntity, completion: @escaping (Result<Void, CallErrorEntity>) -> Void)
     func addLocalVideo(for chatId: HandleEntity, localVideoListener: some CallLocalVideoListenerRepositoryProtocol)
     func removeLocalVideo(for chatId: HandleEntity, localVideoListener: some CallLocalVideoListenerRepositoryProtocol)
     func videoDeviceSelected() -> String?
-    func selectCamera(withLocalizedName localizedName: String, completion: @escaping (Result<Void, CameraSelectionErrorEntity>) -> Void)
+    func selectCamera(withLocalizedName localizedName: String, completion: @escaping (Result<Void, Error>) -> Void)
     func openVideoDevice(completion: @escaping (Result<Void, CallErrorEntity>) -> Void)
     func releaseVideoDevice(completion: @escaping (Result<Void, CallErrorEntity>) -> Void)
 }
 
-protocol CallLocalVideoListenerRepositoryProtocol {
+public protocol CallLocalVideoListenerRepositoryProtocol {
     func localVideoFrameData(width: Int, height: Int, buffer: Data)
     func localVideoChangedCameraPosition()
 }
