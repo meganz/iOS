9bf5247672fefb6e9cd612092186fe5f0951f332
Mauritz Mollentze <mmo@mega.co.nz>
2023-08-22 09:32:50 +1200

    CC-5017: Show empty album view for import album

---
+++ iMEGA/Media Consumption/Album Library/EmptyAlbumView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct EmptyAlbumView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 32) {
+            Image(uiImage: Asset.Images.Home.allPhotosEmptyState.image)
+                .resizable()
+                .frame(width: 120, height: 120)
+            
+            Text(Strings.Localizable.CameraUploads.Albums.Empty.title)
+                .font(.body)
+        }
+    }
+}
+
+struct EmptyAlbumView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyAlbumView()
+            .previewLayout(.sizeThatFits)
+    }
+}
