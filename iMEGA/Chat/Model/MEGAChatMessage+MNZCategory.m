1f6094d6417924dc8b5102e713cb3722edf27b09
Javier Navarro <jn@mega.co.nz>
2023-08-30 21:57:28 +1200

    IOS-6350: replace MEGAGenericRequestDelegate with RequestDelegate in NicknameViewController

--- iMEGA/Chat/Model/MEGAChatMessage+MNZCategory.m
+++ iMEGA/Chat/Model/MEGAChatMessage+MNZCategory.m
@@ -7,7 +7,6 @@
 
 #import "MEGAChatGenericRequestDelegate.h"
 #import "MEGAFetchNodesRequestDelegate.h"
-#import "MEGAGenericRequestDelegate.h"
 #import "MEGASdkManager.h"
 #import "MEGAStore.h"
 #import "NSAttributedString+MNZCategory.h"
