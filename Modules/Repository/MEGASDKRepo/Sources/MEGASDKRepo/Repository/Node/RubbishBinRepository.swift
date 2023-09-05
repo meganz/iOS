797b08b5605e1e6ac2baf30d219a66d2d4bed496
João Pedro de Amorim <jpa@mega.co.nz>
2023-09-05 15:28:37 -0300

    IOS-7619: Move RubbishBinRepository to MEGASdkRepo

--- Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Repository/Node/RubbishBinRepository.swift
+++ Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Repository/Node/RubbishBinRepository.swift
@@ -1,39 +1,38 @@
+import MEGASdk
 import MEGADomain
 
 public struct RubbishBinRepository: RubbishBinRepositoryProtocol {
-    private let sdk: MEGASdk
-    private let nodeValidationRepository: any NodeValidationRepositoryProtocol
-    
     public static var newRepo: RubbishBinRepository {
-        RubbishBinRepository(sdk: MEGASdkManager.sharedMEGASdk(), nodeValidationRepository: NodeValidationRepository.newRepo)
+        RubbishBinRepository(sdk: MEGASdk.sharedSdk)
     }
-    
+
+    private let sdk: MEGASdk
+
     private enum Constants {
         static let syncDebrisFolderName = "SyncDebris"
         static let syncDebrisNodePath = "//bin/SyncDebris"
     }
-    
-    public init(sdk: MEGASdk, nodeValidationRepository: any NodeValidationRepositoryProtocol) {
+
+    public init(sdk: MEGASdk) {
         self.sdk = sdk
-        self.nodeValidationRepository = nodeValidationRepository
     }
-    
+
     public func isSyncDebrisNode(_ node: NodeEntity) -> Bool {
         guard let syncDebrisNodes = syncDebrisNodes(), syncDebrisNodes.isNotEmpty else { return false }
-        
+
         return isSyncDebrisChild(node)
     }
-    
+
     private func isSyncDebrisChild(_ node: NodeEntity) -> Bool {
         guard let megaNode = node.toMEGANode(in: sdk),
               let path = sdk.nodePath(for: megaNode) else { return false }
-        
+
         return path.hasPrefix(Constants.syncDebrisNodePath)
     }
-    
+
     private func syncDebrisNodes() -> [NodeEntity]? {
         guard let rubbishNode = sdk.rubbishNode else { return nil }
-        
+
         return sdk.children(forParent: rubbishNode)
             .toNodeArray()
             .filter {
