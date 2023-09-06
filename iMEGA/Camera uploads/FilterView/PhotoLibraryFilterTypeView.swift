8e7ce1348dce4ce6ffe7a7fdaadde397e30818e8
Tomek Kuźma <tku@mega.co.nz>
2023-09-06 23:39:20 +1200

    FM-795: create PillView in MEGASwiftUI and use it in the Photos filter

--- iMEGA/Camera uploads/FilterView/PhotoLibraryFilterTypeView.swift
+++ iMEGA/Camera uploads/FilterView/PhotoLibraryFilterTypeView.swift
@@ -1,3 +1,4 @@
+import MEGASwiftUI
 import SwiftUI
 
 struct PhotoLibraryFilterTypeView: View {
@@ -5,46 +6,33 @@ struct PhotoLibraryFilterTypeView: View {
     
     @ObservedObject var filterViewModel: PhotoLibraryFilterViewModel
     
-    var btnSelectedLabel: some View {
-        HStack(spacing: 4) {
-            Image(systemName: "checkmark")
-            Text(type.localization)
-        }
-        .foregroundColor(.white)
-        .padding(.horizontal, 12)
-        .padding(.vertical, 6)
-        .background(Color(Colors.Photos.filterTypeSelectionBackground.color))
-    }
-    
-    var btnNormalLabel: some View {
-        Text(type.localization)
-            .foregroundColor(Color(Colors.Photos.filterNormalTextForeground.color))
-            .padding(.horizontal, 12)
-            .padding(.vertical, 6)
-            .background(Color(Colors.Photos.filterTypeNormalBackground.color))
-    }
-    
-    var btnNormal: some View {
-        Button {
-            filterViewModel.selectedMediaType = type
-        } label: {
-            btnNormalLabel
-        }
-    }
-    
-    var selected: Bool {
+    private var selected: Bool {
         type == filterViewModel.selectedMediaType
     }
     
+    private var foreground: Color {
+        selected ? .white : Colors.Photos.filterNormalTextForeground.swiftUIColor
+    }
+    
+    private var background: Color {
+        selected ? Colors.Photos.filterTypeSelectionBackground.swiftUIColor : Colors.Photos.filterTypeNormalBackground.swiftUIColor
+    }
+    
+    private var icon: Image? {
+        selected ? Image(systemName: "checkmark") : nil
+    }
+    
     var body: some View {
-        ZStack {
-            if selected {
-                btnSelectedLabel
-            } else {
-                btnNormal
-            }
+        PillView(
+            viewModel: .init(
+                title: type.localization,
+                icon: icon,
+                foreground: foreground,
+                background: background
+            )
+        )
+        .onTapGesture {
+            filterViewModel.selectedMediaType = type
         }
-        .font(.system(size: 15, weight: .medium, design: .default))
-        .cornerRadius(8)
     }
 }
