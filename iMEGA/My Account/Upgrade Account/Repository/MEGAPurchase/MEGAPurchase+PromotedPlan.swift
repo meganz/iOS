4d30136e5bed74d5aa712a870fdc7d9286dbb439
Glazel Magpayo <gma@mega.co.nz>
2023-09-08 16:18:21 +1200

    AP-365 Prevent pro flexi and business account from purchasing promoted plan - T11749688 T11749703

--- iMEGA/My Account/Upgrade Account/Repository/MEGAPurchase/MEGAPurchase+PromotedPlan.swift
+++ iMEGA/My Account/Upgrade Account/Repository/MEGAPurchase/MEGAPurchase+PromotedPlan.swift
@@ -6,6 +6,7 @@ extension MEGAPurchase {
         case alreadyPurchasedPlan
         case noLoggedInUser
         case noAccountDetails
+        case proFlexiOrBusinessAccount
     }
     
     // MARK: - Promoted plan purchase
@@ -39,6 +40,11 @@ extension MEGAPurchase {
             throw InAppPurchaseStoreError.noLoggedInUser
         }
         
+        guard try !isProFlexiOrBusinessAccount() else {
+            MEGALogInfo("[StoreKit] Cancel promoted plan purchase for \(product.productIdentifier). User's current account is either Pro Flexi or Business account.")
+            throw InAppPurchaseStoreError.proFlexiOrBusinessAccount
+        }
+        
         guard try !isProductAlreadyPurchased(product) else {
             MEGALogInfo("[StoreKit] Cancel promoted plan purchase. Plan \(product.productIdentifier) is already purchased.")
             throw InAppPurchaseStoreError.alreadyPurchasedPlan
@@ -48,16 +54,25 @@ extension MEGAPurchase {
         return true
     }
     
+    private func accountDetails() throws -> AccountDetailsEntity {
+        guard let accountDetails = MEGASdk.shared.mnz_accountDetails?.toAccountDetailsEntity() else {
+            throw InAppPurchaseStoreError.noAccountDetails
+        }
+        return accountDetails
+    }
+    
     private func isUserLoggedIn() -> Bool {
         let accountUseCase = AccountUseCase(repository: AccountRepository.newRepo)
         return accountUseCase.isLoggedIn()
     }
     
+    private func isProFlexiOrBusinessAccount() throws -> Bool {
+        let accountDetails = try accountDetails()
+        return accountDetails.proLevel == .proFlexi || accountDetails.proLevel == .business
+    }
+    
     private func isProductAlreadyPurchased(_ product: SKProduct) throws -> Bool {
-        guard let accountDetails = MEGASdk.shared.mnz_accountDetails?.toAccountDetailsEntity() else {
-            throw InAppPurchaseStoreError.noAccountDetails
-        }
-        
+        let accountDetails = try accountDetails()
         let plan = product.toAccountPlanEntity()
         return plan.type == accountDetails.proLevel && plan.subscriptionCycle == accountDetails.subscriptionCycle
     }
@@ -68,6 +83,8 @@ extension MEGAPurchase {
             SVProgressHUD.showError(withStatus: Strings.Localizable.UpgradeAccountPlan.Selection.Message.alreadyHaveRecurringSubscriptionOfPlan)
         case .noLoggedInUser:
             SVProgressHUD.showError(withStatus: Strings.Localizable.pleaseLogInToYourAccount)
+        case .proFlexiOrBusinessAccount:
+            SVProgressHUD.showError(withStatus: Strings.Localizable.Account.Upgrade.NotAvailableWithCurrentPlan.message)
         default:
             SVProgressHUD.showError(withStatus: Strings.Localizable.somethingWentWrong)
         }
