1108bd393676e5c84ff6cdb4ec877bb2c3bcd574
Glazel Magpayo <gma@mega.co.nz>
2023-09-09 02:54:03 +1200

    T11749615: AP-365 Fixed promoted plan message when there is no account details

--- iMEGA/My Account/Upgrade Account/Repository/MEGAPurchase/MEGAPurchase+PromotedPlan.swift
+++ iMEGA/My Account/Upgrade Account/Repository/MEGAPurchase/MEGAPurchase+PromotedPlan.swift
@@ -15,14 +15,21 @@ extension MEGAPurchase {
         do {
             return try canProcessStorePayment(for: product)
         } catch {
-            SVProgressHUD.dismiss()
+            guard let error = error as? InAppPurchaseStoreError else {
+                SVProgressHUD.dismiss()
+                return false
+            }
             
-            guard let error = error as? InAppPurchaseStoreError else { return false }
             if error == .noLoggedInUser {
                 // Defer until user has logged in, save the product and return false
                 savePendingPromotedProduct(product)
+            } else if error == .noAccountDetails {
+                // Defer until the account details is fetched and return false
+                fetchAccountDetailsToPurchaseProduct(product)
+                return false
             }
             
+            SVProgressHUD.dismiss()
             showStoreErrorMessage(error)
             return false
         }
@@ -31,6 +38,9 @@ extension MEGAPurchase {
     @objc func processAnyPendingPromotedPlanPayment() {
         guard let pendingProduct = pendingPromotedProductForPayment() else { return }
         
+        // Check if payment can still resume
+        guard shouldAddStorePayment(for: pendingProduct) else { return }
+        
         // Continuing a previously deferred payment
         MEGALogInfo("[StoreKit] Deferred promoted plan \(pendingProduct.localizedTitle) will resume purchase.")
         purchaseProduct(pendingProduct)
@@ -38,17 +48,29 @@ extension MEGAPurchase {
     
     // MARK: Helpers
     private func canProcessStorePayment(for product: SKProduct) throws -> Bool {
-        guard isUserLoggedIn() else {
+        let accountUseCase = AccountUseCase(repository: AccountRepository.newRepo)
+        
+        guard accountUseCase.isLoggedIn() else {
             MEGALogInfo("[StoreKit] Defer promoted plan \(product.productIdentifier). Purchase will continue after login.")
             throw InAppPurchaseStoreError.noLoggedInUser
         }
         
-        guard try !isProFlexiOrBusinessAccount() else {
+        guard let accountDetails = accountUseCase.currentAccountDetails else {
+            MEGALogInfo("[StoreKit] No current account details")
+            throw InAppPurchaseStoreError.noAccountDetails
+        }
+        
+        let isProFlexiOrBusinessAccount = accountDetails.proLevel == .proFlexi ||
+                                            accountDetails.proLevel == .business
+        guard !isProFlexiOrBusinessAccount else {
             MEGALogInfo("[StoreKit] Cancel promoted plan purchase for \(product.productIdentifier). User's current account is either Pro Flexi or Business account.")
             throw InAppPurchaseStoreError.proFlexiOrBusinessAccount
         }
         
-        guard try !isProductAlreadyPurchased(product) else {
+        let plan = product.toAccountPlanEntity()
+        let isProductAlreadyPurchased = plan.type == accountDetails.proLevel &&
+                                        plan.subscriptionCycle == accountDetails.subscriptionCycle
+        guard !isProductAlreadyPurchased else {
             MEGALogInfo("[StoreKit] Cancel promoted plan purchase. Plan \(product.productIdentifier) is already purchased.")
             throw InAppPurchaseStoreError.alreadyPurchasedPlan
         }
@@ -57,27 +79,22 @@ extension MEGAPurchase {
         return true
     }
     
-    private func accountDetails() throws -> AccountDetailsEntity {
-        guard let accountDetails = MEGASdk.shared.mnz_accountDetails?.toAccountDetailsEntity() else {
-            throw InAppPurchaseStoreError.noAccountDetails
+    private func fetchAccountDetailsToPurchaseProduct(_ product: SKProduct) {
+        Task {
+            do {
+                let accountUseCase = AccountUseCase(repository: AccountRepository.newRepo)
+                _ = try await accountUseCase.refreshCurrentAccountDetails()
+                
+                guard shouldAddStorePayment(for: product) else { return }
+                
+                MEGALogInfo("[StoreKit] Deferred promoted plan \(product.localizedTitle) will resume purchase.")
+                purchaseProduct(product)
+            } catch {
+                MEGALogError("[StoreKit] Error loading account details. Error: \(error)")
+                await SVProgressHUD.dismiss()
+                await SVProgressHUD.showError(withStatus: Strings.Localizable.somethingWentWrong)
+            }
         }
-        return accountDetails
-    }
-    
-    private func isUserLoggedIn() -> Bool {
-        let accountUseCase = AccountUseCase(repository: AccountRepository.newRepo)
-        return accountUseCase.isLoggedIn()
-    }
-    
-    private func isProFlexiOrBusinessAccount() throws -> Bool {
-        let accountDetails = try accountDetails()
-        return accountDetails.proLevel == .proFlexi || accountDetails.proLevel == .business
-    }
-    
-    private func isProductAlreadyPurchased(_ product: SKProduct) throws -> Bool {
-        let accountDetails = try accountDetails()
-        let plan = product.toAccountPlanEntity()
-        return plan.type == accountDetails.proLevel && plan.subscriptionCycle == accountDetails.subscriptionCycle
     }
     
     private func showStoreErrorMessage(_ error: InAppPurchaseStoreError) {
@@ -85,11 +102,12 @@ extension MEGAPurchase {
         case .alreadyPurchasedPlan:
             SVProgressHUD.showError(withStatus: Strings.Localizable.UpgradeAccountPlan.Selection.Message.alreadyHaveRecurringSubscriptionOfPlan)
         case .noLoggedInUser:
+            SVProgressHUD.setErrorImage(Asset.Images.Hud.hudError.image)
             SVProgressHUD.showError(withStatus: Strings.Localizable.pleaseLogInToYourAccount)
         case .proFlexiOrBusinessAccount:
             SVProgressHUD.showError(withStatus: Strings.Localizable.Account.Upgrade.NotAvailableWithCurrentPlan.message)
-        default:
-            SVProgressHUD.showError(withStatus: Strings.Localizable.somethingWentWrong)
+        case .noAccountDetails:
+            return
         }
     }
 }
