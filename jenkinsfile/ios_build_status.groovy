2be081211cb8833fa4e0ddb4f34c750c8d0c2148
Lancy <lf@mega.co.nz>
2023-09-11 10:54:45 +1200

    Using the mac studio 1 for CI/CD (Xcode 15)

--- jenkinsfile/ios_build_status.groovy
+++ jenkinsfile/ios_build_status.groovy
@@ -10,7 +10,7 @@ def injectEnvironments(Closure body) {
 }
 
 pipeline {
-    agent { label 'mac-jenkins-slave-ios' }
+    agent { label 'macstudio1' }
     options {
         timeout(time: 1, unit: 'HOURS') 
         gitLabConnection('GitLabConnection')
