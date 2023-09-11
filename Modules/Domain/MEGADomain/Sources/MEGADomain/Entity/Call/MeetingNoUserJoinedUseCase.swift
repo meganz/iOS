634abd788512cd34fa7b3fe20fac1d5df8969e06
João Pedro de Amorim <jpa@mega.co.nz>
2023-09-12 08:23:48 +1200

    IOS-7640: Move MeetingNoUserJoinedUseCase to MEGADomain

---
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/Call/MeetingNoUserJoinedUseCase.swift
@@ -0,0 +1,27 @@
+import Combine
+import Foundation
+
+public protocol MeetingNoUserJoinedUseCaseProtocol {
+    var monitor: AnyPublisher<Void, Never> { get }
+    func start(timerDuration duration: TimeInterval, chatId: HandleEntity)
+}
+
+public struct MeetingNoUserJoinedUseCase: MeetingNoUserJoinedUseCaseProtocol {
+    public enum Constants {
+        public static let timerDuration: TimeInterval = 5 * 60
+    }
+    
+    private let repository: any MeetingNoUserJoinedRepositoryProtocol
+    
+    public var monitor: AnyPublisher<Void, Never> {
+        repository.monitor
+    }
+    
+    public init(repository: some MeetingNoUserJoinedRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    public func start(timerDuration duration: TimeInterval = Constants.timerDuration, chatId: HandleEntity) {
+        repository.start(timerDuration: duration, chatId: chatId)
+    }
+}
