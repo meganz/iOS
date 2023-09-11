592bd3d2f3f9545e14bcf44fd5ef82c633c73519
Arifin Firdaus <af@mega.co.nz>
2023-09-11 15:13:57 +1200

    CC-3259: Fix Location is not displayed in Image Info

---
+++ iMEGA/Utils/Shared Views/Shared Controls/AssetCreationRequestLocationManager.swift
@@ -0,0 +1,51 @@
+import CoreLocation
+
+protocol AssetCreationRequestLocationManagerProtocol {
+    func requestWhenInUseAuthorization()
+    
+    /// This method can only be called from inside of -[PHPhotoLibrary performChanges:completionHandler:] or -[PHPhotoLibrary performChangesAndWait:error:]
+    /// - Parameter assetCreationRequest: assetCreationRequest object
+    func registerLocationMetaData(to assetCreationRequest: PHAssetCreationRequest)
+}
+
+final class AssetCreationRequestLocationManager: AssetCreationRequestLocationManagerProtocol {
+    private let locationManager: CLLocationManager
+    private let shouldIncludeGPSTags: Bool
+    
+    init(
+        locationManager: CLLocationManager = CLLocationManager(),
+        shouldIncludeGPSTags: Bool = CameraUploadManager.shouldIncludeGPSTags
+    ) {
+        self.locationManager = locationManager
+        self.shouldIncludeGPSTags = shouldIncludeGPSTags
+    }
+    
+    func requestWhenInUseAuthorization() {
+        if shouldIncludeGPSTags {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func registerLocationMetaData(to assetCreationRequest: PHAssetCreationRequest) {
+        if CLLocationManager.locationServicesEnabled() && shouldIncludeGPSTags {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        if let location = locationManager.location, shouldIncludeGPSTags {
+            assetCreationRequest.location = location
+        }
+    }
+}
+
+@objc final class AssetCreationRequestLocationManagerOCWrapper: NSObject, AssetCreationRequestLocationManagerProtocol {
+    private let adaptee = AssetCreationRequestLocationManager()
+    
+    @objc func requestWhenInUseAuthorization() {
+        adaptee.requestWhenInUseAuthorization()
+    }
+    
+    @objc func registerLocationMetaData(to assetCreationRequest: PHAssetCreationRequest) {
+        adaptee.registerLocationMetaData(to: assetCreationRequest)
+    }
+}
