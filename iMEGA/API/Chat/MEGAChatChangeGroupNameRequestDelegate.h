1916e25ef925d518474926ce01793c40cd811a3f
Javier Navarro <jn@mega.co.nz>
2023-09-13 17:57:44 +0200

    IOS-7653: remove MEGAChatChangeGroupNameRequestDelegate

--- iMEGA/API/Chat/MEGAChatChangeGroupNameRequestDelegate.h
+++
@@ -1,9 +0,0 @@
-#import "MEGAChatBaseRequestDelegate.h"
-
-@interface MEGAChatChangeGroupNameRequestDelegate : MEGAChatBaseRequestDelegate
-
-- (id)init NS_UNAVAILABLE;
-
-- (instancetype)initWithCompletion:(void (^)(MEGAChatError *error))completion;
-
-@end
