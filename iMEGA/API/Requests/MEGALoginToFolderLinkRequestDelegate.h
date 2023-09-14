aa17b0f73f3a650482613e60119b473bff5ceded
Javier Navarro <jn@mega.co.nz>
2023-09-14 14:19:20 +0200

    IOS-7658: Remove MEGALoginToFolderLinkRequestDelegate

--- iMEGA/API/Requests/MEGALoginToFolderLinkRequestDelegate.h
+++
@@ -1,7 +0,0 @@
-#import "MEGARequestDelegate.h"
-
-@interface MEGALoginToFolderLinkRequestDelegate : NSObject <MEGARequestDelegate>
-
-- (instancetype)initWithCompletion:(void (^)(MEGARequest *request))completion;
-
-@end
