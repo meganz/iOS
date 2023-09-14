f637ea8e17afcb73753f1f6ab2661957f7ab0efc
Javier Navarro <jn@mega.co.nz>
2023-09-14 10:38:53 +0200

    IOS-7655: Remove MEGAChatEnableDisableAudioRequestDelegate

--- iMEGA/API/Chat/MEGAChatEnableDisableAudioRequestDelegate.h
+++
@@ -1,9 +0,0 @@
-#import "MEGAChatBaseRequestDelegate.h"
-
-@interface MEGAChatEnableDisableAudioRequestDelegate : MEGAChatBaseRequestDelegate
-
-- (id)init NS_UNAVAILABLE;
-
-- (instancetype)initWithCompletion:(void (^)(MEGAChatError *error))completion;
-
-@end
