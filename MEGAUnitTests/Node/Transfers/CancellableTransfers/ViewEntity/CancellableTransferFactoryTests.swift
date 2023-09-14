fcef4e70e551651f12bc52a0eeb8022b7c01afb5
Arifin Firdaus <af@mega.co.nz>
2023-09-14 22:23:58 +1200

    CC-4983: iOS - Fix Transfer failed something went wrong message appears when user try to make a audio file offline from inside audio player

---
+++ MEGAUnitTests/Node/Transfers/CancellableTransfers/ViewEntity/CancellableTransferFactoryTests.swift
@@ -0,0 +1,76 @@
+@testable import MEGA
+import MEGADomain
+import MEGASDKRepo
+import MEGASDKRepoMock
+import XCTest
+
+final class CancellableTransferFactoryTests: XCTestCase {
+    
+    func testMake_fromFolderLink_createsFolderLinkTransferSetup() {
+        let node = MockNode(handle: 1)
+        let isNodeFromFolderLink = true
+        let messageId: HandleEntity = .invalid
+        let chatId: HandleEntity = .invalid
+        let sut = CancellableTransfer.Factory(node: node, isNodeFromFolderLink: isNodeFromFolderLink, messageId: messageId, chatId: chatId)
+        
+        let transfer = sut.make()
+        
+        XCTAssertEqual(transfer.handle, node.handle)
+        XCTAssertEqual(transfer.name, nil)
+        XCTAssertEqual(transfer.appData, nil)
+        XCTAssertEqual(transfer.priority, false)
+        XCTAssertEqual(transfer.isFile, node.isFile())
+        XCTAssertEqual(transfer.type, .download)
+        XCTAssertEqual(transfer.messageId, messageId)
+        XCTAssertEqual(transfer.chatId, chatId)
+    }
+    
+    func testMake_fromChat_createsChatDownloadTransferSetup() {
+        let node = MockNode(handle: 1)
+        let isNodeFromFolderLink = false
+        let messageId: HandleEntity = 1
+        let chatId: HandleEntity = 2
+        let sut = CancellableTransfer.Factory(node: node, isNodeFromFolderLink: isNodeFromFolderLink, messageId: messageId, chatId: chatId)
+        
+        let transfer = sut.make()
+        
+        XCTAssertEqual(transfer.handle, node.handle)
+        XCTAssertEqual(transfer.name, nil)
+        XCTAssertEqual(transfer.appData, nil)
+        XCTAssertEqual(transfer.priority, false)
+        XCTAssertEqual(transfer.isFile, node.isFile())
+        XCTAssertEqual(transfer.type, .downloadChat)
+        XCTAssertEqual(transfer.messageId, messageId)
+        XCTAssertEqual(transfer.chatId, chatId)
+    }
+    
+    func testMake_notFromFolderLinkNorChat_createsDefaultDownloadTransferSetup() {
+        typealias Combination = (chatId: HandleEntity?, messageId: HandleEntity?)
+        var combinations = [Combination]()
+        combinations.append(contentsOf: [
+            Combination(nil, nil),
+            Combination(nil, .invalid),
+            Combination(.invalid, nil),
+            Combination(.invalid, .invalid)
+        ])
+        
+        combinations.enumerated().forEach { (index, combination) in
+            let messageId = combination.messageId ?? .invalid
+            let chatId = combination.chatId ?? .invalid
+            let node = MockNode(handle: 1)
+            let isNodeFromFolderLink = false
+            let sut = CancellableTransfer.Factory(node: node, isNodeFromFolderLink: isNodeFromFolderLink, messageId: messageId, chatId: chatId)
+            
+            let transfer = sut.make()
+            
+            XCTAssertEqual(transfer.handle, node.handle, "failed at index: \(index) on value: \(combination)")
+            XCTAssertEqual(transfer.name, nil, "failed at index: \(index) on value: \(combination)")
+            XCTAssertEqual(transfer.appData, nil, "failed at index: \(index) on value: \(combination)")
+            XCTAssertEqual(transfer.priority, false, "failed at index: \(index) on value: \(combination)")
+            XCTAssertEqual(transfer.isFile, node.isFile(), "failed at index: \(index) on value: \(combination)")
+            XCTAssertEqual(transfer.type, .download, "failed at index: \(index) on value: \(combination)")
+            XCTAssertEqual(transfer.messageId, .invalid, "failed at index: \(index) on value: \(combination)")
+            XCTAssertEqual(transfer.chatId, .invalid, "failed at index: \(index) on value: \(combination)")
+        }
+    }
+}
--- iMEGA/Node/Transfers/CancellableTransfers/ViewEntity/CancellableTransfer.swift
+++ iMEGA/Node/Transfers/CancellableTransfers/ViewEntity/CancellableTransfer.swift
@@ -42,4 +42,46 @@ import MEGADomain
         self.isFile = isFile
         self.type = type
     }
+    
+    struct Factory {
+        let node: MEGANode
+        let isNodeFromFolderLink: Bool
+        let messageId: HandleEntity?
+        let chatId: HandleEntity?
+        
+        func make() -> CancellableTransfer {
+            let type: () -> CancellableTransferType = {
+                if let messageId = messageId, let chatId = chatId, messageId != .invalid || chatId != .invalid {
+                    return .downloadChat
+                } else {
+                    return .download
+                }
+            }
+            
+            let messageIdIfValid: (_ messageId: HandleEntity?) -> HandleEntity = { messageId in
+                if let messageId, messageId != .invalid {
+                    return messageId
+                }
+                return .invalid
+            }
+            
+            let chatIdIfValid: (_ chatId: HandleEntity?) -> HandleEntity = { chatId in
+                if let chatId, chatId != .invalid {
+                    return chatId
+                }
+                return .invalid
+            }
+            
+            return .init(
+                handle: node.handle,
+                messageId: messageIdIfValid(messageId),
+                chatId: chatIdIfValid(chatId),
+                name: nil,
+                appData: nil,
+                priority: false,
+                isFile: node.isFile(),
+                type: type()
+            )
+        }
+    }
 }
