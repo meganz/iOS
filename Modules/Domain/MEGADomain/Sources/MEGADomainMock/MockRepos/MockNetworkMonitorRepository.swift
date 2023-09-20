34f5ba32e439b97972c44e1b0f3091be3e5b7c33
Mauritz Mollentze <mmo@mega.co.nz>
2023-09-21 11:38:20 +1200

    CC-5353: Refactor NetworkMonitorRepository pathUpdateHandler

--- Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockRepos/MockNetworkMonitorRepository.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockRepos/MockNetworkMonitorRepository.swift
@@ -2,10 +2,13 @@ import MEGADomain
 
 public struct MockNetworkMonitorRepository: NetworkMonitorRepositoryProtocol {
     
+    public let connectionChangedStream: AsyncStream<Bool>
     public var connected: Bool
     
-    public init(connected: Bool = false) {
+    public init(connected: Bool = false,
+                connectionChangedStream: AsyncStream<Bool> = AsyncStream<Bool> { $0.finish() }) {
         self.connected = connected
+        self.connectionChangedStream = connectionChangedStream
     }
     
     public func networkPathChanged(completion: @escaping (Bool) -> Void) {
