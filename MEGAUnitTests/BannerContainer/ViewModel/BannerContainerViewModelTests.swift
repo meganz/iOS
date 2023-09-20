d5c9d29362b975d167292b87bbc9dbe1f036ba52
Banghua Zhao <bz@mega.co.nz>
2023-09-20 12:39:24 +1200

    IOS-7672: Fix failed test case in BannerContainerViewModelTests

--- MEGAUnitTests/BannerContainer/ViewModel/BannerContainerViewModelTests.swift
+++ MEGAUnitTests/BannerContainer/ViewModel/BannerContainerViewModelTests.swift
@@ -1,39 +1,30 @@
 @testable import MEGA
+import MEGADomain
 import MEGADomainMock
 import XCTest
 
 final class BannerContainerViewModelTests: XCTestCase {
     
-    let router = MockBannerContainerViewRouter()
-    
-    lazy var viewModel = BannerContainerViewModel(router: router, message: "Banner message example", type: BannerType.warning)
-    
     func testAction_onViewWillAppear() {
-        test(viewModel: viewModel, action: .onViewWillAppear, expectedCommands: [.hideBanner(animated: false)])
+        let sut = makeSUT(withOfflineLogOutWarningDismissed: true)
+        test(viewModel: sut.viewModel, action: .onViewWillAppear, expectedCommands: [.hideBanner(animated: false)])
     }
     
     func testAction_OnViewDidLoad_WarningDismissed() {
-        let preference = MockPreferenceUseCase()
-        XCTAssertTrue(preference[.offlineLogOutWarningDismissed] == Optional<Bool>.none)
+        let sut = makeSUT()
         
-        let viewModel = BannerContainerViewModel(router: router, message: "Banner message example", type: BannerType.warning, preferenceUseCase: preference)
+        XCTAssertTrue(sut.preference[.offlineLogOutWarningDismissed] == Optional<Bool>.none)
+        test(viewModel: sut.viewModel, action: .onClose, expectedCommands: [.hideBanner(animated: true)])
         
-        test(viewModel: viewModel, action: .onClose, expectedCommands: [.hideBanner(animated: true)])
-        
-        XCTAssertTrue(preference[.offlineLogOutWarningDismissed] == true)
-        
-        test(viewModel: viewModel,
-             action: .onViewDidLoad(UITraitCollection()),
-             expectedCommands: [])
+        XCTAssertTrue(sut.preference[.offlineLogOutWarningDismissed] == true)
+        test(viewModel: sut.viewModel, action: .onViewDidLoad(UITraitCollection()), expectedCommands: [])
     }
     
     func testAction_OnViewDidLoad_WarningNotDismissed() {
-        let preference = MockPreferenceUseCase()
-        XCTAssertTrue(preference[.offlineLogOutWarningDismissed] == Optional<Bool>.none)
+        let sut = makeSUT()
         
-        let viewModel = BannerContainerViewModel(router: router, message: "Banner message example", type: BannerType.warning, preferenceUseCase: preference)
-
-        test(viewModel: viewModel,
+        XCTAssertTrue(sut.preference[.offlineLogOutWarningDismissed] == Optional<Bool>.none)
+        test(viewModel: sut.viewModel,
              action: .onViewDidLoad(UITraitCollection()),
              expectedCommands: [.configureView(message: "Banner message example",
                                                backgroundColor: BannerType.warning.bgColor,
@@ -42,7 +33,7 @@ final class BannerContainerViewModelTests: XCTestCase {
     }
     
     func testAction_OnTrailCollectionDidChange() {
-        test(viewModel: viewModel,
+        test(viewModel: makeSUT().viewModel,
              action: .onTraitCollectionDidChange(UITraitCollection()),
              expectedCommands: [
                 .configureView(message: "Banner message example",
@@ -52,6 +43,24 @@ final class BannerContainerViewModelTests: XCTestCase {
     }
     
     func testAction_onClose() {
-        test(viewModel: viewModel, action: .onClose, expectedCommands: [.hideBanner(animated: true)])
+        test(viewModel: makeSUT().viewModel, action: .onClose, expectedCommands: [.hideBanner(animated: true)])
+    }
+    
+    // MARK: - Private methods
+    
+    private func makeSUT(
+        withOfflineLogOutWarningDismissed offlineLogOutWarningDismissed: Bool? = nil
+    ) -> (viewModel: BannerContainerViewModel, preference: some PreferenceUseCaseProtocol) {
+        let preferenceUseCase = MockPreferenceUseCase()
+        if let offlineLogOutWarningDismissed {
+            preferenceUseCase.dict[.offlineLogOutWarningDismissed] = offlineLogOutWarningDismissed
+        }
+        
+        return (BannerContainerViewModel(
+            router: MockBannerContainerViewRouter(),
+            message: "Banner message example",
+            type: .warning,
+            preferenceUseCase: preferenceUseCase
+        ), preferenceUseCase)
     }
 }
