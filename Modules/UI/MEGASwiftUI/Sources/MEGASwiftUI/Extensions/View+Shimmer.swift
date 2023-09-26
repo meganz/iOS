bfd2af74260c7edf3effc4a25224fefdc1291584
Aleksa Simic <ami@mega.co.nz>
2023-09-26 21:23:53 +1300

    FM-1088: Update shimmer loader animation

--- Modules/UI/MEGASwiftUI/Sources/MEGASwiftUI/Extensions/View+Shimmer.swift
+++ Modules/UI/MEGASwiftUI/Sources/MEGASwiftUI/Extensions/View+Shimmer.swift
@@ -1,81 +1,87 @@
 import SwiftUI
 
-struct Shimmer: ViewModifier {
-    let animation = Animation.linear(duration: 1.5).repeatForever(autoreverses: false)
-    let scaleEffect: CGFloat
+// For more reference, please visit: https://github.com/markiv/SwiftUI-Shimmer/blob/main/Sources/Shimmer/Shimmer.swift
+public struct Shimmer: ViewModifier {
+    @State private var isInitialState = true
+    @Environment(\.layoutDirection) private var layoutDirection
+    @Environment(\.colorScheme) var colorScheme
 
-    @State private var phase: CGFloat = 0
+    private let animation = Animation.linear(duration: 1.5).delay(0.25).repeatForever(autoreverses: false)
+    // Unit point dimensions beyond the gradient's edges by the band size of 0.3
+    private let min: CGFloat = -0.3
+    private let max: CGFloat = 1.3
 
-    init(scaleEffect: CGFloat) {
-        self.scaleEffect = scaleEffect
+    private var gradient: Gradient {
+        .init(
+            colors: colorScheme == .light ? [.black.opacity(0.1), .black.opacity(0.2), .black.opacity(0.1)] : [.white.opacity(0.2), .white.opacity(0.3), .white.opacity(0.2)]
+        )
     }
 
-    func body(content: Content) -> some View {
+    /// The start unit point of our gradient, adjusting for layout direction.
+    private var startPoint: UnitPoint {
+        if layoutDirection == .rightToLeft {
+            return isInitialState ? UnitPoint(x: max, y: min) : UnitPoint(x: 0, y: 1)
+        } else {
+            return isInitialState ? UnitPoint(x: min, y: min) : UnitPoint(x: 1, y: 1)
+        }
+    }
+
+    /// The end unit point of our gradient, adjusting for layout direction.
+    private var endPoint: UnitPoint {
+        if layoutDirection == .rightToLeft {
+            return isInitialState ? UnitPoint(x: 1, y: 0) : UnitPoint(x: min, y: max)
+        } else {
+            return isInitialState ? UnitPoint(x: 0, y: 0) : UnitPoint(x: max, y: max)
+        }
+    }
+
+    public func body(content: Content) -> some View {
         content
-            .modifier(
-                AnimatedMask(
-                    scaleEffect: scaleEffect,
-                    phase: phase
-                ).animation(animation)
-            )
-            .onAppear { phase = 0.8 }
+            .mask(LinearGradient(gradient: gradient, startPoint: startPoint, endPoint: endPoint))
+            .onAppear {
+                withAnimation(animation) {
+                    isInitialState = false
+                }
+            }
     }
+}
 
-    struct AnimatedMask: AnimatableModifier {
-        let scaleEffect: CGFloat
-        var phase: CGFloat
-
-        var animatableData: CGFloat {
-            get { phase }
-            set { phase = newValue }
-        }
-
-        func body(content: Content) -> some View {
-            content
-                .mask(GradientMask(phase: phase).scaleEffect(scaleEffect))
-        }
-    }
-
-    struct GradientMask: View {
-        let phase: CGFloat
-        let centerColor = Color.black
-        let edgeColor = Color.black.opacity(0.7)
-
-        @Environment(\.layoutDirection)
-        private var layoutDirection
-
-        var body: some View {
-            LinearGradient(
-                gradient: Gradient(stops: [
-                    .init(color: edgeColor, location: phase),
-                    .init(color: centerColor, location: phase + 0.1),
-                    .init(color: edgeColor, location: phase + 0.2)
-                ]),
-                startPoint: .leading,
-                endPoint: .trailing
-            )
+public extension View {
+    /// Adds an animated shimmering effect to any view, typically to show that an operation is in progress.
+    /// - Parameters:
+    ///   - active: Convenience parameter to conditionally enable the effect. Defaults to `true`.
+    @ViewBuilder func shimmering(
+        active: Bool = true
+    ) -> some View {
+        if active {
+            modifier(Shimmer())
+        } else {
+            self
         }
     }
 }
 
-extension View {
-    /// Applies a shimmer effect to a view when active.
-    ///
-    /// - Parameters:
-    ///   - active: A boolean value indicating whether the shimmer effect should be active. Default is true.
-    ///   - scaleEffect: The scale effect to be applied to the shimmer, determining its width. Default is 5.
-    /// - Returns: A view with the shimmer effect applied if active, otherwise the original view.
-    @ViewBuilder
-    public func shimmering(
-        active: Bool = true,
-        scaleEffect: CGFloat = 5
-    ) -> some View {
-        if active {
-            modifier(Shimmer(scaleEffect: scaleEffect))
-                .foregroundColor(Color.primary.opacity(0.1))
-                .redacted(reason: .placeholder)
-        } else {
-            self
+struct Shimmer_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            Text("SwiftUI Shimmer").preferredColorScheme(.light)
+            Text("SwiftUI Shimmer").preferredColorScheme(.dark)
+
+            VStack(alignment: .leading) {
+                Text("Loading...").font(.title)
+                Text(String(repeating: "Shimmer", count: 12))
+                    .redacted(reason: .placeholder)
+            }.frame(maxWidth: 200)
         }
+        .padding()
+        .shimmering()
+        .previewLayout(.sizeThatFits)
+
+        VStack(alignment: .leading) {
+            Text("← Right-to-left layout direction").font(.body)
+        }
+        .font(.largeTitle)
+        .shimmering()
+        .environment(\.layoutDirection, .rightToLeft)
     }
 }
