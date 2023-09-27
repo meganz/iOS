d98fdc760e3b449f5f2380bc32d44fb5c61d8824
Javier Navarro <jn@mega.co.nz>
2023-09-27 20:24:05 +1300

    IOS-6967: use remote URLs for binaryTarget frameworks

--- fastlane/actions/copy_sdk_libraries_to_chat_sdk.rb
+++ fastlane/actions/copy_sdk_libraries_to_chat_sdk.rb
@@ -9,27 +9,20 @@ module Fastlane
 
           chat_sdk_manager = ChatSDKManager.new(sdk_third_party_path, chat_third_party_path)
 
-          create_include_and_lib_folders chat_sdk_manager
+          create_include_folder chat_sdk_manager
           copy_headers chat_sdk_manager
-          move_libraries chat_sdk_manager
       end
 
-      def self.create_include_and_lib_folders(chat_sdk_manager)
-        UI.important "Creating include and lib folders in 3rdparty for chat"
-        chat_sdk_manager.create_include_and_lib_folders
-        UI.success "Successfully created include and lib folders in 3rdparty for chat ✅"
+      def self.create_include_folder(chat_sdk_manager)
+        UI.important "Creating include folder in 3rdparty for chat"
+        chat_sdk_manager.create_include_folder
+        UI.success "Successfully created include folder in 3rdparty for chat ✅"
       end
 
       def self.copy_headers(chat_sdk_manager)
         UI.important "Copying headers needed by MEGAChat"
         chat_sdk_manager.copy_headers
         UI.success "Successfully Copied headers needed by MEGAChat ✅"
-      end
-
-      def self.move_libraries(chat_sdk_manager)
-        UI.important "Move xcframeworks needed by MEGAChat"
-        chat_sdk_manager.move_libraries
-        UI.success "Successfully moved xcframeworks needed by MEGAChat ✅"
       end
 
       def self.description
--- fastlane/ruby/chat_sdk_manager.rb
+++ fastlane/ruby/chat_sdk_manager.rb
@@ -4,12 +4,10 @@ class ChatSDKManager
         @chat_third_party_path = chat_third_party_path
     end
 
-    def create_include_and_lib_folders
+    def create_include_folder
         chat_include_path = File.join(@chat_third_party_path, "include")
-        chat_lib_path = File.join(@chat_third_party_path, "lib")
 
         FileUtils.mkdir_p chat_include_path unless Dir.exist? chat_include_path
-        FileUtils.mkdir_p chat_lib_path unless Dir.exist? chat_lib_path
     end
 
     def copy_headers
@@ -28,32 +26,5 @@ class ChatSDKManager
         FileUtils.cp_r "#{sdk_third_party_private_folder}/MEGANodeList+init.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
         FileUtils.cp_r "#{sdk_third_party_private_folder}/MEGASdk+init.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
         FileUtils.cp_r "#{sdk_third_party_private_folder}/MEGAStringList+init.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
-    end
-
-    def move_libraries
-        sdk_lib_path = File.join(@sdk_third_party_path, "lib")
-        chat_lib_path = File.join(@chat_third_party_path, "lib")
-
-        frameworks = [
-            "libnative_api.xcframework",
-            "libnative_video.xcframework",
-            "libvideocapture_objc.xcframework",
-            "libvideoframebuffer_objc.xcframework",
-            "libwebsockets.xcframework"
-        ]
-
-        frameworks.each { |framework| 
-            from_path = File.join("#{sdk_lib_path}", framework)
-            remove_folder File.join("#{chat_lib_path}", framework)
-            FileUtils.move "#{from_path}", "#{chat_lib_path}", :verbose => true
-        }
-    end
-
-    private def remove_folder(folder_path)
-        FileUtils.rm_rf(folder_path) if folder_exists?(folder_path)
-    end
-
-    private def folder_exists?(folder_path)
-        File.directory?(folder_path)
     end
 end
