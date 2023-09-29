d0f61aa65f6f3b115ec2af7c194b919958bd7e1c
Banghua Zhao <bz@mega.co.nz>
2023-09-29 16:14:58 +1300

    MEET-3090 Add unit tests to WaitingRoomRepository

---
+++ Modules/Repository/ChatRepo/Sources/ChatRepoMock/SDK/MockChatRoom.swift
@@ -0,0 +1,49 @@
+import ChatRepo
+import MEGAChatSdk
+
+public final class MockChatRoom: MEGAChatRoom {
+    private let _chatId: UInt64
+    private let peerPrivilage: MEGAChatRoomPrivilege
+    private let ownPrivilage: MEGAChatRoomPrivilege
+    private let _isPreview: Bool
+    private let _isActive: Bool
+    
+    public init(
+        chatId: UInt64 = 1,
+        peerPrivilage: MEGAChatRoomPrivilege = .unknown,
+        ownPrivilage: MEGAChatRoomPrivilege = .unknown,
+        isPreview: Bool = false,
+        isActive: Bool = false
+    ) {
+        self._chatId = chatId
+        self.peerPrivilage = peerPrivilage
+        self.ownPrivilage = ownPrivilage
+        self._isPreview = isPreview
+        self._isActive = isActive
+        super.init()
+    }
+    
+    public override var chatId: UInt64 {
+        _chatId
+    }
+    
+    public override var ownPrivilege: MEGAChatRoomPrivilege {
+        ownPrivilage
+    }
+    
+    public override func peerPrivilege(byHandle userHande: UInt64) -> Int {
+        peerPrivilage.rawValue
+    }
+    
+    public override var isPreview: Bool {
+        _isPreview
+    }
+    
+    public override var isActive: Bool {
+        _isActive
+    }
+    
+    public override var authorizationToken: String {
+        ""
+    }    
+}
---
+++ Modules/Repository/ChatRepo/Sources/ChatRepoMock/SDK/MockChatSDK.swift
@@ -0,0 +1,32 @@
+import ChatRepo
+import MEGAChatSdk
+
+public final class MockChatSDK: MEGAChatSdk {
+    private let chatRoom: MEGAChatRoom?
+    
+    public var autojoinPublicChatCalled = 0
+    public var autorejoinPublicChatCalled = 0
+    
+    public init(
+        chatRoom: MEGAChatRoom? = MockChatRoom()
+    ) {
+        self.chatRoom = chatRoom
+        super.init()
+    }
+    
+    public override func chatRoom(forChatId chatId: UInt64) -> MEGAChatRoom? {
+        chatRoom
+    }
+    
+    public override func autojoinPublicChat(_ chatId: UInt64, delegate: any MEGAChatRequestDelegate) {
+        autojoinPublicChatCalled += 1
+        let request = MockChatRequest(chatHandle: chatId)
+        delegate.onChatRequestFinish?(self, request: request, error: MockChatError())
+    }
+    
+    public override func autorejoinPublicChat(_ chatId: UInt64, publicHandle: UInt64, delegate: MEGAChatRequestDelegate) {
+        autorejoinPublicChatCalled += 1
+        let request = MockChatRequest(chatHandle: chatId)
+        delegate.onChatRequestFinish?(self, request: request, error: MockChatError())
+    }
+}
