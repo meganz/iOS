b9cd1cf31f6b4e0c441c83a40d72f5c14db9d08c
Banghua ZHao <bz@mega.co.nz>
2023-10-03 11:59:21 +1300

    MEET-3100: Add Speaker icon with Bluetooth symbol in waiting room

--- iMEGA/CallScene/WaitingRoom/Views/WaitingRoomControlsView.swift
+++ iMEGA/CallScene/WaitingRoom/Views/WaitingRoomControlsView.swift
@@ -5,6 +5,7 @@ struct WaitingRoomControlsView: View {
     @Binding var isVideoEnabled: Bool
     @Binding var isMicrophoneMuted: Bool
     @Binding var isSpeakerEnabled: Bool
+    @Binding var speakerOnIcon: ImageResource
     @Binding var isBluetoothAudioRouteAvailable: Bool
     
     var body: some View {
@@ -16,7 +17,7 @@ struct WaitingRoomControlsView: View {
                                iconOn: .micOff,
                                enabled: $isMicrophoneMuted)
             WaitingRoomControl(iconOff: .speakerOff,
-                               iconOn: .speakerOn,
+                               iconOn: speakerOnIcon,
                                enabled: $isSpeakerEnabled)
             .overlay(
                 AirPlayButton()
@@ -37,9 +38,10 @@ struct WaitingRoomControl: View {
             enabled.toggle()
         } label: {
             Image(enabled ? iconOn : iconOff)
+                .resizable()
+                .frame(width: 56, height: 56)
                 .clipShape(Circle())
         }
-
     }
 }
 
@@ -47,7 +49,8 @@ struct WaitingRoomControlsView_Previews: PreviewProvider {
     static var previews: some View {
         WaitingRoomControlsView(isVideoEnabled: .constant(false),
                                 isMicrophoneMuted: .constant(true),
-                                isSpeakerEnabled: .constant(true),
+                                isSpeakerEnabled: .constant(true), 
+                                speakerOnIcon: .constant(.speakerOn),
                                 isBluetoothAudioRouteAvailable: .constant(false))
             .background(Color.black)
             .previewLayout(.sizeThatFits)
