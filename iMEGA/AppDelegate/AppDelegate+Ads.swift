d5a8a411e7523c46cc9a17e52a0120c45400b6db
Glazel Magpayo <gma@mega.co.nz>
2023-10-09 17:59:53 +0800

    IOS-7719 Folder links not opening when the app is not open in the background

--- iMEGA/AppDelegate/AppDelegate+Ads.swift
+++ iMEGA/AppDelegate/AppDelegate+Ads.swift
@@ -4,7 +4,7 @@ import SwiftUI
 
 extension AppDelegate {
     @objc func isAdsMainTabBarRootView() -> Bool {
-        window.isKind(of: UIHostingController<AdsSlotView<MainTabBarWrapper>>.self)
+        window.rootViewController?.isKind(of: UIHostingController<AdsSlotView<MainTabBarWrapper>>.self) ?? false
     }
     
     @objc func adsMainTabBarController(_ tabBar: MainTabBarController) -> UIViewController {
