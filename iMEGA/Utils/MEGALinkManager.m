e38eaf66c2f053fd0d2465d76ab8a9f712bf3e30
Glazel Magpayo <gma@mega.co.nz>
2023-10-10 14:06:56 +0800

    IOS-7719 Folder links not opening when the app is not open in the background

--- iMEGA/Utils/MEGALinkManager.m
+++ iMEGA/Utils/MEGALinkManager.m
@@ -635,7 +635,9 @@ static NSMutableSet<NSString *> *joiningOrLeavingChatBase64Handles;
     
     folderlinkVC.player = [AudioPlayerManager.shared currentPlayer];
     
-    [UIApplication.mnz_visibleViewController presentViewController:folderNavigationController animated:YES completion:nil];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [UIApplication.mnz_visibleViewController presentViewController:folderNavigationController animated:YES completion:nil];
+    });
 }
 
 + (void)showBackupLinkView {
