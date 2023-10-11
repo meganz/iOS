6adfa68a7f3f82128d6e42ae4c106bda63dc7a84
Rahul Umap <rdu@mega.co.nz>
2023-10-12 03:36:29 +1300

    IOS-7729: Adapt code for nullability audit of MEGAContactRequest.h

--- Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/User/ContactRequestEntity.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/User/ContactRequestEntity.swift
@@ -5,12 +5,12 @@ public struct ContactRequestEntity {
     public let sourceEmail: String?
     public let sourceMessage: String?
     public let targetEmail: String?
-    public let creationTime: Date
-    public let modificationTime: Date
+    public let creationTime: Date?
+    public let modificationTime: Date?
     public let isOutgoing: Bool
     public let status: ContactRequestStatusEntity?
     
-    public init(handle: HandleEntity, sourceEmail: String?, sourceMessage: String?, targetEmail: String?, creationTime: Date, modificationTime: Date, isOutgoing: Bool, status: ContactRequestStatusEntity?) {
+    public init(handle: HandleEntity, sourceEmail: String?, sourceMessage: String?, targetEmail: String?, creationTime: Date?, modificationTime: Date?, isOutgoing: Bool, status: ContactRequestStatusEntity?) {
         self.handle = handle
         self.sourceEmail = sourceEmail
         self.sourceMessage = sourceMessage
