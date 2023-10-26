b5f99fa2569809f43a23b559ab956c424a7025cc
Javier Navarro <jn@mega.co.nz>
2023-10-27 02:10:41 +1300

    IOS-7744: Remove un used colors from uicolor+additions

--- iMEGA/Utils/Categories/UIColor+Additions.swift
+++ iMEGA/Utils/Categories/UIColor+Additions.swift
@@ -81,19 +81,6 @@ extension UIColor {
             return UIColor.white
         }
     }
-    
-    @objc class func mnz_quaternaryBackgroundElevated(_ traitCollection: UITraitCollection) -> UIColor {
-        switch traitCollection.userInterfaceStyle {
-        case .unspecified, .light:
-            return UIColor.white.withAlphaComponent(0.78)
-            
-        case .dark:
-            return mnz_black252525().withAlphaComponent(0.78)
-            
-        @unknown default:
-            return UIColor.white
-        }
-    }
         
     // MARK: - Main Bar
     @objc(mnz_mainBarsForTraitCollection:)
@@ -592,23 +579,6 @@ extension UIColor {
     
     // MARK: - Toolbar
     
-    class func mnz_toolbarButtonBackground(_ traitCollection: UITraitCollection) -> UIColor {
-        switch traitCollection.userInterfaceStyle {
-        case .unspecified, .light:
-            return white
-            
-        case .dark:
-            if traitCollection.accessibilityContrast == .high {
-                return mnz_grayE5E5E5()
-            } else {
-                return mnz_black1C1C1E()
-            }
-            
-        @unknown default:
-            return white
-        }
-    }
-    
     class func mnz_toolbarTextColor(_ traitCollection: UITraitCollection) -> UIColor {
         switch traitCollection.userInterfaceStyle {
         case .unspecified, .light:
@@ -679,7 +649,7 @@ extension UIColor {
     
     // MARK: Black
     
-    @objc class func mnz_black161616() -> UIColor {
+    class func mnz_black161616() -> UIColor {
         return Colors.General.Black._161616.color
     }
     
@@ -687,49 +657,49 @@ extension UIColor {
         return Colors.General.Black._1c1c1e.color
     }
     
-    @objc class func mnz_black2C2C2E() -> UIColor {
+    class func mnz_black2C2C2E() -> UIColor {
         return Colors.General.Black._2c2c2e.color
     }
     
-    @objc class func mnz_black252525() -> UIColor {
+    class func mnz_black252525() -> UIColor {
         return Colors.General.Black._252525.color
     }
     
     // MARK: Gray
     
-    @objc class func mnz_gray363638() -> UIColor {
+    class func mnz_gray363638() -> UIColor {
         return Colors.General.Gray._363638.color
     }
     
-    @objc class func mnz_gray3A3A3C() -> UIColor {
+    class func mnz_gray3A3A3C() -> UIColor {
         return Colors.General.Gray._3A3A3C.color
     }
     
-    @objc class func mnz_gray3C3C43() -> UIColor {
+    class func mnz_gray3C3C43() -> UIColor {
         return Colors.General.Gray._3C3C43.color
     }
     
-    @objc class func mnz_gray3D3D3D() -> UIColor {
+    class func mnz_gray3D3D3D() -> UIColor {
         return Colors.General.Gray._3D3D3D.color
     }
     
-    @objc class func mnz_gray3F3F42() -> UIColor {
+    class func mnz_gray3F3F42() -> UIColor {
         return Colors.General.Gray._3F3F42.color
     }
     
-    @objc class func mnz_gray474747() -> UIColor {
+    class func mnz_gray474747() -> UIColor {
         return Colors.General.Gray._474747.color
     }
     
-    @objc class func mnz_gray515151() -> UIColor {
+    class func mnz_gray515151() -> UIColor {
         return Colors.General.Gray._515151.color
     }
     
-    @objc class func mnz_gray535356() -> UIColor {
+    class func mnz_gray535356() -> UIColor {
         return Colors.General.Gray._535356.color
     }
     
-    @objc class func mnz_gray545458() -> UIColor {
+    class func mnz_gray545458() -> UIColor {
         return Colors.General.Gray._545458.color
     }
     
@@ -737,64 +707,64 @@ extension UIColor {
         return Colors.General.Gray._545457.color
     }
     
-    @objc class func mnz_gray676767() -> UIColor {
+    class func mnz_gray676767() -> UIColor {
         return Colors.General.Gray._676767.color
     }
     
-    @objc class func mnz_gray848484() -> UIColor {
+    class func mnz_gray848484() -> UIColor {
         return Colors.General.Gray._848484.color
     }
     
-    @objc class func mnz_gray949494() -> UIColor {
+    class func mnz_gray949494() -> UIColor {
         return Colors.General.Gray._949494.color
     }
     
-    @objc class func mnz_grayB5B5B5() -> UIColor {
+    class func mnz_grayB5B5B5() -> UIColor {
         return Colors.General.Gray.b5B5B5.color
     }
     
-    @objc class func mnz_grayBBBBBB() -> UIColor {
+    class func mnz_grayBBBBBB() -> UIColor {
         return Colors.General.Gray.bbbbbb.color
     }
     
-    @objc class func mnz_grayC9C9C9() -> UIColor {
+    class func mnz_grayC9C9C9() -> UIColor {
         return Colors.General.Gray.c9C9C9.color
     }
     
-    @objc class func mnz_grayD1D1D1() -> UIColor {
+    class func mnz_grayD1D1D1() -> UIColor {
         return Colors.General.Gray.d1D1D1.color
     }
     
-    @objc class func mnz_grayE2E2E2() -> UIColor {
+    class func mnz_grayE2E2E2() -> UIColor {
         return Colors.General.Gray.e2E2E2.color
     }
     
-    @objc class func mnz_grayE5E5E5() -> UIColor {
+    class func mnz_grayE5E5E5() -> UIColor {
         return Colors.General.Gray.e5E5E5.color
     }
     
-    @objc class func mnz_grayE6E6E6() -> UIColor {
+    class func mnz_grayE6E6E6() -> UIColor {
         return Colors.General.Gray.e6E6E6.color
     }
     
-    @objc class func mnz_grayF4F4F4() -> UIColor {
+    class func mnz_grayF4F4F4() -> UIColor {
         return Colors.General.Gray.f4F4F4.color
     }
     
-    @objc class func mnz_gray04040F() -> UIColor {
+    class func mnz_gray04040F() -> UIColor {
         return Colors.General.Gray._04040F.color
     }
     
-    @objc class func mnz_grayEBEBF5() -> UIColor {
+    class func mnz_grayEBEBF5() -> UIColor {
         return Colors.General.Gray.ebebf5.color
     }
     
-    @objc class func mnz_gray333333() -> UIColor {
+    class func mnz_gray333333() -> UIColor {
         return Colors.General.Gray._333333.color
     }
     
-    @objc class func mnz_grayBABABC() -> UIColor {
-        return Colors.General.White.f7F7F7.color
+    class func mnz_grayBABABC() -> UIColor {
+        return Colors.General.Gray.bababc.color
     }
     
     @objc class func mnz_grayDBDBDB() -> UIColor {
@@ -869,19 +839,19 @@ extension UIColor {
     
     // MARK: Blue
     
-    @objc class func mnz_blue0089C7() -> UIColor {
+    class func mnz_blue0089C7() -> UIColor {
         return Colors.General.Blue._0089C7.color
     }
     
-    @objc class func mnz_blue009AE0() -> UIColor {
+    class func mnz_blue009AE0() -> UIColor {
         return Colors.General.Blue._009Ae0.color
     }
     
-    @objc class func mnz_blue059DE2() -> UIColor {
+    class func mnz_blue059DE2() -> UIColor {
         return Colors.General.Blue._059De2.color
     }
     
-    @objc class func mnz_blue38C1FF() -> UIColor {
+    class func mnz_blue38C1FF() -> UIColor {
         return Colors.General.Blue._38C1Ff.color
     }
     
@@ -909,19 +879,19 @@ extension UIColor {
     
     // MARK: Green
     
-    @objc class func mnz_green00A886() -> UIColor {
+    class func mnz_green00A886() -> UIColor {
         return Colors.General.Green._00A886.color
     }
     
-    @objc class func mnz_green00C29A() -> UIColor {
+    class func mnz_green00C29A() -> UIColor {
         return Colors.General.Green._00C29A.color
     }
     
-    @objc class func mnz_green00E9B9() -> UIColor {
+    class func mnz_green00E9B9() -> UIColor {
         return Colors.General.Green._00E9B9.color
     }
     
-    @objc class func mnz_green347467() -> UIColor {
+    class func mnz_green347467() -> UIColor {
         return Colors.General.Green._347467.color
     }
     
@@ -961,23 +931,23 @@ extension UIColor {
     
     // MARK: Red
     
-    @objc class func mnz_redF30C14() -> UIColor {
+    class func mnz_redF30C14() -> UIColor {
         return Colors.General.Red.f30C14.color
     }
     
-    @objc class func mnz_redCE0A11() -> UIColor {
+    class func mnz_redCE0A11() -> UIColor {
         return Colors.General.Red.ce0A11.color
     }
     
-    @objc class func mnz_redF7363D() -> UIColor {
+    class func mnz_redF7363D() -> UIColor {
         return Colors.General.Red.f7363D.color
     }
     
-    @objc class func mnz_redF95C61() -> UIColor {
+    class func mnz_redF95C61() -> UIColor {
         return Colors.General.Red.f95C61.color
     }
     
-    @objc class func mnz_redFF453A() -> UIColor {
+    class func mnz_redFF453A() -> UIColor {
         return Colors.General.Red.ff453A.color
     }
     
@@ -1009,43 +979,43 @@ extension UIColor {
     
     // MARK: White
     
-    @objc class func mnz_whiteEEEEEE() -> UIColor {
+    class func mnz_whiteEEEEEE() -> UIColor {
         return Colors.General.White.eeeeee.color
     }
     
-    @objc class func mnz_whiteF2F2F2() -> UIColor {
+    class func mnz_whiteF2F2F2() -> UIColor {
         return Colors.General.White.f2F2F2.color
     }
     
-    @objc class func mnz_whiteFCFCFC() -> UIColor {
+    class func mnz_whiteFCFCFC() -> UIColor {
         return Colors.General.White.fcfcfc.color
     }
     
-    @objc class func mnz_whiteF7F7F7() -> UIColor {
+    class func mnz_whiteF7F7F7() -> UIColor {
         return Colors.General.White.f7F7F7.color
     }
     
-    @objc class func mnz_whiteEFEFEF() -> UIColor {
+    class func mnz_whiteEFEFEF() -> UIColor {
         return Colors.General.White.efefef.color
     }
     
     // MARK: Yellow
     
-    @objc class func mnz_yellowFED429() -> UIColor {
+    class func mnz_yellowFED429() -> UIColor {
         return Colors.General.Yellow.fed429.color
     }
     
-    @objc class func mnz_yellow9D8319() -> UIColor {
+    class func mnz_yellow9D8319() -> UIColor {
         return Colors.General.Yellow._9D8319.color
     }
     
-    @objc class func mnz_yellowF8D552() -> UIColor {
+    class func mnz_yellowF8D552() -> UIColor {
         return Colors.General.Yellow.f8D552.color
     }
     
     // MARK: Brown
     
-    @objc class func mnz_Brown544b27() -> UIColor {
+    class func mnz_brown544b27() -> UIColor {
         return Colors.General.Brown._544b27.color
     }
 }
