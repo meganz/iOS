54f8d482e2970552e27794f7016259585514c473
Mauritz Mollentze <mmo@mega.co.nz>
2023-11-08 17:28:33 +1300

    CC-5711: Update camera upload rate

--- Modules/Domain/MEGADomain/Sources/MEGADomain/UseCase/CameraUploads/FakeCameraUploadUseCase.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/UseCase/CameraUploads/FakeCameraUploadUseCase.swift
@@ -4,11 +4,13 @@ import MEGASwift
 public struct FakeCameraUploadSuccessfulUseCase: MonitorCameraUploadUseCaseProtocol {
     public let monitorUploadStatus: AnyAsyncSequence<Result<CameraUploadStatsEntity, Error>>
     
-    public init(photoUploadCount: UInt = 10_000,
-                initialDelayInNanoSeconds: UInt64 = 1_000_000_000) {
+    public init(photoUploadCount: UInt = 100,
+                initialDelayInNanoSeconds: UInt64 = 1_000_000_000,
+                delayBetweenItemsInNanoSeconds: UInt64 = 100_000_000) {
         monitorUploadStatus = UploadPhotosAsyncSequence(
             uploadCount: photoUploadCount,
-            initialDelayInNanoSeconds: initialDelayInNanoSeconds
+            initialDelayInNanoSeconds: initialDelayInNanoSeconds,
+            delayBetweenItemsInNanoSeconds: delayBetweenItemsInNanoSeconds
         ).eraseToAnyAsyncSequence()
     }
     
@@ -16,11 +18,14 @@ public struct FakeCameraUploadSuccessfulUseCase: MonitorCameraUploadUseCaseProto
         typealias Element = Result<CameraUploadStatsEntity, Error>
         private let uploadCount: UInt
         private let initialDelayInNanoSeconds: UInt64
+        private let delayBetweenItemsInNanoSeconds: UInt64
         
         init(uploadCount: UInt,
-             initialDelayInNanoSeconds: UInt64) {
+             initialDelayInNanoSeconds: UInt64,
+             delayBetweenItemsInNanoSeconds: UInt64) {
             self.uploadCount = uploadCount
             self.initialDelayInNanoSeconds = initialDelayInNanoSeconds
+            self.delayBetweenItemsInNanoSeconds = delayBetweenItemsInNanoSeconds
         }
         
         struct UploadPhotosAsyncIterator: AsyncIteratorProtocol {
@@ -28,11 +33,15 @@ public struct FakeCameraUploadSuccessfulUseCase: MonitorCameraUploadUseCaseProto
             
             private let uploadCount: UInt
             private let initialDelayInNanoSeconds: UInt64
+            private let delayBetweenItemsInNanoSeconds: UInt64
             private var currentCount: UInt = 0
             
-            init(uploadCount: UInt, initialDelayInNanoSeconds: UInt64) {
+            init(uploadCount: UInt,
+                 initialDelayInNanoSeconds: UInt64,
+                 delayBetweenItemsInNanoSeconds: UInt64) {
                 self.uploadCount = uploadCount
                 self.initialDelayInNanoSeconds = initialDelayInNanoSeconds
+                self.delayBetweenItemsInNanoSeconds = delayBetweenItemsInNanoSeconds
             }
             
             mutating func next() async -> Result<CameraUploadStatsEntity, Error>? {
@@ -40,9 +49,9 @@ public struct FakeCameraUploadSuccessfulUseCase: MonitorCameraUploadUseCaseProto
                       currentCount <= uploadCount else {
                     return nil
                 }
-                if currentCount == 0 {
-                    try? await Task.sleep(nanoseconds: initialDelayInNanoSeconds)
-                }
+                try? await Task.sleep(nanoseconds: currentCount == 0 ? initialDelayInNanoSeconds : delayBetweenItemsInNanoSeconds)
+                guard !Task.isCancelled else { return nil }
+                
                 let nextValue = CameraUploadStatsEntity(
                     progress: Float(currentCount) / Float(uploadCount),
                     pendingFilesCount: uploadCount - currentCount)
@@ -53,7 +62,8 @@ public struct FakeCameraUploadSuccessfulUseCase: MonitorCameraUploadUseCaseProto
         
         func makeAsyncIterator() -> UploadPhotosAsyncIterator {
             UploadPhotosAsyncIterator(uploadCount: uploadCount,
-                                      initialDelayInNanoSeconds: initialDelayInNanoSeconds)
+                                      initialDelayInNanoSeconds: initialDelayInNanoSeconds,
+                                      delayBetweenItemsInNanoSeconds: delayBetweenItemsInNanoSeconds)
         }
     }
 }
