c8d9ad3ce8cf3562672f14757b818abb028f1779
Roberto Gómez <rg@mega.nz>
2023-11-11 00:04:50 +1300

    BAC-688. Limit device name label to two lines in the device list

--- Modules/Presentation/Features/DeviceCenter/Sources/DeviceCenter/DeviceCenterItem/DeviceCenterItemView.swift
+++ Modules/Presentation/Features/DeviceCenter/Sources/DeviceCenter/DeviceCenterItem/DeviceCenterItemView.swift
@@ -17,7 +17,7 @@ struct DeviceCenterItemView: View {
                 Text(viewModel.name)
                     .font(.subheadline)
                     .fontWeight(.medium)
-                    .lineLimit(1)
+                    .lineLimit(2)
                     .foregroundColor(colorScheme == .dark ? .white : .black)
                 HStack(spacing: 4) {
                     if viewModel.shouldShowBackupPercentage {
