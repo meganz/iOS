8c2031e6652ca5b30161b9395c4cfb4bf1d3ffd4
rdu-mega <rdu@mega.co.nz>
2023-11-21 09:30:19 +0530

    T12652259: Fix fonts on meeting and photos screen and update the bottom indicator height to be same as chats.

--- iMEGA/ChatRoomsListScene/Views/ChatTabsSelectorView.swift
+++ iMEGA/ChatRoomsListScene/Views/ChatTabsSelectorView.swift
@@ -5,10 +5,7 @@ struct ChatTabsSelectorView: View {
     @Environment(\.colorScheme) private var colorScheme
     var chatViewMode: ChatViewMode
     var action: (ChatViewMode) -> Void
-    
-    private let defaultTabFont = Font.system(.subheadline, design: .default).weight(.regular)
-    private let selectedTabFont = Font.system(.subheadline, design: .default).weight(.medium)
-    
+
     var body: some View {
         HStack(spacing: 0) {
             VStack {
@@ -17,7 +14,7 @@ struct ChatTabsSelectorView: View {
                     action(.chats)
                 }, label: {
                     Text(Strings.Localizable.Chat.Selector.chat)
-                        .font(chatViewMode == .chats ? selectedTabFont : defaultTabFont)
+                        .font(Font.system(.subheadline, design: .default).weight(.medium))
                         .foregroundColor(Color(chatViewMode == .chats ? Colors.Chat.Tabs.chatTabSelectedText.color : Colors.Chat.Tabs.chatTabNormalText.color))
                 })
                 Divider()
@@ -31,7 +28,7 @@ struct ChatTabsSelectorView: View {
                     action(.meetings)
                 }, label: {
                     Text(Strings.Localizable.Chat.Selector.meeting)
-                        .font(chatViewMode == .meetings ? selectedTabFont : defaultTabFont)
+                        .font(Font.system(.subheadline, design: .default).weight(.medium))
                         .foregroundColor(Color(chatViewMode == .meetings ? Colors.Chat.Tabs.chatTabSelectedText.color : Colors.Chat.Tabs.chatTabNormalText.color))
                 })
                 Divider()
