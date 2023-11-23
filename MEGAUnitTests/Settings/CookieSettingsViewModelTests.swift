5279fe12d9df8a8f76ab6ba1a546e50f04e25694
Glazel Magpayo <gma@mega.co.nz>
2023-11-23 23:51:50 +1300

    AP-879 Add advertisement cookie on Cookie Settings

--- MEGAUnitTests/Settings/CookieSettingsViewModelTests.swift
+++ MEGAUnitTests/Settings/CookieSettingsViewModelTests.swift
@@ -2,20 +2,37 @@
 import MEGADomain
 import MEGADomainMock
 import MEGAL10n
+import MEGAPresentation
+import MEGAPresentationMock
 import XCTest
 
 class CookieSettingsViewModelTests: XCTestCase {
-    private let mockRouter = MockCookieSettingsRouter()
     private let footersArray: [String] = [Strings.Localizable.Settings.Accept.Cookies.footer,
                                                Strings.Localizable.Settings.Cookies.Essential.footer,
                                                Strings.Localizable.Settings.Cookies.PerformanceAndAnalytics.footer]
     
-    func testAction_configView_cookieSettings_success() {
+    func testIsFeatureFlagForInAppAdsEnabled_inAppAdsEnabled_shouldBeEnabled() {
+        let sut = makeSUT(featureFlags: [.inAppAds: true])
+        XCTAssertTrue(sut.isFeatureFlagForInAppAdsEnabled)
+    }
+    
+    func testIsFeatureFlagForInAppAdsEnabled_inAppAdsDisabled_shouldBeDisabled() {
+        let sut = makeSUT(featureFlags: [.inAppAds: false])
+        XCTAssertFalse(sut.isFeatureFlagForInAppAdsEnabled)
+    }
+    
+    func testActionConfigView_cookieSettings_featureFlagInAppAdsEnabled_success() {
+        let sut = makeSUT(featureFlags: [.inAppAds: true])
+        var expectedFooters = footersArray
+        expectedFooters.append(Strings.Localizable.Settings.Cookies.AdvertisingCookies.footer)
         
-        let cookieSettingsUseCase = MockCookieSettingsUseCase(cookieBannerEnable: true,
-                                                              cookieSettings: .success(10))
-        let sut = CookieSettingsViewModel(cookieSettingsUseCase: cookieSettingsUseCase,
-                                          router: mockRouter)
+        test(viewModel: sut,
+             action: .configView,
+             expectedCommands: [.configCookieSettings(CookiesBitmap(rawValue: 10)), .updateFooters(expectedFooters)])
+    }
+    
+    func testAction_configView_cookieSettings_success() {
+        let sut = makeSUT(cookieSettings: .success(10))
         
         test(viewModel: sut,
              action: .configView,
@@ -23,10 +40,7 @@ class CookieSettingsViewModelTests: XCTestCase {
     }
     
     func testAction_configView_cookieSettings_fail_bitmapNotSet() {
-        let cookieSettingsUseCase = MockCookieSettingsUseCase(cookieBannerEnable: true,
-                                                              cookieSettings: .failure(.bitmapNotSet))
-        let sut = CookieSettingsViewModel(cookieSettingsUseCase: cookieSettingsUseCase,
-                                          router: mockRouter)
+        let sut = makeSUT(cookieSettings: .failure(.bitmapNotSet))
         
         test(viewModel: sut,
              action: .configView,
@@ -34,9 +48,7 @@ class CookieSettingsViewModelTests: XCTestCase {
     }
     
     func testAction_configView_cookieSettings_fail_generic() {
-        let cookieSettingsUseCase = MockCookieSettingsUseCase(cookieBannerEnable: true)
-        let sut = CookieSettingsViewModel(cookieSettingsUseCase: cookieSettingsUseCase,
-                                          router: mockRouter)
+        let sut = makeSUT(cookieSettings: .failure(.generic))
         
         test(viewModel: sut,
              action: .configView,
@@ -44,10 +56,7 @@ class CookieSettingsViewModelTests: XCTestCase {
     }
     
     func testAction_configView_cookieSettings_fail_invalidBitmap() {
-        let cookieSettingsUseCase = MockCookieSettingsUseCase(cookieBannerEnable: true,
-                                                              cookieSettings: .failure(.invalidBitmap))
-        let sut = CookieSettingsViewModel(cookieSettingsUseCase: cookieSettingsUseCase,
-                                          router: mockRouter)
+        let sut = makeSUT(cookieSettings: .failure(.invalidBitmap))
         
         test(viewModel: sut,
              action: .configView,
@@ -55,14 +64,30 @@ class CookieSettingsViewModelTests: XCTestCase {
     }
     
     func testAction_save_setCookieSettings_success() {
-        let cookieSettingsUseCase = MockCookieSettingsUseCase(cookieBannerEnable: true,
-                                                              setCookieSettings: .success(10))
-        let sut = CookieSettingsViewModel(cookieSettingsUseCase: cookieSettingsUseCase,
-                                          router: mockRouter)
+        let sut = makeSUT(cookieSettings: .success(10))
         
         test(viewModel: sut,
              action: .save,
              expectedCommands: [.cookieSettingsSaved])
+    }
+    
+    // MARK: Helper
+    private func makeSUT(
+        cookieBannerEnable: Bool = true,
+        cookieSettings: Result<Int, CookieSettingsErrorEntity> = .success(10),
+        featureFlags: [FeatureFlagKey: Bool] = [FeatureFlagKey.inAppAds: false],
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> CookieSettingsViewModel {
+        let mockRouter = MockCookieSettingsRouter()
+        let featureFlagProvider = MockFeatureFlagProvider(list: featureFlags)
+        let cookieSettingsUseCase = MockCookieSettingsUseCase(cookieBannerEnable: cookieBannerEnable, cookieSettings: cookieSettings)
+        
+        let sut = CookieSettingsViewModel(cookieSettingsUseCase: cookieSettingsUseCase,
+                                          router: mockRouter,
+                                          featureFlagProvider: featureFlagProvider)
+        trackForMemoryLeaks(on: sut, file: file, line: line)
+        return sut
     }
 }
 
