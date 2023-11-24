40db0cbdcf3a0ab3e54c8d51e66bac19541348f7
Javier Navarro <jn@mega.co.nz>
2023-11-24 09:51:02 +0100

    IOS-7800: Rename UIColor+Addition.swift in MEGAUIKit module

--- Modules/UI/MEGAUI/Sources/MEGAUI/UIColor+Image.swift
+++ Modules/UI/MEGAUI/Sources/MEGAUI/UIColor+Image.swift
@@ -1,15 +1,6 @@
 import UIKit
 
 public extension UIColor {
-
-    var hexString: String {
-        let components = cgColor.components
-        let r: CGFloat = components?[0] ?? 0.0
-        let g: CGFloat = components?[1] ?? 0.0
-        let b: CGFloat = components?[2] ?? 0.0
-
-        return String(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
-     }
     
     /// Will generate an `UIImage` with given size filled by invoker `UIColor`(self).
     /// - Parameter size: `CGSize` instance, size of the image. Image will fill from point (0, 0) - origin.
