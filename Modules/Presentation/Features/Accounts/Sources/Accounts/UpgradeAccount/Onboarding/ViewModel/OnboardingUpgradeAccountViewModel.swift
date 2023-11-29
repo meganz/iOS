0762fd5a475ae56b6fa9b9808a8838c56371f4cb
Glazel Magpayo <gma@mega.co.nz>
2023-11-29 14:34:17 +1300

    AP-861 Onboarding upselling dialog Variant B - UI

--- Modules/Presentation/Features/Accounts/Sources/Accounts/UpgradeAccount/Onboarding/ViewModel/OnboardingUpgradeAccountViewModel.swift
+++ Modules/Presentation/Features/Accounts/Sources/Accounts/UpgradeAccount/Onboarding/ViewModel/OnboardingUpgradeAccountViewModel.swift
@@ -8,11 +8,19 @@ import MEGAPresentation
 public final class OnboardingUpgradeAccountViewModel: ObservableObject {
     private let purchaseUseCase: any AccountPlanPurchaseUseCaseProtocol
     private let tracker: any AnalyticsTracking
-    private let viewProPlanAction: () -> Void
     private var subscriptions: Set<AnyCancellable> = []
     
     @Published private(set) var shouldDismiss: Bool = false
     @Published private(set) var lowestProPlan: AccountPlanEntity = AccountPlanEntity()
+    
+    // Variant A only
+    private let viewProPlanAction: () -> Void
+    
+    // Variant B only
+    @Published var selectedCycleTab: SubscriptionCycleEntity = .yearly
+    @Published private(set) var selectedPlanType: AccountTypeEntity?
+    private var planList: [AccountPlanEntity] = []
+    private(set) var recommendedPlanType: AccountTypeEntity?
     
     public init(
         purchaseUseCase: some AccountPlanPurchaseUseCaseProtocol,
@@ -25,8 +33,8 @@ public final class OnboardingUpgradeAccountViewModel: ObservableObject {
         
         setupSubscriptions()
     }
-    
-    func setupSubscriptions() {
+
+    private func setupSubscriptions() {
         NotificationCenter.default
             .publisher(for: .dismissOnboardingProPlanDialog)
             .receive(on: DispatchQueue.main)
@@ -35,14 +43,6 @@ public final class OnboardingUpgradeAccountViewModel: ObservableObject {
                 shouldDismiss = true
             }
             .store(in: &subscriptions)
-    }
-    
-    @MainActor
-    public func setUpLowestProPlan() async {
-        let planList = await self.purchaseUseCase.accountPlanProducts()
-        
-        guard let plan = planList.sorted(by: { $0.price < $1.price }).first else { return }
-        lowestProPlan = plan
     }
     
     public var storageContentMessage: String {
@@ -55,10 +55,108 @@ public final class OnboardingUpgradeAccountViewModel: ObservableObject {
             .replacingOccurrences(of: "[B]", with: storageComponents[1]) // Storage unit
         return message
     }
+   
+    // MARK: - Variant A with View Pro Plans
+    
+    @MainActor
+    public func setUpLowestProPlan() async {
+        let planList = await self.purchaseUseCase.accountPlanProducts()
+        lowestProPlan = lowestPlan(planList: planList)
+    }
     
     func showProPlanView() {
         tracker.trackAnalyticsEvent(with: OnboardingUpsellingDialogVariantAViewProPlansButtonEvent())
         
         viewProPlanAction()
+    }
+    
+    // MARK: - Variant B with list of free and all Pro Plans
+    
+    @MainActor
+    func setupPlans() async {
+        planList = await purchaseUseCase.accountPlanProducts()
+        
+        // Set lowest pro plan
+        lowestProPlan = lowestPlan(planList: planList)
+        
+        // Add free account on plan selection list
+        planList.insert(freeAccountPlan, at: 0)
+        
+        // Set default recommended and selected plan
+        setDefaultRecommendedSelectedPlan()
+    }
+    
+    private let freeAccountPlan = AccountPlanEntity(
+        type: .free,
+        name: "Free",
+        subscriptionCycle: .none,
+        storage: "20 GB",
+        transfer: "Limited",
+        formattedPrice: "Free"
+    )
+    
+    var selectedPlanName: String {
+        selectedPlanType?.toAccountTypeDisplayName() ?? ""
+    }
+    
+    var filteredPlanList: [AccountPlanEntity] {
+        planList.filter { $0.subscriptionCycle == selectedCycleTab || $0.subscriptionCycle == .none }
+    }
+    
+    var currentSelectedPlan: AccountPlanEntity? {
+        guard let selectedPlanType,
+              let selectedPlan = filteredPlanList.first(where: { $0.type == selectedPlanType }) else {
+            return nil
+        }
+        return selectedPlan
+    }
+    
+    private func setDefaultRecommendedSelectedPlan() {
+        switch lowestProPlan.type {
+        case .free, .lite:
+            recommendedPlanType = .proI
+        case .proI:
+            recommendedPlanType = .proII
+        case .proII:
+            recommendedPlanType = .proIII
+        default:
+            return
+        }
+        selectedPlanType = recommendedPlanType
+    }
+    
+    func createAccountPlanViewModel(_ plan: AccountPlanEntity) -> AccountPlanViewModel {
+        AccountPlanViewModel(
+            plan: plan,
+            planTag: planTag(plan),
+            isSelected: isPlanSelected(plan),
+            isSelectionEnabled: true,
+            didTapPlan: {
+                self.setSelectedPlan(plan)
+            })
+    }
+    
+    private func planTag(_ plan: AccountPlanEntity) -> AccountPlanTagEntity {
+        guard let recommendedPlanType,
+              plan.subscriptionCycle == selectedCycleTab,
+              plan.type == recommendedPlanType else {
+            return .none
+        }
+        
+        return .recommended
+    }
+    
+    private func isPlanSelected(_ plan: AccountPlanEntity) -> Bool {
+        guard let selectedPlanType else { return false }
+        return selectedPlanType == plan.type
+    }
+    
+    func setSelectedPlan(_ plan: AccountPlanEntity) {
+        selectedPlanType = plan.type
+    }
+    
+    // MARK: - Helper
+    private func lowestPlan(planList: [AccountPlanEntity]) -> AccountPlanEntity {
+        return planList.sorted(by: { $0.price < $1.price }).first ?? AccountPlanEntity()
     }
 }
