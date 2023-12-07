5e76b75e5e5c804b2d31034c282393049afc4820
Harry Yan <hy@mega.co.nz>
2023-12-08 09:50:56 +1300

    IOS-7651: Update repo initilization way and see if we can get more logs

--- iMEGA/My Account/Contacts/ItemList/ItemListViewController+Additions.swift
+++ iMEGA/My Account/Contacts/ItemList/ItemListViewController+Additions.swift
@@ -13,7 +13,7 @@ extension ItemListViewController {
         cell.nameLabel.text = item.name
         cell.avatarView.avatarImageView.layer.cornerRadius = cell.avatarView.avatarImageView.frame.size.width / 2
         cell.avatarView.avatarImageView.layer.masksToBounds = true
-
+        
         if item.isGroup {
             guard let chatRoom = MEGAChatSdk.shared.chatRoom(forChatId: item.handle) else { return }
             cell.avatarView.setup(for: chatRoom)
@@ -24,35 +24,35 @@ extension ItemListViewController {
                     cell.avatarView.avatarImageView.image = image
                 }
             )
-
+            
             cell.avatarView.configure(mode: .single)
         }
-
+        
         guard let user = item.user, MEGASdk.shared.isContactVerificationWarningEnabled
         else {
             cell.contactVerifiedImageView.isHidden = true
             return
         }
-
+        
         cell.contactVerifiedImageView.isHidden = !MEGASdk.shared.areCredentialsVerified(of: user)
     }
-
+    
     private func fetchUserAvatar(for item: ItemListModel, completion: @escaping (UIImage) -> Void) {
         let megaHandleUseCase = MEGAHandleUseCase(repo: MEGAHandleRepository.newRepo)
-
+        
         guard let base64Handle = megaHandleUseCase.base64Handle(forUserHandle: item.handle),
               let avatarBackgroundHexColor = MEGASdk.avatarColor(forBase64UserHandle: base64Handle) else {
             MEGALogDebug("Contacts list: base64 handle not found for handle \(item.handle)")
             return
         }
-
+        
         let userImageUseCase = UserImageUseCase(
-            userImageRepo: UserImageRepository(sdk: MEGASdk.shared),
-            userStoreRepo: UserStoreRepository(store: MEGAStore.shareInstance()),
+            userImageRepo: UserImageRepository.newRepo,
+            userStoreRepo: UserStoreRepository.newRepo,
             thumbnailRepo: ThumbnailRepository.newRepo,
             fileSystemRepo: FileSystemRepository.newRepo
         )
-
+        
         userImageUseCase.fetchUserAvatar(withUserHandle: item.handle,
                                          base64Handle: base64Handle,
                                          avatarBackgroundHexColor: avatarBackgroundHexColor,
