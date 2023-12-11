7bc54b98bb7cdb4dd65ac0b191fcd370ba844a7c
Devyish Gandhi <dg@mega.co.nz>
2023-12-12 11:58:18 +1300

    CC-5945: Redo debounceImmediate to consistently pass

--- Modules/Infrastracture/MEGASwift/Tests/MEGASwiftTests/Publisher+DebounceImmediateTests.swift
+++ Modules/Infrastracture/MEGASwift/Tests/MEGASwiftTests/Publisher+DebounceImmediateTests.swift
@@ -4,79 +4,57 @@ import XCTest
 
 final class PublisherDebounceImmediateTests: XCTestCase {
     
-    func testDebounceImmediate_emitsFirstValueOnlyBeforeTimerEnds() {
+    func testDebounceImmediate_emitsFirstValueOnlyBeforeTimerEnds() async {
         
         // Arrange
-        let expectation = XCTestExpectation(description: "Emits test elements to completiom")
-        let events = [0, 1, 2, 3, 4, 5, 7]
-            .map { value in Just(value).delay(for: .milliseconds(value * 200), scheduler: DispatchQueue.main) }
+        let events = [0, 1, 2, 3, 4, 5]
+            .map { value in Just(value).delay(for: .milliseconds(value * 90), scheduler: DispatchQueue.main) }
         
         // Act
-        var results: [Int] = []
-        
-        let subscription = Publishers
+        let results = await Publishers
             .MergeMany(events)
-            .timeout(.seconds(0.05), scheduler: DispatchQueue.main)
             .debounceImmediate(for: .milliseconds(100), scheduler: DispatchQueue.main)
-            .sink(
-                receiveCompletion: { _ in expectation.fulfill() },
-                receiveValue: { results.append($0) })
+            .collect()
+            .values
+            .first { _ in true }
         
-        wait(for: [expectation], timeout: 1)
-        subscription.cancel()
-
         // Assert
         let expected = [0]
         XCTAssertEqual(expected, results)
     }
     
-    func testDebounceImmediate_emitsFirstValueThenDebouncesAnotherValueBeforeTimer() {
+    func testDebounceImmediate_emitsFirstValueThenDebouncesAnotherValueBeforeTimer() async {
         
         // Arrange
-        let expectation = XCTestExpectation(description: "Emits test elements to completiom")
-        let events = [0, 1, 4, 5, 6]
-            .map { value in Just(value).delay(for: .milliseconds(value * 200), scheduler: DispatchQueue.main) }
+        let events = [0, 0.1, 0.3, 0.4, 0.6]
+            .map { value in Just(value).delay(for: .init(floatLiteral: value), scheduler: DispatchQueue.main) }
         
         // Act
-        var results: [Int] = []
-        
-        let subscription = Publishers
+        let results = await Publishers
             .MergeMany(events)
-            .timeout(.seconds(0.2), scheduler: DispatchQueue.main)
             .debounceImmediate(for: .milliseconds(100), scheduler: DispatchQueue.main)
-            .sink(
-                receiveCompletion: { _ in expectation.fulfill() },
-                receiveValue: { results.append($0) })
-        
-        wait(for: [expectation], timeout: 1)
-        
-        subscription.cancel()
+            .collect()
+            .values
+            .first { _ in true }
+
         // Assert
-        let expected = [0, 1]
+        let expected: [Double] = [0, 0.1, 0.4]
         XCTAssertEqual(expected, results)
     }
     
-    func testDebounceImmediate_emitsFirstValueThenDebouncesOverMultipleValues() {
+    func testDebounceImmediate_emitsFirstValueThenDebouncesOverMultipleValues() async {
         
         // Arrange
-        let expectation = XCTestExpectation(description: "Emits test elements to completiom")
         let events = [0, 1, 2, 3, 10]
             .map { value in Just(value).delay(for: .milliseconds(value * 200), scheduler: DispatchQueue.main) }
         
         // Act
-        var results: [Int] = []
-        
-        let subscription = Publishers
+        let results = await Publishers
             .MergeMany(events)
-            .timeout(.seconds(0.3), scheduler: DispatchQueue.main)
             .debounceImmediate(for: .milliseconds(220), scheduler: DispatchQueue.main)
-            .sink(
-                receiveCompletion: { _ in expectation.fulfill() },
-                receiveValue: { results.append($0) })
-        
-        wait(for: [expectation], timeout: 1.5)
-        
-        subscription.cancel()
+            .collect()
+            .values
+            .first { _ in true }
         
         // Assert
         let expected = [0, 3]
