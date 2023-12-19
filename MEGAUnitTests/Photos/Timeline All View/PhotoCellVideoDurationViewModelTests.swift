4c638704105f9c169f9b8fc71ead1b2821b60a43
Mauritz Mollentze <mmo@mega.co.nz>
2023-12-19 16:12:32 +1300

    CC-5587: Remove play icon from video timestamp

--- MEGAUnitTests/Photos/Timeline All View/PhotoCellVideoDurationViewModelTests.swift
+++ MEGAUnitTests/Photos/Timeline All View/PhotoCellVideoDurationViewModelTests.swift
@@ -2,35 +2,33 @@
 import XCTest
 
 final class PhotoCellVideoDurationViewModelTests: XCTestCase {
-    func testShouldShowDurationView_video() {
-        var sut = PhotoCellVideoDurationViewModel(isVideo: true, duration: "")
-        for scaleFactor in PhotoLibraryZoomState.ScaleFactor.allCases {
-            sut.scaleFactor = scaleFactor
-            XCTAssertEqual(sut.shouldShowDurationView, scaleFactor != .thirteen)
-        }
-    }
-
-    func testShouldShowDurationView_notVideo() {
-        var sut = PhotoCellVideoDurationViewModel(isVideo: false, duration: "")
-        for scaleFactor in PhotoLibraryZoomState.ScaleFactor.allCases {
-            sut.scaleFactor = scaleFactor
-            XCTAssertFalse(sut.shouldShowDurationView)
-        }
-    }
-    
     func testShouldShowDurationDetail_video_validDuration() {
         let sut = PhotoCellVideoDurationViewModel(isVideo: true, duration: "01:00")
-        XCTAssertTrue(sut.shouldShowDurationDetail)
+        XCTAssertTrue(sut.shouldShowDuration)
     }
     
     func testShouldShowDurationDetail_video_invalidDuration() {
         let sut = PhotoCellVideoDurationViewModel(isVideo: true, duration: "")
-        XCTAssertFalse(sut.shouldShowDurationDetail)
+        XCTAssertFalse(sut.shouldShowDuration)
     }
     
     func testShouldShowDurationDetail_notVideo_invalidDuration() {
         let sut = PhotoCellVideoDurationViewModel(isVideo: false, duration: "")
-        XCTAssertFalse(sut.shouldShowDurationDetail)
+        XCTAssertFalse(sut.shouldShowDuration)
+    }
+    
+    func testDurationYOffset_scaleFactor_correctOffset() {
+        var sut = PhotoCellVideoDurationViewModel(isVideo: true, duration: "01:00")
+        
+        [(PhotoLibraryZoomState.ScaleFactor.one, -6),
+         (.three, -5),
+         (.five, -2),
+         (.thirteen, 0)
+        ].forEach {
+            sut.scaleFactor = $0.0
+            
+            XCTAssertEqual(sut.durationYOffset, $0.1)
+        }
     }
 
 }
