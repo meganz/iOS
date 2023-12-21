b3e5b1f8a3896b214b239d643ff97bcecb825b75
Mauritz Mollentze <mmo@mega.co.nz>
2023-12-22 12:09:05 +1300

    CC-6062: Fix album toolbar incorrect state edit mode exit

--- MEGAUnitTests/CameraUpload/PhotoAlbumContainerViewModelTests.swift
+++ MEGAUnitTests/CameraUpload/PhotoAlbumContainerViewModelTests.swift
@@ -1,5 +1,6 @@
 @testable import MEGA
 import MEGAAnalyticsiOS
+import MEGAPresentation
 import MEGAPresentationMock
 import MEGATest
 import XCTest
@@ -7,27 +8,47 @@ import XCTest
 final class PhotoAlbumContainerViewModelTests: XCTestCase {
     
     func testDidAppear_shouldTrackPhotoScreenViewEvent() {
-        let mockTracker = MockTracker()
-        let sut = PhotoAlbumContainerViewModel(tracker: mockTracker)
+        let tracker = MockTracker()
+        let sut = makeSUT(tracker: tracker)
         
         sut.didAppear()
 
         assertTrackAnalyticsEventCalled(
-            trackedEventIdentifiers: mockTracker.trackedEventIdentifiers,
+            trackedEventIdentifiers: tracker.trackedEventIdentifiers,
             with: [PhotoScreenEvent()]
         )
     }
     
     func testShareLinksTapped_shouldSetShowShareAlbumLinksToTrueAndTrackEvent() {
-        let mockTracker = MockTracker()
-        let sut = PhotoAlbumContainerViewModel(tracker: mockTracker)
+        let tracker = MockTracker()
+        let sut = makeSUT(tracker: tracker)
         
         sut.shareLinksTapped()
         
         XCTAssertTrue(sut.showShareAlbumLinks)
         assertTrackAnalyticsEventCalled(
-            trackedEventIdentifiers: mockTracker.trackedEventIdentifiers,
+            trackedEventIdentifiers: tracker.trackedEventIdentifiers,
             with: [AlbumListShareLinkMenuItemEvent()]
         )
+    }
+    
+    func testShowToolbar_onEditModeUpdate_shouldChange() {
+        let sut = makeSUT()
+        XCTAssertFalse(sut.showToolbar)
+        
+        sut.editMode = .active
+        XCTAssertTrue(sut.showToolbar)
+        
+        sut.editMode = .inactive
+        XCTAssertFalse(sut.showToolbar)
+    }
+    
+    private func makeSUT(tracker: some AnalyticsTracking = MockTracker(),
+                         file: StaticString = #file,
+                         line: UInt = #line
+    ) -> PhotoAlbumContainerViewModel {
+       let sut = PhotoAlbumContainerViewModel(tracker: tracker)
+        trackForMemoryLeaks(on: sut, file: file, line: line)
+        return sut
     }
 }
