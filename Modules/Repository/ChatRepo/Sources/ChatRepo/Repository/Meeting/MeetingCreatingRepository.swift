b131b66746618a9f69a8be8bab3a14f218c26047
Banghua Zhao <bz@mega.co.nz>
2024-01-08 15:59:47 +1300

    IOS-7491: Move MeetingCreatingRepository to ChatRepo

--- Modules/Repository/ChatRepo/Sources/ChatRepo/Repository/Meeting/MeetingCreatingRepository.swift
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/Repository/Meeting/MeetingCreatingRepository.swift
@@ -1,12 +1,12 @@
-import ChatRepo
 import Foundation
+import MEGAChatSdk
 import MEGADomain
 import MEGASDKRepo
 import MEGASwift
 
-final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreatingRepositoryProtocol {
+public final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreatingRepositoryProtocol {
     
-    static var newRepo: MeetingCreatingRepository {
+    public static var newRepo: MeetingCreatingRepository {
         MeetingCreatingRepository(chatSdk: .sharedChatSdk, sdk: .sharedSdk)
     }
     
@@ -19,19 +19,19 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
         self.sdk = sdk
     }
     
-    func username() -> String {
+    public func username() -> String {
         chatSdk.userFullnameFromCache(byUserHandle: MEGASdk.currentUserHandle()?.uint64Value ?? 0) ?? ""
     }
     
-    func userEmail() -> String? {
+    public func userEmail() -> String? {
         sdk.myEmail
     }
     
-    func createChatLink(forChatId chatId: UInt64) {
+    public func createChatLink(forChatId chatId: UInt64) {
         chatSdk.createChatLink(chatId)
     }
     
-    func createMeeting(_ startCall: StartCallEntity) async throws -> ChatRoomEntity {
+    public func createMeeting(_ startCall: StartCallEntity) async throws -> ChatRoomEntity {
         try await withAsyncThrowingValue { result in
             let delegate = ChatRequestDelegate { [weak self] completion in
                 switch completion {
@@ -56,13 +56,12 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
             )
         }
     }
-
-    func joinChatCall(forChatId chatId: UInt64, enableVideo: Bool, enableAudio: Bool, userHandle: UInt64, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void) {
+    
+    public func joinChatCall(forChatId chatId: UInt64, enableVideo: Bool, enableAudio: Bool, userHandle: UInt64, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void) {
         let delegate = ChatRequestDelegate { [weak self] result in
             switch result {
             case .success(let request):
                 guard let self, let megaChatRoom = chatSdk.chatRoom(forChatId: request.chatHandle) else {
-                    MEGALogDebug("ChatRoom not found with chat handle \(MEGASdk.base64Handle(forUserHandle: request.chatHandle) ?? "-1")")
                     completion(.failure(.generic))
                     return
                 }
@@ -76,21 +75,19 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
         
         if let megaChatRoom = chatSdk.chatRoom(forChatId: chatId),
            !megaChatRoom.isPreview,
-           !megaChatRoom.isActive {            
+           !megaChatRoom.isActive {
             chatSdk.autorejoinPublicChat(chatId, publicHandle: userHandle, delegate: delegate)
         } else {
-            MEGALogDebug("Create meeting: Autojoin public chat with chatId - \(MEGASdk.base64Handle(forUserHandle: chatId) ?? "-1")")
             chatSdk.autojoinPublicChat(chatId, delegate: delegate)
         }
     }
-
-    func checkChatLink(link: String, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void) {
+    
+    public func checkChatLink(link: String, completion: @escaping (Result<ChatRoomEntity, CallErrorEntity>) -> Void) {
         guard let url = URL(string: link) else {
             completion(.failure(.generic))
             return
         }
         
-        MEGALogDebug("Create meeting: check chat link \(link)")
         chatSdk.checkChatLink(url, delegate: ChatRequestDelegate { [weak self] result in
             guard let self else {
                 completion(.failure(.generic))
@@ -98,26 +95,25 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
             }
             switch result {
             case .success(let request):
-                
                 guard let chatroom = chatSdk.chatRoom(forChatId: request.chatHandle) else {
-                    MEGALogDebug("Create meeting: ChatRoom not found with chat handle \(request.chatHandle)")
                     completion(.failure(.generic))
                     return
                 }
                 
-                MEGALogDebug("Create meeting: check chat link succeeded with chatroom \(chatroom)")
                 completion(.success(chatroom.toChatRoomEntity()))
-            case .failure(let error):
-                if error.type == .MegaChatErrorTypeExist {
-                    MEGALogDebug("Create meeting: failed to check chat link \(link)")
-                }
+            case .failure:
                 completion(.failure(.generic))
             }
         })
     }
     
-    func createEphemeralAccountAndJoinChat(firstName: String, lastName: String, link: String, completion: @escaping (Result<Void, GenericErrorEntity>) -> Void, karereInitCompletion: @escaping () -> Void) {
-        MEGALogDebug("Create meeting: Now logging out of anonymous account")
+    public func createEphemeralAccountAndJoinChat(
+        firstName: String,
+        lastName: String,
+        link: String,
+        completion: @escaping (Result<Void, GenericErrorEntity>) -> Void,
+        karereInitCompletion: @escaping () -> Void
+    ) {
         chatSdk.logout(with: ChatRequestDelegate { [weak self] result in
             guard let self else {
                 completion(.failure(GenericErrorEntity()))
@@ -127,27 +123,21 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
             case .success:
                 chatSdk.initKarere(withSid: nil)
                 karereInitCompletion()
-                MEGALogDebug("Create meeting: Now creating ephemeral account plus plus with firstname - \(firstName) and lastname - \(lastName)")
                 sdk.createEphemeralAccountPlusPlus(withFirstname: firstName, lastname: lastName, delegate: RequestDelegate { [weak self] result in
                     guard let self else {
                         completion(.failure(GenericErrorEntity()))
                         return
                     }
                     switch result {
-                    case .failure(let errorType):
-                        MEGALogDebug("Create meeting: failed creating ephemeral account plus plus with error \(errorType)")
+                    case .failure:
                         completion(.failure(GenericErrorEntity()))
                     case .success(let request):
-                        MEGALogDebug("Create meeting: success creating ephemeral account plus plus")
                         if request.paramType == AccountActionType.resumeEphemeralPlusPlus.rawValue {
-                            MEGALogDebug("Create meeting: Now fetching node for ephemeral account")
                             sdk.fetchNodes(with: RequestDelegate { [weak self] result in
                                 switch result {
                                 case .success:
-                                    MEGALogDebug("Create meeting: success fetching node for ephemeral account and now connecting to chat")
                                     self?.connectToChat(link: link, completion: completion)
-                                case .failure(let error):
-                                    MEGALogDebug("Create meeting: failure fetching node for ephemeral account \(error)")
+                                case .failure:
                                     completion(.failure(GenericErrorEntity()))
                                 }
                             })
@@ -156,8 +146,7 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
                         }
                     }
                 })
-            case .failure(let error):
-                MEGALogDebug("Create meeting: failed to logout of anonymous account \(error)")
+            case .failure:
                 completion(.failure(GenericErrorEntity()))
             }
         })
@@ -165,12 +154,10 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
     
     private func connectToChat(link: String, completion: @escaping (Result<Void, GenericErrorEntity>) -> Void) {
         guard let url = URL(string: link) else {
-            MEGALogDebug("Create meeting: invalid url \(link)")
             completion(.failure(GenericErrorEntity()))
             return
         }
         
-        MEGALogDebug("Create meeting: open chat preview for url \(url)")
         chatSdk.openChatPreview(url, delegate: ChatRequestDelegate { [weak self]  result in
             guard let self else {
                 completion(.failure(GenericErrorEntity()))
@@ -178,7 +165,6 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
             }
             switch result {
             case .success(let chatRequest):
-                MEGALogDebug("Create meeting: open chat preview succeeded with request \(chatRequest)")
                 chatResultDelegate = MEGAChatResultDelegate { [weak self] _, chatId, newState in
                     guard let self else {
                         completion(.failure(GenericErrorEntity()))
@@ -192,10 +178,26 @@ final class MeetingCreatingRepository: NSObject, MEGAChatDelegate, MeetingCreati
                 if let chatResultDelegate {
                     chatSdk.add(chatResultDelegate)
                 }
-            case .failure(let error):
-                MEGALogDebug("Create meeting: open chat preview failure \(error)")
+            case .failure:
                 completion(.failure(GenericErrorEntity()))
             }
         })
+    }
+}
+
+typealias MEGAChatResultDelegateCompletion = (_ api: MEGAChatSdk, _ chatId: HandleEntity, _ newState: MEGAChatConnection) -> Void
+
+class MEGAChatResultDelegate: NSObject, MEGAChatDelegate {
+    let completion: MEGAChatResultDelegateCompletion
+    
+    init(completion: @escaping MEGAChatResultDelegateCompletion) {
+        self.completion = completion
+    }
+    
+    func onChatConnectionStateUpdate(_ api: MEGAChatSdk, chatId: UInt64, newState: Int32) {
+        guard let intNewState = MEGAChatConnection(rawValue: Int(newState)) else {
+            return
+        }
+        completion(api, chatId, intNewState)
     }
 }
