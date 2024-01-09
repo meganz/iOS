6d0a01cb0f0379849bb869a27218d5b16866905a
Tomek Kuźma <tku@mega.co.nz>
2024-01-09 20:02:12 +1300

    IOS-7659: Fix crash and missing button

--- iMEGA/Cloud drive/NodeInfo/NodeVersionsViewController+Additions.swift
+++ iMEGA/Cloud drive/NodeInfo/NodeVersionsViewController+Additions.swift
@@ -27,4 +27,76 @@ extension NodeVersionsViewController {
         }
         return Strings.Localizable.General.Format.itemsSelected(selectedCount)
     }
+    
+    @objc func open(_ node: MEGANode) {
+        if node.name?.fileExtensionGroup.isVisualMedia == true {
+            let sdk = MEGASdk.shared
+            guard let mediaNodes = sdk.versions(for: self.node).mnz_mediaNodesMutableArrayFromNodeList() else { return }
+            let displayMode: DisplayMode = self.node.mnz_isInRubbishBin() ? .rubbishBin : .nodeVersions
+            let photoBrowserVC = MEGAPhotoBrowserViewController.photoBrowser(
+                withMediaNodes: mediaNodes,
+                api: sdk,
+                displayMode: displayMode,
+                presenting: node
+            )
+            self.navigationController?.present(photoBrowserVC, animated: true, completion: nil)
+        } else {
+            node.mnz_open(in: navigationController, folderLink: false, fileLink: nil, messageId: nil, chatId: nil, allNodes: nil)
+        }
+    }
+    
+    @objc func previousVersionsSection() -> NodeVersionSection {
+        return sections[1]
+    }
+    
+    @objc func allNodesSelected() -> Bool {
+        return previousVersionsSection().items.count == selectedNodesArray.count
+    }
+    
+    @objc func isNodeWithHandlePreviousVersion(_ base64Handle: String) -> Bool {
+        let section = previousVersionsSection()
+        return section.hasNode(base64Handle)
+    }
+}
+
+@objc class NodeVersionItem: NSObject {
+    @objc let node: MEGANode
+    
+    @objc init(node: MEGANode) {
+        self.node = node
+        super.init()
+    }
+}
+
+@objc class NodeVersionSection: NSObject {
+    
+    let items: [NodeVersionItem]
+    
+    @objc func itemAt(index: Int) -> NodeVersionItem {
+        items[index]
+    }
+    
+    @objc func itemCount() -> Int {
+        items.count
+    }
+    
+    @objc init(items: [NodeVersionItem]) {
+        self.items = items
+        super.init()
+    }
+    
+    @objc var nodes: [MEGANode] {
+        var array: [MEGANode] = []
+        items.forEach { item in
+            array.append(item.node)
+        }
+        return array
+    }
+    
+    @objc func hasNode(_ base64Handle: String) -> Bool {
+        for node in nodes where node.base64Handle == base64Handle {
+            return true
+        }
+        return false
+    }
 }
