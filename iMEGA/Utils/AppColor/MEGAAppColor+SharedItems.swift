4daa64c3f94a230d01db16615942f968b5d928a4
Harry Yan <hy@mega.co.nz>
2024-01-12 17:10:36 +1300

    IOS-8029: Move sharedView, PSA colorS to MEGAAppColor

--- iMEGA/Utils/AppColor/MEGAAppColor+SharedItems.swift
+++ iMEGA/Utils/AppColor/MEGAAppColor+SharedItems.swift
@@ -54,4 +54,85 @@ extension MEGAAppColor {
             }
         }
     }
+    
+    enum SharedView {
+        case explorerAudioFirstGradient
+        case explorerAudioSecondGradient
+        case explorerDocumentsFirstGradient
+        case explorerDocumentsSecondGradient
+        case explorerForegroundDark
+        case explorerGradientDarkBlue
+        case explorerGradientLightBlue
+        case gradientPink
+        case gradientRed
+        case pasteImageBorder
+        case verifyEmailFirstGradient
+        case verifyEmailSecondGradient
+        
+        var uiColor: UIColor {
+            DIContainer.featureFlagProvider.isFeatureFlagEnabled(for: .videoRevamp) ? designTokenColor : legacyColor
+        }
+        
+        var color: Color {
+            DIContainer.featureFlagProvider.isFeatureFlagEnabled(for: .videoRevamp) ? designTokenColor.swiftUI : legacyColor.swiftUI
+        }
+        
+        private var designTokenColor: UIColor {
+            switch self {
+            case .explorerAudioFirstGradient:
+                TokenColors.Button.brand
+            case .explorerAudioSecondGradient:
+                TokenColors.Button.brand
+            case .explorerDocumentsFirstGradient:
+                TokenColors.Button.brand
+            case .explorerDocumentsSecondGradient:
+                TokenColors.Button.brand
+            case .explorerForegroundDark:
+                TokenColors.Text.primary
+            case .explorerGradientDarkBlue:
+                TokenColors.Button.brand
+            case .explorerGradientLightBlue:
+                TokenColors.Button.brand
+            case .gradientPink:
+                TokenColors.Button.brand
+            case .gradientRed:
+                TokenColors.Button.brand
+            case .pasteImageBorder:
+                TokenColors.Button.brand
+            case .verifyEmailFirstGradient:
+                TokenColors.Button.brand
+            case .verifyEmailSecondGradient:
+                TokenColors.Button.brand
+            }
+        }
+        
+        private var legacyColor: UIColor {
+            switch self {
+            case .explorerAudioFirstGradient:
+                UIColor.explorerAudioFirstGradient
+            case .explorerAudioSecondGradient:
+                UIColor.explorerAudioSecondGradient
+            case .explorerDocumentsFirstGradient:
+                UIColor.explorerDocumentsFirstGradient
+            case .explorerDocumentsSecondGradient:
+                UIColor.explorerDocumentsSecondGradient
+            case .explorerForegroundDark:
+                UIColor.explorerForegroundDark
+            case .explorerGradientDarkBlue:
+                UIColor.explorerGradientDarkBlue
+            case .explorerGradientLightBlue:
+                UIColor.explorerGradientLightBlue
+            case .gradientPink:
+                UIColor.gradientPink
+            case .gradientRed:
+                UIColor.gradientRed
+            case .pasteImageBorder:
+                UIColor.pasteImageBorder
+            case .verifyEmailFirstGradient:
+                UIColor.verifyEmailFirstGradient
+            case .verifyEmailSecondGradient:
+                UIColor.verifyEmailSecondGradient
+            }
+        }
+    }
 }
