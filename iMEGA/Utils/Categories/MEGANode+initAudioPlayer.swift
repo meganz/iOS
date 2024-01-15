8ad988e578e74505baa3723e17c513f4fee204ad
Tomasz Kuźma <tku@mega.co.nz>
2024-01-15 23:25:15 +0100

    CC-6170:  fix audio player not working in cloud drive, also fixes CC-5598

--- iMEGA/Utils/Categories/MEGANode+initAudioPlayer.swift
+++ iMEGA/Utils/Categories/MEGANode+initAudioPlayer.swift
@@ -1,5 +1,15 @@
 extension MEGANode {
     @objc func initFullScreenPlayer(node: MEGANode?, fileLink: String?, filePaths: [String]?, isFolderLink: Bool, presenter: UIViewController, messageId: NSNumber?, chatId: NSNumber?, allNodes: [MEGANode]?) {
+        
+        // fixes [CC-5598] as we were passing in all nodes instead of just audio ones
+        // this is the same check we do on a single node, when deciding if we can pass it to
+        // audio player
+        let allAudioNodes: [MEGANode]? = allNodes?.filter { node in
+            node.name?.fileExtensionGroup.isMultiMedia == true &&
+            node.name?.fileExtensionGroup.isVideo == false &&
+            node.mnz_isPlayable()
+        }
+        
         AudioPlayerManager.shared.initFullScreenPlayer(
             node: node,
             fileLink: fileLink,
@@ -8,7 +18,7 @@ extension MEGANode {
             presenter: presenter,
             messageId: messageId?.uint64Value ?? .invalid,
             chatId: chatId?.uint64Value ?? .invalid,
-            allNodes: allNodes
+            allNodes: allAudioNodes
         )
     }
 }
