1472b479e314a52f86192dfe4c4f6c14fad6928f
Arifin Firdaus <af@mega.co.nz>
2024-01-30 02:16:59 +1300

    IOS-8082: Adopt Semantic Color for TabView

--- iMEGA/Videos/Infrastructure/VideoConfig+Live.swift
+++ iMEGA/Videos/Infrastructure/VideoConfig+Live.swift
@@ -1,9 +1,10 @@
+import MEGADesignToken
 import SwiftUI
 import Video
 
 extension VideoConfig {
     
-    static let live = VideoConfig(
+    static func live(isDesignTokenEnabled: Bool) -> VideoConfig { VideoConfig(
         videoListAssets: VideoConfig.VideoListAssets(
             noResultVideoImage: UIImage.noResultsVideo
         ),
@@ -31,14 +32,13 @@ extension VideoConfig {
             secondaryDarkTextColor: MEGAAppColor.Gray._D1D1D1.color,
             whiteColor: MEGAAppColor.White._FFFFFF.color,
             durationTextBackgroundColor: MEGAAppColor.Black._161616.color.opacity(0.5),
-            tabActiveIndicatorColor: MEGAAppColor.Red._F30C14.color,
-            tabInactiveIndicatorColor: Color.clear,
-            tabInactiveTextColor: MEGAAppColor.Gray._515151.color,
+            tabActiveIndicatorColor: isDesignTokenEnabled ? TokenColors.Button.brand.swiftUI : MEGAAppColor.Red._F30C14.color,
+            tabInactiveIndicatorColor: isDesignTokenEnabled ? TokenColors.Background.surface1.swiftUI : Color.clear,
+            tabInactiveTextColor: isDesignTokenEnabled ? TokenColors.Icon.secondary.swiftUI : MEGAAppColor.Gray._515151.color,
             addPlaylistButtonBackgroundColor: Color.videoPlaylistAddButtonBackground,
             chromeTabOrToolBarLight: Color.navigationBg,
             chromeTabOrToolBarDark: MEGAAppColor.Black._161616.color,
-            navigationBarLightColor: Color.navigationBg,
-            navigationBarDarkColor: MEGAAppColor.Black._161616.color,
+            navigationBgColor: isDesignTokenEnabled ? TokenColors.Background.surface1.swiftUI : Color.navigationBg,
             emptyFavoriteThumbnailLightBackgroundColor: MEGAAppColor.Gray._E2E2E2.color,
             emptyFavoriteThumbnailDarkBackgroundColor: MEGAAppColor.Black._2C2C2E.color,
             emptyFavoriteThumbnaillImageLightForegroundColor: MEGAAppColor.Gray._BBBBBB.color,
@@ -55,5 +55,5 @@ extension VideoConfig {
             hudMinusImage: UIImage.hudMinus,
             moreListImage: UIImage.moreList
         )
-    )
+    )}
 }
