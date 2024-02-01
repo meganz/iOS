40a23c26eb706fbaea11819e452ec0fa68e2917c
Binh Le <bl@mega.co.nz>
2024-02-01 13:10:55 +0700

    IOS-8101: Replace MEGAChatGenericRequestDelegate with ChatRequestDelegate in MEGASdkManager+Cleanup

--- iMEGA/Utils/MEGASDK/MEGASdkManager+CleanUp.m
+++ iMEGA/Utils/MEGASDK/MEGASdkManager+CleanUp.m
@@ -1,13 +1,14 @@
 #import "MEGASdkManager+CleanUp.h"
 #import "MEGAGenericRequestDelegate.h"
-#import "MEGAChatGenericRequestDelegate.h"
+
+@import ChatRepo;
 
 @implementation MEGASdkManager (CleanUp)
 
 + (void)localLogout {
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     [MEGASdkManager.sharedMEGASdk localLogoutWithDelegate:[MEGAGenericRequestDelegate.alloc initWithCompletion:^(MEGARequest * _Nonnull request, MEGAError * _Nonnull error) {
-        [MEGASdkManager.sharedMEGAChatSdk localLogoutWithDelegate:[MEGAChatGenericRequestDelegate.alloc initWithCompletion:^(MEGAChatRequest * _Nonnull request, MEGAChatError * _Nonnull error) {
+        [MEGASdkManager.sharedMEGAChatSdk localLogoutWithDelegate:[ChatRequestDelegate.alloc initWithCompletion:^(MEGAChatRequest * _Nonnull request, MEGAChatError * _Nonnull error) {
             dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), ^{
                 dispatch_semaphore_signal(semaphore);
             });
