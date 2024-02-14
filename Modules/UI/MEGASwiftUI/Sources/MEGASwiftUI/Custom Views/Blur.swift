625f92f2a69c06bb5b5d1dcd7693f6b74e6c34cd
Harry Yan <hy@mega.co.nz>
2024-02-15 12:08:28 +1300

    IOS-8072: Adopt Semantic Color for Timeline list

---
+++ Modules/UI/MEGASwiftUI/Sources/MEGASwiftUI/Custom Views/Blur.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Blur: UIViewRepresentable {
+    var style: UIBlurEffect.Style = .systemMaterial
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
--- iMEGA/Media Consumption/Photo Library/Card View/PhotoCardTitle.swift
+++ iMEGA/Media Consumption/Photo Library/Card View/PhotoCardTitle.swift
@@ -3,11 +3,11 @@ import SwiftUI
 struct PhotoCardTitle: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .foregroundColor(MEGAAppColor.White._FFFFFF.color)
+            .foregroundColor(UIColor.whiteFFFFFF.swiftUI)
             .padding(20)
             .frame(maxWidth: .infinity, alignment: .leading)
             .background(
-                LinearGradient(colors: [MEGAAppColor.Black._000000.color.opacity(0.3), MEGAAppColor.Black._000000.color.opacity(0)], startPoint: .top, endPoint: .bottom)
+                LinearGradient(colors: [.black000000.opacity(0.3), .black000000.opacity(0)], startPoint: .top, endPoint: .bottom)
             )
     }
 }
