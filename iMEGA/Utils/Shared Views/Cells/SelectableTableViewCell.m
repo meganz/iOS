f315e7f7045b10a2868619261700dbf02b38b336
Javier Navarro <jn@mega.co.nz>
2024-02-16 06:03:34 +1300

    T14796256: fix checkmark tint color in chat settings

--- iMEGA/Utils/Shared Views/Cells/SelectableTableViewCell.m
+++ iMEGA/Utils/Shared Views/Cells/SelectableTableViewCell.m
@@ -1,5 +1,11 @@
 #import "SelectableTableViewCell.h"
+#import "MEGA-Swift.h"
 
 @implementation SelectableTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self imageViewDesignToken];
+}
 
 @end
