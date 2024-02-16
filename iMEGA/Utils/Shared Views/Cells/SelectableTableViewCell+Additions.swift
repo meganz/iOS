24c3c4fc30e3146454eebf33d4e2bef027c66387
Javier Navarro <jn@mega.co.nz>
2024-02-16 11:05:31 +0100

    T14796256: fix crash using optional chaining

--- iMEGA/Utils/Shared Views/Cells/SelectableTableViewCell+Additions.swift
+++ iMEGA/Utils/Shared Views/Cells/SelectableTableViewCell+Additions.swift
@@ -3,8 +3,8 @@ import MEGADesignToken
 extension SelectableTableViewCell {
     @objc func imageViewDesignToken() {
         if UIColor.isDesignTokenEnabled() {
-            redCheckmarkImageView.image = UIImage.turquoiseCheckmark
-            redCheckmarkImageView.tintColor = TokenColors.Support.success
+            redCheckmarkImageView?.image = UIImage.turquoiseCheckmark
+            redCheckmarkImageView?.tintColor = TokenColors.Support.success
         }
     }
 }
