e64e2dee31441906162a913303ba4648b22ab2a4
Harry Yan <hy@mega.co.nz>
2024-02-19 14:25:32 +1300

    IOS-8195: Adopt Semantic color for Timeline empty status

--- iMEGA/Camera uploads/EnableCameraUploadsEmptyView.swift
+++ iMEGA/Camera uploads/EnableCameraUploadsEmptyView.swift
@@ -1,3 +1,4 @@
+import MEGADesignToken
 import MEGAL10n
 import SwiftUI
 
@@ -14,22 +15,24 @@ struct EnableCameraUploadsEmptyView: View {
                 
                 Text(Strings.Localizable.enableCameraUploadsButton)
                     .font(.headline)
+                    .foregroundStyle(isDesignTokenEnabled ? TokenColors.Text.primary.swiftUI : .primary)
                 
                 Text(Strings.Localizable.automaticallyBackupYourPhotosAndVideosToTheCloudDrive)
                     .font(.subheadline)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 40)
+                    .foregroundStyle(isDesignTokenEnabled ? TokenColors.Text.secondary.swiftUI : .primary)
             }
             .frame(maxHeight: .infinity)
             
             Button(action: action) {
                 Text(Strings.Localizable.enable)
                     .font(.body.weight(.semibold))
-                    .foregroundColor( MEGAAppColor.White._FFFFFF.color)
+                    .foregroundColor(isDesignTokenEnabled ? TokenColors.Text.colorInverse.swiftUI : .whiteFFFFFF)
                     .frame(width: 288, height: 50)
-                    .background(MEGAAppColor.Green._00A886.color)
+                    .background(isDesignTokenEnabled ? TokenColors.Button.primary.swiftUI : .green00A886)
                     .cornerRadius(8)
-                    .shadow(color: MEGAAppColor.Black._000000.color.opacity(0.15), radius: 2, x: 0, y: 1)
+                    .shadow(color: .black000000.opacity(0.15), radius: 2, x: 0, y: 1)
                     .contentShape(Rectangle())
             }
         }
