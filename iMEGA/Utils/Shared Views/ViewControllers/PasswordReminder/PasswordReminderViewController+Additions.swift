c508a3e050730804ef9b78a037d8debd3fef88f5
Rahul Umap <rdu@mega.co.nz>
2024-02-20 19:00:47 +1300

    IOS-8194: Password reminder screen semantic color adoption

--- iMEGA/Utils/Shared Views/ViewControllers/PasswordReminder/PasswordReminderViewController+Additions.swift
+++ iMEGA/Utils/Shared Views/ViewControllers/PasswordReminder/PasswordReminderViewController+Additions.swift
@@ -1,3 +1,5 @@
+import MEGADesignToken
+
 extension PasswordReminderViewController {
     
     @objc func requestStopAudioPlayerSession() {
@@ -6,4 +8,39 @@ extension PasswordReminderViewController {
             streamingInfoUseCase.stopServer()
         }
     }
+    
+    @objc func updateAppearance() {
+        if UIColor.isDesignTokenEnabled() {
+            alphaView.backgroundColor = TokenColors.Background.page
+            backgroundView.backgroundColor = TokenColors.Background.page
+            
+            titleLabel.textColor = TokenColors.Text.primary
+            descriptionLabel.textColor = TokenColors.Text.primary
+            switchInfoLabel.textColor = TokenColors.Text.primary
+            
+            doNotShowMeAgainView.backgroundColor = TokenColors.Background.page
+            doNotShowMeAgainTopSeparatorView.backgroundColor = TokenColors.Border.strong
+            doNotShowMeAgainBottomSeparatorView.backgroundColor = TokenColors.Border.strong
+            
+            testPasswordButton.setTitleColor(TokenColors.Text.accent, for: .normal)
+            testPasswordButton.backgroundColor = TokenColors.Button.secondary
+            
+            dismissButton.backgroundColor = .clear
+            dismissButton.setTitleColor(TokenColors.Text.primary, for: .normal)
+        } else {
+            backgroundView.backgroundColor = UIColor.systemBackground
+            
+            descriptionLabel.textColor = UIColor.mnz_subtitles(for: traitCollection)
+            
+            doNotShowMeAgainView.backgroundColor = UIColor.mnz_backgroundElevated(traitCollection)
+            doNotShowMeAgainTopSeparatorView.backgroundColor = UIColor.mnz_separator(for: traitCollection)
+            doNotShowMeAgainBottomSeparatorView.backgroundColor = UIColor.mnz_separator(for: traitCollection)
+            
+            testPasswordButton.mnz_setupBasic(traitCollection)
+            dismissButton.mnz_setupCancel(traitCollection)
+        }
+        
+        backupKeyButton.mnz_setupPrimary(traitCollection)
+    }
+
 }
--- iMEGA/Utils/Shared Views/ViewControllers/PasswordReminder/PasswordReminderViewController.h
+++ iMEGA/Utils/Shared Views/ViewControllers/PasswordReminder/PasswordReminderViewController.h
@@ -2,6 +2,23 @@
 
 @interface PasswordReminderViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *switchInfoLabel;
+@property (weak, nonatomic) IBOutlet UIButton *testPasswordButton;
+@property (weak, nonatomic) IBOutlet UIButton *backupKeyButton;
+@property (weak, nonatomic) IBOutlet UIButton *dismissButton;
+
+@property (weak, nonatomic) IBOutlet UIView *doNotShowMeAgainView;
+@property (weak, nonatomic) IBOutlet UIView *doNotShowMeAgainTopSeparatorView;
+@property (weak, nonatomic) IBOutlet UISwitch *dontShowAgainSwitch;
+@property (weak, nonatomic) IBOutlet UIView *doNotShowMeAgainBottomSeparatorView;
+
+@property (weak, nonatomic) IBOutlet UIView *alphaView;
+@property (weak, nonatomic) IBOutlet UIImageView *keyImageView;
+
+@property (weak, nonatomic) IBOutlet UIView *backgroundView;
+
 @property (assign, getter=isLoggingOut) BOOL logout;
 
 @end
