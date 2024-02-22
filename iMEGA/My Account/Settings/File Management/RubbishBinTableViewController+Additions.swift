e78b9e70f21bce84055e18dc8b39b1a4116e7589
João Pedro de Amorim <jpa@mega.co.nz>
2024-02-21 22:40:14 -0300

    T14796249: Add support for FileVersioning/RubbishBin controllers

--- iMEGA/My Account/Settings/File Management/RubbishBinTableViewController+Additions.swift
+++ iMEGA/My Account/Settings/File Management/RubbishBinTableViewController+Additions.swift
@@ -1,5 +1,28 @@
+import MEGADesignToken
+import UIKit
+
 extension RubbishBinTableViewController {
     @objc func showUpgradeToPro() {
         UpgradeAccountRouter().presentUpgradeTVC()
+    }
+
+    @objc func updateAppearance() {
+        tableView.separatorColor = UIColor.mnz_separator(for: traitCollection)
+        tableView.backgroundColor = UIColor.mnz_backgroundGrouped(for: traitCollection)
+
+        if UIColor.isDesignTokenEnabled() {
+            rubbishBinCleaningSchedulerLabel.textColor = TokenColors.Text.primary
+            removeFilesOlderThanLabel.textColor = TokenColors.Text.primary
+            clearRubbishBinDetailLabel.textColor = TokenColors.Text.secondary
+            removeFilesOlderThanDetailLabel.textColor = TokenColors.Text.secondary
+            clearRubbishBinLabel.textColor = TokenColors.Text.error
+        } else {
+            clearRubbishBinLabel.textColor = UIColor.mnz_red(for: traitCollection)
+            clearRubbishBinDetailLabel.textColor = UIColor.secondaryLabel
+            removeFilesOlderThanDetailLabel.textColor = UIColor.secondaryLabel
+        }
+
+        setupTableViewHeaderAndFooter()
+        tableView.reloadData()
     }
 }
