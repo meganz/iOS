46f89b9526c9f4fb51dac8c3e4f57be0d9d40c1b
Binh Le <bl@mega.co.nz>
2024-02-26 23:09:21 +1300

    IOS-6357: Replace MEGAGenericRequestDelegate with RequestDelegate in FolderLinkViewController

--- Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/SDK/RequestDelegate/RequestDelegate.swift
+++ Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/SDK/RequestDelegate/RequestDelegate.swift
@@ -2,14 +2,18 @@ import Foundation
 import MEGASdk
 
 public typealias MEGARequestCompletion = (_ result: Result<MEGARequest, MEGAError>) -> Void
+public typealias MEGARequestStartHandler = (MEGARequest) -> Void
 
 public class RequestDelegate: NSObject, MEGARequestDelegate {
     let completion: MEGARequestCompletion
+    let onRequestStartHandler: MEGARequestStartHandler?
+    
     private let successCodes: [MEGAErrorType]
     
-    public init(successCodes: [MEGAErrorType] = [.apiOk], completion: @escaping MEGARequestCompletion) {
+    public init(successCodes: [MEGAErrorType] = [.apiOk], onRequestStartHandler: MEGARequestStartHandler? = nil, completion: @escaping MEGARequestCompletion) {
         self.successCodes = successCodes
         self.completion = completion
+        self.onRequestStartHandler = onRequestStartHandler
         super.init()
     }
     
@@ -25,11 +29,32 @@ public class RequestDelegate: NSObject, MEGARequestDelegate {
         self.init(completion: completion)
     }
     
+    @objc public convenience init(onRequestStartHandler: @escaping (MEGARequest) -> Void, completion: @escaping (MEGARequest?, MEGAError?) -> Void) {
+        let completion: MEGARequestCompletion = { result in
+            switch result {
+            case let .success(request):
+                completion(request, nil)
+            case let .failure(error):
+                completion(nil, error)
+            }
+        }
+        let onRequestStartHandler: MEGARequestStartHandler = { request in
+            onRequestStartHandler(request)
+        }
+        self.init(onRequestStartHandler: onRequestStartHandler, completion: completion)
+    }
+    
     public func onRequestFinish(_ api: MEGASdk, request: MEGARequest, error: MEGAError) {
         if successCodes.contains(error.type) {
             self.completion(.success(request))
         } else {
             self.completion(.failure(error))
+        }
+    }
+    
+    public func onRequestStart(_ api: MEGASdk, request: MEGARequest) {
+        if let onRequestStartHandler {
+            onRequestStartHandler(request)
         }
     }
 }
