51742795cff90847f7ce6d313751de64da628f47
João Pedro de Amorim <jpa@mega.co.nz>
2024-03-06 09:21:20 -0300

    Use correct path for new transifex script in PrepareRelease

--- scripts/ReleaseScripts/PrepareRelease/Sources/transifex.swift
+++ scripts/ReleaseScripts/PrepareRelease/Sources/transifex.swift
@@ -1,6 +1,6 @@
 import SharedReleaseScript
 
-private let pathToTransifexScript = "./transifex/iosTransifex.py"
+private let pathToTransifexScript = "./iosTransifex/iosTransifex.py"
 
 func pruneTransifexStrings() throws {
     try runInShell("\(pathToTransifexScript) -m clean")
