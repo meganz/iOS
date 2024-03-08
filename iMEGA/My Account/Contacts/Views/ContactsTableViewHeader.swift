9efecef1f9f847bd35eac502ff9af3cf14535b8d
Roberto Gómez <rg@mega.nz>
2024-03-09 08:05:20 +1300

    IOS-8124: Adopt Semantic Color for ContactTableViewHeader view

--- iMEGA/My Account/Contacts/Views/ContactsTableViewHeader.swift
+++ iMEGA/My Account/Contacts/Views/ContactsTableViewHeader.swift
@@ -1,10 +1,11 @@
 import Foundation
+import MEGADesignToken
 import MEGAL10n
 
 class ContactsTableViewHeader: UIView {
-    
     @objc var navigationController: UINavigationController!
-    
+    @IBOutlet weak var disclosureIndicatorRequestImageView: UIImageView!
+    @IBOutlet weak var disclosureIndicatorGroupsImageView: UIImageView!
     @IBOutlet weak var requestsImageView: UIImageView!
     @IBOutlet weak var requestsLabel: UILabel!
     @IBOutlet weak var requestsDetailLabel: UILabel!
@@ -18,16 +19,18 @@ class ContactsTableViewHeader: UIView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        requestsImageView.image = UIImage.contactRequests.imageFlippedForRightToLeftLayoutDirection()
         requestsLabel.text = Strings.Localizable.requests
-        
-        configDetailsLabel()
-        
-        groupsImageView.image = UIImage.contactGroups.imageFlippedForRightToLeftLayoutDirection()
         groupsLabel.text = Strings.Localizable.groups
         
+        configDetailsLabel()
         updateAppearance()
+        
+        if UIColor.isDesignTokenEnabled() {
+            disclosureIndicatorRequestImageView.image?.withRenderingMode(.alwaysTemplate)
+            disclosureIndicatorRequestImageView.tintColor = TokenColors.Icon.secondary
+            disclosureIndicatorGroupsImageView.image?.withRenderingMode(.alwaysTemplate)
+            disclosureIndicatorGroupsImageView.tintColor = TokenColors.Icon.secondary
+        }
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
@@ -41,10 +44,18 @@ class ContactsTableViewHeader: UIView {
     // MARK: - Private
     
     private func updateAppearance() {
-        backgroundColor = .mnz_backgroundElevated(traitCollection)
+        backgroundColor = UIColor.isDesignTokenEnabled() ? TokenColors.Background.page : UIColor.mnz_backgroundElevated(traitCollection)
         
-        requestsDetailLabel.textColor = UIColor.secondaryLabel
+        requestsLabel.textColor = UIColor.isDesignTokenEnabled() ? TokenColors.Text.primary : UIColor.label
+        groupsLabel.textColor = UIColor.isDesignTokenEnabled() ? TokenColors.Text.secondary : UIColor.label
+        requestsDetailLabel.textColor = UIColor.isDesignTokenEnabled() ? TokenColors.Text.secondary : UIColor.secondaryLabel
+        
         requestsSeparatorView.backgroundColor = UIColor.mnz_separator(for: traitCollection)
+        
+        // contactRequestsIcon should be deleted on assets when design token is permanently applied. It is only used here.
+        requestsImageView.image = (UIColor.isDesignTokenEnabled() ? UIImage.contactRequests : UIImage.contactRequestsIcon).imageFlippedForRightToLeftLayoutDirection()
+        // contactGroupsIcon should be deleted on assets when design token is permanently applied. It is only used here.
+        groupsImageView.image = (UIColor.isDesignTokenEnabled() ? UIImage.contactGroups : UIImage.contactGroupsIcon).imageFlippedForRightToLeftLayoutDirection()
     }
     
     private func configDetailsLabel() {
