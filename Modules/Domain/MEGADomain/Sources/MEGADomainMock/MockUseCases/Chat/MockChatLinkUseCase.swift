d68e31f856ca2f53b27635a297158a9f8d73b290
Javier Navarro <jn@mega.co.nz>
2024-03-12 10:38:06 +0100

    IOS-8294: Move MockChatLinkUseCase to MEGADomain module

---
+++ Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockUseCases/Chat/MockChatLinkUseCase.swift
@@ -0,0 +1,58 @@
+import Combine
+import MEGADomain
+
+public struct MockChatLinkUseCase: ChatLinkUseCaseProtocol {
+    private let link: String?
+    private let chatLinkUpdateSubject = PassthroughSubject<String?, Never>()
+    private let error: ChatRoomErrorEntity?
+    
+    public init(
+        link: String? = nil,
+        error: ChatRoomErrorEntity? = nil
+    ) {
+        self.link = link
+        self.error = error
+    }
+    
+    public func queryChatLink(for chatRoom: ChatRoomEntity) async throws -> String {
+        if let error {
+            throw error
+        } else if let link {
+            return link
+        } else {
+            throw ChatRoomErrorEntity.meetingLinkQueryError
+        }
+    }
+    
+    public func createChatLink(for chatRoom: ChatRoomEntity) async throws -> String {
+        if let error {
+            throw error
+        } else if let link {
+            return link
+        } else {
+            throw ChatRoomErrorEntity.meetingLinkCreateError
+        }
+    }
+    
+    public func removeChatLink(for chatRoom: ChatRoomEntity) async throws {
+        if let error {
+            throw error
+        }
+    }
+    
+    public func queryChatLink(for chatRoom: ChatRoomEntity) {
+        chatLinkUpdateSubject.send(link)
+    }
+    
+    public func createChatLink(for chatRoom: ChatRoomEntity) {
+        chatLinkUpdateSubject.send("New chat or meeting link")
+    }
+    
+    public func removeChatLink(for chatRoom: ChatRoomEntity) {
+        chatLinkUpdateSubject.send(nil)
+    }
+    
+    public mutating func monitorChatLinkUpdate(for chatRoom: ChatRoomEntity) -> AnyPublisher<String?, Never> {
+        chatLinkUpdateSubject.eraseToAnyPublisher()
+    }
+}
