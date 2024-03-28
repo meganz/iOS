e06211686ba1e34137f2a53e53a34bb525862589
Mike Cap <mvc@mega.co.nz>
2024-03-28 14:28:30 +0700

    SAO-1135: Re-organize NewCD folder structure

--- iMEGA/Cloud drive/NewCloudDrive/Factories/CloudDriveViewControllerFactory.swift
+++ iMEGA/Cloud drive/NewCloudDrive/Factories/CloudDriveViewControllerFactory.swift
@@ -10,60 +10,6 @@ import Search
 import SwiftUI
 import UIKit
 
-extension DisplayMode {
-    var carriedOverDisplayMode: DisplayMode? {
-        // for those 3 special cases, we carry over the display mode so that children are configured properly
-        // [bug in the comments in FM-1461]
-        if self == .rubbishBin || self == .backup || self == .cloudDrive {
-            return self
-        }
-        return nil
-    }
-}
-
-struct NodeBrowserConfig {
-    var displayMode: DisplayMode?
-    var isFromViewInFolder: Bool?
-    var isFromUnverifiedContactSharedFolder: Bool?
-    var isFromSharedItem: Bool?
-    var showsAvatar: Bool?
-    var shouldRemovePlayerDelegate: Bool?
-    var warningViewModel: WarningViewModel?
-    // this should enabled for non-root nodes
-    var mediaDiscoveryAutomaticDetectionEnabled: () -> Bool = { false }
-    // Determines whether the NodeBrowserView should handle upgrade encouragement flow or not, default value is true
-    var supportsUpgradeEncouragement: Bool = true
-    
-    /// Provider closure to get the AdsVisibilityConfigurating for showing/hiding external ads.
-    /// The AdsVisibilityConfigurating can be access via `UIApplication.mainTabBarRootViewController())` which
-    /// might not be available at the time this config is created so we need to use closure to refer to it at a later time.
-    var adsConfiguratorProvider: () -> (any AdsVisibilityConfigurating)? = { nil }
-    static var `default`: Self {
-        .init()
-    }
-    
-    /// small helper function to make it easier to pass down and package display mode into a config
-    /// display mode must be carried over into a child folder when presenting in rubbish or backups mode
-    static func withOptionalDisplayMode(_ displayMode: DisplayMode?) -> Self {
-        var config = Self.default
-        config.displayMode = displayMode
-        return config
-    }
-    
-    static func withOptionalDisplayMode(_ displayMode: DisplayMode?, warningViewModel: WarningViewModel?) -> Self {
-        var config = Self.default
-        config.displayMode = displayMode
-        config.warningViewModel = warningViewModel
-        return config
-    }
-    
-    static func withSupportsUpgradeEncouragement(_ supportsUpgradeEncouragement: Bool) -> Self {
-        var config = Self.default
-        config.supportsUpgradeEncouragement = supportsUpgradeEncouragement
-        return config
-    }
-}
-
 struct CloudDriveViewControllerFactory {
     private let featureFlagProvider: any FeatureFlagProviderProtocol
     private let abTestProvider: any ABTestProviderProtocol
