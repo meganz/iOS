6e15940d27907fab40e523b6870f43b941a95d72
Glazel Magpayo <gma@mega.co.nz>
2024-04-09 00:19:44 +1200

    IOS-8301 Adopt semantic color for Invite your friends page

--- iMEGA/My Account/Achievements/InviteYourFriendsViewController+Additions.swift
+++ iMEGA/My Account/Achievements/InviteYourFriendsViewController+Additions.swift
@@ -1,3 +1,4 @@
+import MEGADesignToken
 import MEGAL10n
 
 extension InviteFriendsViewController {
@@ -6,5 +7,22 @@ extension InviteFriendsViewController {
         let title = Strings.Localizable.Account.Achievement.Referral.title
         navigationItem.title = title
         setMenuCapableBackButtonWith(menuTitle: title)
+    }
+    
+    // MARK: - Token colors
+    @objc var defaultBackgroundColor: UIColor {
+        TokenColors.Background.page
+    }
+    
+    @objc var primaryTextColor: UIColor {
+        TokenColors.Text.primary
+    }
+    
+    @objc var secondayTextColor: UIColor {
+        TokenColors.Text.secondary
+    }
+    
+    @objc var separatorColor: UIColor {
+        TokenColors.Border.strong
     }
 }
