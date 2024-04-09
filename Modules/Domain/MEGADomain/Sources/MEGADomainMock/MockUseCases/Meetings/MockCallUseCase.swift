402683b75fa093984884abf90f61280df001d62d
Javier Navarro <jn@mega.co.nz>
2024-04-09 16:21:09 +0200

    IOS-8442: Move MockCallUseCase to MEGADomain module

--- Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockUseCases/Meetings/MockCallUseCase.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockUseCases/Meetings/MockCallUseCase.swift
@@ -1,40 +1,39 @@
 import Combine
-@testable import MEGA
 import MEGADomain
 
-final class MockCallUseCase: CallUseCaseProtocol {
-    var startListeningForCall_CalledTimes = 0
+public final class MockCallUseCase: CallUseCaseProtocol {
+    public var startListeningForCall_CalledTimes = 0
     var stopListeningForCall_CalledTimes = 0
-    var createActiveSessions_calledTimes = 0
-    var hangCall_CalledTimes = 0
+    public var createActiveSessions_calledTimes = 0
+    public var hangCall_CalledTimes = 0
     var endCall_CalledTimes = 0
-    var addPeer_CalledTimes = 0
+    public var addPeer_CalledTimes = 0
     var removePeer_CalledTimes = 0
-    var allowedUsersJoinCall = [HandleEntity]()
-    var allowUsersJoinCall_CalledTimes = 0
-    var kickUsersFromCall_CalledTimes = 0
+    public var allowedUsersJoinCall = [HandleEntity]()
+    public var allowUsersJoinCall_CalledTimes = 0
+    public var kickUsersFromCall_CalledTimes = 0
     var pushUsersIntoWaitingRoom_CalledTimes = 0
     var makePeerAsModerator_CalledTimes = 0
     var removePeerAsModerator_CalledTimes = 0
-    var callAbsentParticipant_CalledTimes = 0
-    var muteParticipant_CalledTimes = 0
+    public var callAbsentParticipant_CalledTimes = 0
+    public var muteParticipant_CalledTimes = 0
 
-    var call: CallEntity?
-    var callCompletion: Result<CallEntity, CallErrorEntity>
+    public var call: CallEntity?
+    public var callCompletion: Result<CallEntity, CallErrorEntity>
     var answerCallCompletion: Result<CallEntity, CallErrorEntity>
     
-    var callbacksDelegate: (any CallCallbacksUseCaseProtocol)?
+    public var callbacksDelegate: (any CallCallbacksUseCaseProtocol)?
     var networkQuality: NetworkQuality = .bad
-    var chatRoom: ChatRoomEntity?
+    public var chatRoom: ChatRoomEntity?
     var video: Bool = false
     var audio: Bool = false
     var chatSession: ChatSessionEntity?
     var participantHandle: HandleEntity = .invalid
-    var callWaitingRoomUsersUpdateSubject = PassthroughSubject<CallEntity, Never>()
-    var callUpdateSubject: PassthroughSubject<CallEntity, Never>
+    public var callWaitingRoomUsersUpdateSubject = PassthroughSubject<CallEntity, Never>()
+    public var callUpdateSubject: PassthroughSubject<CallEntity, Never>
     var muteParticipantCompletion: Result<Void, GenericErrorEntity>
     
-    init(
+    public init(
         call: CallEntity? = CallEntity(),
         callCompletion: Result<CallEntity, CallErrorEntity> = .failure(.generic),
         answerCallCompletion: Result<CallEntity, CallErrorEntity> = .failure(.generic),
@@ -48,23 +47,23 @@ final class MockCallUseCase: CallUseCaseProtocol {
         self.muteParticipantCompletion = muteParticipantCompletion
     }
     
-    func startListeningForCallInChat<T: CallCallbacksUseCaseProtocol>(_ chatId: HandleEntity, callbacksDelegate: T) {
+    public func startListeningForCallInChat<T: CallCallbacksUseCaseProtocol>(_ chatId: HandleEntity, callbacksDelegate: T) {
         startListeningForCall_CalledTimes += 1
     }
     
-    func stopListeningForCall() {
+    public func stopListeningForCall() {
         stopListeningForCall_CalledTimes += 1
     }
     
-    func call(for chatId: HandleEntity) -> CallEntity? {
+    public func call(for chatId: HandleEntity) -> CallEntity? {
         call
     }
     
-    func answerCall(for chatId: HandleEntity, completion: @escaping (Result<CallEntity, CallErrorEntity>) -> Void) {
+    public func answerCall(for chatId: HandleEntity, completion: @escaping (Result<CallEntity, CallErrorEntity>) -> Void) {
         completion(answerCallCompletion)
     }
     
-    func answerCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool) async throws -> CallEntity {
+    public func answerCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool) async throws -> CallEntity {
         switch answerCallCompletion {
         case .success(let callEntity):
             return callEntity
@@ -73,11 +72,11 @@ final class MockCallUseCase: CallUseCaseProtocol {
         }
     }
     
-    func startCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool, notRinging: Bool, completion: @escaping (Result<CallEntity, CallErrorEntity>) -> Void) {
+    public func startCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool, notRinging: Bool, completion: @escaping (Result<CallEntity, CallErrorEntity>) -> Void) {
         completion(callCompletion)
     }
     
-    func startCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool, notRinging: Bool) async throws -> CallEntity {
+    public func startCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool, notRinging: Bool) async throws -> CallEntity {
         switch callCompletion {
         case .success(let callEntity):
             return callEntity
@@ -86,59 +85,59 @@ final class MockCallUseCase: CallUseCaseProtocol {
         }
     }
     
-    func createActiveSessions() {
+    public func createActiveSessions() {
         createActiveSessions_calledTimes += 1
     }
     
-    func hangCall(for callId: HandleEntity) {
+    public func hangCall(for callId: HandleEntity) {
         hangCall_CalledTimes += 1
     }
     
-    func endCall(for callId: HandleEntity) {
+    public func endCall(for callId: HandleEntity) {
         endCall_CalledTimes += 1
     }
     
-    func addPeer(toCall call: CallEntity, peerId: UInt64) {
+    public func addPeer(toCall call: CallEntity, peerId: UInt64) {
         addPeer_CalledTimes += 1
     }
     
-    func removePeer(fromCall call: CallEntity, peerId: UInt64) {
+    public func removePeer(fromCall call: CallEntity, peerId: UInt64) {
         removePeer_CalledTimes += 1
     }
     
-    func allowUsersJoinCall(_ call: CallEntity, users: [HandleEntity]) {
+    public func allowUsersJoinCall(_ call: CallEntity, users: [HandleEntity]) {
         allowedUsersJoinCall = users
         allowUsersJoinCall_CalledTimes += 1
     }
     
-    func kickUsersFromCall(_ call: CallEntity, users: [HandleEntity]) {
+    public func kickUsersFromCall(_ call: CallEntity, users: [HandleEntity]) {
         kickUsersFromCall_CalledTimes += 1
     }
     
-    func pushUsersIntoWaitingRoom(for scheduledMeeting: ScheduledMeetingEntity, users: [HandleEntity]) {
+    public func pushUsersIntoWaitingRoom(for scheduledMeeting: ScheduledMeetingEntity, users: [HandleEntity]) {
         pushUsersIntoWaitingRoom_CalledTimes += 1
     }
     
-    func makePeerAModerator(inCall call: CallEntity, peerId: UInt64) {
+    public func makePeerAModerator(inCall call: CallEntity, peerId: UInt64) {
         makePeerAsModerator_CalledTimes += 1
     }
     
-    func removePeerAsModerator(inCall call: CallEntity, peerId: UInt64) {
+    public func removePeerAsModerator(inCall call: CallEntity, peerId: UInt64) {
         removePeerAsModerator_CalledTimes += 1
     }
-    func callWaitingRoomUsersUpdate(forCall call: CallEntity) -> AnyPublisher<CallEntity, Never> {
+    public func callWaitingRoomUsersUpdate(forCall call: CallEntity) -> AnyPublisher<CallEntity, Never> {
         callWaitingRoomUsersUpdateSubject.eraseToAnyPublisher()
     }
     
-    func onCallUpdate() -> AnyPublisher<CallEntity, Never> {
+    public func onCallUpdate() -> AnyPublisher<CallEntity, Never> {
         callUpdateSubject.eraseToAnyPublisher()
     }
     
-    func callAbsentParticipant(inChat chatId: ChatIdEntity, userId: HandleEntity, timeout: Int) {
+    public func callAbsentParticipant(inChat chatId: ChatIdEntity, userId: HandleEntity, timeout: Int) {
         callAbsentParticipant_CalledTimes += 1
     }
     
-    func muteUser(inChat chatRoom: ChatRoomEntity, clientId: ChatIdEntity) async throws {
+    public func muteUser(inChat chatRoom: ChatRoomEntity, clientId: ChatIdEntity) async throws {
         switch muteParticipantCompletion {
         case .success:
             muteParticipant_CalledTimes += 1
@@ -147,108 +146,101 @@ final class MockCallUseCase: CallUseCaseProtocol {
         }
     }
     
-    func setCallLimit(inChat chatRoom: ChatRoomEntity, duration: Int?, maxUsers: Int?, maxClientPerUser: Int?, maxClients: Int?, divider: Int?) async throws { }
+    public func setCallLimit(inChat chatRoom: ChatRoomEntity, duration: Int?, maxUsers: Int?, maxClientPerUser: Int?, maxClients: Int?, divider: Int?) async throws { }
 }
 
 extension MockCallUseCase: CallCallbacksRepositoryProtocol {
     
-    func createdSession(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
+    public func createdSession(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
         guard let chatSession = chatSession, let chat = self.chatRoom else {
-            MEGALogDebug("Error getting mock properties")
             return
         }
         callbacksDelegate?.participantJoined(participant: CallParticipantEntity(session: chatSession, chatRoom: chat, privilege: privilege))
     }
     
-    func destroyedSession(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
+    public func destroyedSession(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
         guard let chatSession = chatSession, let chat = self.chatRoom else {
-            MEGALogDebug("Error getting mock properties")
             return
         }
         callbacksDelegate?.participantLeft(participant: CallParticipantEntity(session: chatSession, chatRoom: chat, privilege: privilege))
     }
     
-    func avFlagsUpdated(for session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
+    public func avFlagsUpdated(for session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
         guard let chatSession = chatSession, let chat = self.chatRoom else {
-            MEGALogDebug("Error getting mock properties")
             return
         }
         callbacksDelegate?.updateParticipant(CallParticipantEntity(session: chatSession, chatRoom: chat, privilege: privilege))
     }
     
-    func audioLevel(for session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
+    public func audioLevel(for session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
         guard let chatSession = chatSession, let chat = self.chatRoom else {
-            MEGALogDebug("Error getting mock properties")
             return
         }
         callbacksDelegate?.audioLevel(for: CallParticipantEntity(session: chatSession, chatRoom: chat, privilege: privilege))
     }
     
-    func callTerminated(_ call: CallEntity) {
+    public func callTerminated(_ call: CallEntity) {
         callbacksDelegate?.callTerminated(call)
     }
     
-    func ownPrivilegeChanged(to privilege: ChatRoomPrivilegeEntity, in chatRoom: ChatRoomEntity) {
+    public func ownPrivilegeChanged(to privilege: ChatRoomPrivilegeEntity, in chatRoom: ChatRoomEntity) {
         guard let chatRoom = self.chatRoom else {
-            MEGALogDebug("Error getting mock properties")
             return
         }
         callbacksDelegate?.ownPrivilegeChanged(to: chatRoom.ownPrivilege, in: chatRoom)
     }
     
-    func participantAdded(with handle: HandleEntity) {
+    public func participantAdded(with handle: HandleEntity) {
         callbacksDelegate?.participantAdded(with: participantHandle)
     }
     
-    func participantRemoved(with handle: HandleEntity) {
+    public func participantRemoved(with handle: HandleEntity) {
         callbacksDelegate?.participantRemoved(with: participantHandle)
     }
     
-    func connecting() {
+    public func connecting() {
         callbacksDelegate?.connecting()
     }
     
-    func inProgress() {
+    public func inProgress() {
         callbacksDelegate?.inProgress()
     }
     
-    func onHiResSessionChanged(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
+    public func onHiResSessionChanged(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
         guard let chatSession = chatSession, let chat = self.chatRoom else {
-            MEGALogDebug("Error getting mock properties")
             return
         }
         callbacksDelegate?.highResolutionChanged(for: CallParticipantEntity(session: chatSession, chatRoom: chat, privilege: privilege))
     }
     
-    func onLowResSessionChanged(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
+    public func onLowResSessionChanged(_ session: ChatSessionEntity, in chatRoom: ChatRoomEntity, privilege: ChatRoomPrivilegeEntity) {
         guard let chatSession = chatSession, let chat = self.chatRoom else {
-            MEGALogDebug("Error getting mock properties")
             return
         }
         callbacksDelegate?.lowResolutionChanged(for: CallParticipantEntity(session: chatSession, chatRoom: chat, privilege: privilege))
     }
     
-    func localAvFlagsUpdated(video: Bool, audio: Bool) {
+    public func localAvFlagsUpdated(video: Bool, audio: Bool) {
         callbacksDelegate?.localAvFlagsUpdated(video: video, audio: audio)
     }
     
-    func chatTitleChanged(chatRoom: ChatRoomEntity) {
+    public func chatTitleChanged(chatRoom: ChatRoomEntity) {
         callbacksDelegate?.chatTitleChanged(chatRoom: chatRoom)
     }
     
-    func networkQualityChanged(_ quality: NetworkQuality) {
+    public func networkQualityChanged(_ quality: NetworkQuality) {
         callbacksDelegate?.networkQualityChanged(networkQuality)
     }
     
-    func outgoingRingingStopReceived() {
+    public func outgoingRingingStopReceived() {
         callbacksDelegate?.outgoingRingingStopReceived()
     }
     
-    func waitingRoomUsersAllow(with handles: [HandleEntity]) {
+    public func waitingRoomUsersAllow(with handles: [HandleEntity]) {
         callbacksDelegate?.waitingRoomUsersAllow(with: handles)
     }
     
-    func mutedByClient(handle: HandleEntity) {
+    public func mutedByClient(handle: HandleEntity) {
         callbacksDelegate?.mutedByClient(handle: handle)
     }
 }
