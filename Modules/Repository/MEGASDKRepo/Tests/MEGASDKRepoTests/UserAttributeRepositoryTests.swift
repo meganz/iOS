96f165234ba11f6961a3f8b6268835351ac389fe
Devyish Gandhi <dg@mega.co.nz>
2024-05-06 10:54:39 +1200

    IOS-8524: Resolve issue where json structure from user attribute fails to fetch for new accounts

--- Modules/Repository/MEGASDKRepo/Tests/MEGASDKRepoTests/UserAttributeRepositoryTests.swift
+++ Modules/Repository/MEGASDKRepo/Tests/MEGASDKRepoTests/UserAttributeRepositoryTests.swift
@@ -101,8 +101,35 @@ final class UserAttributeRepositoryTests: XCTestCase {
             XCTAssertEqual(sdk.contentConsumptionPreferences[key]?.sorted(), try XCTUnwrap(targetJson).sorted())
     }
     
-    private func sut(contentConsumptionPreferences: [String: String] = [:]) -> (UserAttributeRepository, MockSdk) {
-        let sdk = MockSdk(contentConsumptionPreferences: contentConsumptionPreferences)
+    func testMergeUserAttribute_withErrorWhenRetrievingAttribute_shouldSave() async throws {
+        let targetJson = """
+            {"ios":{"timeline":{"mediaType":"videos","location":"cloudDrive","usePreference":false},"sensitives":{"showHiddenNodes":false}},"sensitives":{"onboarded":false}}
+        """.trim
+        let key = ContentConsumptionKeysEntity.key
+        let (sut, sdk) = sut(megaSetError: .apiERange)
+        let objectToSave = ContentConsumptionEntity(
+            ios: .init(
+                timeline: .init(mediaType: .videos, location: .cloudDrive, usePreference: false),
+                sensitives: .init(showHiddenNodes: false)),
+            sensitives: .init(onboarded: false))
+        
+        try await sut.mergeUserAttribute(
+            .contentConsumptionPreferences,
+            key: ContentConsumptionKeysEntity.key,
+            object: objectToSave)
+        
+        XCTAssertEqual(sdk.contentConsumptionPreferences[key]?.sorted(), try XCTUnwrap(targetJson).sorted())
+    }
+    
+    private func sut(
+        contentConsumptionPreferences: [String: String] = [:],
+        megaSetError: MEGAErrorType = .apiOk,
+        setUserAttributeTypeMegaSetError: @escaping (MEGAUserAttribute) -> MEGAErrorType = { _ in .apiOk }
+    ) -> (UserAttributeRepository, MockSdk) {
+        let sdk = MockSdk(
+            megaSetError: megaSetError,
+            contentConsumptionPreferences: contentConsumptionPreferences,
+            setUserAttributeTypeMegaSetError: setUserAttributeTypeMegaSetError)
         return (UserAttributeRepository(sdk: sdk), sdk)
     }
 }
