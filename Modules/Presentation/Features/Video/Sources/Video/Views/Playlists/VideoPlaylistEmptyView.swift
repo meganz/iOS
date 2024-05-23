a35fe4f185159451f63ca9f718badd82bc70c768
Arifin Firdaus <af@mega.co.nz>
2024-05-23 15:12:23 +1200

    CC-6632: iOS - Search playlist

---
+++ Modules/Presentation/Features/Video/Sources/Video/Views/Playlists/VideoPlaylistEmptyView.swift
@@ -0,0 +1,24 @@
+import MEGADesignToken
+import MEGAL10n
+import SwiftUI
+
+struct VideoPlaylistEmptyView: View {
+    
+    let videoConfig: VideoConfig
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(uiImage: videoConfig.playlistContentAssets.noResultVideoPlaylistImage)
+                .foregroundStyle(videoConfig.colorAssets.secondaryIconColor)
+                .frame(width: 120, height: 120)
+            Text(Strings.Localizable.Videos.Tab.Playlist.Content.emptyState)
+                .font(.body)
+                .foregroundStyle(videoConfig.colorAssets.primaryTextColor)
+        }
+        .background(videoConfig.colorAssets.pageBackgroundColor)
+    }
+}
+
+#Preview {
+    VideoPlaylistEmptyView(videoConfig: .preview)
+}
