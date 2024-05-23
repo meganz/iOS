6ab665025da0fda0d72eb984539e0f11c4593e03
Roberto Gómez <rg@mega.nz>
2024-05-23 22:13:23 +1200

    IOS-8580. Refactor NavigationBarView to Use EmptyView() for Leading, Center, and Trailing Components

--- Modules/UI/MEGASwiftUI/Sources/MEGASwiftUI/Custom Views/Navigation/NavigationBarView.swift
+++ Modules/UI/MEGASwiftUI/Sources/MEGASwiftUI/Custom Views/Navigation/NavigationBarView.swift
@@ -5,17 +5,17 @@ import SwiftUI
  To tackle that problem, NavigationBarView is used. 
  */
 public struct NavigationBarView <Leading: View, Trailing: View, Center: View>: View {
-    private let leading: () -> Leading?
-    private let trailing: () -> Trailing?
+    private let leading: () -> Leading
+    private let trailing: () -> Trailing
     private let center: () -> Center
     private let leadingWidth: Double
     private let trailingWidth: Double
     private let backgroundColor: Color
     
     public init(
-        @ViewBuilder leading: @escaping () -> Leading? = { nil },
-        @ViewBuilder trailing: @escaping () -> Trailing? = { nil },
-        @ViewBuilder center: @escaping () -> Center,
+        @ViewBuilder leading: @escaping () -> Leading = { EmptyView() },
+        @ViewBuilder trailing: @escaping () -> Trailing = { EmptyView() },
+        @ViewBuilder center: @escaping () -> Center = { EmptyView() },
         leadingWidth: Double = 100,
         trailingWidth: Double = 100,
         backgroundColor: Color
@@ -73,13 +73,66 @@ public struct NavigationBarView <Leading: View, Trailing: View, Center: View>: V
 }
 
 #Preview {
-    NavigationBarView(leading: {
-        Button(action: {}, label: { Text("Button 1") })
-    }, trailing: {
-        Button(action: {}, label: { Text("Button 2") })
-    }, center: {
-        NavigationTitleView(title: "Title")
-    }, backgroundColor: Color.black
+    NavigationBarView(
+        leading: {
+            Button(action: {}, label: { Text("Button 1") })
+        }, 
+        trailing: {
+            Button(action: {}, label: { Text("Button 2") })
+        }, 
+        center: {
+            NavigationTitleView(title: "Title")
+        }, 
+        backgroundColor: Color.black
     )
+    .preferredColorScheme(.dark)
+}
+
+#Preview {
+    let button1 = {
+        Button(
+            action: {},
+            label: {
+                Text("Button 1")
+            }
+        )
+    }
+    
+    let button2 = {
+        Button(
+            action: {},
+            label: {
+                Text("Button 2")
+            }
+        )
+    }
+    
+    let title = {
+        NavigationTitleView(title: "Title")
+    }
+                      
+    return Group {
+        NavigationBarView(
+            leading: button1,
+            backgroundColor: Color.black
+        )
+        
+        NavigationBarView(
+            trailing: button2,
+            backgroundColor: Color.black
+        )
+        
+        NavigationBarView(
+            center: title,
+            backgroundColor: Color.black
+        )
+        
+        NavigationBarView(
+            leading: button1,
+            trailing: button2,
+            center: title,
+            backgroundColor: Color.black
+        )
+    }
     .preferredColorScheme(.dark)
 }
