9cb10f09f40534dcd04c2aaa70b5a58874ea15ab
Glazel Magpayo <gma@mega.co.nz>
2024-05-24 18:19:17 +1200

    AP-1501 Remove Feature Flag - Onboarding Upselling upselling dialog Variant A and Variant B

--- Modules/Presentation/MEGAPresentation/Sources/MEGAPresentation/FeatureFlag/FeatureFlagKey.swift
+++ Modules/Presentation/MEGAPresentation/Sources/MEGAPresentation/FeatureFlag/FeatureFlagKey.swift
@@ -3,7 +3,6 @@ import MEGADomain
 public enum FeatureFlagKey: FeatureFlagName, CaseIterable {
     case newHomeSearch = "New Home Search"
     case albumPhotoCache = "Album and Photo Cache"
-    case onboardingProPlan = "Onboarding Pro Plan Upselling Dialog"
     case chipsGroups = "Chips groups and dropdown chips picker"
     case designToken = "MEGADesignToken"
     case newCloudDrive = "New Cloud Drive"
