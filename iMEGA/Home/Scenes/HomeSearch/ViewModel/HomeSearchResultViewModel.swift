b16a5ca8e0aad6f9f309aad302f58c06f7c199d6
Mauritz Mollentze <mmo@mega.co.nz>
2024-06-07 17:36:46 +1200

    CC-7386: Update HomeSearchResultViewModel to set sensitivity on search based of setting

--- iMEGA/Home/Scenes/HomeSearch/ViewModel/HomeSearchResultViewModel.swift
+++ iMEGA/Home/Scenes/HomeSearch/ViewModel/HomeSearchResultViewModel.swift
@@ -6,123 +6,158 @@ import MEGAPresentation
 import MEGASDKRepo
 
 protocol HomeAccountSearchResultViewModelInputs {
-
+    
     func didHighlightEmptySearchBar()
-
+    
     func didInputText(text: String)
-
+    
     func didSelectNode(_ nodeHandle: HandleEntity)
-
+    
     func didSelectHint(_ hintText: String)
+    
+    func recalculateExcludeSensitivityOnNextSearch()
 }
 
 protocol HomeAccountSearchResultViewModelOutputs {
-
+    
     var viewState: HomeSearchState { get }
 }
 
 protocol HomeAccountSearchResultViewModelType {
-
+    
     var inputs: any HomeAccountSearchResultViewModelInputs { get }
-
+    
     var notifyUpdate: ((any HomeAccountSearchResultViewModelOutputs) -> Void)? { get set }
 }
 
 final class HomeSearchResultViewModel {
-
+    
     // MARK: - State Variable
-
-    private var searchingInProgressCount: Int = 0
-
+    
+    private var searchTask: Task<Void, any Error>?
+    private var sensitiveTask: Task<Bool, Never>?
+    
     // MARK: - Output
-
+    
     var notifyUpdate: ((any HomeAccountSearchResultViewModelOutputs) -> Void)?
-
+    
     // MARK: - Components
-
-    private var router: any NodeRouting
-
+    
+    private let router: any NodeRouting
+    
     // MARK: - Use Case
-
-    private let searchFileUseCase: any SearchFileUseCaseProtocol
+    
+    private let fileSearchUseCase: any FilesSearchUseCaseProtocol
     private let searchFileHistoryUseCase: any SearchFileHistoryUseCaseProtocol
     private let nodeDetailUseCase: any NodeDetailUseCaseProtocol
+    private let contentConsumptionUserAttributeUseCase: any ContentConsumptionUserAttributeUseCaseProtocol
     private let tracker: any AnalyticsTracking
+    private let featureFlagProvider: any FeatureFlagProviderProtocol
     private let sdk: MEGASdk
+    private let debounceTimeInNanoseconds: UInt64
     
     init(
-        searchFileUseCase: some SearchFileUseCaseProtocol,
+        fileSearchUseCase: some FilesSearchUseCaseProtocol,
         searchFileHistoryUseCase: some SearchFileHistoryUseCaseProtocol,
         nodeDetailUseCase: some NodeDetailUseCaseProtocol,
+        contentConsumptionUserAttributeUseCase: some ContentConsumptionUserAttributeUseCaseProtocol,
         router: some NodeRouting,
         tracker: some AnalyticsTracking,
-        sdk: MEGASdk
+        featureFlagProvider: some FeatureFlagProviderProtocol,
+        sdk: MEGASdk,
+        debounceTimeInNanoseconds: UInt64 = UInt64(300_000_000)
     ) {
-        self.searchFileUseCase = searchFileUseCase
+        self.fileSearchUseCase = fileSearchUseCase
         self.searchFileHistoryUseCase = searchFileHistoryUseCase
         self.nodeDetailUseCase = nodeDetailUseCase
+        self.contentConsumptionUserAttributeUseCase = contentConsumptionUserAttributeUseCase
         self.router = router
         self.tracker = tracker
+        self.featureFlagProvider = featureFlagProvider
         self.sdk = sdk
+        self.debounceTimeInNanoseconds = debounceTimeInNanoseconds
+    }
+    
+    deinit {
+        sensitiveTask?.cancel()
+        searchTask?.cancel()
+    }
+    
+    @MainActor
+    private func notifyUpdateResults(state: HomeSearchResultState) {
+        notifyUpdate?(HomeSearchViewModel(viewState: .results(state)))
+    }
+    
+    /// Ensure that exclude sensitivity is only calculated once.
+    private func shouldExcludeSensitive() async -> Bool {
+        guard featureFlagProvider.isFeatureFlagEnabled(for: .hiddenNodes) else {
+            return false
+        }
+        if let sensitiveTask {
+            return await sensitiveTask.value
+        }
+        let sensitiveTask = Task { [weak self] in
+            guard let self else { return false }
+            return await !contentConsumptionUserAttributeUseCase.fetchSensitiveAttribute().showHiddenNodes
+        }
+        self.sensitiveTask = sensitiveTask
+        return await sensitiveTask.value
     }
 }
 
 extension HomeSearchResultViewModel: HomeAccountSearchResultViewModelInputs {
-
+    
     func didHighlightEmptySearchBar() {
-        searchFileUseCase.cancelCurrentSearch()
-
+        searchTask?.cancel()
+        
         let hints = searchFileHistoryUseCase.searchHistoryEntries().map {
             HomeSearchHintViewModel(text: $0.text, searchTime: $0.timeWhenSearchOccur)
         }
-
+        
         self.notifyUpdate?(HomeSearchViewModel(viewState: .hints(hints)))
     }
-
+    
     func didInputText(text: String) {
-        searchingInProgressCount += 1
-
-        let filter = MEGASearchFilter()
-        filter.term = text
-        filter.nodeType = Int32(MEGANodeType.unknown.rawValue)
-        filter.category = Int32(MEGANodeFormatType.unknown.rawValue)
-        filter.sensitivity = false
-
-        searchFileUseCase.searchFiles(
-            withFilter: filter,
-            recursive: true,
-            sortOrder: nil,
-            searchPath: .root,
-            completion: { [weak self] nodeListEntity in
-                guard let self else { return }
-                let sdkNodes = nodeListEntity?.toNodeEntities() ?? []
-
-                self.searchFileHistoryUseCase.saveSearchHistoryEntry(
-                    SearchFileHistoryEntryDomain(text: text,
-                                                 timeWhenSearchOccur: Date()
-                                                )
-                )
-
-                let searchFilesResult = sdkNodes.map { file in
-                    return self.transformeNode(file)
-                }
-
-                self.searchingInProgressCount -= 1
-                self.triggerUpdate(
-                    withNumberOfSearchInProgress: self.searchingInProgressCount,
-                    resultFiles: searchFilesResult
-                )
-            }
-        )
+        searchTask?.cancel()
+        searchTask = Task { [weak self] in
+            guard let self else { return }
+            
+            try await Task.sleep(nanoseconds: debounceTimeInNanoseconds)
+            
+            let filter = SearchFilterEntity(
+                searchText: text,
+                recursive: true,
+                supportCancel: true,
+                sortOrderType: .creationAsc,
+                formatType: .unknown,
+                excludeSensitive: await shouldExcludeSensitive(),
+                nodeTypeEntity: .unknown)
+            
+            let results: [NodeEntity] = try await fileSearchUseCase.search(
+                filter: filter, cancelPreviousSearchIfNeeded: true)
+            
+            try Task.checkCancellation()
+            
+            searchFileHistoryUseCase.saveSearchHistoryEntry(
+                SearchFileHistoryEntryDomain(text: text,
+                                             timeWhenSearchOccur: Date()))
+            
+            let searchFilesResult = results.map(transformNode(_:))
+            
+            try Task.checkCancellation()
+            
+            await notifyUpdateResults(state: .data(searchFilesResult))
+        }
     }
-
-    fileprivate func transformeNode(_ file: NodeEntity) -> HomeSearchResultFileViewModel {
+    
+    fileprivate func transformNode(_ file: NodeEntity) -> HomeSearchResultFileViewModel {
         let ownerFolder = self.nodeDetailUseCase.ownerFolder(of: file.handle)
 
-        let moreAction: (HandleEntity, UIButton) -> Void = { _, button in
-            self.tracker.trackAnalyticsEvent(with: SearchResultOverflowMenuItemEvent())
-            self.router.didTapMoreAction(on: file.handle, button: button)
+        let moreAction: (HandleEntity, UIButton) -> Void = { [weak self] _, button in
+            self?.tracker.trackAnalyticsEvent(with: SearchResultOverflowMenuItemEvent())
+            self?.router.didTapMoreAction(on: file.handle, button: button)
         }
+
         return HomeSearchResultFileViewModel(
             node: file,
             ownerFolder: ownerFolder?.name ?? "",
@@ -135,21 +170,9 @@ extension HomeSearchResultViewModel: HomeAccountSearchResultViewModelInputs {
             featureFlagProvider: DIContainer.featureFlagProvider,
             moreAction: moreAction)
     }
-
-    private func triggerUpdate(
-        withNumberOfSearchInProgress numberOfSearchInProgress: Int,
-        resultFiles: [HomeSearchResultFileViewModel]
-    ) {
-        asyncOnMain(weakify(self) { strongSelf in
-            if numberOfSearchInProgress > 0 {
-                strongSelf.notifyUpdate?(HomeSearchViewModel(viewState: .results(.loading)))
-            }
-            strongSelf.notifyUpdate?(HomeSearchViewModel(viewState: .results(.data(resultFiles))))
-        })
-    }
-
+    
     // MARK: - Did Select Node
-
+    
     func didSelectNode(_ nodeHandle: HandleEntity) {
         router.didTapNode(nodeHandle: nodeHandle)
         if let node = sdk.node(forHandle: nodeHandle) {
@@ -160,19 +183,26 @@ extension HomeSearchResultViewModel: HomeAccountSearchResultViewModelInputs {
         }
         
     }
-
+    
     // MARK: - Hint Search
-
+    
     func didSelectHint(_ hintText: String) {
         didInputText(text: hintText)
         notifyUpdate?(HomeSearchViewModel(viewState: .didSelectHint(hintText)))
     }
+    // MARK: - Sensitivity
+    
+    func recalculateExcludeSensitivityOnNextSearch() {
+        guard featureFlagProvider.isFeatureFlagEnabled(for: .hiddenNodes) else { return }
+            
+        sensitiveTask = nil
+    }
 }
 
 extension HomeSearchResultViewModel: HomeAccountSearchResultViewModelType {
-
+    
     var inputs: any HomeAccountSearchResultViewModelInputs { return self }
-
+    
     struct HomeSearchViewModel: HomeAccountSearchResultViewModelOutputs {
         var viewState: HomeSearchState
     }
