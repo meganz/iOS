4fb8b0386982dac62a62ae9977920387c0b6adb3
Tomek Kuźma <tku@mega.co.nz>
2024-06-07 21:03:42 +1200

    MEET-3971: update repo and use case layers for raise hands feature

---
+++ Modules/Domain/MEGADomain/Tests/MEGADomainTests/MockCallRepository.swift
@@ -0,0 +1,138 @@
+import Combine
+import MEGADomain
+import MEGADomainMock
+
+class MockCallRepository: CallRepositoryProtocol {
+    func startListeningForCallInChat(
+        _ chatId: HandleEntity,
+        callbacksDelegate: CallCallbacksRepositoryProtocol
+    ) {
+        
+    }
+    
+    func stopListeningForCall() {
+        
+    }
+    
+    func call(for chatId: HandleEntity) -> CallEntity? {
+        nil
+    }
+    
+    func answerCall(for chatId: HandleEntity, completion: @escaping (Result<CallEntity, CallErrorEntity>) -> Void) {
+        
+    }
+    
+    func answerCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool) async throws -> CallEntity {
+        CallEntity()
+    }
+    
+    func startCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool, notRinging: Bool, completion: @escaping (Result<CallEntity, CallErrorEntity>) -> Void) {
+        
+    }
+    
+    func startCall(for chatId: HandleEntity, enableVideo: Bool, enableAudio: Bool, notRinging: Bool) async throws -> CallEntity {
+        CallEntity()
+    }
+    
+    func hangCall(for callId: HandleEntity) {
+        
+    }
+    
+    func endCall(for callId: HandleEntity) {
+        
+    }
+    
+    func addPeer(toCall call: CallEntity, peerId: UInt64) {
+        
+    }
+    
+    func removePeer(fromCall call: CallEntity, peerId: UInt64) {
+        
+    }
+    
+    func allowUsersJoinCall(_ call: CallEntity, users: [UInt64]) {
+        
+    }
+    
+    func kickUsersFromCall(_ call: CallEntity, users: [UInt64]) {
+        
+    }
+    
+    func pushUsersIntoWaitingRoom(for scheduledMeeting: ScheduledMeetingEntity, users: [UInt64]) {
+        
+    }
+    
+    func makePeerAModerator(inCall call: CallEntity, peerId: UInt64) {
+        
+    }
+    
+    func removePeerAsModerator(inCall call: CallEntity, peerId: UInt64) {
+        
+    }
+    
+    func createActiveSessions() {
+        
+    }
+    
+    func localAvFlagsChaged(forCallId callId: HandleEntity) -> AnyPublisher<CallEntity, Never> {
+        Just(CallEntity()).eraseToAnyPublisher()
+    }
+    
+    func callStatusChaged(forCallId callId: HandleEntity) -> AnyPublisher<CallEntity, Never> {
+        Just(CallEntity()).eraseToAnyPublisher()
+    }
+    
+    func callWaitingRoomUsersUpdate(forCall call: CallEntity) -> AnyPublisher<CallEntity, Never> {
+        Just(CallEntity()).eraseToAnyPublisher()
+    }
+    
+    func onCallUpdate() -> AnyPublisher<CallEntity, Never> {
+        Just(CallEntity()).eraseToAnyPublisher()
+    }
+    
+    func callAbsentParticipant(inChat chatId: ChatIdEntity, userId: HandleEntity, timeout: Int) {
+        
+    }
+    
+    func muteUser(inChat chatRoom: ChatRoomEntity, clientId: ChatIdEntity) async throws {
+        
+    }
+    
+    func setCallLimit(inChat chatRoom: ChatRoomEntity, duration: Int?, maxUsers: Int?, maxClientPerUser: Int?, maxClients: Int?, divider: Int?) async throws {
+        
+    }
+    
+    func enableAudioForCall(in chatRoom: ChatRoomEntity) async throws {
+        
+    }
+    
+    func disableAudioForCall(in chatRoom: ChatRoomEntity) async throws {
+        
+    }
+    
+    func enableAudioMonitor(forCall call: CallEntity) {
+        
+    }
+    
+    func disableAudioMonitor(forCall call: CallEntity) {
+        
+    }
+    
+    var errorToThrow: (any Error)?
+    
+    var raiseHandCalls: [CallEntity] = []
+    func raiseHand(forCall call: CallEntity) async throws {
+        raiseHandCalls.append(call)
+        if let errorToThrow {
+            throw errorToThrow
+        }
+    }
+    
+    var lowerHandCalls: [CallEntity] = []
+    func lowerHand(forCall call: CallEntity) async throws {
+        lowerHandCalls.append(call)
+        if let errorToThrow {
+            throw errorToThrow
+        }
+    }
+}
