65ae8c98ff9df22751fb3f6c70efa2e30adc38df
Devyish Gandhi <dg@mega.co.nz>
2024-06-07 14:28:55 +1200

    CC-7379: Show warning dialog when sharing album with hidden nodes

--- MEGAUnitTests/Utils/XCTestCase+ActionCommand.swift
+++ MEGAUnitTests/Utils/XCTestCase+ActionCommand.swift
@@ -71,6 +71,36 @@ extension XCTestCase {
         wait(for: [commandExpectation], timeout: timeout)
     }
     
+    func test<T: ViewModelType>(viewModel: T, trigger: () -> Void, expectedCommands: [T.Command], timeout: TimeInterval = 3.0, expectationValidation: @escaping (T.Command, T.Command) -> Bool, file: StaticString = #filePath, line: UInt = #line) {
+        var expectedCommands = expectedCommands
+        var viewModel = viewModel
+        
+        let commandExpectation = expectation(description: "all commands are executed")
+        
+        // Fulfill the expectation if we don't expect any commands invoked from the view model
+        if expectedCommands.isEmpty {
+            commandExpectation.fulfill()
+        }
+        
+        // Subscribe the invoke command event
+        viewModel.invokeCommand = { command in
+            let expect = expectedCommands.removeFirstWithAssertion()
+            guard expectationValidation(expect, command) else {
+                XCTFail("received command \(command) is not \(expect)", file: file, line: line)
+                return
+            }
+            
+            if expectedCommands.isEmpty {
+                commandExpectation.fulfill()
+            }
+        }
+        
+        // Dispatch actions
+        trigger()
+        
+        wait(for: [commandExpectation], timeout: timeout)
+    }
+    
     func test<T: ViewModelType>(viewModel: T, actions: [T.Action], relaysCommand: T.Command, timeout: TimeInterval = 3.0) where T.Command: Equatable {
         var viewModel = viewModel
         let commandExpectation = expectation(description: "relays command")
