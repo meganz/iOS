971eb7ae7972ffd8464425215b23c53e6f2e7899
Binh Le <bl@mega.co.nz>
2024-06-12 13:52:23 +0700

    SAO-1590: Replace new search api in SharedItemsSearchOperation

--- Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Node/SearchNodeRepositoryProtocol.swift
+++
@@ -1,4 +0,0 @@
-public protocol SearchNodeRepositoryProtocol: RepositoryProtocol {
-    func search(type: SearchNodeTypeEntity, text: String, sortType: SortOrderEntity) async throws -> [NodeEntity]
-    func cancelSearch()
-}
--- Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Mappers/Files/SearchFilterEntity+Mapper.swift
+++ Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Mappers/Files/SearchFilterEntity+Mapper.swift
@@ -8,24 +8,52 @@ extension SearchFilterEntity {
     /// - Parameter defaultParentHandle: The default root handle to use when the parentNode is nil. This should generally pass the root node.
     /// - Returns: MEGASearchFilter
     func toMEGASearchFilter(defaultParentHandle: HandleEntity) -> MEGASearchFilter {
-        var timeFrame: MEGASearchFilterTimeFrame?
-        if let modificationTimeFrame {
-            timeFrame = MEGASearchFilterTimeFrame(
+        let term = searchText ?? ""
+        let nodeType = (nodeTypeEntity ?? NodeTypeEntity.unknown).toInt32()
+        let category = formatType.toInt32()
+        let favouriteFilter = favouriteFilterOption.toInt32()
+        
+        let timeFrame: MEGASearchFilterTimeFrame? = if let modificationTimeFrame {
+            MEGASearchFilterTimeFrame(
                 lowerLimit: modificationTimeFrame.startDate,
                 upperLimit: modificationTimeFrame.endDate
             )
-        }
+        } else { nil }
         
-        return MEGASearchFilter(
-            term: searchText ?? "",
-            parentNodeHandle: parentNode?.handle ?? defaultParentHandle,
-            nodeType: (nodeTypeEntity ?? NodeTypeEntity.unknown).toInt32(),
-            category: formatType.toInt32(),
-            sensitivity: excludeSensitive, 
-            favouriteFilter: favouriteFilterOption.toInt32(),
-            creationTimeFrame: nil,
-            modificationTimeFrame: timeFrame
-        )
+        return if let parentNode {
+            MEGASearchFilter(
+                term: term,
+                parentNodeHandle: parentNode.handle,
+                nodeType: nodeType,
+                category: category,
+                sensitivity: excludeSensitive,
+                favouriteFilter: favouriteFilter,
+                creationTimeFrame: nil,
+                modificationTimeFrame: timeFrame
+            )
+        } else if let folderTargetEntity {
+            MEGASearchFilter(
+                term: term,
+                nodeType: nodeType,
+                category: category,
+                sensitivity: excludeSensitive,
+                favouriteFilter: favouriteFilter,
+                locationType: folderTargetEntity.toInt32(),
+                creationTimeFrame: nil,
+                modificationTimeFrame: timeFrame
+            )
+        } else {
+            MEGASearchFilter(
+                term: term,
+                parentNodeHandle: defaultParentHandle,
+                nodeType: nodeType,
+                category: category,
+                sensitivity: excludeSensitive,
+                favouriteFilter: favouriteFilter,
+                creationTimeFrame: nil,
+                modificationTimeFrame: timeFrame
+            )
+        }
     }
 }
 
