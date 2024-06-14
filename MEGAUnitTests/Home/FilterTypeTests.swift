c73c0b3d2b282c4658a46ae05a88b527eaa7d3e9
Yessen Yermukhanbet <yy@mega.co.nz>
2024-06-14 19:08:20 +1200

    IOS-8609 Create FilterTypeTests

---
+++ MEGAUnitTests/Home/FilterTypeTests.swift
@@ -0,0 +1,30 @@
+@testable import MEGA
+
+import MEGAL10n
+import XCTest
+
+final class FilterTypeTests: XCTestCase {
+    func testLocalisation_localizedStringIsAllMedia() {
+        let expectedAllMediaValue = Strings.Localizable.CameraUploads.Timeline.Filter.MediaType.allMedia
+        let localizedAllMediaValue = FilterType.allMedia.localizedString
+        XCTAssertEqual(expectedAllMediaValue, localizedAllMediaValue, "Localized string for 'allMedia' should be \(expectedAllMediaValue)")
+    }
+
+    func testLocalisation_localizedStringIsImages() {
+        let expectedImagesValue = Strings.Localizable.CameraUploads.Timeline.Filter.MediaType.images
+        let localizedImagesValue = FilterType.images.localizedString
+        XCTAssertEqual(expectedImagesValue, localizedImagesValue, "Localized string for 'images' should be \(expectedImagesValue)")
+    }
+
+    func testLocalisation_localizedStringIsVideos() {
+        let expectedVideosValue = Strings.Localizable.CameraUploads.Timeline.Filter.MediaType.videos
+        let localizedVideosValue = FilterType.videos.localizedString
+        XCTAssertEqual(expectedVideosValue, localizedVideosValue, "Localized string for 'videos' should be \(expectedVideosValue)")
+    }
+
+    func testLocalisation_EmptyStringIsNoneCase() {
+        let expectedNoneValue = ""
+        let localizedNoneValue = FilterType.none.localizedString
+        XCTAssertEqual(expectedNoneValue, localizedNoneValue, "Localized string for 'none' should be \(expectedNoneValue)")
+    }
+}
