129b64fc38faa67330e5fe37b0df0c70aac5485b
Javier Navarro <jn@mega.co.nz>
2024-06-18 13:44:31 +0200

    IOS-8449: Move MockUserImageUseCase to MEGADomain module

---
+++ Modules/Domain/MEGADomain/Sources/MEGADomainMock/MockUseCases/Meetings/MockUserImageUseCase.swift
@@ -0,0 +1,42 @@
+import Combine
+import MEGADomain
+
+public struct MockUserImageUseCase: UserImageUseCaseProtocol {
+    private let fetchAvatarResult: Result<ImageFilePathEntity, UserImageLoadErrorEntity>
+    private let clearAvatarCacheCompletion: ((Base64HandleEntity) -> Void)?
+    private let downloadAvatarCompletion: ((Base64HandleEntity) -> Void)?
+    public let avatarChangePublisher: PassthroughSubject<[HandleEntity], Never>
+    
+    public init(fetchAvatarResult: Result<ImageFilePathEntity, UserImageLoadErrorEntity> = .failure(.generic),
+                clearAvatarCacheCompletion: ((Base64HandleEntity) -> Void)? = nil,
+                downloadAvatarCompletion: ((Base64HandleEntity) -> Void)? = nil,
+                avatarChangePublisher: PassthroughSubject<[HandleEntity], Never> = PassthroughSubject<[HandleEntity], Never>()
+    ) {
+        self.fetchAvatarResult = fetchAvatarResult
+        self.clearAvatarCacheCompletion = clearAvatarCacheCompletion
+        self.downloadAvatarCompletion = downloadAvatarCompletion
+        self.avatarChangePublisher = avatarChangePublisher
+    }
+    
+    public func avatarColorHex(forBase64UserHandle handle: Base64HandleEntity) -> String? {
+        ""
+    }
+    
+    public func clearAvatarCache(base64Handle: Base64HandleEntity) {
+        clearAvatarCacheCompletion?(base64Handle)
+    }
+    
+    public func fetchAvatar(base64Handle: Base64HandleEntity, forceDownload: Bool) async throws -> ImageFilePathEntity {
+        downloadAvatarCompletion?(base64Handle)
+        switch fetchAvatarResult {
+        case .success(let imageFilePath):
+            return imageFilePath
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    mutating public func requestAvatarChangeNotification(forUserHandles handles: [HandleEntity]) -> AnyPublisher<[HandleEntity], Never> {
+        avatarChangePublisher.eraseToAnyPublisher()
+    }
+}
