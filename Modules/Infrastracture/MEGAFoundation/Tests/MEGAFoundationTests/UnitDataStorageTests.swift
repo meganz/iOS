c5ee22d230c354297913d54f31819c03cd227f0c
yermukhanbet <yy@mega.co.nz>
2024-06-21 15:52:38 +0900

    IOS-8681 Create UnitDataStorageTests

---
+++ Modules/Infrastracture/MEGAFoundation/Tests/MEGAFoundationTests/UnitDataStorageTests.swift
@@ -0,0 +1,46 @@
+import Foundation
+import XCTest
+
+@testable import MEGAFoundation
+
+final class UnitDataStorageTests: XCTestCase {
+    func test_baseUnit_returnsBits() {
+        XCTAssertEqual(UnitDataStorage.baseUnit().symbol, "bit", "baseUnit should return bits")
+    }
+
+    func test_measurementBytes_returnsSymbolB() {
+        let measurement = Measurement.bytes(of: 1024)
+        XCTAssertEqual(measurement.value, 1024.0, "Measurement.bytes should correctly store the value")
+        XCTAssertEqual(measurement.unit.symbol, "B", "Measurement.bytes should correctly store the unit")
+    }
+
+    func test_measurementKilobytes_returnsSymbolKB() {
+        let measurement = Measurement.kilobytes(of: 1)
+        XCTAssertEqual(measurement.value, 1.0, "Measurement.kilobytes should correctly store the value")
+        XCTAssertEqual(measurement.unit.symbol, "KB", "Measurement.kilobytes should correctly store the unit")
+    }
+
+    func test_measurementMegabytes_returnsSymbolMB() {
+        let measurement = Measurement.megabytes(of: 1)
+        XCTAssertEqual(measurement.value, 1.0, "Measurement.megabytes should correctly store the value")
+        XCTAssertEqual(measurement.unit.symbol, "MB", "Measurement.megabytes should correctly store the unit")
+    }
+
+    func test_measurementGigabytes_returnsSymbolGB() {
+        let measurement = Measurement.gigabytes(of: 1)
+        XCTAssertEqual(measurement.value, 1.0, "Measurement.gigabytes should correctly store the value")
+        XCTAssertEqual(measurement.unit.symbol, "GB", "Measurement.gigabytes should correctly store the unit")
+    }
+
+    func test_measurementTerabytes_returnsSymbolTB() {
+        let measurement = Measurement.terabytes(of: 1)
+        XCTAssertEqual(measurement.value, 1.0, "Measurement.terabytes should correctly store the value")
+        XCTAssertEqual(measurement.unit.symbol, "TB", "Measurement.terabytes should correctly store the unit")
+    }
+
+    func test_measurementPetabytes_returnsSymbolPB() {
+        let measurement = Measurement.petabytes(of: 1)
+        XCTAssertEqual(measurement.value, 1.0, "Measurement.petabytes should correctly store the value")
+        XCTAssertEqual(measurement.unit.symbol, "PB", "Measurement.petabytes should correctly store the unit")
+    }
+}
