d7a2d655979a2e72dd6469934b4dc6f26951e28e
Yessen Yermukhanbet <yy@mega.co.nz>
2024-06-24 19:24:03 +1200

    IOS-8685: [bug] Add local and TimeZone to DateFormatter tests

--- Modules/Infrastracture/MEGAFoundation/Tests/MEGAFoundationTests/DateFormatting/DateFormatter+TemplateFormatterTests.swift
+++ Modules/Infrastracture/MEGAFoundation/Tests/MEGAFoundationTests/DateFormatting/DateFormatter+TemplateFormatterTests.swift
@@ -8,19 +8,28 @@ final class DateFormatter_TemplateFormatterTests: XCTestCase {
     private let date = Date(timeIntervalSince1970: 0)
 
     func test_dateMediumWithWeekday_returnsCorrectFormat() {
-        let formatter = DateFormatter.dateMediumWithWeekday()
+        let formatter = DateFormatter.dateMediumWithWeekday(
+            timeZone: TimeZone.GMT,
+            locale: Locale(identifier: "en_US")
+        )
         let dateString = formatter.localisedString(from: date)
         XCTAssertEqual(dateString, "Thursday, Jan 01, 1970", "dateMediumWithWeekday should format the date correctly")
     }
 
     func test_yearTemplate_defaultValue_returnsCorrectFormat() {
-        let formatter = DateFormatter.yearTemplate()
+        let formatter = DateFormatter.yearTemplate(
+            timeZone: TimeZone.GMT,
+            locale: Locale(identifier: "en_US")
+        )
         let dateString = formatter.localisedString(from: date)
         XCTAssertEqual(dateString, "1970", "yearTemplate should format the date correctly")
     }
 
     func test_monthTemplate_defaultValue_returnsCorrectFormat() {
-        let formatter = DateFormatter.monthTemplate()
+        let formatter = DateFormatter.monthTemplate(
+            timeZone: TimeZone.GMT,
+            locale: Locale(identifier: "en_US")
+        )
         let dateString = formatter.localisedString(from: date)
         XCTAssertEqual(dateString, "Jan 1970", "monthTemplate should format the date correctly")
     }
