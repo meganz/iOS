6c8c2c80fb3742fc0352264b34a2c090fdb7a3a0
Yessen Yermukhanbet <yy@mega.co.nz>
2024-06-25 18:49:40 +1200

    IOS-8684 TimeZone+AdditionsTests

---
+++ Modules/Infrastracture/MEGAFoundation/Tests/MEGAFoundationTests/DateFormatting/TimeZone+Additions.swift
@@ -0,0 +1,16 @@
+import Foundation
+import XCTest
+
+@testable import MEGAFoundation
+
+final class TimeZone_Additions: XCTestCase {
+    func test_TimeZoneGMT_shouldReturnZeroSecondsFromGMT() {
+        let gmtTimeZone = TimeZone.GMT
+        XCTAssertEqual(gmtTimeZone.secondsFromGMT(), 0, "GMT should return a TimeZone with 0 seconds from GMT")
+    }
+
+    func test_TimeZoneGMT_shouldReturnGMTIdentifier() {
+        let gmtTimeZone = TimeZone.GMT
+        XCTAssertEqual(gmtTimeZone.identifier, "GMT", "GMT should return a TimeZone with the identifier 'GMT'")
+    }
+}
