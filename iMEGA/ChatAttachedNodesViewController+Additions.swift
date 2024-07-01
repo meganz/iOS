2c38e31db576fb49ee01ee71ef563a6967e51975
Glazel Magpayo <gma@mega.co.nz>
2024-07-01 14:13:30 +0800

    IOS-8134 Pluralize attachedXFiles string

--- iMEGA/ChatAttachedNodesViewController+Additions.swift
+++ iMEGA/ChatAttachedNodesViewController+Additions.swift
@@ -2,6 +2,10 @@ import Foundation
 import MEGAL10n
 
 extension ChatAttachedNodesViewController {
+    @objc func setNavigationItemTitle(attachmentCount: Int) {
+        self.navigationItem.title = Strings.Localizable.Chat.Message.numberOfAttachments(attachmentCount)
+    }
+    
     @objc func nodeCountTitle(_ count: Int) -> String {
         guard count > 0 else {
             return Strings.Localizable.selectTitle
