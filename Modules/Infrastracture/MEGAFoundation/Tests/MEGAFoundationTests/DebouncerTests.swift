1d5f390c206ed6bb145a61712ec063b03127adb5
Roberto Gómez <rg@mega.nz>
2024-07-02 21:25:09 +1200

    IOS-8697. Add unit tests for Debouncer in MEGAFoundation

--- Modules/Infrastracture/MEGAFoundation/Tests/MEGAFoundationTests/DebouncerTests.swift
+++ Modules/Infrastracture/MEGAFoundation/Tests/MEGAFoundationTests/DebouncerTests.swift
@@ -1,39 +1,47 @@
+import ConcurrencyExtras
 @testable import MEGAFoundation
 import XCTest
 
 final class DebouncerTests: XCTestCase {
     let defaultDelay: TimeInterval = 0.1
-    let defaultTimeOut: TimeInterval = 1.0
+    let defaultTimeOut: TimeInterval = 2.0
 
     func testInitialization_whenCalled_shouldCreateDebouncer() {
         let debouncer = Debouncer(delay: 1.0)
-        XCTAssertNotNil(debouncer)
+        XCTAssertNotNil(debouncer, "Debouncer should be created successfully.")
     }
 
-    func testStartAction_whenCalled_shouldExecuteActionAfterDelay() {
-        let expectation = XCTestExpectation(description: "Action should be called after delay")
-        let debouncer = Debouncer(delay: defaultDelay)
+    func testStartAction_whenCalled_shouldExecuteActionAfterDelay() async {
+        await withMainSerialExecutor {
+            let expectation = XCTestExpectation(description: "Action should be called after delay")
+            let debouncer = Debouncer(delay: defaultDelay)
+            let startTime = Date()
 
-        debouncer.start {
-            expectation.fulfill()
+            debouncer.start {
+                let elapsedTime = Date().timeIntervalSince(startTime)
+                XCTAssertGreaterThanOrEqual(elapsedTime, self.defaultDelay, "Action should be executed after the delay")
+                expectation.fulfill()
+            }
+            
+            await fulfillment(of: [expectation], timeout: defaultTimeOut + defaultDelay)
         }
-
-        wait(for: [expectation], timeout: defaultTimeOut)
     }
 
     func testCancel_whenCalled_shouldPreventActionExecution() async throws {
-        let expectation = XCTestExpectation(description: "Action should not be called")
-         expectation.isInverted = true
-
-         let debouncer = Debouncer(delay: defaultDelay)
-         debouncer.start {
-             expectation.fulfill()
-         }
-
-         try await Task.sleep(nanoseconds: 50_000_000)
-         debouncer.cancel()
-
-         await fulfillment(of: [expectation], timeout: defaultTimeOut)
+        try await withMainSerialExecutor {
+            let expectation = XCTestExpectation(description: "Action should not be called")
+            expectation.isInverted = true
+            
+            let debouncer = Debouncer(delay: defaultDelay)
+            debouncer.start {
+                expectation.fulfill()
+            }
+            
+            try await Task.sleep(nanoseconds: 50_000_000)
+            debouncer.cancel()
+            
+            await fulfillment(of: [expectation], timeout: defaultTimeOut)
+        }
     }
 
     func testDebounceAsync_whenCalled_shouldWaitForSpecifiedDelay() async throws {
@@ -48,25 +56,27 @@ final class DebouncerTests: XCTestCase {
     }
 
     func testMultipleStarts_whenCalledMultipleTimes_shouldOnlyExecuteLastAction() async throws {
-        let expectation = XCTestExpectation(description: "Only the last action should be called")
-        let debouncer = Debouncer(delay: defaultDelay)
-        var callCount = 0
+        try await withMainSerialExecutor {
+            let expectation = XCTestExpectation(description: "Only the last action should be called")
+            let debouncer = Debouncer(delay: defaultDelay)
+            var callCount = 0
 
-        func performStartAction(with delay: TimeInterval) async throws {
-            try? await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
-            debouncer.start {
-                callCount += 1
-                expectation.fulfill()
+            func performStartAction(with delay: TimeInterval) async throws {
+                try? await Task.sleep(nanoseconds: UInt64(delay * 1_000_000_000))
+                debouncer.start {
+                    callCount += 1
+                    expectation.fulfill()
+                }
             }
+
+            try await performStartAction(with: 0.0)
+            try await performStartAction(with: 0.02)
+            try await performStartAction(with: 0.04)
+
+            await fulfillment(of: [expectation], timeout: defaultTimeOut)
+
+            XCTAssertEqual(callCount, 1)
         }
-
-        try await performStartAction(with: 0.0)
-        try await performStartAction(with: 0.02)
-        try await performStartAction(with: 0.04)
-
-        await fulfillment(of: [expectation], timeout: defaultTimeOut)
-
-        XCTAssertEqual(callCount, 1)
     }
 
     func testConcurrentAccess_whenCalledConcurrently_shouldDebounceCorrectly() {
