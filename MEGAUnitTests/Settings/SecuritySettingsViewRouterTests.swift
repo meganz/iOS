ec35dd2771262239bc99c70b8c38903e81b7c1ba
Yessen Yermukhanbet <yy@mega.co.nz>
2024-07-03 20:23:21 +1200

    IOS-8703 Create SecuritySettingsViewRouterTests

---
+++ MEGAUnitTests/Settings/SecuritySettingsViewRouterTests.swift
@@ -0,0 +1,33 @@
+import MEGATest
+import XCTest
+
+@testable import MEGA
+
+final class SecuritySettingsViewRouterTests: XCTestCase {
+    func test_build_returnsSecurityOptionsTableViewController() {
+        let (sut, _) = makeSUT()
+
+        let resultViewController = sut.build()
+
+        XCTAssert(resultViewController is SecurityOptionsTableViewController)
+    }
+
+    func test_start_pushesSecurityOptionsTableViewController() throws {
+        let (sut, mockPresenter) = makeSUT()
+
+        sut.start()
+
+        let pushedVC = try XCTUnwrap(
+            mockPresenter.viewControllers.first,
+            "no vc was pushed"
+        )
+
+        XCTAssertTrue(pushedVC is SecurityOptionsTableViewController)
+    }
+
+    private func makeSUT() -> (sut: SecuritySettingsViewRouter, mockPresenter: MockNavigationController) {
+        let mockPresenter = MockNavigationController()
+        let sut = SecuritySettingsViewRouter(presenter: mockPresenter)
+        return (sut, mockPresenter)
+    }
+}
