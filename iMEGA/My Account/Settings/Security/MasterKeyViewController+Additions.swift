3345eec2da3b8994e795e511bf028af26a7ef3ac
rdu-mega <rdu@mega.co.nz>
2024-07-04 10:28:25 +0530

    IOS-8719: Fix localization of recovery screen

--- iMEGA/My Account/Settings/Security/MasterKeyViewController+Additions.swift
+++ iMEGA/My Account/Settings/Security/MasterKeyViewController+Additions.swift
@@ -1,7 +1,20 @@
 import MEGADesignToken
+import MEGAL10n
 import UIKit
 
 extension MasterKeyViewController {
+    @objc func setupContent() {
+        navigationItem.title = Strings.Localizable.recoveryKey
+        carbonCopyMasterKeyButton?.setTitle(Strings.Localizable.copy, for: .normal)
+        saveMasterKey?.setTitle(Strings.Localizable.save, for: .normal)
+
+        whyDoINeedARecoveryKeyButton?.setTitle(Strings.Localizable.whyDoINeedARecoveryKey, for: .normal)
+
+        whyDoINeedARecoveryKeyButton?.titleLabel?.numberOfLines = 0
+        whyDoINeedARecoveryKeyButton?.titleLabel?.lineBreakMode = .byWordWrapping
+        whyDoINeedARecoveryKeyButton?.titleLabel?.textAlignment = .center
+    }
+
     @objc func updateAppearance() {
         view.backgroundColor = UIColor.isDesignTokenEnabled() ?
         TokenColors.Background.page : UIColor.systemBackground
--- iMEGA/My Account/Settings/Security/MasterKeyViewController.m
+++ iMEGA/My Account/Settings/Security/MasterKeyViewController.m
@@ -7,8 +7,6 @@
 
 #import "NSURL+MNZCategory.h"
 
-@import MEGAL10nObjc;
-
 @interface MasterKeyViewController ()
 @end
 
@@ -19,14 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.navigationItem.title = LocalizedString(@"recoveryKey", @"Label for any 'Recovery Key' button, link, text, title, etc. Preserve uppercase - (String as short as possible). The Recovery Key is the new name for the account 'Master Key', and can unlock (recover) the account if the user forgets their password.");
-    
-    [self.carbonCopyMasterKeyButton setTitle:LocalizedString(@"copy", @"List option shown on the details of a file or folder") forState:UIControlStateNormal];
-    
-    [self.saveMasterKey setTitle:LocalizedString(@"save", @"Button title to 'Save' the selected option") forState:UIControlStateNormal];
-    
-    self.whyDoINeedARecoveryKeyButton.titleLabel.text = LocalizedString(@"whyDoINeedARecoveryKey", @"Question button to present a view where it's explained what is the Recovery Key");
-    
+    [self setupContent];
     [self updateAppearance];
 }
 
