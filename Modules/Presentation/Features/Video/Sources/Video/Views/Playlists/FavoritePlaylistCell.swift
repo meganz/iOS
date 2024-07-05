cdf8a75bc098cde5ebe9d54b8c11b8c484c026d8
Arifin Firdaus <af@mega.co.nz>
2024-07-05 08:57:17 +0700

    CC-7595: iOS - Playlist - System playlist row should not have more button

--- Modules/Presentation/Features/Video/Sources/Video/Views/Playlists/FavoritePlaylistCell.swift
+++ Modules/Presentation/Features/Video/Sources/Video/Views/Playlists/FavoritePlaylistCell.swift
@@ -38,10 +38,6 @@ struct FavoritePlaylistCell: View {
                     .frame(maxHeight: .infinity, alignment: .top)
             }
             .frame(maxWidth: .infinity, alignment: .topLeading)
-            
-            Image(uiImage: videoConfig.rowAssets.moreImage)
-                .foregroundColor(videoConfig.colorAssets.secondaryIconColor)
-                .onTapGesture { viewModel.onTappedMoreOptions() }
         }
         .padding(0)
         .background(videoConfig.colorAssets.pageBackgroundColor)
