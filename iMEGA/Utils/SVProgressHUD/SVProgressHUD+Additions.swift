07fa073fcf17af05d151cefa226121b7025c544f
Carlos Martín Acera <cma@mega.nz>
2024-07-12 23:00:32 +1200

    IOS-8525 Fix HUD color when presenting in call UI

--- iMEGA/Utils/SVProgressHUD/SVProgressHUD+Additions.swift
+++
@@ -1,7 +0,0 @@
-public extension SVProgressHUD {
-    static func configureHudDarkMode() {
-        SVProgressHUD.setHudViewCustomBlurEffect(UIBlurEffect.init(style: UIBlurEffect.Style.systemMaterialDark))
-        SVProgressHUD.setForegroundColor(MEGAAppColor.White._FFFFFF.uiColor)
-        SVProgressHUD.setForegroundImageColor(MEGAAppColor.White._FFFFFF.uiColor)
-    }
-}
