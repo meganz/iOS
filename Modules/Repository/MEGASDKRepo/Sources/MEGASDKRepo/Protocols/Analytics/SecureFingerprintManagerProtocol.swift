29bd0af0b45d3cbf46b44a5f722f7bc1d387b479
Javier Navarro <jn@mega.co.nz>
2024-07-16 13:56:33 +0200

    IOS-8338: remove SecureFingerprintManagerProtocol

--- Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Protocols/Analytics/SecureFingerprintManagerProtocol.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public protocol SecureFingerprintManagerProtocol {
-    var secureFingerprintVerification: Bool { get set }
-    var onToggleSecureFingerprintFlag: (() -> Void)? { get set }
-    func secureFingerprintStatus() -> String
-}
