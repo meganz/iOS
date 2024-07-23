e55efdcbb159d2f19ce77a4335fe2c0845cdfb89
Harry Yan <hy@mega.co.nz>
2024-07-23 20:13:57 +1200

    IOS-8784: Add sendable to AchievementUseCaseProtocol

--- Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Account/AchievementRepositoryProtocol.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Account/AchievementRepositoryProtocol.swift
@@ -1,7 +1,7 @@
 import Foundation
 import MEGAFoundation
 
-public protocol AchievementRepositoryProtocol: RepositoryProtocol {
+public protocol AchievementRepositoryProtocol: RepositoryProtocol, Sendable {
     func checkIsAchievementsEnabled() -> Bool
     func getAchievementStorage(by type: AchievementTypeEntity) async throws -> Measurement<UnitDataStorage>
     func getAchievementDetails() async throws -> AchievementDetailsEntity
