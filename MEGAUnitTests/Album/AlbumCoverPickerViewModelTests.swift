b81c2622295ceed03a3854386560d5ed8d2090c1
Arifin Firdaus <af@mega.co.nz>
2024-07-24 15:43:34 +1200

    IOS-8385: Swift Targeted Concurrency - Fix data race risk in AlbumCoverPickerViewModel

--- MEGAUnitTests/Album/AlbumCoverPickerViewModelTests.swift
+++ MEGAUnitTests/Album/AlbumCoverPickerViewModelTests.swift
@@ -6,6 +6,7 @@ import XCTest
 
 final class AlbumCoverPickerViewModelTests: XCTestCase {
 
+    @MainActor
     func testOnSave_whenUserSelectACoverPic_shouldSetTheNewPicAsCoverPic() {
         let exp = expectation(description: "Should call completion handler and set isDismiss to true")
         
@@ -25,6 +26,7 @@ final class AlbumCoverPickerViewModelTests: XCTestCase {
         XCTAssertTrue(sut.isDismiss)
     }
     
+    @MainActor
     func testOnCancel_whenUserTapCancel_shouldDismissTheScreen() {
         let sut = albumCoverPickerViewModel()
         
@@ -33,6 +35,7 @@ final class AlbumCoverPickerViewModelTests: XCTestCase {
         XCTAssertTrue(sut.isDismiss)
     }
     
+    @MainActor
     func testLoadAlbumContents_whenUserOpenSelectCoverPicScreen_shouldLoadAlbumContents() async {
         let sut = albumCoverPickerViewModel()
         
@@ -42,6 +45,7 @@ final class AlbumCoverPickerViewModelTests: XCTestCase {
         XCTAssertEqual(sut.photos.count, 2)
     }
     
+    @MainActor
     func testSelectedCoverPicFromLoadedAlbum_whenUserOpenTheSelectCoverPicScreen_shouldSelectNodeFromAlbumCoverNode() async {
         let coverNode = NodeEntity(handle: 10)
         let coverPhoto = AlbumPhotoEntity(photo: coverNode)
@@ -53,6 +57,7 @@ final class AlbumCoverPickerViewModelTests: XCTestCase {
         XCTAssertEqual(sut.photoSelection.selectedPhoto, coverPhoto)
     }
     
+    @MainActor
     func testSelectedCoverPicFromLoadedAlbumContents_whenUserOpenTheSelectCoverPicScreen_shouldSelectNodeFromAlbumContents() async {
         let node4 = AlbumPhotoEntity(photo: NodeEntity(handle: 4, modificationTime: Date.distantFuture))
         let node5 = AlbumPhotoEntity(photo: NodeEntity(handle: 5, modificationTime: Date.distantPast))
@@ -64,6 +69,7 @@ final class AlbumCoverPickerViewModelTests: XCTestCase {
         XCTAssertEqual(sut.photoSelection.selectedPhoto, AlbumPhotoEntity(photo: node4.photo))
     }
     
+    @MainActor
     func testSelectedCoverPicFromLoadedAlbumContents_whenOnTheSelectCoverPicScreen_shouldSameModificationTimeBasedNodesSortedByHandleInDescendingOrder() async {
         let photo4 = AlbumPhotoEntity(photo: NodeEntity(handle: 4, modificationTime: Date.distantPast))
         let photo5 = AlbumPhotoEntity(photo: NodeEntity(handle: 5, modificationTime: Date.distantPast))
@@ -75,15 +81,29 @@ final class AlbumCoverPickerViewModelTests: XCTestCase {
         XCTAssertEqual(sut.photos, [photo5, photo4])
     }
     
+    @MainActor
     func testIsSaveButtonDisabled_whenSelectedNodeChange_ignoreTheInitialSelectionAndEnableForTheNextOne() {
         let sut = albumCoverPickerViewModel()
         
         XCTAssertTrue(sut.isSaveButtonDisabled)
+        
+        var receivedValue: Bool?
+        let exp = expectation(description: "wait for subscription")
+        let cancellable = sut.$isSaveButtonDisabled
+            .dropFirst()
+            .sink { value in
+                receivedValue = value
+                exp.fulfill()
+            }
+        
         sut.photoSelection.selectedPhoto = AlbumPhotoEntity(photo: NodeEntity(handle: 2))
         sut.photoSelection.selectedPhoto = AlbumPhotoEntity(photo: NodeEntity(handle: 2))
-        XCTAssertFalse(sut.isSaveButtonDisabled)
+        wait(for: [exp], timeout: 0.1)
+        
+        XCTAssertTrue(receivedValue == false)
     }
     
+    @MainActor
     private func albumCoverPickerViewModel(
         coverNode: NodeEntity? = nil,
         photos: [AlbumPhotoEntity] = [AlbumPhotoEntity(photo: NodeEntity(handle: 2)), AlbumPhotoEntity(photo: NodeEntity(handle: 3))],
