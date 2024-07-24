258bf4659150cb77563b162cd6ad1cd62f222164
Harry <hy@mega.co.nz>
2024-07-24 13:42:29 +0800

    IOS-8788: Add sendable to NameCollisionUseCaseProtocol

--- Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Node/NodeActionsRepositoryProtocol.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Node/NodeActionsRepositoryProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public protocol NodeActionsRepositoryProtocol: RepositoryProtocol {
+public protocol NodeActionsRepositoryProtocol: RepositoryProtocol, Sendable {
     func copyNodeIfExistsWithSameFingerprint(at path: String, parentHandle: HandleEntity, newName: String?) -> Bool
     func copyNode(handle: HandleEntity, in parentHandle: HandleEntity, newName: String?, isFolderLink: Bool) async throws -> HandleEntity
     func moveNode(handle: HandleEntity, in parentHandle: HandleEntity, newName: String?) async throws -> HandleEntity
