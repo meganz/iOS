b1b3000950b4c5a81804d7bb84cb0a82d1aef49e
Joanda Febrian <jfe@mega.co.nz>
2024-08-02 12:23:40 +0700

    IOS-8810: T14796259 Fix color changes in Figma

--- iMEGA/Utils/Categories/LTHPasscodeViewController+Additions.swift
+++ iMEGA/Utils/Categories/LTHPasscodeViewController+Additions.swift
@@ -8,16 +8,16 @@ extension LTHPasscodeViewController {
         backgroundColor = TokenColors.Background.surface1
         passcodeBackgroundColor = .clear
         coverViewBackgroundColor = TokenColors.Background.page
-        failedAttemptLabelBackgroundColor = TokenColors.Support.error
+        failedAttemptLabelBackgroundColor = .clear
         enterPasscodeLabelBackgroundColor = .clear
         eraseLocalDataLabelBackgroundColor = .clear
 
         // Text
         labelTextColor = TokenColors.Text.primary
         passcodeTextColor = TokenColors.Text.primary
-        failedAttemptLabelTextColor = TokenColors.Text.onColor
+        failedAttemptLabelTextColor = TokenColors.Support.error
         eraseLocalDataLabelTextColor = TokenColors.Support.error
-        optionsButtonTextColor = TokenColors.Support.success
+        optionsButtonTextColor = TokenColors.Link.primary
         textFieldBorderColor = TokenColors.Border.strong
     }
 }
