94717929bdd929d93f83f7035b6844392263a593
Javier Navarro <jn@mega.co.nz>
2024-08-05 18:22:50 +1200

    IOS-8832: Swift Targeted Concurrency - Add Sendable to AnalyticsRepositoryProtocol

--- Modules/Domain/MEGAAnalyticsDomain/Sources/MEGAAnalyticsDomainMock/MockRepos/MockAnalyticsRepository.swift
+++ Modules/Domain/MEGAAnalyticsDomain/Sources/MEGAAnalyticsDomainMock/MockRepos/MockAnalyticsRepository.swift
@@ -1,13 +1,18 @@
+import Foundation
 import MEGAAnalyticsDomain
 
-public final class MockAnalyticsRepository: AnalyticsRepositoryProtocol {
+public final class MockAnalyticsRepository: AnalyticsRepositoryProtocol, @unchecked Sendable {
     public static var newRepo: MockAnalyticsRepository {
         MockAnalyticsRepository()
     }
     
+    private let queue = DispatchQueue(label: "com.mega.MockAnalyticsRepository")
+    
     public private(set) var sendAnalyticsEvent_Calls = [EventEntity]()
     
     public func sendAnalyticsEvent(_ eventEntity: EventEntity) {
-        sendAnalyticsEvent_Calls.append(eventEntity)
+        queue.sync {
+            sendAnalyticsEvent_Calls.append(eventEntity)
+        }
     }
 }
