05f6ff41609058a90912e5ffd376ff35176c1adf
Tomek Kuźma <tku@mega.co.nz>
2024-08-08 01:14:57 +1200

    MEET-4280 fix slow or missing Chat Message push notifications

--- Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Mappers/Analytics/NSEAnalyticsEventEntity+Mapper.swift
+++ Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Mappers/Analytics/NSEAnalyticsEventEntity+Mapper.swift
@@ -3,23 +3,23 @@ import MEGADomain
 extension NSEAnalyticsEventEntity: AnalyticsEventProtocol {
     var code: Int {
         switch self {
-        case .delayBetweenChatdAndApi: return 99300
-        case .delayBetweenApiAndPushserver: return 99301
-        case .delayBetweenPushserverAndNSE: return 99302
-        case .willExpireAndMessageNotFound: return 99303
+        case .delayBetweenChatdAndApi: 99300
+        case .delayBetweenApiAndPushserver: 99301
+        case .delayBetweenPushserverAndNSE: 99302
+        case .willExpireAndMessageNotFound: 99303
         }
     }
     
     var description: String {
         switch self {
         case .delayBetweenChatdAndApi:
-            return "Delay between chatd and api"
+            "Delay between chatd and api"
         case .delayBetweenApiAndPushserver:
-            return "Delay between api and pushserver"
+            "Delay between api and pushserver"
         case .delayBetweenPushserverAndNSE:
-            return "Delay between pushserver and Apple/device/NSE"
+            "Delay between pushserver and Apple/device/NSE"
         case .willExpireAndMessageNotFound:
-            return "NSE will expire and message not found"
+            "NSE will expire and message not found"
         }
     }
 }
