8818b54ab43aa81f49fc4777a30f9c54baf501ea
Mauritz Mollentze <mmo@mega.co.nz>
2024-08-13 16:01:12 +1200

    CC-7875: Remove copyright check when sharing a folder

--- iMEGA/Home/Scenes/Home/Router/NodeShareRouter.swift
+++ iMEGA/Home/Scenes/Home/Router/NodeShareRouter.swift
@@ -1,9 +1,5 @@
-import Foundation
 import MEGADomain
-import MEGAPresentation
-import MEGASDKRepo
-import MessageKit
-import SwiftUI
+import UIKit
 
 struct NodeShareRouter {
 
@@ -29,12 +25,8 @@ struct NodeShareRouter {
     func showSharingFolders(for nodes: [MEGANode]) {
         guard let viewController = viewController else { return }
         BackupNodesValidator(presenter: viewController, nodes: nodes.toNodeEntities()).showWarningAlertIfNeeded {
-            let viewControllerToPresent = if DIContainer.featureFlagProvider.isFeatureFlagEnabled(for: .hiddenNodes) {
-                copyrightWrappedShareFoldersViewController(nodes: nodes)
-            } else {
-                makeContactsShareFoldersViewController(nodes: nodes)
-            }
-            viewController.present(viewControllerToPresent, animated: true, completion: nil)
+            viewController.present(makeContactsShareFoldersViewController(nodes: nodes),
+                                   animated: true, completion: nil)
         }
     }
     
@@ -80,23 +72,5 @@ struct NodeShareRouter {
         contactsVC.contactsMode = .shareFoldersWith
         contactsVC.nodesArray = nodes
         return MEGANavigationController(rootViewController: contactsVC)
-    }
-    
-    private func copyrightWrappedShareFoldersViewController(nodes: [MEGANode]) -> UIViewController {
-        let copyrightUseCase = CopyrightUseCase(
-            shareUseCase: ShareUseCase(
-                shareRepository: ShareRepository.newRepo,
-                filesSearchRepository: FilesSearchRepository.newRepo,
-                nodeRepository: NodeRepository.newRepo),
-            userAlbumRepository: UserAlbumRepository.newRepo)
-        let viewModel = EnforceCopyrightWarningViewModel(preferenceUseCase: PreferenceUseCase.default,
-                                                         copyrightUseCase: copyrightUseCase)
-        
-        let view = EnforceCopyrightWarningView(viewModel: viewModel) {
-            ContactsView(nodes: nodes,
-                         mode: .shareFoldersWith)
-            .ignoresSafeArea(edges: .bottom)
-        }
-        return UIHostingController(rootView: view)
     }
 }
