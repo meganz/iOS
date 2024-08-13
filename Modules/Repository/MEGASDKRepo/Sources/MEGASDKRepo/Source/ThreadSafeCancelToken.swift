df3532a138b5cfc4729516c3599bb840f20d1a25
Lancy Norbert Fernandes <lf@mega.co.nz>
2024-08-13 16:15:06 +1200

    SAO-1941: Adding Tests to ThreadSafeCancelToken

--- Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Source/ThreadSafeCancelToken.swift
+++ Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Source/ThreadSafeCancelToken.swift
@@ -3,19 +3,21 @@ import MEGASdk
 /// A thread-safe wrapper for MEGACancelToken that ensures cancellation is performed safely across multiple threads.
 public struct ThreadSafeCancelToken: @unchecked Sendable {
     /// The underlying cancellation token.
-    public let value = MEGACancelToken()
+    public let value: MEGACancelToken
 
     /// A private serial dispatch queue to ensure thread-safe operations on the cancellation token.
     private let queue = DispatchQueue(label: "ThreadSafeCancelTokenSerialQueue")
     
-    public init() { }
+    public init(value: MEGACancelToken = MEGACancelToken()) { 
+        self.value = value
+    }
 
     /// Cancels the underlying MEGACancelToken in a thread-safe manner.
     ///
     /// This method ensures that the cancellation token is only cancelled once, even if called from multiple threads.
-    func cancel() {
+    public func cancel() {
         queue.async {
-            if !value.isCancelled  {
+            if !value.isCancelled {
                 value.cancel()
             }
         }
