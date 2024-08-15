f9e1911659257e50351936831d4877ba543a47a0
Glazel Magpayo <gma@mega.co.nz>
2024-08-15 21:15:12 +1200

    T14796244 IOS-8131 Adopt Semantic Color for Group Chat Info page

--- iMEGA/My Account/Contacts/ContactDetailsViewController.m
+++ iMEGA/My Account/Contacts/ContactDetailsViewController.m
@@ -254,7 +254,7 @@ typedef NS_ENUM(NSUInteger, ContactDetailsRow) {
     ContactTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"ContactDetailsDefaultTypeID" forIndexPath:indexPath];
     cell.avatarImageView.image = [UIImage imageNamed:@"sharedFiles"];
     cell.nameLabel.text = LocalizedString(@"Shared Files", @"Header of block with all shared files in chat.");
-    cell.nameLabel.textColor = [self primaryTextColor];
+    cell.nameLabel.textColor = UIColor.primaryTextColor;;
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 
     return cell;
@@ -264,7 +264,7 @@ typedef NS_ENUM(NSUInteger, ContactDetailsRow) {
     ContactTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"ContactDetailsDefaultTypeID" forIndexPath:indexPath];
     cell.avatarImageView.image = [UIImage imageNamed:@"rename"];
     cell.nameLabel.text = self.userNickname.length == 0 ? LocalizedString(@"Set Nickname", @"Contact details screen: Set the alias(nickname) for a user") : LocalizedString(@"Edit Nickname", @"Contact details screen: Edit the alias(nickname) for a user");
-    cell.nameLabel.textColor = [self primaryTextColor];
+    cell.nameLabel.textColor = UIColor.primaryTextColor;
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     
     return cell;
@@ -274,7 +274,7 @@ typedef NS_ENUM(NSUInteger, ContactDetailsRow) {
     ContactTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"ContactDetailsVerifyCredentialsTypeID" forIndexPath:indexPath];
     cell.avatarImageView.image = [UIImage imageNamed:@"verifyCredentials"];
     cell.nameLabel.text = LocalizedString(@"verifyCredentials", @"Title for a section on the fingerprint warning dialog. Below it is a button which will allow the user to verify their contact's fingerprint credentials.");
-    cell.nameLabel.textColor = [self primaryTextColor];
+    cell.nameLabel.textColor = UIColor.primaryTextColor;
     cell.permissionsImageView.hidden = !self.areCredentialsVerified;
     return cell;
 }
@@ -331,9 +331,9 @@ typedef NS_ENUM(NSUInteger, ContactDetailsRow) {
 - (ContactTableViewCell *)cellForArchiveChatWithIndexPath:(NSIndexPath *)indexPath {
     ContactTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"ContactDetailsDefaultTypeID" forIndexPath:indexPath];
     cell.avatarImageView.image = self.chatRoom.isArchived ? [UIImage imageNamed:@"unArchiveChat"] : [UIImage imageNamed:@"archiveChat"];
-    cell.avatarImageView.tintColor = self.chatRoom.isArchived ? [self redIconColor] : [UIColor mnz_primaryGrayForTraitCollection:self.traitCollection];
+    cell.avatarImageView.tintColor = [self primaryIconColor];
     cell.nameLabel.text = self.chatRoom.isArchived ? LocalizedString(@"unarchiveChat", @"The title of the dialog to unarchive an archived chat.") : LocalizedString(@"archiveChat", @"Title of button to archive chats.");
-    cell.nameLabel.textColor = self.chatRoom.isArchived ? [self redTextColor] : [self primaryTextColor];
+    cell.nameLabel.textColor = UIColor.primaryTextColor;
     cell.userInteractionEnabled = cell.avatarImageView.userInteractionEnabled = cell.nameLabel.enabled = MEGAReachabilityManager.isReachable && [MEGAChatSdk.shared chatConnectionState:self.chatRoom.chatId] == MEGAChatConnectionOnline;
     
     return cell;
