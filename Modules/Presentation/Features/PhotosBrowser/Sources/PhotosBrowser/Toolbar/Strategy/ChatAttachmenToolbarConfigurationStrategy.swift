a5a24684222e1ccf400da4f20ed760e95ed42e95
Harry Yan <hy@mega.co.nz>
2024-08-15 14:12:00 +1200

    IOS-8852: Refactor PhotosBrowser

--- Modules/Presentation/Features/PhotosBrowser/Sources/PhotosBrowser/Toolbar/Strategy/ChatAttachmenToolbarConfigurationStrategy.swift
+++ Modules/Presentation/Features/PhotosBrowser/Sources/PhotosBrowser/Toolbar/Strategy/ChatAttachmenToolbarConfigurationStrategy.swift
@@ -1,13 +1,19 @@
+import MEGAAssets
 import UIKit
 
 struct ChatAttachmenToolbarConfigurationStrategy: ToolbarConfigurationStrategy {
-    func configure(toolbar: UIToolbar, with images: [UIImage], in viewController: PhotosBrowserViewController) {
+    func configure(toolbar: UIToolbar, in viewController: PhotosBrowserViewController) {
         // Placeholder
-        let actions: [UIAction] = [UIAction { [weak viewController] _ in viewController?.didTapAllMedia() },
-                                   UIAction { [weak viewController] _ in viewController?.didTapSlideShow() },
-                                   UIAction {[weak viewController] _ in viewController?.didTapExport() }
+        let images: [UIImage] = [
+            MEGAAssetsImageProvider.image(named: "thumbnailsThin"),
+            MEGAAssetsImageProvider.image(named: "export")
+        ].compactMap { $0 }
+        
+        let actions: [UIAction] = [
+            UIAction { [weak viewController] _ in viewController?.didTapAllMedia() },
+            UIAction {[weak viewController] _ in viewController?.didTapExport() }
         ]
         
-        toolbar.configure(with: images, actions: actions, target: viewController)
+        toolbar.configure(with: images, actions: actions)
     }
 }
--- Modules/Presentation/Features/PhotosBrowser/Sources/PhotosBrowser/Toolbar/Strategy/CloudDriveToolbarConfigurationStrategy.swift
+++ Modules/Presentation/Features/PhotosBrowser/Sources/PhotosBrowser/Toolbar/Strategy/CloudDriveToolbarConfigurationStrategy.swift
@@ -1,12 +1,20 @@
+import MEGAAssets
 import UIKit
 
 struct CloudDriveToolbarConfigurationStrategy: ToolbarConfigurationStrategy {
-    func configure(toolbar: UIToolbar, with images: [UIImage], in viewController: PhotosBrowserViewController) {
-        let actions: [UIAction] = [UIAction { [weak viewController] _ in viewController?.didTapAllMedia() },
-                                   UIAction { [weak viewController] _ in viewController?.didTapSlideShow() },
-                                   UIAction {[weak viewController] _ in viewController?.didTapExport() }
+    func configure(toolbar: UIToolbar, in viewController: PhotosBrowserViewController) {
+        let images: [UIImage] = [
+            MEGAAssetsImageProvider.image(named: "thumbnailsThin"),
+            UIImage(systemName: "play.rectangle"),
+            MEGAAssetsImageProvider.image(named: "export")
+        ].compactMap { $0 }
+        
+        let actions: [UIAction] = [
+            UIAction { [weak viewController] _ in viewController?.didTapAllMedia() },
+            UIAction { [weak viewController] _ in viewController?.didTapSlideShow() },
+            UIAction {[weak viewController] _ in viewController?.didTapExport() }
         ]
         
-        toolbar.configure(with: images, actions: actions, target: viewController)
+        toolbar.configure(with: images, actions: actions)
     }
 }
