9e22ec4e0c68c835638a90ff098941a93860c02d
Javier Navarro <jn@mega.co.nz>
2024-08-26 20:17:06 +1200

    CHT-921: Static analyser issues in MEGAChatScheduledMeetingOccurrence

--- Modules/Repository/ChatRepo/Sources/ChatRepo/Mappers/ScheduledMeeting/ScheduledMeetingOccurrenceEntity+Mapper.swift
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/Mappers/ScheduledMeeting/ScheduledMeetingOccurrenceEntity+Mapper.swift
@@ -14,7 +14,7 @@ fileprivate extension ScheduledMeetingOccurrenceEntity {
             scheduledId: scheduledMeetingOccurrence.scheduledId,
             parentScheduledId: scheduledMeetingOccurrence.parentScheduledId,
             overrides: scheduledMeetingOccurrence.overrides,
-            timezone: scheduledMeetingOccurrence.timezone,
+            timezone: scheduledMeetingOccurrence.timezone ?? "Pacific/Auckland",
             startDate: Date(timeIntervalSince1970: TimeInterval(scheduledMeetingOccurrence.startDateTime)), endDate: Date(timeIntervalSince1970: TimeInterval(scheduledMeetingOccurrence.endDateTime))
         )
     }
