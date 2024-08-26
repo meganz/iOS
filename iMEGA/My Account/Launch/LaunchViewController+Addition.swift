0edd9c869d86092d7958a38d178d3e6bc1bab836
Banghua Zhao <bz@mega.co.nz>
2024-08-26 16:33:50 +1200

    T17552980: Adopt Semantic Color Token for the Activity Indicator in the onboarding screen

---
+++ iMEGA/My Account/Launch/LaunchViewController+Addition.swift
@@ -0,0 +1,7 @@
+import MEGADesignToken
+
+extension LaunchViewController {
+    @objc func setupActivityIndicatorColor() {
+        activityIndicatorView.color = TokenColors.Icon.secondary
+    }
+}
