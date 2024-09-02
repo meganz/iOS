498b07e49986ad90f0452a6d3dfae0601aea67c3
Tomek Kuźma <tku@mega.co.nz>
2024-09-03 02:18:46 +1200

    MEET-4054: Fix clipped text, and background color mismatch

--- iMEGA/ChatRoomsListScene/Views/NewChatEmptyView/NewChatEmptyView.swift
+++ iMEGA/ChatRoomsListScene/Views/NewChatEmptyView/NewChatEmptyView.swift
@@ -3,23 +3,36 @@ import MEGASwift
 import SwiftUI
 
 struct NewChatRoomsEmptyView: View {
-    let state: ChatRoomsEmptyViewState
+    var state: ChatRoomsEmptyViewState
+    var maxHeight: CGFloat = 50
     
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     
     var body: some View {
-        VStack(spacing: 24) {
-            Spacer()
-                .frame(maxHeight: 100)
+        VStack(
+            spacing: 0
+        ) {
+            // making sure description is not clipped in:
+            // 1. iPhone landscape
+            // 2. ContactsViewController table footer view
+            if maxHeight > 0 && verticalSizeClass != .compact {
+                Spacer()
+                    .frame(maxHeight: maxHeight)
+            } else {
+                Spacer()
+            }
             
-            NewChatRoomsEmptyCenterView(state: state.center)
-                .padding(.leading, 30)
-                .padding(.trailing, 30)
-                .frame(maxWidth: horizontalSizeClass == .regular ? 400 : .infinity)
-            
-            AdaptiveStack(spacing: 24) {
-                ForEach(state.bottomButtons) { button in
-                    MenuCapableButton(state: button)
+            VStack(spacing: 24) {
+                NewChatRoomsEmptyCenterView(state: state.center)
+                    .padding(.leading, 30)
+                    .padding(.trailing, 30)
+                    .frame(maxWidth: horizontalSizeClass == .regular ? 400 : .infinity)
+                
+                AdaptiveStack(spacing: 24) {
+                    ForEach(state.bottomButtons) { button in
+                        MenuCapableButton(state: button)
+                    }
                 }
             }
             
