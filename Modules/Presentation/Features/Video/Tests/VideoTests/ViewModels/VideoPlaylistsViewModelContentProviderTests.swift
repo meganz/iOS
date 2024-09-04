b7b0066bde0fdfa7c2a1cccac38857f5c2515344
Arifin Firdaus <af@mega.co.nz>
2024-09-04 17:11:01 +0700

    T17120569: CC-6629 Fix The Favourites playlist is also affected by the order changes

--- Modules/Presentation/Features/Video/Tests/VideoTests/ViewModels/VideoPlaylistsViewModelContentProviderTests.swift
+++ Modules/Presentation/Features/Video/Tests/VideoTests/ViewModels/VideoPlaylistsViewModelContentProviderTests.swift
@@ -6,16 +6,20 @@ import XCTest
 final class VideoPlaylistsViewModelContentProviderTests: XCTestCase {
     
     func testLoadVideoPlaylists_returnsCorrectOrderPlaylists() async throws {
-        let playlists = [
-            VideoPlaylistEntity(id: 1, name: "Favorites", count: 0, type: .favourite, creationTime: Date(), modificationTime: Date()),
-            VideoPlaylistEntity(id: 2, name: "Other Favorites", count: 0, type: .favourite, creationTime: Date().addingTimeInterval(60), modificationTime: Date().addingTimeInterval(60))
+        let systemVideoPlaylists = [ VideoPlaylistEntity(id: 1, name: "Favorites", count: 0, type: .favourite, creationTime: Date(), modificationTime: Date()) ]
+        let userVideoPlaylists = [
+            VideoPlaylistEntity(id: 2, name: "User playlist 1", count: 0, type: .user, creationTime: Date().addingTimeInterval(60), modificationTime: Date().addingTimeInterval(60)),
+            VideoPlaylistEntity(id: 3, name: "User playlist 2", count: 0, type: .user, creationTime: Date().addingTimeInterval(120), modificationTime: Date().addingTimeInterval(120))
         ]
-        let videoPlaylistsUseCase = MockVideoPlaylistUseCase(userVideoPlaylistsResult: playlists)
+        let videoPlaylistsUseCase = MockVideoPlaylistUseCase(
+            systemVideoPlaylistsResult: systemVideoPlaylists,
+            userVideoPlaylistsResult: userVideoPlaylists
+        )
         let sut = sut(videoPlaylistsUseCase: videoPlaylistsUseCase)
-    
+        
         let result = try await sut.loadVideoPlaylists(searchText: "", sortOrder: .modificationDesc)
         
-        XCTAssertEqual(playlists.reversed(), result)
+        XCTAssertEqual(result.map(\.id), systemVideoPlaylists.map(\.id) + userVideoPlaylists.reversed().map(\.id))
     }
     
     func testLoadVideoPlaylists_withSearchText_returnsCorrectOrderedAndFilteredPlaylists() async throws {
