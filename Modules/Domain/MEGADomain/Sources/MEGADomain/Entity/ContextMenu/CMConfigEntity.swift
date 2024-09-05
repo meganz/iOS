79bf727b2a22939b6472bd4bf04ae0dbea2ee91d
Tomek Kuźma <tku@mega.co.nz>
2024-09-06 00:49:06 +1200

    MEET-4055: New Meetings Empty Screen

--- Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/ContextMenu/CMConfigEntity.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/ContextMenu/CMConfigEntity.swift
@@ -90,7 +90,54 @@ public struct CMConfigEntity: Sendable {
     public var isHidden: Bool?
     public var isCameraUploadsEnabled: Bool
 
-    public init(menuType: CMElementTypeEntity, viewMode: ViewModePreferenceEntity? = nil, accessLevel: ShareAccessLevelEntity? = nil, sortType: SortOrderEntity? = nil, filterType: FilterEntity? = nil, isAFolder: Bool = false, isRubbishBinFolder: Bool = false, isViewInFolder: Bool = false, isRestorable: Bool = false, isInVersionsView: Bool = false, isOfflineFolder: Bool = false, isSharedItems: Bool = false, isIncomingShareChild: Bool = false, isHome: Bool = false, isFavouritesExplorer: Bool = false, isDocumentExplorer: Bool = false, isAudiosExplorer: Bool = false, isVideosExplorer: Bool = false, isVideosRevampExplorer: Bool = false, isVideosRevampExplorerVideoPlaylists: Bool = false, isVideoPlaylistContent: Bool = false, isCameraUploadExplorer: Bool = false, albumType: AlbumEntityType? = nil, isFilterEnabled: Bool = false, isDoNotDisturbEnabled: Bool = false, isBackupsRootNode: Bool = false, isBackupsChild: Bool = false, isSelectHidden: Bool = false, isShareAvailable: Bool = false, isSharedItemsChild: Bool = false, isOutShare: Bool = false, isExported: Bool = false, isEmptyState: Bool = false, timeRemainingToDeactiveDND: String? = nil, versionsCount: Int = 0, showMediaDiscovery: Bool = false, chatStatus: ChatStatusEntity = .invalid, shouldStartMeeting: Bool = false, shouldJoiningMeeting: Bool = false, shouldScheduleMeeting: Bool = false, sharedLinkStatus: SharedLinkStatusEntity? = nil, isArchivedChatsVisible: Bool = false, isMediaFile: Bool = false, isFilterActive: Bool = false, isHidden: Bool? = nil, isCameraUploadsEnabled: Bool = false) {
+    public init(
+        menuType: CMElementTypeEntity,
+        viewMode: ViewModePreferenceEntity? = nil,
+        accessLevel: ShareAccessLevelEntity? = nil,
+        sortType: SortOrderEntity? = nil,
+        filterType: FilterEntity? = nil,
+        isAFolder: Bool = false,
+        isRubbishBinFolder: Bool = false,
+        isViewInFolder: Bool = false,
+        isRestorable: Bool = false,
+        isInVersionsView: Bool = false,
+        isOfflineFolder: Bool = false,
+        isSharedItems: Bool = false,
+        isIncomingShareChild: Bool = false,
+        isHome: Bool = false,
+        isFavouritesExplorer: Bool = false,
+        isDocumentExplorer: Bool = false,
+        isAudiosExplorer: Bool = false,
+        isVideosExplorer: Bool = false,
+        isVideosRevampExplorer: Bool = false,
+        isVideosRevampExplorerVideoPlaylists: Bool = false,
+        isVideoPlaylistContent: Bool = false,
+        isCameraUploadExplorer: Bool = false,
+        albumType: AlbumEntityType? = nil,
+        isFilterEnabled: Bool = false,
+        isDoNotDisturbEnabled: Bool = false,
+        isBackupsRootNode: Bool = false,
+        isBackupsChild: Bool = false,
+        isSelectHidden: Bool = false,
+        isShareAvailable: Bool = false,
+        isSharedItemsChild: Bool = false,
+        isOutShare: Bool = false,
+        isExported: Bool = false,
+        isEmptyState: Bool = false,
+        timeRemainingToDeactiveDND: String? = nil,
+        versionsCount: Int = 0,
+        showMediaDiscovery: Bool = false,
+        chatStatus: ChatStatusEntity = .invalid,
+        shouldStartMeeting: Bool = false,
+        shouldJoiningMeeting: Bool = false,
+        shouldScheduleMeeting: Bool = false,
+        sharedLinkStatus: SharedLinkStatusEntity? = nil,
+        isArchivedChatsVisible: Bool = false,
+        isMediaFile: Bool = false,
+        isFilterActive: Bool = false,
+        isHidden: Bool? = nil,
+        isCameraUploadsEnabled: Bool = false
+    ) {
         self.menuType = menuType
         self.viewMode = viewMode
         self.accessLevel = accessLevel
