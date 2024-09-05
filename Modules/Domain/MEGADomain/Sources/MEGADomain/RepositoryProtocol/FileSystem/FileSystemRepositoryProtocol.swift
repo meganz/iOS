64e948d1b9eaefc463ad1077382e79bd06f1e697
Javier Navarro <jn@mega.co.nz>
2024-09-05 19:56:12 +1200

    IOS-8930: Hang at OfflineUseCase.removeItem

--- Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/FileSystem/FileSystemRepositoryProtocol.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/FileSystem/FileSystemRepositoryProtocol.swift
@@ -6,6 +6,7 @@ public protocol FileSystemRepositoryProtocol: RepositoryProtocol, Sendable {
     func moveFile(at sourceURL: URL, to destinationURL: URL) -> Bool
     func copyFile(at sourceURL: URL, to destinationURL: URL) -> Bool
     func removeItem(at url: URL) throws
+    func removeItem(at url: URL) async throws
     func fileSize(at url: URL) -> UInt64?
     func fileCreationDate(at url: URL) -> Date?
     func relativePathToDocumentsDirectory(for url: URL) -> String
