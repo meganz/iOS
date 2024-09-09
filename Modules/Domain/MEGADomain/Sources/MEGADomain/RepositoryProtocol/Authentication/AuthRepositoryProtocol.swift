cc16db4d9a4e4023871add150598a6322a941c2f
thl <thl@mega.co.nz>
2024-09-09 19:05:22 +0700

    IOS-8927: Fix data race risk in VerificationCodeViewModel

--- Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Authentication/AuthRepositoryProtocol.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Authentication/AuthRepositoryProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public protocol AuthRepositoryProtocol: RepositoryProtocol {
+public protocol AuthRepositoryProtocol: RepositoryProtocol, Sendable {
     func logout()
     func login(sessionId: String) async throws
 }
