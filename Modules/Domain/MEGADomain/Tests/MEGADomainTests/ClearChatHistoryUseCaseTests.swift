91fc61a8f06624c1fabd0b9c8cd97a954c0d4910
Thien Liu <thl@mega.co.nz>
2024-09-11 22:23:55 +1200

    IOS-8760: Get rid of closures in favor of async/await in ManageChatHistoryRepositoryProtocol

--- Modules/Domain/MEGADomain/Tests/MEGADomainTests/ClearChatHistoryUseCaseTests.swift
+++ Modules/Domain/MEGADomain/Tests/MEGADomainTests/ClearChatHistoryUseCaseTests.swift
@@ -3,72 +3,63 @@ import MEGADomainMock
 import XCTest
 
 final class ClearChatHistoryUseCaseTests: XCTestCase {
-    func test_clearChatHistory_success() {
+    func test_clearChatHistory_success() async {
         let repo = MockManageChatHistoryRepository(clearChatHistory: .success)
         let sut = ClearChatHistoryUseCase(repository: repo)
-        sut.clearChatHistory(for: 123) { result in
-            switch result {
-            case .success:
-                XCTAssertTrue(true)
-            case .failure:
-                XCTFail("errors are not expected!")
-            }
+        do {
+            try await sut.clearChatHistory(for: 123)
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("errors are not expected!")
         }
     }
     
-    func test_clearChatHistory_errorGeneric() {
+    func test_clearChatHistory_errorGeneric() async {
         let mockError: ManageChatHistoryErrorEntity = .generic
         let repo = MockManageChatHistoryRepository()
         let sut = ClearChatHistoryUseCase(repository: repo)
-        sut.clearChatHistory(for: 123) { result in
-            switch result {
-            case .success:
-                XCTFail("error \(mockError) is expected!")
-            case .failure(let error):
-                XCTAssertEqual(mockError, error)
-            }
+        do {
+            try await sut.clearChatHistory(for: 123)
+            XCTFail("error \(mockError) is expected!")
+        } catch {
+            XCTAssertEqual(mockError, error as? ManageChatHistoryErrorEntity)
         }
     }
     
-    func test_clearChatHistory_error_chatIdInvalid () {
+    func test_clearChatHistory_error_chatIdInvalid() async {
         let mockError: ManageChatHistoryErrorEntity = .chatIdInvalid
         let repo = MockManageChatHistoryRepository(clearChatHistory: .failure(.chatIdInvalid))
         let sut = ClearChatHistoryUseCase(repository: repo)
-        sut.clearChatHistory(for: 123) { result in
-            switch result {
-            case .success:
-                XCTFail("error \(mockError) is expected!")
-            case .failure(let error):
-                XCTAssertEqual(mockError, error)
-            }
+        
+        do {
+            try await sut.clearChatHistory(for: 123)
+            XCTFail("error \(mockError) is expected!")
+        } catch {
+            XCTAssertEqual(mockError, error as? ManageChatHistoryErrorEntity)
         }
     }
     
-    func test_clearChatHistory_error_chatIdDoesNotExist () {
+    func test_clearChatHistory_error_chatIdDoesNotExist() async {
         let mockError: ManageChatHistoryErrorEntity = .chatIdDoesNotExist
         let repo = MockManageChatHistoryRepository(clearChatHistory: .failure(.chatIdDoesNotExist))
         let sut = ClearChatHistoryUseCase(repository: repo)
-        sut.clearChatHistory(for: 123) { result in
-            switch result {
-            case .success:
-                XCTFail("error \(mockError) is expected!")
-            case .failure(let error):
-                XCTAssertEqual(mockError, error)
-            }
+        do {
+            try await sut.clearChatHistory(for: 123)
+            XCTFail("error \(mockError) is expected!")
+        } catch {
+            XCTAssertEqual(mockError, error as? ManageChatHistoryErrorEntity)
         }
     }
     
-    func test_clearChatHistory_error_notEnoughPrivileges () {
+    func test_clearChatHistory_error_notEnoughPrivileges() async {
         let mockError: ManageChatHistoryErrorEntity = .notEnoughPrivileges
         let repo = MockManageChatHistoryRepository(clearChatHistory: .failure(.notEnoughPrivileges))
         let sut = ClearChatHistoryUseCase(repository: repo)
-        sut.clearChatHistory(for: 123) { result in
-            switch result {
-            case .success:
-                XCTFail("error \(mockError) is expected!")
-            case .failure(let error):
-                XCTAssertEqual(mockError, error)
-            }
+        do {
+            try await sut.clearChatHistory(for: 123)
+            XCTFail("error \(mockError) is expected!")
+        } catch {
+            XCTAssertEqual(mockError, error as? ManageChatHistoryErrorEntity)
         }
     }
 }
--- Modules/Repository/ChatRepo/Sources/ChatRepo/Repository/Chat/ManageChatHistoryRepository.swift
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/Repository/Chat/ManageChatHistoryRepository.swift
@@ -1,6 +1,7 @@
 import Foundation
 import MEGAChatSdk
 import MEGADomain
+import MEGASwift
 
 public struct ManageChatHistoryRepository: ManageChatHistoryRepositoryProtocol {
     private let chatSdk: MEGAChatSdk
@@ -8,36 +9,40 @@ public struct ManageChatHistoryRepository: ManageChatHistoryRepositoryProtocol {
     public init(chatSdk: MEGAChatSdk) {
         self.chatSdk = chatSdk
     }
-
-    public func chatRetentionTime(for chatId: ChatIdEntity, completion: @escaping (Result<UInt, ManageChatHistoryErrorEntity>) -> Void) {
+    
+    public func chatRetentionTime(for chatId: ChatIdEntity) async throws -> UInt {
         if let chatRoom = chatSdk.chatRoom(forChatId: chatId) {
-            completion(.success(chatRoom.retentionTime))
+            return chatRoom.retentionTime
         } else {
-            completion(.failure(.generic))
+            throw ManageChatHistoryErrorEntity.generic
         }
     }
     
-    public func setChatRetentionTime(for chatId: ChatIdEntity, period: UInt, completion: @escaping (Result<UInt, ManageChatHistoryErrorEntity>) -> Void) {
-        chatSdk.setChatRetentionTime(chatId, period: period, delegate: ChatRequestDelegate { result in
-            switch result {
-            case .success(let request):
-                let requestPeriod = UInt(truncatingIfNeeded: request.number)
-                completion(.success(requestPeriod))
-            case .failure(let error):
-                completion(.failure(error.toManageChatHistoryErrorEntity()))
-            }
-        })
+    public func setChatRetentionTime(for chatId: ChatIdEntity, period: UInt) async throws -> UInt {
+        try await withAsyncThrowingValue { completion in
+            chatSdk.setChatRetentionTime(chatId, period: period, delegate: ChatRequestDelegate { result in
+                switch result {
+                case .success(let request):
+                    let requestPeriod = UInt(truncatingIfNeeded: request.number)
+                    completion(.success(requestPeriod))
+                case .failure(let error):
+                    completion(.failure(error.toManageChatHistoryErrorEntity()))
+                }
+            })
+        }
     }
     
-    public func clearChatHistory(for chatId: ChatIdEntity, completion: @escaping (Result<Void, ManageChatHistoryErrorEntity>) -> Void) {
-        chatSdk.clearChatHistory(chatId, delegate: ChatRequestDelegate { request in
-            switch request {
-            case .success:
-                completion(.success(()))
-            case .failure(let error):
-                completion(.failure(error.toManageChatHistoryErrorEntity()))
-            }
-        })
+    public func clearChatHistory(for chatId: ChatIdEntity) async throws {
+        try await withAsyncThrowingValue { completion in
+            chatSdk.clearChatHistory(chatId, delegate: ChatRequestDelegate { request in
+                switch request {
+                case .success:
+                    completion(.success(()))
+                case .failure(let error):
+                    completion(.failure(error.toManageChatHistoryErrorEntity()))
+                }
+            })
+        }
     }
 }
 
