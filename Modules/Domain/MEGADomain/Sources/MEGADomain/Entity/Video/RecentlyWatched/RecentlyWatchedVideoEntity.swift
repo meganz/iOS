e1b37082e3fc432050bef06b26a81bd95899fb98
Arifin Firdaus <af@mega.co.nz>
2024-09-19 10:31:30 +0700

    CC-8145: iOS - Dev - Adjust new property for RecentlyWatchedVideo

--- Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/Video/RecentlyWatched/RecentlyWatchedVideoEntity.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/Video/RecentlyWatched/RecentlyWatchedVideoEntity.swift
@@ -3,15 +3,15 @@ import Foundation
 public struct RecentlyWatchedVideoEntity: Sendable, Equatable {
     public let video: NodeEntity
     public let lastWatchedDate: Date?
-    public let lastWatchedTimestamp: Date
+    public let mediaDestination: MediaDestinationEntity?
     
     public init(
         video: NodeEntity,
         lastWatchedDate: Date?,
-        lastWatchedTimestamp: Date
+        mediaDestination: MediaDestinationEntity?
     ) {
         self.video = video
         self.lastWatchedDate = lastWatchedDate
-        self.lastWatchedTimestamp = lastWatchedTimestamp
+        self.mediaDestination = mediaDestination
     }
 }
