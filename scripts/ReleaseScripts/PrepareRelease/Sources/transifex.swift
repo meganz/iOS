9950a53b659a760d5975e68c414ea0502ddcfab1
João Pedro de Amorim <jpa@mega.co.nz>
2024-09-24 00:49:37 -0300

    IOS-8984: Add new transifex pruning to PrepareRelease

--- scripts/ReleaseScripts/PrepareRelease/Sources/transifex.swift
+++ scripts/ReleaseScripts/PrepareRelease/Sources/transifex.swift
@@ -4,6 +4,7 @@ private let pathToTransifexScript = "./iosTransifex/iosTransifex.py"
 
 func pruneTransifexStrings() throws {
     try runInShell("\(pathToTransifexScript) -m clean")
+    try runInShell("\(pathToTransifexScript) -m clean -r lib")
     try runInShell("\(pathToTransifexScript) -m lang -r Localizable")
 }
 
