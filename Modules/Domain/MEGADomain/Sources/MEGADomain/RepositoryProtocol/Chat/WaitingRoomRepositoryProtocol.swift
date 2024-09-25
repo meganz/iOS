69c92467b4df7904a82b6294266cb42742dc1252
Javier Navarro <jn@mega.co.nz>
2024-09-25 17:31:39 +0200

    IOS-8967: Remove WaitingRoomUseCase and WaitingRoomRepository

--- Modules/Domain/MEGADomain/Sources/MEGADomain/RepositoryProtocol/Chat/WaitingRoomRepositoryProtocol.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public protocol WaitingRoomRepositoryProtocol: RepositoryProtocol, Sendable {
-    func userName() -> String
-    func joinChat(forChatId chatId: UInt64, userHandle: UInt64) async throws -> ChatRoomEntity
-}
