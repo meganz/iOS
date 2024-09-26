9c81d985bbb70c1772ba5dae34340af23de364bf
Javier Navarro <jn@mega.co.nz>
2024-09-26 14:39:02 +0200

    IOS-9045: Review traitCollectionDidChange in Home -> Recents folder

--- iMEGA/Home/Recents/RecentsTableViewHeaderFooterView.m
+++ iMEGA/Home/Recents/RecentsTableViewHeaderFooterView.m
@@ -6,30 +6,15 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-
-    [self setupWithTrait:self.traitCollection];
+    
+    [self configureTokenColors];
     
     self.bottomSeparatorView.layer.borderWidth = 0.5;
 }
 
 - (void)prepareForReuse {
     [super prepareForReuse];
-
-    [self setupWithTrait:self.traitCollection];
-}
-
-#pragma mark - Private
-
-- (void)setupWithTrait:(UITraitCollection *)trait {
     [self configureTokenColors];
-}
-
-- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
-    [super traitCollectionDidChange:previousTraitCollection];
-    
-    if ([self.traitCollection hasDifferentColorAppearanceComparedToTraitCollection:previousTraitCollection]) {
-        [self setupWithTrait:self.traitCollection];
-    }
 }
 
 @end
