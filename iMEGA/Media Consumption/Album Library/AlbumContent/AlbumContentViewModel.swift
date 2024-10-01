2fbd627b131141cf6bc0af906bab84cf303be905
Devyish Gandhi <dg@mega.co.nz>
2024-10-01 13:08:27 +1300

    T18292507 - CC-8075: Ensure that Right Navigation bar updates on editing mode changes

--- iMEGA/Media Consumption/Album Library/AlbumContent/AlbumContentViewModel.swift
+++ iMEGA/Media Consumption/Album Library/AlbumContent/AlbumContentViewModel.swift
@@ -138,7 +138,6 @@ final class AlbumContentViewModel: ViewModelType {
         case .deleteAlbum:
             deleteAlbum()
         case .configureContextMenu(let isSelectHidden):
-            guard isPhotoSelectionHidden != isSelectHidden else { return }
             isPhotoSelectionHidden = isSelectHidden
             updateRightBarButtons()
         case .shareLink:
