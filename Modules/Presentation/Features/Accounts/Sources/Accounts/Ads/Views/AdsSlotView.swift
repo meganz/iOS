52a8bc6bc31d13e12af88e535e5d27ecc21fd92e
Glazel Magpayo <gma@mega.co.nz>
2024-10-08 19:09:06 +1300

    AP-2053 - T18387123 and T18387146 Fix ads with keyboard and refresh upon purchase

--- Modules/Presentation/Features/Accounts/Sources/Accounts/Ads/Views/AdsSlotView.swift
+++ Modules/Presentation/Features/Accounts/Sources/Accounts/Ads/Views/AdsSlotView.swift
@@ -4,8 +4,8 @@ import MEGASwiftUI
 import SwiftUI
 
 /// AdsSlotView is a view that displays the main content of the app, with an optional banner ad positioned at the bottom of the screen.
-/// `shouldContainAds`: This checks whether ads should be displayed on the view. It evaluates both the current verticalSizeClass and the state of ad enablement. VerticalSizeClass should be `.regular` only, applicable for both iPhone and iPad.
-/// `displayAds`: This determines if ads should be hidden. Even when shouldContainAds returns true, ads might still be hidden if the contentView is navigated to a screen where ads should not be displayed, such as the "Chats" or "Shared Items" tabs. For instance, when a user navigates to the "Account" page from the Home, Photos or Cloud drive tab, the ad will be hidden.
+/// `shouldContainAds`: This checks whether ads should be displayed on a tab such as Home, Photos and Cloud Drive tab. Chat and Shared Item tab will not contain ads. It evaluates both the current verticalSizeClass and the state of ad enablement. VerticalSizeClass should be `.regular` only, applicable for both iPhone and iPad.
+/// `displayAds`: This determines if ads should be hidden. Even when shouldContainAds returns true, ads might still be hidden if the contentView is navigated to a screen where ads should not be displayed. For instance, when a user navigates to the "Account" page from the Home, Photos or Cloud drive tab, the ad will be hidden.
 public struct AdsSlotView<T: View>: View {
     @Environment(\.verticalSizeClass) var verticalSizeClass
     @StateObject var viewModel: AdsSlotViewModel
@@ -33,9 +33,8 @@ public struct AdsSlotView<T: View>: View {
         .onAppear {
             viewModel.setupSubscriptions()
         }
-        .ignoresSafeArea(
-            edges: shouldContainAds && viewModel.displayAds ? [.top] : .all
-        )
+        .ignoresSafeArea(.keyboard)
+        .ignoresSafeArea(edges: shouldContainAds && viewModel.displayAds ? [.top] : .all)
     }
     
     private var shouldContainAds: Bool {
