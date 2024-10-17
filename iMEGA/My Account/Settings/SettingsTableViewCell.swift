27cef2e0f26150e1201bb756a8e7628c40a82bd9
Glazel Magpayo <gma@mega.co.nz>
2024-10-17 10:25:07 +0800

    IOS-9225 Fix Delete Account button color on Settings

--- iMEGA/My Account/Settings/SettingsTableViewCell.swift
+++ iMEGA/My Account/Settings/SettingsTableViewCell.swift
@@ -12,7 +12,7 @@ class SettingsTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        displayValueLabel.textColor = UIColor.secondaryLabel
+
         setupColors()
     }
     
@@ -24,12 +24,15 @@ class SettingsTableViewCell: UITableViewCell {
         titleLabel.text = viewModel.title
         titleLabel.textAlignment = isDestructive ? .center : .left
         trailingIcon.isHidden = isDestructive
-        trailingIcon.tintColor = TokenColors.Icon.secondary
         displayValueLabel.text = viewModel.displayValue
         displayValueLabel.isHidden = viewModel.displayValue.isEmpty
+        
+        setupColors()
     }
     
     private func setupColors() {
+        displayValueLabel.textColor = UIColor.secondaryLabel
+        trailingIcon.tintColor = TokenColors.Icon.secondary
         titleLabel.textColor = isDestructive ? TokenColors.Text.error : TokenColors.Text.primary
     }
 }
