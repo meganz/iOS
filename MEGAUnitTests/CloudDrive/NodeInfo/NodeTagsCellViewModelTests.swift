4b47720be95dffbda76b21f565d109e1697b02a8
Lancy <lf@mega.co.nz>
2024-10-18 17:18:04 +1300

    SAO-2319: Node tags - Pro tag issue fix.

--- MEGAUnitTests/CloudDrive/NodeInfo/NodeTagsCellViewModelTests.swift
+++ MEGAUnitTests/CloudDrive/NodeInfo/NodeTagsCellViewModelTests.swift
@@ -9,19 +9,19 @@ struct NodeTagsCellViewModelTests {
     @Test(
         "Check if the Pro only tag needs to be shown",
         arguments: [
-            (proLevel: AccountTypeEntity.free, isExpiredAccount: true, result: true),
-            (proLevel: AccountTypeEntity.proI, isExpiredAccount: true, result: false),
-            (proLevel: AccountTypeEntity.proII, isExpiredAccount: true, result: false),
-            (proLevel: AccountTypeEntity.proIII, isExpiredAccount: true, result: false),
-            (proLevel: AccountTypeEntity.lite, isExpiredAccount: true, result: false),
+            (proLevel: AccountTypeEntity.free, isExpiredAccount: false, result: true),
+            (proLevel: AccountTypeEntity.proI, isExpiredAccount: false, result: false),
+            (proLevel: AccountTypeEntity.proII, isExpiredAccount: false, result: false),
+            (proLevel: AccountTypeEntity.proIII, isExpiredAccount: false, result: false),
+            (proLevel: AccountTypeEntity.lite, isExpiredAccount: false, result: false),
+            (proLevel: AccountTypeEntity.starter, isExpiredAccount: false, result: false),
+            (proLevel: AccountTypeEntity.basic, isExpiredAccount: false, result: false),
+            (proLevel: AccountTypeEntity.essential, isExpiredAccount: false, result: false),
+            (proLevel: AccountTypeEntity.feature, isExpiredAccount: false, result: false),
             (proLevel: AccountTypeEntity.business, isExpiredAccount: false, result: false),
             (proLevel: AccountTypeEntity.business, isExpiredAccount: true, result: false),
             (proLevel: AccountTypeEntity.proFlexi, isExpiredAccount: false, result: false),
-            (proLevel: AccountTypeEntity.proFlexi, isExpiredAccount: true, result: false),
-            (proLevel: AccountTypeEntity.starter, isExpiredAccount: true, result: false),
-            (proLevel: AccountTypeEntity.basic, isExpiredAccount: true, result: false),
-            (proLevel: AccountTypeEntity.essential, isExpiredAccount: true, result: false),
-            (proLevel: AccountTypeEntity.feature, isExpiredAccount: true, result: false)
+            (proLevel: AccountTypeEntity.proFlexi, isExpiredAccount: true, result: false)
         ]
     )
     func shouldShowProTag(
