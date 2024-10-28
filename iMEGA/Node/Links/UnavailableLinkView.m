3507a400ed5ea52797696d5f524b4268e0aa3c79
Javier Navarro <jn@mega.co.nz>
2024-10-28 13:19:14 +0100

    IOS-9074: Review traitCollectionDidChange in Node -> Links folder

--- iMEGA/Node/Links/UnavailableLinkView.m
+++ iMEGA/Node/Links/UnavailableLinkView.m
@@ -14,14 +14,6 @@
     [self updateAppearance];
 }
 
-- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
-    [super traitCollectionDidChange:previousTraitCollection];
-    
-    if ([self.traitCollection hasDifferentColorAppearanceComparedToTraitCollection:previousTraitCollection]) {
-        [self updateAppearance];
-    }
-}
-
 #pragma mark - Private
 
 - (void)updateAppearance {
--- iMEGA/Utils/Categories/UILabel+CustomNavigationTitleLabel.swift
+++ iMEGA/Utils/Categories/UILabel+CustomNavigationTitleLabel.swift
@@ -5,7 +5,7 @@ import MEGAUIKit
 extension UILabel {
     /// Create a 2-line Label to display title and subtitles in a NavigationBar whose color aligns with Design Token colors.
     @objc static func customNavigationBarLabel(title: String, subtitle: String?, traitCollection: UITraitCollection) -> UILabel {
-        let titleColor = UIColor.primaryTextColor()
+        let titleColor = TokenColors.Text.primary
         let subtitleColor = TokenColors.Text.secondary
         
         return customNavBarLabel(title: title, subtitle: subtitle, titleColor: titleColor, subtitleColor: subtitleColor)
