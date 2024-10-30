2f8387008901ede384f59e1ab12e9d8eb4fee4ea
Javier Navarro <jn@mega.co.nz>
2024-10-30 13:00:11 +0100

    IOS-9275: Update links for C++ third party libraries

--- Modules/DataSource/MEGASDK/Package.swift
+++ Modules/DataSource/MEGASDK/Package.swift
@@ -87,37 +87,37 @@ let package = Package(
         ),
         .binaryTarget(
             name: "libcryptopp",
-            url: "https://s3.g.s4.mega.io/010996547823786/ios-xcframeworks/libcryptopp_xcframework.zip",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libcryptopp_xcframework.zip",
             checksum: "250c07fe6c1f071ebf455f4e4d2b2d15a3cdbd2aebcf36e2fd9c68c0c58e08ef"
         ),
         .binaryTarget(
             name: "libmediainfo",
-            url: "https://s3.g.s4.mega.io/010996547823786/ios-xcframeworks/libmediainfo_xcframework.zip",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libmediainfo_xcframework.zip",
             checksum: "24fdbe1e3df3642799b1c890d5c810c88088bbe2a1d7bd34c766587f7405b0fa"
         ),
         .binaryTarget(
             name: "libuv",
-            url: "https://s3.g.s4.mega.io/010996547823786/ios-xcframeworks/libuv_xcframework.zip",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libuv_xcframework.zip",
             checksum: "d8e478ccdfc454e8636a7a65add7e4d0199cecdcdbb6e46eddc6f536df1d023e"
         ),
         .binaryTarget(
             name: "libcurl",
-            url: "https://s3.g.s4.mega.io/010996547823786/ios-xcframeworks/libcurl_xcframework.zip",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libcurl_xcframework.zip",
             checksum: "599cfcb782f3bd0839746eb56b9e45f29c0d9961f0512531744b11145773f5de"
         ),
         .binaryTarget(
             name: "libsodium",
-            url: "https://s3.g.s4.mega.io/010996547823786/ios-xcframeworks/libsodium_xcframework.zip",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libsodium_xcframework.zip",
             checksum: "2d8d325921110ba7175573623095f56fb3ed2f8044ad6053a966b825710b853d"
         ),
         .binaryTarget(
             name: "libwebrtc",
-            url: "https://s3.g.s4.mega.io/010996547823786/ios-xcframeworks/libwebrtc_xcframework.zip",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libwebrtc_xcframework.zip",
             checksum: "ee61b35cd31522c51dc693a30feefcbb05cac273b0bec9455b986ed8950a7e29"
         ),
         .binaryTarget(
             name: "libzen",
-            url: "https://s3.g.s4.mega.io/010996547823786/ios-xcframeworks/libzen_xcframework.zip",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libzen_xcframework.zip",
             checksum: "f3759e829d921875c4d9ec55354b225e8c5354488ca492d777bf10b0e9d38ebb"
         )
     ],
