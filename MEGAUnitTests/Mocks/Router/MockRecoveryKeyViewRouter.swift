a2586f7908d74446892dde8103dda275e3a354b9
Glazel Magpayo <gma@mega.co.nz>
2024-11-08 11:35:21 +0800

    IOS-9301 Fix crash on MasterKeyViewController.dispatchOnViewDidLoadAction()
    (cherry picked from commit 727eedadf6b1873abd9baed266bcfa4233195c00)

--- MEGAUnitTests/Mocks/Router/MockRecoveryKeyViewRouter.swift
+++ MEGAUnitTests/Mocks/Router/MockRecoveryKeyViewRouter.swift
@@ -2,6 +2,7 @@
 
 final class MockRecoveryKeyViewRouter: RecoveryKeyViewRouting {
     private(set) var showSecurityLink_calledTimes = 0
+    private(set) var presentView_calledTimes = 0
     private let _recoveryKeyViewController: UIViewController?
     
     init(recoveryKeyViewController: UIViewController? = nil) {
@@ -14,5 +15,9 @@ final class MockRecoveryKeyViewRouter: RecoveryKeyViewRouting {
     
     func showSecurityLink() {
         showSecurityLink_calledTimes += 1
+    }
+    
+    func presentView() {
+        presentView_calledTimes += 1
     }
 }
