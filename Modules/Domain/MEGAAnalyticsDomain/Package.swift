f56474a5e0397bab9995947cb35564af58f5c0e6
Binh Le <bl@mega.co.nz>
2024-11-12 03:35:47 +1300

    IOS-9184: Enable Swift 6 for MEGAAnalyticsDomain

--- Modules/Domain/MEGAAnalyticsDomain/Package.swift
+++ Modules/Domain/MEGAAnalyticsDomain/Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let settings: [SwiftSetting] = [
     .unsafeFlags(["-warnings-as-errors"]),
-    .enableExperimentalFeature("ExistentialAny"),
-    .enableExperimentalFeature("StrictConcurrency=targeted")
+    .enableExperimentalFeature("ExistentialAny")
 ]
 
 let package = Package(
@@ -45,5 +44,6 @@ let package = Package(
             ],
             swiftSettings: settings
         )
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
