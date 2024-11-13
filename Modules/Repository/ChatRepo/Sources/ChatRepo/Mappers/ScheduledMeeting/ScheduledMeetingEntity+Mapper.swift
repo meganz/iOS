df10ff66b3f516ff58fd1f12604b57ed86f3c72e
Tomek Kuźma <tku@mega.co.nz>
2024-11-13 22:04:48 +1300

    CHT-919: static analyser issues MEGAChatScheduledMeeting

--- Modules/Repository/ChatRepo/Sources/ChatRepo/Mappers/ScheduledMeeting/ScheduledMeetingEntity+Mapper.swift
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/Mappers/ScheduledMeeting/ScheduledMeetingEntity+Mapper.swift
@@ -2,13 +2,25 @@ import MEGAChatSdk
 import MEGADomain
 
 public extension MEGAChatScheduledMeeting {
-    func toScheduledMeetingEntity() -> ScheduledMeetingEntity {
+    func toScheduledMeetingEntity() -> ScheduledMeetingEntity? {
         ScheduledMeetingEntity(with: self)
     }
 }
 
 fileprivate extension ScheduledMeetingEntity {
-    init(with scheduledMeeting: MEGAChatScheduledMeeting) {
+    init?(with scheduledMeeting: MEGAChatScheduledMeeting) {
+        
+        guard
+            let sdkFlags = scheduledMeeting.flags,
+            let sdkRules = scheduledMeeting.rules,
+            let title = scheduledMeeting.title,
+            let description = scheduledMeeting.description,
+            let timezone = scheduledMeeting.timezone,
+            let attributes = scheduledMeeting.attributes
+        else {
+            return nil
+        }
+        
         self.init(
             cancelled: scheduledMeeting.isCancelled,
             new: scheduledMeeting.isNew,
@@ -17,15 +29,15 @@ fileprivate extension ScheduledMeetingEntity {
             scheduledId: scheduledMeeting.scheduledId,
             parentScheduledId: scheduledMeeting.parentScheduledId,
             organizerUserId: scheduledMeeting.organizerUserId,
-            timezone: scheduledMeeting.timezone,
+            timezone: timezone,
             startDate: Date(timeIntervalSince1970: TimeInterval(scheduledMeeting.startDateTime)),
             endDate: Date(timeIntervalSince1970: TimeInterval(scheduledMeeting.endDateTime)),
-            title: scheduledMeeting.title,
-            description: scheduledMeeting.description,
-            attributes: scheduledMeeting.attributes,
+            title: title,
+            description: description,
+            attributes: attributes,
             overrides: Date(timeIntervalSince1970: TimeInterval(scheduledMeeting.overrides)),
-            flags: ScheduledMeetingFlagsEntity(with: scheduledMeeting.flags),
-            rules: ScheduledMeetingRulesEntity(with: scheduledMeeting.rules)
+            flags: ScheduledMeetingFlagsEntity(with: sdkFlags),
+            rules: ScheduledMeetingRulesEntity(with: sdkRules)
         )
     }
 }
