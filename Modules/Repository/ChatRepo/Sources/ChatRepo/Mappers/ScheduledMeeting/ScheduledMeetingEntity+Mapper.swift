397fb58d3cd9b6274952a98fd67bc8e1009f82c6
Carlos Martín Acera <cma@mega.nz>
2024-11-19 01:50:36 +1300

    MEET-4595 Start button lack and schedule meeting not working properly

--- Modules/Repository/ChatRepo/Sources/ChatRepo/Mappers/ScheduledMeeting/ScheduledMeetingEntity+Mapper.swift
+++ Modules/Repository/ChatRepo/Sources/ChatRepo/Mappers/ScheduledMeeting/ScheduledMeetingEntity+Mapper.swift
@@ -8,17 +8,27 @@ public extension MEGAChatScheduledMeeting {
 }
 
 fileprivate extension ScheduledMeetingEntity {
-    init?(with scheduledMeeting: MEGAChatScheduledMeeting) {
+    init(with scheduledMeeting: MEGAChatScheduledMeeting) {
         
-        guard
-            let sdkFlags = scheduledMeeting.flags,
-            let sdkRules = scheduledMeeting.rules,
-            let title = scheduledMeeting.title,
-            let description = scheduledMeeting.description,
-            let timezone = scheduledMeeting.timezone,
-            let attributes = scheduledMeeting.attributes
-        else {
-            return nil
+        let flagsEntity: ScheduledMeetingFlagsEntity
+        if let flags = scheduledMeeting.flags {
+            flagsEntity = ScheduledMeetingFlagsEntity(with: flags)
+        } else {
+            flagsEntity = ScheduledMeetingFlagsEntity(emailsEnabled: false)
+        }
+        
+        let rulesEntity: ScheduledMeetingRulesEntity
+        if let rules = scheduledMeeting.rules {
+            rulesEntity = ScheduledMeetingRulesEntity(with: rules)
+        } else {
+            rulesEntity = ScheduledMeetingRulesEntity(
+                frequency: .invalid,
+                interval: 0,
+                until: nil,
+                monthDayList: nil,
+                weekDayList: nil,
+                monthWeekDayList: nil
+            )
         }
         
         self.init(
@@ -29,15 +39,15 @@ fileprivate extension ScheduledMeetingEntity {
             scheduledId: scheduledMeeting.scheduledId,
             parentScheduledId: scheduledMeeting.parentScheduledId,
             organizerUserId: scheduledMeeting.organizerUserId,
-            timezone: timezone,
+            timezone: scheduledMeeting.timezone ?? "",
             startDate: Date(timeIntervalSince1970: TimeInterval(scheduledMeeting.startDateTime)),
             endDate: Date(timeIntervalSince1970: TimeInterval(scheduledMeeting.endDateTime)),
-            title: title,
-            description: description,
-            attributes: attributes,
+            title: scheduledMeeting.title ?? "",
+            description: scheduledMeeting.description ?? "",
+            attributes: scheduledMeeting.attributes ?? "",
             overrides: Date(timeIntervalSince1970: TimeInterval(scheduledMeeting.overrides)),
-            flags: ScheduledMeetingFlagsEntity(with: sdkFlags),
-            rules: ScheduledMeetingRulesEntity(with: sdkRules)
+            flags: flagsEntity,
+            rules: rulesEntity
         )
     }
 }
