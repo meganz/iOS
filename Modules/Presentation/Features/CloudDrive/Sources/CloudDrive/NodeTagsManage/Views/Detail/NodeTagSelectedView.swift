662fc8043dab737b957266f830ae96aeb222adb4
Lancy <lf@mega.co.nz>
2024-11-27 16:33:41 +1300

    SAO-2506: The complete tag is not displayed when the text size is set to 80%.

--- Modules/Presentation/Features/CloudDrive/Sources/CloudDrive/NodeTagsManage/Views/Detail/NodeTagSelectedView.swift
+++ Modules/Presentation/Features/CloudDrive/Sources/CloudDrive/NodeTagsManage/Views/Detail/NodeTagSelectedView.swift
@@ -3,10 +3,14 @@ import SwiftUI
 
 struct NodeTagSelectedView: View {
     let tag: String
+    @ScaledMetric private var imageSize = 20.0
 
     var body: some View {
         HStack(spacing: 0) {
             Image(.turquoiseCheckmark)
+                .resizable()
+                .scaledToFit()
+                .frame(width: imageSize, height: imageSize)
                 .foregroundStyle(TokenColors.Icon.inverse.swiftUI)
 
             Text(tag)
--- Modules/Presentation/Features/CloudDrive/Tests/CloudDriveTests/NodeTagsViewModelTests.swift
+++ Modules/Presentation/Features/CloudDrive/Tests/CloudDriveTests/NodeTagsViewModelTests.swift
@@ -3,20 +3,7 @@ import Testing
 
 @Suite("NodeTagsViewModel Tests")
 struct NodeTagsViewModelTests {
-
-    @MainActor
-    @Test("Test the update method")
-    func updateTagsWidthModelWhenUpdateInvoked() {
-        let tags = ["#tags1", "#tags2", "#tags3"]
-        let tagViewModels = tags.map { NodeTagViewModel(tag: $0, isSelectionEnabled: false, isSelected: false)}
-        let viewModel = NodeTagsViewModel(tagViewModels: tagViewModels)
-        #expect(viewModel.tagsWidth.count == 0)
-        viewModel.update("#noTag", with: 0)
-        #expect(viewModel.tagsWidth.count == 0)
-        viewModel.update("#tags2", with: 10)
-        #expect(viewModel.tagsWidth.count == 1)
-    }
-
+    
     @MainActor
     @Test("Test the insertion of a tag at the start")
     func testInsertionOfATagAtTheStart() {
