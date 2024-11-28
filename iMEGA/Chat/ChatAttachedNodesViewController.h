68165792e1a2679e259802ba1da1d755bc3d6e7f
Tomasz Kuźma <tku@mega.co.nz>
2024-11-28 09:44:54 +0100

    MEET-4626: remove ChatAttachedNodesViewController

--- iMEGA/Chat/ChatAttachedNodesViewController.h
+++
@@ -1,8 +0,0 @@
-@class MEGAChatMessage;
-
-@interface ChatAttachedNodesViewController : UIViewController
-
-@property (nonatomic, strong) MEGAChatMessage *message;
-@property (nonatomic) MEGAHandle chatId;
-
-@end
