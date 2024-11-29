57b3c757c9c74562023800788c6823a254ca7872
Roberto Gómez <rg@mega.nz>
2024-11-29 23:21:10 +1300

    AP-2174. Achievements texts are not scrollable when in landscape mode and not readable when in large text size

--- iMEGA/My Account/Achievements/AchievementsDetailsViewController.h
+++ iMEGA/My Account/Achievements/AchievementsDetailsViewController.h
@@ -13,5 +13,7 @@
 @property (weak, nonatomic) IBOutlet UILabel * _Nullable howItWorksLabel;
 @property (weak, nonatomic) IBOutlet UILabel * _Nullable howItWorksExplanationLabel;
 @property (weak, nonatomic) IBOutlet UIButton * _Nullable addPhoneNumberButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *scrollViewBottomSpacingConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *scrollViewBottonConstraint;
 
 @end
