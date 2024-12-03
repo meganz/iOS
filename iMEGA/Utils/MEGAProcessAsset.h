ba8dc3fc9ca5e9b554ca2d3be631e7ea11acae3c
Tomek Kuźma <tku@mega.co.nz>
2024-12-04 04:16:25 +1300

    MEET-4136 intermittent assert exporter alert

--- iMEGA/Utils/MEGAProcessAsset.h
+++ iMEGA/Utils/MEGAProcessAsset.h
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <Photos/Photos.h>
 
+NS_ASSUME_NONNULL_BEGIN
 static NSString *MEGAProcessAssetErrorDomain = @"MEGAProcessAssetErrorDomain";
 
 NS_SWIFT_SENDABLE
@@ -9,10 +10,26 @@ NS_SWIFT_SENDABLE
 /* if YES keep asset original name, for example: IMG_XXXX.JPG */
 @property (nonatomic, getter=isOriginalName) BOOL originalName;
 
-- (instancetype)initWithAsset:(PHAsset *)asset filePath:(void (^)(NSString *filePath))filePath error:(void (^)(NSError *error))error;
-- (instancetype)initToShareThroughChatWithAssets:(NSArray <PHAsset *> *)assets filePaths:(void (^)(NSArray <NSString *> *filePaths))filePaths errors:(void (^)(NSArray <NSError *> *errors))errors;
-- (instancetype)initToShareThroughChatWithVideoURL:(NSURL *)videoURL filePath:(void (^)(NSString *filePath))filePath error:(void (^)(NSError *error))error;
-- (instancetype)initToShareThroughChatWithVideoURL:(NSURL *)videoURL filePath:(void (^)(NSString *filePath))filePath error:(void (^)(NSError *error))error presenter:(UIViewController *)presenter;
+- (instancetype)initWithAsset:(PHAsset *)asset
+                    presenter:(nullable UIViewController *)presenter
+                     filePath:(void (^)(NSString *filePath))filePath
+                        error:(void (^)(NSError *error))error;
+
+- (instancetype)initToShareThroughChatWithAssets:(NSArray <PHAsset *> *)assets
+                                       presenter:(UIViewController *)presenter
+                                       filePaths:(void (^)(NSArray <NSString *> * _Nullable filePaths))filePaths
+                                          errors:(void (^)(NSArray <NSError *> * _Nullable errors))errors;
+
+- (instancetype)initToShareThroughChatWithVideoURL:(NSURL *)videoURL
+                                         presenter:(UIViewController *)presenter
+                                          filePath:(void (^)(NSString * _Nullable filePath))filePath
+                                             error:(void (^)(NSError * _Nullable error))error;
+
+- (instancetype)initToShareThroughChatWithVideoURL:(NSURL *)videoURL
+                                          filePath:(void (^)(NSString * _Nullable filePath))filePath
+                                             error:(void (^)(NSError * _Nullable error))error
+                                         presenter:(UIViewController *)presenter;
 - (void)prepare;
 
 @end
+NS_ASSUME_NONNULL_END
