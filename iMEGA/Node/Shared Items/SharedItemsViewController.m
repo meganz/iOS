53202f3849a7dd3840a9c03ea77e52280c816d65
Mike Cap <mvc@mega.co.nz>
2025-01-03 16:01:50 +1300

    SAO-363: Disable tab selection while in edit mode in Share screen

--- iMEGA/Node/Shared Items/SharedItemsViewController.m
+++ iMEGA/Node/Shared Items/SharedItemsViewController.m
@@ -561,7 +561,7 @@
 #pragma mark - Utils
 
 - (void)selectSegment:(NSUInteger)index {
-    if (index == 0) {
+    if (index == 0 && self.incomingButton.enabled) {
         [self incomingTouchUpInside:self.incomingButton];
     } else if (index == 1) {
         [self outgoingTouchUpInside:self.outgoingButton];
@@ -613,9 +613,10 @@
     [self.tableView setEditing:editing animated:animated];
     
     [self updateNavigationBarTitle];
-    
     [self setNavigationBarButtons];
-    
+
+    [self updateSegmentedButtonsForEditingState:editing];
+
     if (editing) {
         if (![self.tabBarController.view.subviews containsObject:self.toolbar]) {
             [self.toolbar setAlpha:0.0];
