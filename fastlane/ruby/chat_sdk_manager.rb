66736b6985c2f91a604c8985f273294862d879c7
Javier Navarro <jn@mega.co.nz>
2025-01-10 21:28:20 +1300

    IOS-9385: Update build system after ccronexpr path is changed in the SDK

--- fastlane/ruby/chat_sdk_manager.rb
+++ fastlane/ruby/chat_sdk_manager.rb
@@ -17,6 +17,7 @@ class ChatSDKManager
         sdk_mega_folder = File.join(sdk_third_party_parent_folder, "mega")
         chat_mega_folder = File.join(@chat_third_party_path, "include", "mega")
         FileUtils.cp_r "#{sdk_mega_folder}/.", chat_mega_folder, remove_destination: true, :verbose => true
+        sdk_root_path = Pathname.new(@sdk_third_party_path).parent.parent.parent.to_s
 
         sdk_third_party_private_folder = File.join(sdk_third_party_parent_folder, "private")
         FileUtils.cp_r "#{sdk_third_party_private_folder}/DelegateMEGARequestListener.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
@@ -26,5 +27,6 @@ class ChatSDKManager
         FileUtils.cp_r "#{sdk_third_party_private_folder}/MEGANodeList+init.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
         FileUtils.cp_r "#{sdk_third_party_private_folder}/MEGASdk+init.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
         FileUtils.cp_r "#{sdk_third_party_private_folder}/MEGAStringList+init.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
+        FileUtils.cp_r "#{sdk_root_path}/third_party/ccronexpr/ccronexpr.h",  "#{@chat_third_party_path}/include", remove_destination: true, :verbose => true
     end
 end
