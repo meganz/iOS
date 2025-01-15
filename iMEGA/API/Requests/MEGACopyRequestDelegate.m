6683c986c59d3befdcef9159a8a2622c6417bb0b
Carlos Martín Acera <cma@mega.nz>
2025-01-15 21:52:34 +1300

    MEET-3128 Incorrect message shows up when you forward the received...

--- iMEGA/API/Requests/MEGACopyRequestDelegate.m
+++ iMEGA/API/Requests/MEGACopyRequestDelegate.m
@@ -27,8 +27,11 @@
 
 - (void)onRequestFinish:(MEGASdk *)api request:(MEGARequest *)request error:(MEGAError *)error {    
     if (error.type) {
-        [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeNone];
-        [SVProgressHUD showErrorWithStatus:[NSString stringWithFormat:@"%@ %@", request.requestString, LocalizedString(error.name, @"")]];
+        // OverQuota errors are handled in AppDelegate::onRequestFinish by showing an alert, no need to show error with this HUD
+        if (error.type != MEGAErrorTypeApiEOverQuota) {
+            [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeNone];
+            [SVProgressHUD showErrorWithStatus:[NSString stringWithFormat:@"%@ %@", request.requestString, LocalizedString(error.name, @"")]];
+        }
         return;
     }
     
