bb6e0be0bf5400faa15d0f1d3f817a27729a6bab
Roberto Gómez <rg@mega.nz>
2025-01-17 23:42:40 +1300

    SAO-2379. Takedown folder in Outgoing shares is not displayed in red

--- iMEGA/Node/Shared Items/SharedItemsTableViewCell.swift
+++ iMEGA/Node/Shared Items/SharedItemsTableViewCell.swift
@@ -9,6 +9,8 @@ final class SharedItemsTableViewCell: UITableViewCell {
 
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var nameLabel: MEGALabel!
+    @IBOutlet weak var takeDownView: UIView!
+    @IBOutlet weak var takeDownImageView: UIImageView!
     @IBOutlet weak var labelView: UIView!
     @IBOutlet weak var labelImageView: UIImageView!
     @IBOutlet weak var favouriteView: UIView!
@@ -23,6 +25,8 @@ final class SharedItemsTableViewCell: UITableViewCell {
     @objc var delegate: (any SharedItemsTableViewCellDelegate)?
     
     @objc var nodeHandle: UInt64 = 0
+    
+    @objc var isTakenDownNode: Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -41,14 +45,28 @@ final class SharedItemsTableViewCell: UITableViewCell {
     // Pragma mark: - Private
     
     private func updateAppearance() {
-        nameLabel.tintColor = TokenColors.Text.primary
+        nameLabel.tintColor = isTakenDownNode ? TokenColors.Text.error : TokenColors.Text.primary
         infoLabel.textColor = TokenColors.Text.secondary
         backgroundColor = TokenColors.Background.page
         infoButton.tintColor = TokenColors.Icon.secondary
         descriptionLabel.textColor = TokenColors.Text.secondary
+        takeDownView.isHidden = !isTakenDownNode
     }
 
     @objc func setNodeDescription(_ desc: String?) {
         descriptionLabel?.text = desc
+    }
+    
+    @objc func configureNode(_ name: String, isTakenDown: Bool) {
+        nameLabel.text = name
+        isTakenDownNode = isTakenDown
+        takeDownView.isHidden = !isTakenDown
+
+        if isTakenDown {
+            takeDownImageView.image = UIImage.isTakedown.withTintColorAsOriginal(TokenColors.Support.error)
+            nameLabel.textColor = TokenColors.Text.error
+        } else {
+            nameLabel.textColor = TokenColors.Text.primary
+        }
     }
 }
