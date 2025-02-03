264bb01f15fcd9df2f6bd3695bdb631e207081aa
Javier Navarro <jn@mega.co.nz>
2025-02-03 12:34:52 +0100

    IOS-9549: Crash at MEGAPhotoPicker.pickAssets

--- Modules/UI/MEGAUI/Sources/MEGAUI/MEGAPhotoPicker/PhotoPickerDelegate.swift
+++ Modules/UI/MEGAUI/Sources/MEGAUI/MEGAPhotoPicker/PhotoPickerDelegate.swift
@@ -2,12 +2,15 @@ import PhotosUI
 
 final class PhotoPickerDelegate: PHPickerViewControllerDelegate {
     private let completion: ([PHAsset]) -> Void
+    private var hasFinishedPicking = false
     
     init(completion: @escaping ([PHAsset]) -> Void) {
         self.completion = completion
     }
     
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {        
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        guard !hasFinishedPicking else { return }
+        hasFinishedPicking = true
         let assets = results.toPHAssets()
         completion(assets)
         picker.dismiss(animated: true)
