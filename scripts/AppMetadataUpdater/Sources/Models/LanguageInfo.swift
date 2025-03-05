e0ba8024f33142bf033ef694964d4b864df77875
Glazel Magpayo <gma@mega.co.nz>
2025-03-05 11:32:04 +0800

    IOS-9710 Remove Turkish on AppMetadataUpdater temporarily

--- scripts/AppMetadataUpdater/Sources/Models/LanguageInfo.swift
+++ scripts/AppMetadataUpdater/Sources/Models/LanguageInfo.swift
@@ -23,8 +23,7 @@ struct LanguageInfo {
             LanguageInfo(name: "Vietnamese", transifexCode: "l:vi", fastlaneMetadataFolders: ["vi"]),
             LanguageInfo(name: "Chinese Simpified", transifexCode: "l:zh_CN", fastlaneMetadataFolders: ["zh-Hans"]),
             LanguageInfo(name: "Chinese Traditional", transifexCode: "l:zh_TW", fastlaneMetadataFolders: ["zh-Hant"]),
-            LanguageInfo(name: "German", transifexCode: "l:de", fastlaneMetadataFolders: ["de-DE"]),
-            LanguageInfo(name: "Turkish", transifexCode: "l:tr", fastlaneMetadataFolders: ["tr"])
+            LanguageInfo(name: "German", transifexCode: "l:de", fastlaneMetadataFolders: ["de-DE"])
         ]
     }
 }
