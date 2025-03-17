5084abd45f0fedf7ff9d02dacf808b25fe586d13
Lancy <lf@mega.co.nz>
2025-03-17 20:16:18 +1300

    IOS-9735: Add more logs to AppMetadataUpdater Script

--- scripts/AppMetadataUpdater/Sources/Operations/Fetcher.swift
+++ scripts/AppMetadataUpdater/Sources/Operations/Fetcher.swift
@@ -25,11 +25,24 @@ struct Fetcher {
     }
 
     private func downloadData(from url: URL) async throws -> Data {
-        return try await sendRequest(
+        let data = try await sendRequest(
             url: url,
             method: .get,
             headers: api.headers,
             body: nil
         )
+
+        print(
+        """
+        Sending request:
+            - URL: \(url.absoluteString)
+            - headers: \(String(describing: api.headers))
+        Data:
+            - utf8 data: \(String(data: data, encoding: .utf8) ?? "Could not convert to utf8")
+            - utf16 data: \(String(data: data, encoding: .utf16) ?? "Could not convert to utf16")
+        """
+        )
+
+        return data
     }
 }
