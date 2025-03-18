1cf3a357921325c819c76615e1009346ac3ce2a0
Mauritz Mollentze <mmo@mega.co.nz>
2025-03-18 17:53:12 +1300

    CC-7978: Add hide node info button analytics

--- iMEGA/Utils/Delegates/DefaultNodeActionAccessoryDelegate.swift
+++ iMEGA/Utils/Delegates/DefaultNodeActionAccessoryDelegate.swift
@@ -1,16 +1,19 @@
-import Foundation
+import MEGAAnalyticsiOS
+import MEGAPresentation
 
 @objc final class DefaultNodeAccessoryActionDelegate: NSObject, NodeAccessoryActionDelegate {
+    nonisolated override init() {
+        super.init()
+    }
+    
     func nodeAccessoryAction(_ nodeAction: NodeActionViewController, didSelect action: MegaNodeActionType) {
         switch action {
         case .hide:
+            DIContainer.tracker.trackAnalyticsEvent(with: HideNodeInfoButtonPressedEvent())
+            
             HideFilesAndFoldersRouter(presenter: nodeAction)
                 .showOnboardingInfo()
         default: break
         }
-    }
-    
-    nonisolated override init() {
-        super.init()
     }
 }
