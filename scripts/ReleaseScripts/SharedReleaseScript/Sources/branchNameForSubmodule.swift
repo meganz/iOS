570a2650ac8f17c15070c23be67b3f25e82984c0
Lancy <lf@mega.co.nz>
2025-03-20 10:38:09 +1300

    IOS-8106: Improving the logic for fetching the SDK and MEGAChat SDK releases.

--- scripts/ReleaseScripts/SharedReleaseScript/Sources/branchNameForSubmodule.swift
+++ scripts/ReleaseScripts/SharedReleaseScript/Sources/branchNameForSubmodule.swift
@@ -1,14 +1,34 @@
 import Foundation
 
-public func branchNameForSubmodule(with path: String) throws -> String {
+public enum SubmoduleReferenceType: Sendable {
+    case tag(String)
+    case branch(String)
+
+    public func description(type: String) -> String {
+        switch self {
+        case .tag(let tag):
+            "*\(type) release:* `\(tag)`"
+        case .branch(let branch):
+            "*\(type) Branch:* `\(branch)`"
+        }
+    }
+}
+
+public func tagOrBranchNameForSubmodule(with path: String) throws -> SubmoduleReferenceType {
     let directoryManager = DirectoryManager()
     let originalDirectory = directoryManager.currentDirectoryPath
     let projectRootDirectoryURL = try directoryManager.projectFileDirectory()
 
     try directoryManager.change(to: projectRootDirectoryURL + "/" + path)
     try runInShell("git fetch --all")
-    let branchName = try runInShell("git branch -a --contains | grep '/release/' | sed -E 's/^[ \t]*//; s#remotes/origin/##' | sort -V | tail -n 1")
-    try directoryManager.change(to: originalDirectory)
-
-    return branchName.components(separatedBy: .newlines).joined()
+    do {
+        let tag = try runInShell("git describe --exact-match --tags")
+        try directoryManager.change(to: originalDirectory)
+        return .tag(tag.components(separatedBy: .newlines).joined())
+    } catch {
+        let command = "git branch -a --contains | grep '/release/' | sed -E 's/^[ \t]*//; s#remotes/origin/##' | sort -V | head -n 1"
+        let branchName = try runInShell(command)
+        try directoryManager.change(to: originalDirectory)
+        return .branch(branchName.components(separatedBy: .newlines).joined())
+    }
 }
