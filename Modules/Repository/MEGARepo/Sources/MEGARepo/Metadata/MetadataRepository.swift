c64c9da3fe9c244cf018245cd3c0a33e0dabc222
Javier Navarro <jn@mega.co.nz>
2025-03-21 13:29:55 +0100

    IOS-9419: stringValue property of AVMetadataItem is deprecated

--- Modules/Repository/MEGARepo/Sources/MEGARepo/Metadata/MetadataRepository.swift
+++ Modules/Repository/MEGARepo/Sources/MEGARepo/Metadata/MetadataRepository.swift
@@ -21,15 +21,18 @@ public final class MetadataRepository: MetadataRepositoryProtocol {
     public func coordinateForVideo(at url: URL) async -> Coordinate? {
         let asset = AVAsset(url: url)
         guard let metadata = try? await asset.load(.metadata) else { return nil }
-        for item in metadata {
-            if item.key?.isEqual(AVMetadataKey.quickTimeMetadataKeyLocationISO6709) == true,
-               let locationDescription = item.stringValue,
-               /// Location description will look something like "+13.0716+074.9953+114.877/". we need to extract the latitude and longitude from the string.
-               let latitude = Double(String(locationDescription.prefix(8))),
-               case let start = locationDescription.index(locationDescription.startIndex, offsetBy: 8),
-               case let end = locationDescription.index(locationDescription.startIndex, offsetBy: 17),
-               let longitude = Double(String(locationDescription[start..<end])) {
-                return Coordinate(latitude: latitude, longitude: longitude)
+        for item in metadata where item.key?.isEqual(AVMetadataKey.quickTimeMetadataKeyLocationISO6709) == true {
+            do {
+                /// Location description will look something like "+13.0716+074.9953+114.877/". we need to extract the latitude and longitude from the string.
+                if let locationDescription = try await item.load(.stringValue),
+                   let latitude = Double(String(locationDescription.prefix(8))),
+                   case let start = locationDescription.index(locationDescription.startIndex, offsetBy: 8),
+                   case let end = locationDescription.index(locationDescription.startIndex, offsetBy: 17),
+                   let longitude = Double(String(locationDescription[start..<end])) {
+                    return Coordinate(latitude: latitude, longitude: longitude)
+                }
+            } catch {
+                return nil
             }
         }
 
