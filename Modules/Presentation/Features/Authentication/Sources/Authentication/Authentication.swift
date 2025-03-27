3cc539773a3f9fb502f9ea682521cbae308709b4
Mauritz Mollentze <mmo@mega.co.nz>
2025-03-27 13:12:16 +1300

    IOS-9591:  Add authenticate package

---
+++ Modules/Presentation/Features/Authentication/Sources/Authentication/Authentication.swift
@@ -0,0 +1,10 @@
+import MEGAAuthenticationUIComponents
+import SwiftUI
+
+public struct LoginView: View {
+    public init() {}
+    
+    public var body: some View {
+        LoginHeaderView()
+    }
+}
