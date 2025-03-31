c897dc7cf925b9f03640e4cf4590bace394688eb
Lancy Norbert Fernandes <lf@mega.co.nz>
2025-03-31 14:58:18 +1300

    IOS-9789: Fix the Merge release script issue

--- scripts/MergeRelease/Sources/GithubManager.swift
+++ scripts/MergeRelease/Sources/GithubManager.swift
@@ -7,18 +7,45 @@ struct GithubManager {
         githubAccessToken: String,
         message: String
     ) async throws {
-        try runInShell("git remote add public https://megaiospush:\(githubAccessToken)@github.com/meganz/iOS.git")
-        try runInShell("git fetch public")
-        try runInShell("git fetch origin")
-        try runInShell("git checkout origin/master")
-        try runInShell("git pull origin master")
-        try runInShell("git config http.postBuffer 100000000")
-        try runInShell("git push public origin/master")
+        try pushGitlabBranch(withName: "master", toGithubWithToken: githubAccessToken)
+
         try await createReleaseOnGithub(
             for: version,
             githubBearerToken: githubAccessToken,
             message: message
         )
+    }
+
+    private static func pushGitlabBranch(
+        withName branchName: String,
+        toGithubWithToken githubAccessToken: String
+    ) throws {
+        // checkout specified branch and pull the latest changes
+        do {
+            try runInShell("git checkout \(branchName)")
+            try runInShell("git pull origin \(branchName)")
+        } catch {
+            print("Error checking out and pull from GitLab: \(error)")
+            throw error
+        }
+
+        // Add the GitHub remote
+        do {
+            try runInShell("git remote add public https://megaiospush:\(githubAccessToken)@github.com/meganz/iOS.git")
+            try runInShell("git fetch public")
+        } catch {
+            print("Error adding or fetching from the GitHub remote: \(error)")
+            throw error
+        }
+
+        // Push the GitLab branch to GitHub
+        do {
+            try runInShell("git config http.postBuffer 100000000")
+            try runInShell("git push public \(branchName)")
+        } catch {
+            print("Error pushing to GitHub: \(error)")
+            throw error
+        }
     }
 
     static func createReleaseOnGithub(
