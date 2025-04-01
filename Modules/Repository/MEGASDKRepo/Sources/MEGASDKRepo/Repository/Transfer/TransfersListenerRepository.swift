8f0360b490c1722398ba75b28d7f856bf23c1644
Binh Le <bl@mega.co.nz>
2025-04-01 11:48:05 +0700

    IOS-9802: Adopt MEGAUpdateHandlerManager for TransfersListenerRepository

--- Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Repository/Transfer/TransfersListenerRepository.swift
+++ Modules/Repository/MEGASDKRepo/Sources/MEGASDKRepo/Repository/Transfer/TransfersListenerRepository.swift
@@ -3,21 +3,21 @@ import MEGASdk
 import MEGASwift
 
 public struct TransfersListenerRepository: TransfersListenerRepositoryProtocol {
+    public static var newRepo: TransfersListenerRepository {
+        .init(sdk: MEGASdk.sharedSdk)
+    }
+    
     public var completedTransfers: AnyAsyncSequence<TransferEntity> {
-        AsyncStream { continuation in
-            let delegate = PrivateTransferDelegate {
-                continuation.yield($0)
-            }
-            continuation.onTermination = { @Sendable _ in
-                sdk.remove(delegate)
-            }
-            sdk.add(delegate, queueType: .main)
-        }.eraseToAnyAsyncSequence()
+        MEGAUpdateHandlerManager
+            .shared
+            .transferFinishUpdates
+            .compactMap { try? $0.get() }
+            .eraseToAnyAsyncSequence()
     }
     
     private let sdk: MEGASdk
     
-    public init(sdk: MEGASdk) {
+    init(sdk: MEGASdk) {
         self.sdk = sdk
     }
     
@@ -27,23 +27,5 @@ public struct TransfersListenerRepository: TransfersListenerRepositoryProtocol {
     
     public func resumeTransfers() {
         sdk.pauseTransfers(false)
-    }
-}
-
-private final class PrivateTransferDelegate: NSObject, MEGATransferDelegate, Sendable {
-    private let onTransferFinish: @Sendable (TransferEntity) -> Void
-    
-    init(onTransferFinish: @Sendable @escaping (TransferEntity) -> Void) {
-        self.onTransferFinish = onTransferFinish
-    }
-    
-    func onTransferFinish(_ api: MEGASdk, transfer: MEGATransfer, error: MEGAError) {
-        onTransferFinish(transfer.toTransferEntity())
-    }
-}
-
-public extension TransfersListenerRepository {
-    static var newRepo: TransfersListenerRepository {
-        .init(sdk: MEGASdk.sharedSdk)
     }
 }
