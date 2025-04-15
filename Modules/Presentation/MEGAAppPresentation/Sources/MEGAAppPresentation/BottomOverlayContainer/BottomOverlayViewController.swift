08881b4f62f66806e3e12769b320af96f2ac9ba5
Roberto Gómez <rg@mega.nz>
2025-04-15 23:24:23 +1200

    IOS-9683. Show mini player within Device Centre

---
+++ Modules/Presentation/MEGAAppPresentation/Sources/MEGAAppPresentation/BottomOverlayContainer/BottomOverlayViewController.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+/// A base view controller for displaying SwiftUI content while managing bottom overlay insets. This controller implements the common functionality for adjusting the bottom safe area inset.
+public class OverlayHostingController<Content: View>: UIHostingController<Content>, BottomOverlayPresenterProtocol {
+    public override init(rootView: Content) {
+        super.init(rootView: rootView)
+    }
+    
+    @objc required dynamic public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - BottomOverlayPresenterProtocol
+    public func updateContentView(_ height: CGFloat) {
+        additionalSafeAreaInsets.bottom = height
+    }
+    
+    public func hasUpdatedContentView() -> Bool {
+        additionalSafeAreaInsets.bottom != 0
+    }
+}
+
+/// A view controller that displays a SwiftUI view along with an audio mini-player overlay. Conforming to AudioPlayerPresenterProtocol, this controller indicates that the mini-player
+/// should be visible alongside other bottom overlay elements.
+public final class AudioPlayerHostingController<Content: View>: OverlayHostingController<Content>, AudioPlayerPresenterProtocol {}
