8e2ca43709450fbbad73b305fbc5b26fb9911039
Roberto Gómez <rg@mega.nz>
2025-04-30 08:18:54 +1200

    SAT-2162. Fix an issue showing the completed transfers in the TransfersWidget

--- iMEGA/Utils/Transfers/TransferInventoryUseCaseHelper.swift
+++ iMEGA/Utils/Transfers/TransferInventoryUseCaseHelper.swift
@@ -4,12 +4,14 @@ import MEGARepo
 
 final class TransferInventoryUseCaseHelper: NSObject, Sendable {
     private let transferInventoryUseCase: any TransferInventoryUseCaseProtocol
+    private let sdk: MEGASdk
     private let nodeUseCase: any NodeUseCaseProtocol
     private let fileSystem: any FileSystemRepositoryProtocol
     private let store: MEGAStore
 
     init(
         transferInventoryUseCase: some TransferInventoryUseCaseProtocol = TransferInventoryUseCase(transferInventoryRepository: TransferInventoryRepository.newRepo, fileSystemRepository: FileSystemRepository.newRepo),
+        sdk: MEGASdk = MEGASdk.shared,
         nodeUseCase: some NodeUseCaseProtocol = NodeUseCase(
             nodeDataRepository: NodeDataRepository.newRepo,
             nodeValidationRepository: NodeValidationRepository.newRepo,
@@ -19,17 +21,21 @@ final class TransferInventoryUseCaseHelper: NSObject, Sendable {
         store: MEGAStore = MEGAStore.shareInstance()
     ) {
         self.transferInventoryUseCase = transferInventoryUseCase
+        self.sdk = sdk
         self.nodeUseCase = nodeUseCase
         self.fileSystem = fileSystem
         self.store = store
     }
-
-    func completedTransfers() -> [TransferEntity] {
-        transferInventoryUseCase.completedTransfers(filteringUserTransfers: false)
-            .filter {
-                let node = nodeUseCase.nodeForHandle($0.nodeHandle) ?? $0.publicNode
-                return node?.isFile ?? false && ($0.type == .upload || $0.path?.hasPrefix(FileSystemRepository.newRepo.documentsDirectory().path) ?? false || $0.appData?.contains(TransferMetaDataEntity.saveInPhotos.rawValue) ?? false || $0.appData?.contains(TransferMetaDataEntity.exportFile.rawValue) ?? false)
+    
+    /// We use `MEGATransfer` directly—avoiding round-trip conversion from`TransferEntity` via `transferByTag(_:)`, which doesn’t return completed transfers by tag.
+    func completedTransfers() -> [MEGATransfer] {
+        if let list = sdk.completedTransfers as? [MEGATransfer] {
+            return list.filter {
+                let node = nodeUseCase.nodeForHandle($0.nodeHandle)?.toMEGANode(in: sdk) ?? $0.publicNode
+                return node?.isFile() ?? false && ($0.type == .upload || $0.path?.hasPrefix(FileSystemRepository.newRepo.documentsDirectory().path) ?? false || $0.appData?.contains(TransferMetaDataEntity.saveInPhotos.rawValue) ?? false || $0.appData?.contains(TransferMetaDataEntity.exportFile.rawValue) ?? false)
             }
+        }
+        return []
     }
     
     func transfers() -> [TransferEntity] {
