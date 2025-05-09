453bdc48b5b700081c863a0b0397b0ee03eacfcb
Lancy Norbert Fernandes <lf@mega.co.nz>
2025-05-09 12:56:14 +1200

    IOS-9967: Create Tag And Sync Master - Update the Jenkins script

--- jenkinsfile/ios_create_tag_and_sync_master.groovy
+++ jenkinsfile/ios_create_tag_and_sync_master.groovy
@@ -68,7 +68,7 @@ pipeline {
                     script {
                         envInjector.injectEnvs {
                             util.useGpg() {
-                                dir("scripts/CreateTagAndSyncFromMaster/") {
+                                dir("Modules/MEGASharedRepo/scripts/CreateTagAndSyncFromMaster/") {
                                      sh 'swift run CreateTagAndSyncFromMaster --transifex-authorization \"$TRANSIFIX_AUTHORIZATION_TOKEN\" --release-notes-resource-id \"$IOS_MEGA_CHANGELOG_RESOURCE_ID\" --gitlab-base-url \"$GITLAB_API_BASE_URL\" --gitlab-token \"$GITLAB_BEARER_TOKEN\" --project-id \"$MEGA_IOS_PROJECT_ID\" --branch \"$GIT_BRANCH\"'
                                 }
                             }
