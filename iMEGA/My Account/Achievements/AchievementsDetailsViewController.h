7e6e1fdbf7791afced570decf036716b0bf05952
Joanda Febrian <jfe@mega.co.nz>
2025-05-29 15:41:39 +1200

    IOS-10027: Add new achievements to Start VPN & PWM Free Trial

--- iMEGA/My Account/Achievements/AchievementsDetailsViewController.h
+++ iMEGA/My Account/Achievements/AchievementsDetailsViewController.h
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet UILabel * _Nullable howItWorksLabel;
 @property (weak, nonatomic) IBOutlet UILabel * _Nullable howItWorksExplanationLabel;
 @property (weak, nonatomic) IBOutlet UIButton * _Nullable addPhoneNumberButton;
+@property (weak, nonatomic) IBOutlet UIView *howItWorksTopSeparatorView;
+@property (weak, nonatomic) IBOutlet UIStackView *howItWorksView;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *scrollViewBottomSpacingConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *scrollViewBottonConstraint;
 
