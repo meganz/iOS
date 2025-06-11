02ff49789175a7f59a3cc641c398ddbde59e47bc
Javier Navarro <jn@mega.co.nz>
2025-06-11 13:51:39 +0200

    IOS-10153: Conformance to Sendable was already stated in other protocol's module

--- Modules/Repository/MEGAAppSDKRepo/Sources/MEGAAppSDKRepo/FileManager+Sendable.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-// I attempted to use `@preconcurrency import Foundation` and `@preconcurrency import class Foundation.FileManager` with no luck. Forced me to use @unchecked Sendable for FileManager.
-extension FileManager: @retroactive @unchecked Sendable { }
--- Modules/Repository/MEGARepo/Sources/MEGARepo/Repository/Filesystem/FileManager+Sendable.swift
+++ Modules/Repository/MEGARepo/Sources/MEGARepo/Repository/Filesystem/FileManager+Sendable.swift
@@ -1,3 +1,4 @@
 import Foundation
 
+// I attempted to use `@preconcurrency import Foundation` and `@preconcurrency import class Foundation.FileManager` with no luck. Forced me to use @unchecked Sendable for FileManager.
 extension FileManager: @unchecked @retroactive Sendable { }
