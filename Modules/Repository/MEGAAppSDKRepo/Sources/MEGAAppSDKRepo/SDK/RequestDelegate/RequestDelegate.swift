db11ad5f7264d321a357b254f7566f7f57825372
Harry Yan <hy@mega.co.nz>
2025-06-27 16:58:20 +1200

    IOS-10208: Remove onRequestStartHandler from RequestDelegate

--- Modules/Repository/MEGAAppSDKRepo/Sources/MEGAAppSDKRepo/SDK/RequestDelegate/RequestDelegate.swift
+++ Modules/Repository/MEGAAppSDKRepo/Sources/MEGAAppSDKRepo/SDK/RequestDelegate/RequestDelegate.swift
@@ -2,16 +2,14 @@ import Foundation
 import MEGASdk
 import MEGASDKRepo
 
-public typealias MEGARequestCompletion = (_ result: Result<MEGARequest, MEGAError>) -> Void
+public typealias MEGARequestCompletion = (Result<MEGARequest, MEGAError>) -> Void
 public typealias MEGARequestStartHandler = (MEGARequest) -> Void
 
 public class RequestDelegate: NSObject, MEGARequestDelegate {
     let completion: MEGARequestCompletion
-    let onRequestStartHandler: MEGARequestStartHandler?
     
-    public init(onRequestStartHandler: MEGARequestStartHandler? = nil, completion: @escaping MEGARequestCompletion) {
+    public init(completion: @escaping MEGARequestCompletion) {
         self.completion = completion
-        self.onRequestStartHandler = onRequestStartHandler
         super.init()
     }
     
@@ -27,32 +25,11 @@ public class RequestDelegate: NSObject, MEGARequestDelegate {
         self.init(completion: completion)
     }
     
-    @objc public convenience init(onRequestStartHandler: @escaping (MEGARequest) -> Void, completion: @escaping (MEGARequest?, MEGAError?) -> Void) {
-        let completion: MEGARequestCompletion = { result in
-            switch result {
-            case let .success(request):
-                completion(request, nil)
-            case let .failure(error):
-                completion(nil, error)
-            }
-        }
-        let onRequestStartHandler: MEGARequestStartHandler = { request in
-            onRequestStartHandler(request)
-        }
-        self.init(onRequestStartHandler: onRequestStartHandler, completion: completion)
-    }
-    
     public func onRequestFinish(_ api: MEGASdk, request: MEGARequest, error: MEGAError) {
         if error.type == .apiOk {
             self.completion(.success(request))
         } else {
             self.completion(.failure(error))
-        }
-    }
-    
-    public func onRequestStart(_ api: MEGASdk, request: MEGARequest) {
-        if let onRequestStartHandler {
-            onRequestStartHandler(request)
         }
     }
 }
