87db9f698275aac204d1f87dc2b8344240a3fa89
Javier Navarro <jn@mega.co.nz>
2025-07-18 18:31:27 +0200

    IOS-10222: Create script to compile SDK C++ external libraries using vcpkg and cmake

--- Modules/DataSource/MEGASDK/Package.swift
+++ Modules/DataSource/MEGASDK/Package.swift
@@ -20,12 +20,11 @@ let package = Package(
     targets: [
         .target(
             name: "MEGASdkCpp",
-            dependencies: ["libcryptopp",
-                           "libmediainfo",
-                           "libuv",
-                           "libcurl",
-                           "libsodium",
-                           "libzen"],
+            dependencies: [
+                "libmediainfo",
+                "libzen",
+                "libmegasdk"
+            ],
             path: "Sources/MEGASDK",
             exclude: [
                 "Package.swift",
@@ -76,10 +75,8 @@ let package = Package(
                 .linkedFramework("UIKit"),
                 .linkedFramework("Foundation"),
                 .linkedFramework("UniformTypeIdentifiers"),
-                // Libraries
                 .linkedLibrary("z"),
-                .linkedLibrary("sqlite3"),
-                .linkedLibrary("icucore")
+                .linkedLibrary("sqlite3")
             ]
         ),
         .target(
@@ -94,34 +91,19 @@ let package = Package(
             ]
         ),
         .binaryTarget(
-            name: "libcryptopp",
-            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libcryptopp_xcframework.zip",
-            checksum: "250c07fe6c1f071ebf455f4e4d2b2d15a3cdbd2aebcf36e2fd9c68c0c58e08ef"
-        ),
-        .binaryTarget(
             name: "libmediainfo",
             url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libmediainfo_xcframework.zip",
             checksum: "24fdbe1e3df3642799b1c890d5c810c88088bbe2a1d7bd34c766587f7405b0fa"
         ),
         .binaryTarget(
-            name: "libuv",
-            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libuv_xcframework.zip",
-            checksum: "d8e478ccdfc454e8636a7a65add7e4d0199cecdcdbb6e46eddc6f536df1d023e"
-        ),
-        .binaryTarget(
-            name: "libcurl",
-            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libcurl_xcframework.zip",
-            checksum: "599cfcb782f3bd0839746eb56b9e45f29c0d9961f0512531744b11145773f5de"
-        ),
-        .binaryTarget(
-            name: "libsodium",
-            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libsodium_xcframework.zip",
-            checksum: "2d8d325921110ba7175573623095f56fb3ed2f8044ad6053a966b825710b853d"
-        ),
-        .binaryTarget(
             name: "libzen",
             url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libzen_xcframework.zip",
             checksum: "f3759e829d921875c4d9ec55354b225e8c5354488ca492d777bf10b0e9d38ebb"
+        ),
+        .binaryTarget(
+            name: "libmegasdk",
+            url: "https://s3.g.s4.mega.io/dmlaaezwz52y37atz56mfvmrvltfagrltbgpr/ios-xcframeworks/libmegasdk_20250718.xcframework.zip",
+            checksum: "a11f8b61a088c79de573d45ee16c08bec904ae101b056ba9f6ac3011ad85fb22"
         )
     ],
     cxxLanguageStandard: .cxx17
