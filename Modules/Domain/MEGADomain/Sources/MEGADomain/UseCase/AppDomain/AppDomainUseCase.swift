df8b74a0e0543d1bbecb0ae657d2105c93dccf38
Lancy Norbert Fernandes <lf@mega.co.nz>
2025-08-11 15:27:40 +1200

    IOS-10348: Move from mega.nz to mega.app - Implement AppDomainUseCase

---
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/UseCase/AppDomain/AppDomainUseCase.swift
@@ -0,0 +1,40 @@
+import Foundation
+import MEGAPreference
+
+public protocol AppDomainUseCaseProtocol: Sendable {
+    var domainName: String { get }
+}
+
+public struct AppDomainUseCase: AppDomainUseCaseProtocol {
+    private enum DomainExtension {
+        case app
+        case nz
+
+        var domain: String {
+            switch self {
+            case .app:
+                return "mega.app"
+            case .nz:
+                return "mega.nz"
+            }
+        }
+    }
+
+    @PreferenceWrapper(key: PreferenceKeyEntity.isDomainNameMEGADotApp, defaultValue: false)
+    private var isDomainNameMEGADotApp: Bool
+
+    public var domainName: String {
+        guard isDomainNameMEGADotApp else { return DomainExtension.nz.domain }
+        return DomainExtension.app.domain
+    }
+    
+    public init(
+        preferenceUseCase: any PreferenceUseCaseProtocol,
+        remoteFeatureFlagUseCase: some RemoteFeatureFlagUseCaseProtocol,
+        isLocalFeatureFlagEnabled: Bool
+    ) {
+        $isDomainNameMEGADotApp.useCase = preferenceUseCase
+        isDomainNameMEGADotApp = remoteFeatureFlagUseCase
+            .isFeatureFlagEnabled(for: .dotAppDomainExtension) && isLocalFeatureFlagEnabled
+    }
+}
