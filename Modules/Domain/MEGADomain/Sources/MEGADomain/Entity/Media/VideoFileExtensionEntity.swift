ec434abb302b748c9fcdeeea64a683f03089ba43
Javier Navarro <jn@mega.co.nz>
2025-09-22 13:11:39 +0200

    IOS-10495: Add .ts as video extension

--- Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/Media/VideoFileExtensionEntity.swift
+++ Modules/Domain/MEGADomain/Sources/MEGADomain/Entity/Media/VideoFileExtensionEntity.swift
@@ -2,5 +2,5 @@ import MEGASwift
 
 public struct VideoFileExtensionEntity {
     public init() {}
-    public let videoSupportedExtensions: Set<FileExtension> = ["3gp2", "3gp", "avi", "m4v", "mov", "mp4", "mqv", "qt"]
+    public let videoSupportedExtensions: Set<FileExtension> = ["3gp2", "3gp", "avi", "m4v", "mov", "mp4", "mqv", "qt", "ts"]
 }
