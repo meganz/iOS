ab1b99c4bba32e728c84b7c9198d90af3dc1224b
Yang Fang <yfa@mega.co.nz>
2025-10-08 20:07:17 +1300

    SAO-3110: Leaks when performing "Scan document" and interrupted by open file Link in MEGA

--- iMEGA/Cloud drive/NewCloudDrive/ScanDocumentViewRouter.swift
+++ iMEGA/Cloud drive/NewCloudDrive/ScanDocumentViewRouter.swift
@@ -3,10 +3,22 @@ import MEGAL10n
 import MEGASwift
 import VisionKit
 
+private final class MegaVNDocCameraViewControllerDeallocObserver: Sendable {
+    let action: @Sendable () -> Void
+    init(_ action: @escaping @Sendable () -> Void) { self.action = action }
+    deinit { action() }
+}
+
+extension VNDocumentCameraViewController {
+    private static var megaDocCameraViewControllerDeallocObserverKey: UInt8 = 0
+    func onDeinit(_ action: @escaping @Sendable () -> Void) {
+        objc_setAssociatedObject(self, &VNDocumentCameraViewController.megaDocCameraViewControllerDeallocObserverKey, MegaVNDocCameraViewControllerDeallocObserver(action), .OBJC_ASSOCIATION_RETAIN)
+    }
+}
+
 struct ScanDocumentViewRouter: Sendable {
     private let presenter: UIViewController
     private let parent: NodeEntity
-    private let scanDocumentViewControllerDelegate = ScanDocumentViewControllerDelegate()
 
     init(presenter: UIViewController, parent: NodeEntity) {
         self.presenter = presenter
@@ -14,7 +26,7 @@ struct ScanDocumentViewRouter: Sendable {
     }
 
     func start() async {
-        await presentDocumentScanner()
+        guard let scanDocumentViewControllerDelegate = await presentDocumentScanner() else { return }
         guard let docs = await scanDocumentViewControllerDelegate.scannedImages() else { return }
         await presentDocScannerSaveSettingTableViewController(docs: docs)
     }
@@ -22,15 +34,20 @@ struct ScanDocumentViewRouter: Sendable {
     // MARK: - Private methods
 
     @MainActor
-    private func presentDocumentScanner() {
+    private func presentDocumentScanner() -> ScanDocumentViewControllerDelegate? {
         guard VNDocumentCameraViewController.isSupported else {
             SVProgressHUD.showError(withStatus: Strings.Localizable.documentScanningIsNotAvailable)
-            return
+            return nil
         }
 
         let scanVC = VNDocumentCameraViewController()
+        let scanDocumentViewControllerDelegate = ScanDocumentViewControllerDelegate()
         scanVC.delegate = scanDocumentViewControllerDelegate
+        scanVC.onDeinit { [weak scanDocumentViewControllerDelegate] in
+            scanDocumentViewControllerDelegate?.cancelContinuation()
+        }
         presenter.present(scanVC, animated: true)
+        return scanDocumentViewControllerDelegate
     }
 
     @MainActor
@@ -51,6 +68,15 @@ struct ScanDocumentViewRouter: Sendable {
 
 private final class ScanDocumentViewControllerDelegate: NSObject, @unchecked Sendable, VNDocumentCameraViewControllerDelegate {
     @Atomic private var continuation: CheckedContinuation<[UIImage]?, Never>?
+    
+    deinit {
+        continuation?.resume(with: .success(nil))
+    }
+    
+    func cancelContinuation() {
+        continuation?.resume(with: .success(nil))
+        $continuation.mutate { $0 = nil }
+    }
 
     func scannedImages() async -> [UIImage]? {
         await withCheckedContinuation { continuation in
@@ -64,12 +90,14 @@ private final class ScanDocumentViewControllerDelegate: NSObject, @unchecked Sen
         controller.dismiss(animated: true) {
             let docs = (0..<scan.pageCount).map(scan.imageOfPage)
             self.continuation?.resume(with: .success(docs))
+            self.$continuation.mutate { $0 = nil }
         }
     }
 
     func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
         controller.dismiss(animated: true) {
             self.continuation?.resume(with: .success(nil))
+            self.$continuation.mutate { $0 = nil }
         }
     }
 
@@ -78,6 +106,7 @@ private final class ScanDocumentViewControllerDelegate: NSObject, @unchecked Sen
     ) {
         controller.dismiss(animated: true) {
             self.continuation?.resume(with: .success(nil))
+            self.$continuation.mutate { $0 = nil }
         }
     }
 }
