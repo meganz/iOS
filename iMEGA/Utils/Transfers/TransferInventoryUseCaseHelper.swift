794df419f00edb15a89998a217c8b31318e907bf
Javier Navarro <jn@mega.co.nz>
2025-10-15 10:42:36 +0200

    IOS-10633: completed transfers hangs

--- iMEGA/Utils/Transfers/TransferInventoryUseCaseHelper.swift
+++ iMEGA/Utils/Transfers/TransferInventoryUseCaseHelper.swift
@@ -5,24 +5,17 @@ import MEGARepo
 final class TransferInventoryUseCaseHelper: NSObject, Sendable {
     private let transferInventoryUseCase: any TransferInventoryUseCaseProtocol
     private let sdk: MEGASdk
-    private let nodeUseCase: any NodeUseCaseProtocol
     private let fileSystem: any FileSystemRepositoryProtocol
     private let store: MEGAStore
 
     init(
         transferInventoryUseCase: some TransferInventoryUseCaseProtocol = TransferInventoryUseCase(transferInventoryRepository: TransferInventoryRepository.newRepo, fileSystemRepository: FileSystemRepository.sharedRepo),
         sdk: MEGASdk = MEGASdk.shared,
-        nodeUseCase: some NodeUseCaseProtocol = NodeUseCase(
-            nodeDataRepository: NodeDataRepository.newRepo,
-            nodeValidationRepository: NodeValidationRepository.newRepo,
-            nodeRepository: NodeRepository.newRepo
-        ),
         fileSystem: some FileSystemRepositoryProtocol = FileSystemRepository.sharedRepo,
         store: MEGAStore = MEGAStore.shareInstance()
     ) {
         self.transferInventoryUseCase = transferInventoryUseCase
         self.sdk = sdk
-        self.nodeUseCase = nodeUseCase
         self.fileSystem = fileSystem
         self.store = store
     }
@@ -31,8 +24,7 @@ final class TransferInventoryUseCaseHelper: NSObject, Sendable {
     func completedTransfers() -> [MEGATransfer] {
         if let list = sdk.completedTransfers as? [MEGATransfer] {
             return list.filter {
-                let node = nodeUseCase.nodeForHandle($0.nodeHandle)?.toMEGANode(in: sdk) ?? $0.publicNode
-                return node?.isFile() ?? false && ($0.type == .upload || $0.path?.hasPrefix(fileSystem.documentsDirectory().path) ?? false || $0.appData?.contains(TransferMetaDataEntity.saveInPhotos.rawValue) ?? false || $0.appData?.contains(TransferMetaDataEntity.exportFile.rawValue) ?? false)
+                return !$0.isFolderTransfer && ($0.type == .upload || $0.path?.hasPrefix(fileSystem.documentsDirectory().path) ?? false || $0.appData?.contains(TransferMetaDataEntity.saveInPhotos.rawValue) ?? false || $0.appData?.contains(TransferMetaDataEntity.exportFile.rawValue) ?? false)
             }
         }
         return []
