de7d9063740c386e162342b75b3d3dbe2bd91c9b
Harry <hy@mega.co.nz>
2025-10-17 10:03:17 +1300

    IOS-10634: Fix ChatViewAttachmentCell crash on Liquid Glass

--- iMEGA/Chat Messages/Views/Cells/ChatViewAttachmentCell.swift
+++ iMEGA/Chat Messages/Views/Cells/ChatViewAttachmentCell.swift
@@ -1,18 +1,17 @@
 import MEGADesignToken
 import MessageKit
 
-class ChatViewAttachmentCell: MessageContentCell {
-    
+final class ChatViewAttachmentCell: MessageContentCell {
     private let imageWidth: CGFloat = 40.0
     private let defaultSpacing: CGFloat = 10.0
-
+    
     public lazy var imageView: UIImageView = {
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageWidth, height: imageWidth))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
+    
     public lazy var titleLabel: MEGALabel = {
         let titleLabel = MEGALabel(frame: .zero)
         titleLabel.apply(style: .footnote, weight: .medium)
@@ -20,7 +19,7 @@ class ChatViewAttachmentCell: MessageContentCell {
         titleLabel.lineBreakMode = .byTruncatingMiddle
         return titleLabel
     }()
-
+    
     public lazy var detailLabel: MEGALabel = {
         let detailLabel = MEGALabel(frame: .zero)
         detailLabel.apply(style: .caption1)
@@ -38,11 +37,17 @@ class ChatViewAttachmentCell: MessageContentCell {
         labelsStackView.spacing = 3.0
         return labelsStackView
     }()
-
+    
+    var attachmentViewModel: ChatViewAttachmentCellViewModel? {
+        didSet {
+            configureUI()
+        }
+    }
+    
     // MARK: - Methods
-
+    
     /// Responsible for setting up the constraints of the cell's subviews.
-    open func setupConstraints() {
+    func setupConstraints() {
         NSLayoutConstraint.activate([
             imageView.widthAnchor.constraint(equalToConstant: imageWidth),
             imageView.heightAnchor.constraint(equalToConstant: imageWidth),
@@ -57,9 +62,10 @@ class ChatViewAttachmentCell: MessageContentCell {
             labelsStackView.bottomAnchor.constraint(equalTo: messageContainerView.bottomAnchor, constant: -defaultSpacing)
         ])
     }
-
-    open override func setupSubviews() {
+    
+    override func setupSubviews() {
         super.setupSubviews()
+        
         messageContainerView.addSubview(imageView)
         labelsStackView.addArrangedSubview(titleLabel)
         labelsStackView.addArrangedSubview(detailLabel)
@@ -67,19 +73,13 @@ class ChatViewAttachmentCell: MessageContentCell {
         setupConstraints()
     }
     
-    var attachmentViewModel: ChatViewAttachmentCellViewModel! {
-        didSet {
-            configureUI()
-        }
-    }
-    
     private func configureUI() {
-        titleLabel.text = attachmentViewModel.title
-        detailLabel.text = attachmentViewModel.subtitle
-        attachmentViewModel.set(imageView: imageView)
-        titleLabel.textColor = attachmentViewModel.isFromCurrentSender ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
+        titleLabel.text = attachmentViewModel?.title
+        detailLabel.text = attachmentViewModel?.subtitle
+        attachmentViewModel?.set(imageView: imageView)
+        titleLabel.textColor = attachmentViewModel?.isFromCurrentSender == true ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
         titleLabel.apply(style: .footnote, weight: .bold)
-        detailLabel.textColor = attachmentViewModel.isFromCurrentSender ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
+        detailLabel.textColor = attachmentViewModel?.isFromCurrentSender == true ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
     }
     
     override func configure(with message: any MessageType, at indexPath: IndexPath, and messagesCollectionView: MessagesCollectionView) {
@@ -95,9 +95,9 @@ class ChatViewAttachmentCell: MessageContentCell {
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         
-        titleLabel.textColor = attachmentViewModel.isFromCurrentSender ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
+        titleLabel.textColor = attachmentViewModel?.isFromCurrentSender == true ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
         titleLabel.apply(style: .footnote, weight: .bold)
-        detailLabel.textColor = attachmentViewModel.isFromCurrentSender ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
+        detailLabel.textColor = attachmentViewModel?.isFromCurrentSender == true ? TokenColors.Text.inverseAccent : TokenColors.Text.primary
     }
     
     func sizeThatFits() -> CGSize {
@@ -105,32 +105,31 @@ class ChatViewAttachmentCell: MessageContentCell {
         detailLabel.sizeToFit()
         
         let width = defaultSpacing
-            + imageWidth
-            + defaultSpacing
-            + max(titleLabel.bounds.width, detailLabel.bounds.width)
-            + defaultSpacing
+        + imageWidth
+        + defaultSpacing
+        + max(titleLabel.bounds.width, detailLabel.bounds.width)
+        + defaultSpacing
         
         let height = defaultSpacing
-            + titleLabel.bounds.height
-            + labelsStackView.spacing
-            + detailLabel.bounds.height
-            + defaultSpacing
+        + titleLabel.bounds.height
+        + labelsStackView.spacing
+        + detailLabel.bounds.height
+        + defaultSpacing
         
         return CGSize(width: width, height: height)
     }
 }
 
-open class ChatViewAttachmentCellCalculator: MessageSizeCalculator {
-    
+final class ChatViewAttachmentCellCalculator: MessageSizeCalculator {
     let chatViewAttachmentCell = ChatViewAttachmentCell()
     
-    public override init(layout: MessagesCollectionViewFlowLayout? = nil) {
+    override init(layout: MessagesCollectionViewFlowLayout? = nil) {
         super.init(layout: layout)
         configureAccessoryView()
     }
-
-    open override func messageContainerSize(for message: any MessageType, at indexPath: IndexPath) -> CGSize {
-       guard let chatMessage = message as? ChatMessage else {
+    
+    override func messageContainerSize(for message: any MessageType, at indexPath: IndexPath) -> CGSize {
+        guard let chatMessage = message as? ChatMessage else {
             fatalError("ChatViewAttachmentCellCalculator: wrong type message passed.")
         }
         
