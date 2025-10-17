3fc38d76ae1c4f2f4477be61809fc240d433f128
Binh Le <bl@mega.co.nz>
2025-10-17 09:11:26 +0700

    T21179738: Trying to enable 2FA in Settings - Security results in infinite loading

--- iMEGA/Utils/Shared Views/ViewControllers/CustomModalAlert/Extensions/CustomModalAlertViewController+TwoFactorAuthentication.swift
+++ iMEGA/Utils/Shared Views/ViewControllers/CustomModalAlert/Extensions/CustomModalAlertViewController+TwoFactorAuthentication.swift
@@ -19,10 +19,21 @@ extension CustomModalAlertViewController {
         firstCompletion = { [weak self] in
             self?.dismiss(animated: true) {
                 SVProgressHUD.show()
-                MEGASdk.shared.multiFactorAuthGetCode(with: RequestDelegate { [weak self] result in
-                    guard let self else { return }
+                MEGASdk.shared.multiFactorAuthGetCode(with: RequestDelegate { result in
                     Task { @MainActor in
-                        self.handleMultiFactorAuthGetCodeRequest(result: result)
+                        guard case let .success(request) = result else {
+                            if case let .failure(error) = result {
+                                SVProgressHUD.showError(withStatus: Strings.localized(error.name, comment: ""))
+                            }
+                            return
+                        }
+
+                        SVProgressHUD.dismiss()
+                        let enablingTwoFactorAuthenticationVC = UIStoryboard(name: "TwoFactorAuthentication", bundle: nil).instantiateViewController(withIdentifier: "EnablingTwoFactorAuthenticationViewControllerID") as! EnablingTwoFactorAuthenticationViewController
+                        enablingTwoFactorAuthenticationVC.seed = request.text // Returns the Base32 secret code needed to configure multi-factor authentication.
+                        enablingTwoFactorAuthenticationVC.hidesBottomBarWhenPushed = true
+                        
+                        UIApplication.mnz_visibleViewController().navigationController?.pushViewController(enablingTwoFactorAuthenticationVC, animated: true)
                     }
                 })
             }
@@ -31,21 +42,5 @@ extension CustomModalAlertViewController {
         dismissCompletion = { [weak self] in
             self?.dismiss(animated: true, completion: nil)
         }
-    }
-    
-    private func handleMultiFactorAuthGetCodeRequest(result: Result<MEGARequest, MEGAError>) {
-        guard case let .success(request) = result else {
-            if case let .failure(error) = result {
-                SVProgressHUD.showError(withStatus: Strings.localized(error.name, comment: ""))
-            }
-            return
-        }
-
-        SVProgressHUD.dismiss()
-        let enablingTwoFactorAuthenticationVC = UIStoryboard(name: "TwoFactorAuthentication", bundle: nil).instantiateViewController(withIdentifier: "EnablingTwoFactorAuthenticationViewControllerID") as! EnablingTwoFactorAuthenticationViewController
-        enablingTwoFactorAuthenticationVC.seed = request.text // Returns the Base32 secret code needed to configure multi-factor authentication.
-        enablingTwoFactorAuthenticationVC.hidesBottomBarWhenPushed = true
-        
-        UIApplication.mnz_visibleViewController().navigationController?.pushViewController(enablingTwoFactorAuthenticationVC, animated: true)
     }
 }
