0da52a687d0653988854193eb2906de5c0d3d5e8
Roberto Gómez <rg@mega.nz>
2025-11-20 12:38:30 +0100

    MEET-3491. Extra vertical padding on meetings list section header

--- iMEGA/ChatRoomsListScene/Data Model/MeetingsListHeaderView.swift
+++ iMEGA/ChatRoomsListScene/Data Model/MeetingsListHeaderView.swift
@@ -14,6 +14,6 @@ struct MeetingsListHeaderView: View {
                 .offset(CGSize(width: 16, height: -6))
         }
         .frame(maxWidth: .infinity)
-        .frame(height: 38)
+        .environment(\.defaultMinListRowHeight, 38)
     }
 }
