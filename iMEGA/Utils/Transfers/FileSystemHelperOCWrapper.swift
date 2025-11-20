ae9cc3379e0a921586d367585b3e718e27575057
Javier Navarro <jn@mega.co.nz>
2025-11-20 17:56:14 +0100

    IOS-10859: Remove FileSystemHelperOCWrapper

--- iMEGA/Utils/Transfers/FileSystemHelperOCWrapper.swift
+++
@@ -1,9 +0,0 @@
-import MEGARepo
-
-@objc final class FileSystemHelperOCWrapper: NSObject {
-    let fileSystemRepository = FileSystemRepository.sharedRepo
-    
-     @objc func documentsDirectory() -> URL {
-        fileSystemRepository.documentsDirectory()
-    }
-}
