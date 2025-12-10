4c928dd7d92e4a9754e518d2029fb98301e4d76c
Yang Fang <yfa@mega.co.nz>
2025-12-10 20:44:34 +1300

    IOS-10960: While logged out, Audio File Import should take to log in page...

--- iMEGA/Utils/Categories/MEGANode+MenuActions.swift
+++ iMEGA/Utils/Categories/MEGANode+MenuActions.swift
@@ -2,13 +2,9 @@ extension MEGANode {
     // MARK: - Import
     @MainActor
     func openBrowserToImport(in viewController: UIViewController) {
-        let storyboard = UIStoryboard(name: "Cloud", bundle: nil)
-        guard let browserVC = storyboard.instantiateViewController(withIdentifier: "BrowserViewControllerID") as? BrowserViewController
-        else { return }
-        browserVC.selectedNodesArray = [self]
-        browserVC.browserAction = .import
-        let browserNC = MEGANavigationController(rootViewController: browserVC)
-        browserNC.setToolbarHidden(false, animated: false)
-        viewController.present(browserNC, animated: true, completion: nil)
+        ImportLinkRouter(
+            isFolderLink: false,
+            nodes: [self],
+            presenter: viewController).start()
     }
 }
