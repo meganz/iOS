9b83612febb742a176e6708a45cd3fdf6de42b75
Jeff Fang <yfa@mega.co.nz>
2026-02-05 23:06:44 +0800

    IOS-11272: Home banner slides duplicated

--- Modules/Repository/MEGAAppSDKRepo/Sources/MEGAAppSDKRepo/SDK/ModelMapping/Banner/BannerListEntity+Mapper.swift
+++ Modules/Repository/MEGAAppSDKRepo/Sources/MEGAAppSDKRepo/SDK/ModelMapping/Banner/BannerListEntity+Mapper.swift
@@ -5,6 +5,7 @@ extension MEGABannerList {
     public var bannerEntities: [BannerEntity] {
         (0..<size).compactMap { index -> BannerEntity? in
             guard let banner = banner(at: index) else { return nil }
+            guard banner.variant == 0 else { return nil }
             return banner.bannerEntity
         }
     }
