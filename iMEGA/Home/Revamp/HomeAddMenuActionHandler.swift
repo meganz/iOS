8c2ce8403b84f39e50626fa43b35b94fc6966d30
Mike Cap <mvc@mega.co.nz>
2026-04-21 13:12:59 +0700

    IOS-11725: Allow destination selection for Home Revamp upload

--- iMEGA/Home/Revamp/HomeAddMenuActionHandler.swift
+++ iMEGA/Home/Revamp/HomeAddMenuActionHandler.swift
@@ -1,37 +1,116 @@
 import Home
+import MEGAAnalyticsiOS
+import MEGAAppPresentation
+import MEGADomain
+import MEGAPermissions
+import MEGAUI
 import UIKit
 
 @MainActor
-struct HomeAddMenuActionHandler: HomeAddMenuActionHandling {
-
-    private let uploadAddMenuDelegateHandler: UploadAddMenuDelegateHandler
+final class HomeAddMenuActionHandler: HomeAddMenuActionHandling {
+    private let fileUploadingRouter: FileUploadingRouter
+    private let tracker: any AnalyticsTracking
     private let newChatRouter: NewChatRouter
     private unowned let navigationController: UINavigationController
+    private let uploadPhotoAssetsUseCase: any UploadPhotoAssetsUseCaseProtocol
+
+    private let permissionHandler: any DevicePermissionsHandling
+
+    private let permissionRouter: PermissionAlertRouter
 
     init(
-        uploadAddMenuDelegateHandler: UploadAddMenuDelegateHandler,
+        fileUploadingRouter: FileUploadingRouter,
+        tracker: any AnalyticsTracking,
         newChatRouter: NewChatRouter,
-        navigationController: UINavigationController
+        navigationController: UINavigationController,
+        uploadPhotoAssetsUseCase: some UploadPhotoAssetsUseCaseProtocol,
+        permissionHandler: some DevicePermissionsHandling,
+        permissionRouter: PermissionAlertRouter
     ) {
-        self.uploadAddMenuDelegateHandler = uploadAddMenuDelegateHandler
+        self.tracker = tracker
         self.newChatRouter = newChatRouter
         self.navigationController = navigationController
+        self.fileUploadingRouter = fileUploadingRouter
+        self.uploadPhotoAssetsUseCase = uploadPhotoAssetsUseCase
+        self.permissionHandler = permissionHandler
+        self.permissionRouter = permissionRouter
     }
 
     func handleAction(_ action: HomeAddMenuAction) {
         switch action {
         case .chooseFromPhotos:
-            uploadAddMenuDelegateHandler.uploadAddMenu(didSelect: .chooseFromPhotos)
+            trackChooseFromPhotosEvent()
+            uploadFromPhotos()
         case .capture:
-            uploadAddMenuDelegateHandler.uploadAddMenu(didSelect: .capture)
+
+            uploadFromCamera()
         case .importFromFiles:
-            uploadAddMenuDelegateHandler.uploadAddMenu(didSelect: .importFrom)
+            trackImportFromFilesEvent()
+            fileUploadingRouter.upload(from: .imports)
         case .scanDocument:
-            uploadAddMenuDelegateHandler.uploadAddMenu(didSelect: .scanDocument)
+            scanDocument()
         case .newTextFile:
-            uploadAddMenuDelegateHandler.uploadAddMenu(didSelect: .newTextFile)
+            trackNewTextFileEvent()
+            fileUploadingRouter.upload(from: .textFile)
         case .newChat:
             newChatRouter.presentNewChat(from: navigationController)
         }
+    }
+
+    private func uploadFromPhotos() {
+        if DIContainer.remoteFeatureFlagUseCase.isFeatureFlagEnabled(for: .iosManualUploadPhotos) {
+            fileUploadingRouter.upload(from: .albumNew)
+        } else {
+            permissionHandler.photosPermissionWithCompletionHandler { [weak self] granted in
+                guard let self else { return }
+                if granted {
+                    let selectionHandler: (([PHAsset], MEGANode) -> Void) = { [weak self] assets, targetNode in
+                        guard let self else { return }
+                        self.uploadFiles(fromPhotoAssets: assets, to: targetNode)
+                    }
+                    self.fileUploadingRouter.upload(from: .album(selectionHandler))
+                } else {
+                    permissionRouter.alertPhotosPermission()
+                }
+            }
+        }
+    }
+
+    private func uploadFromCamera() {
+        permissionHandler.requestVideoPermission { [weak self] granted in
+            guard let self else { return }
+            if granted {
+                fileUploadingRouter.upload(from: .camera)
+            } else {
+                permissionRouter.alertVideoPermission()
+            }
+        }
+    }
+
+    private func scanDocument() {
+        permissionHandler.requestVideoPermission { [weak self] granted in
+            guard let self else { return }
+            if granted {
+                fileUploadingRouter.upload(from: .documentScan)
+            } else {
+                permissionRouter.alertVideoPermission()
+            }
+        }
+    }
+
+    private func uploadFiles(fromPhotoAssets assets: [PHAsset], to parentNode: MEGANode) {
+        uploadPhotoAssetsUseCase.upload(photoIdentifiers: assets.map(\.localIdentifier), to: parentNode.handle)
+    }
+
+    private func trackChooseFromPhotosEvent() {
+        tracker.trackAnalyticsEvent(with: CloudDriveChooseFromPhotosMenuToolbarEvent())
+    }
+
+    private func trackImportFromFilesEvent() {
+        tracker.trackAnalyticsEvent(with: CloudDriveImportFromFilesMenuToolbarEvent())
+    }
+
+    private func trackNewTextFileEvent() {
+        tracker.trackAnalyticsEvent(with: CloudDriveNewTextFileMenuToolbarEvent())
     }
 }
